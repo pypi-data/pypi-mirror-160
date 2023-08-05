ec5952e418014bc4f20e569ec3c5693f9f600910
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:34 +0000

    Add package rsqlite-sync/0.0.2/rsqlite_sync-0.0.2.tar.gz

---
+++ packages/rsqlite-sync/rsqlite_sync-0.0.2.tar.gz/rsqlite_sync-0.0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='rsqlite_sync',
+      version='0.0.2',
+      description='Использование SQLite3 в многопоточных программах.',
+      packages=['src'],
+      author_email='rapmbd@gmail.com',
+      zip_safe=False)
