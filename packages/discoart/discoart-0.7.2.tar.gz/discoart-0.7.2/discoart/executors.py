bc2b7ba4938168645a570decd2749f05662b4cab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:33 +0000

    Add package discoart/0.7.2/discoart-0.7.2.tar.gz

---
+++ packages/discoart/discoart-0.7.2.tar.gz/discoart-0.7.2/discoart/executors.py
@@ -0,0 +1,37 @@
+import asyncio
+import os
+from typing import Dict
+
+from jina import Executor, requests, DocumentArray
+
+
+class DiscoArtExecutor(Executor):
+    skip_event = asyncio.Event()
+    stop_event = asyncio.Event()
+
+    @requests(on='/create')
+    async def create_artworks(self, parameters: Dict, **kwargs):
+        await asyncio.get_event_loop().run_in_executor(None, self._create, parameters)
+
+    def _create(self, parameters: Dict, **kwargs):
+        from .create import create
+
+        return create(
+            skip_event=self.skip_event, stop_event=self.stop_event, **parameters
+        )
+
+    @requests(on='/skip')
+    async def skip_create(self, **kwargs):
+        self.skip_event.set()
+
+    @requests(on='/stop')
+    async def stop_create(self, **kwargs):
+        self.stop_event.set()
+
+
+class ResultPoller(Executor):
+    @requests(on='/result')
+    def poll_results(self, parameters: Dict, **kwargs):
+        path = f'{parameters["name_docarray"]}.protobuf.lz4'
+        if os.path.exists(path):
+            return DocumentArray.load_binary(path)
