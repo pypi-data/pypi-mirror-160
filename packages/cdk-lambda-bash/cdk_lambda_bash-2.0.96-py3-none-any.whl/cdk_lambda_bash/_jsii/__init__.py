e44ef33a4ec61672e4d8fa01db12d09d5f588b86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:46 +0000

    Add package cdk-lambda-bash/2.0.96/cdk_lambda_bash-2.0.96-py3-none-any.whl

---
+++ packages/cdk-lambda-bash/cdk_lambda_bash-2.0.96-py3-none-any.whl/cdk_lambda_bash/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-lambda-bash", "2.0.96", __name__[0:-6], "cdk-lambda-bash@2.0.96.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
