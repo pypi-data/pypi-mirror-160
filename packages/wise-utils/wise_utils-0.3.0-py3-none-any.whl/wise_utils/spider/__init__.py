76006237ef85583800f3510f00244e8caaeff4b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:35 +0000

    Add package wise-utils/0.3.0/wise_utils-0.3.0-py3-none-any.whl

---
+++ packages/wise-utils/wise_utils-0.3.0-py3-none-any.whl/wise_utils/spider/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+__all__ = ["BaseSpider", "SeleniumBaseSpider", "PyppeteerBaseSpider"]
+
+from wise_utils.spider.basespider import BaseSpider
+from wise_utils.spider.selenium_spider import SeleniumBaseSpider
+from wise_utils.spider.pyppeteer_spider import PyppeteerBaseSpider
