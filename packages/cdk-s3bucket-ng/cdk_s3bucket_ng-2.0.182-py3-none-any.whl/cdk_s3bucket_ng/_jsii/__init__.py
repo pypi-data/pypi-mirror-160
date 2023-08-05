da8712162c991dd5f3560231cb67c5dde3d5e931
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:55 +0000

    Add package cdk-s3bucket-ng/2.0.182/cdk_s3bucket_ng-2.0.182-py3-none-any.whl

---
+++ packages/cdk-s3bucket-ng/cdk_s3bucket_ng-2.0.182-py3-none-any.whl/cdk_s3bucket_ng/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-s3bucket-ng", "2.0.182", __name__[0:-6], "cdk-s3bucket-ng@2.0.182.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
