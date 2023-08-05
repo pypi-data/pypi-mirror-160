8d02709072e8c4a6cbe09b5374a7fd692dd2e58b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:19 +0000

    Add package plaid-python-old/2.3.3/plaid-python-old-2.3.3.tar.gz

---
+++ packages/plaid-python-old/plaid-python-old-2.3.3.tar.gz/plaid-python-old-2.3.3/plaid_old/api/__init__.py
@@ -0,0 +1,27 @@
+from plaid_old.api.accounts import Accounts
+from plaid_old.api.assets import AssetReport
+from plaid_old.api.auth import Auth
+from plaid_old.api.categories import Categories
+from plaid_old.api.credit_details import CreditDetails
+from plaid_old.api.identity import Identity
+from plaid_old.api.income import Income
+from plaid_old.api.institutions import Institutions
+from plaid_old.api.item import Item
+from plaid_old.api.processor import Processor
+from plaid_old.api.sandbox import Sandbox
+from plaid_old.api.transactions import Transactions
+
+__all__ = [
+    'Accounts',
+    'AssetReport',
+    'Auth',
+    'Categories',
+    'CreditDetails',
+    'Identity',
+    'Income',
+    'Institutions',
+    'Item',
+    'Processor',
+    'Sandbox',
+    'Transactions',
+]
