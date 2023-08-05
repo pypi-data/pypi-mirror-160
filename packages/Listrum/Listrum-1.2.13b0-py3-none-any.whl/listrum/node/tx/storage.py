3a78a336014993946e0b971a89e1fa0986802f69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:43 +0000

    Add package Listrum/1.2.13b0/Listrum-1.2.13b0-py3-none-any.whl

---
+++ packages/Listrum/Listrum-1.2.13b0-py3-none-any.whl/listrum/node/tx/storage.py
@@ -0,0 +1,55 @@
+import os
+from threading import Thread
+
+from listrum.client.nodes import nodes
+from listrum.client.constants import Const
+
+
+class Storage:
+
+    dir = os.path.expanduser("~") + "/listrum/storage/"
+
+    def __init__(self) -> None:
+        try:
+            os.makedirs(self.dir)
+        except:
+            pass
+
+        self.res = []
+
+    def get(self, wallet: str) -> float:
+        try:
+            with open(self.dir + wallet) as f:
+                return float(f.read())
+
+        except:
+            if wallet in self.res:
+                return 0.0
+
+            Thread(target=self.from_node, args=(wallet,)).start()
+
+            return 0.0
+
+    def from_node(self, wallet: str) -> None:
+        balance = nodes.balance(wallet)
+
+        try:
+            open(self.dir + wallet)
+        except:
+            self.res.append(wallet)
+
+            if len(self.res) > Const.temp_storage_len:
+                self.res.pop(0)
+
+            if balance > 0.0:
+                self.set(wallet, balance)
+
+    def set(self, wallet: str, value: float) -> None:
+        if not value:
+            os.remove(self.dir + wallet)
+            return
+
+        with open(self.dir + wallet, "w") as f:
+            f.write(str(value))
+
+storage = Storage()
