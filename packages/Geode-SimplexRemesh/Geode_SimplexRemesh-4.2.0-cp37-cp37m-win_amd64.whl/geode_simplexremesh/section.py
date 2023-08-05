90a9c130c2fc0fbf47e19d35246134a775e99c5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:09 +0000

    Add package Geode-SimplexRemesh/4.2.0/Geode_SimplexRemesh-4.2.0-cp37-cp37m-win_amd64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-4.2.0-cp37-cp37m-win_amd64.whl/geode_simplexremesh/section.py
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core, geometry, helpers, modifier_surface, modifier_edged_curve, modifier_model, cutter_surface
+from geode_parameterization import surface
+
+from .geode_simplexremesh_py_section import *
+
