c62c0766e98b700f6490f1d333c0b460e779df0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:37 +0000

    Add package fairlib/0.0.9/fairlib-0.0.9.tar.gz

---
+++ packages/fairlib/fairlib-0.0.9.tar.gz/fairlib-0.0.9/fairlib/src/networks/__init__.py
@@ -0,0 +1,22 @@
+import sys
+import torch
+import logging
+from .classifier import MLP, BERTClassifier
+from . import utils
+from . import INLP
+from . import FairCL
+from . import DyBT
+from . import adv
+from collections import defaultdict
+
+def get_main_model(args):
+    assert args.encoder_architecture in ["Fixed", "BERT", "DeepMoji"], "Not implemented"
+
+    if args.encoder_architecture == "Fixed":
+        model = MLP(args)
+    elif args.encoder_architecture == "BERT":
+        model = BERTClassifier(args)
+    else:
+        raise "not implemented yet"
+    
+    return model
