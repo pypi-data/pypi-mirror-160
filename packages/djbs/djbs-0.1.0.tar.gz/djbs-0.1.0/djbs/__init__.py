47d94b6ebf24c02da0ee3dd284e1e23d7152527d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:25 +0000

    Add package djbs/0.1.0/djbs-0.1.0.tar.gz

---
+++ packages/djbs/djbs-0.1.0.tar.gz/djbs-0.1.0/djbs/__init__.py
@@ -0,0 +1,45 @@
+import typer
+import subprocess
+import os
+import users
+
+app = typer.Typer()
+
+
+@app.command()
+def startapp(name: str):
+    if os.path.exists(name):
+       print(f"dir ({name}) all ready exist")
+    else:
+        os.makedirs(name)
+        with open(f"./{name}/__init__.py", "w") as file:
+            file.write('')
+        with open(f"./{name}/apps.py", "w") as file:
+            file.write(users.apps)
+    #subprocess.run('ls')
+
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
+
+
+
+
+if __name__ == "__main__":
+    app()
+    
+
+
+
+
+
+#def add_numbers(num1, num2):
+#    results = num1 + num2
+#    return f'{results}'
+
+
