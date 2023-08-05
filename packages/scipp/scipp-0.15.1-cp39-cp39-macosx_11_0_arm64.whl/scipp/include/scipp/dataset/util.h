07df44ddc52819abb650906bbb8736f04e595df5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package scipp/0.15.1/scipp-0.15.1-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/scipp/scipp-0.15.1-cp39-cp39-macosx_11_0_arm64.whl/scipp/include/scipp/dataset/util.h
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Matthew Andrew
+#pragma once
+
+#include "scipp/variable/variable.h"
+
+#include "scipp-dataset_export.h"
+#include "scipp/dataset/dataset.h"
+#include "scipp/dataset/generated_util.h"
+
+namespace scipp {
+
+enum SizeofTag { Underlying, ViewOnly };
+
+SCIPP_DATASET_EXPORT scipp::index size_of(const Variable &view, SizeofTag tag);
+SCIPP_DATASET_EXPORT scipp::index size_of(const DataArray &dataarray,
+                                          SizeofTag tag,
+                                          bool include_aligned_coords = true);
+SCIPP_DATASET_EXPORT scipp::index size_of(const Dataset &dataset,
+                                          SizeofTag tag);
+} // namespace scipp
