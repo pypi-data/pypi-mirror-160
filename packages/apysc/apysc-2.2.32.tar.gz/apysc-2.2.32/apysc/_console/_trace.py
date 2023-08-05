60b154558155356507fe0e56f7b010635ace8720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:22 +0000

    Add package apysc/2.2.32/apysc-2.2.32.tar.gz

---
+++ packages/apysc/apysc-2.2.32.tar.gz/apysc-2.2.32/apysc/_console/_trace.py
@@ -0,0 +1,46 @@
+"""`trace` (console.log expression) interface implementations
+"""
+
+from typing import Any
+from typing import List
+
+from apysc._html.debug_mode import add_debug_info_setting
+
+
+@add_debug_info_setting(module_name=__name__)
+def trace(*args: Any) -> None:
+    """
+    Display arguments information to console.
+    This function saves a JavaScript `console.log` expression.
+
+    Parameters
+    ----------
+    *args : list
+        Any arguments to display to console.
+
+    References
+    ----------
+    - Trace interface document
+        - https://simon-ritchie.github.io/apysc/en/trace.html
+
+    Examples
+    --------
+    >>> import apysc as ap
+    >>> int_val: ap.Int = ap.Int(10)
+    >>> ap.trace('Int value is:', int_val)
+    """
+    import apysc as ap
+    from apysc._string import string_util
+    from apysc._type.variable_name_interface import VariableNameInterface
+
+    expression: str = "console.log("
+    arg_strs: List[str] = []
+    for arg in args:
+        if isinstance(arg, VariableNameInterface):
+            arg_strs.append(f"{arg.variable_name}")
+            continue
+        arg = string_util.escape_str(string=str(arg))
+        arg_strs.append(f'"{arg}"')
+    expression += ", ".join(arg_strs)
+    expression += ");"
+    ap.append_js_expression(expression=expression)
