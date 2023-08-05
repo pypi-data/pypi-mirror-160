7480709dd2f0998b8a4e06c96eb7425b84533aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:59 +0000

    Add package edc-reportable/0.3.20/edc_reportable-0.3.20-py3-none-any.whl

---
+++ packages/edc-reportable/edc_reportable-0.3.20-py3-none-any.whl/edc_reportable/calculators/egfr/egfr_cockcroft_gault.py
@@ -0,0 +1,42 @@
+from edc_constants.constants import FEMALE
+
+from ...units import MICROMOLES_PER_LITER
+from .base_egrfr import BaseEgfr
+
+
+class EgfrCockcroftGault(BaseEgfr):
+
+    """Reference https://www.mdcalc.com/creatinine-clearance-cockcroft-gault-equation
+
+    Cockcroft-Gault
+
+    eGFR (mL/min) = { (140 – age (years)) x weight (kg) x constant*} /
+    serum creatinine (μmol/L)
+    *constant = 1.23 for males and 1.05 for females
+
+    Cockcroft-Gault CrCl, mL/min =
+        (140 – age) × (weight, kg) × (0.85 if female) / (72 × SCr(mg/dL))
+
+    """
+
+    @property
+    def value(self):
+        if (
+            self.gender
+            and self.age_in_years
+            and self.weight
+            and self.scr.get(MICROMOLES_PER_LITER)
+        ):
+            return (self.age_factor * self.weight * self.gender_factor) / (
+                self.scr.get(MICROMOLES_PER_LITER)
+            )
+        return None
+
+    @property
+    def gender_factor(self):
+        return 1.05 if self.gender == FEMALE else 1.23
+        # return 0.85 if self.gender == FEMALE else 1.00
+
+    @property
+    def age_factor(self):
+        return 140 - self.age_in_years
