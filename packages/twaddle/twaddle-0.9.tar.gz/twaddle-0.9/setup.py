11877a32bbb031d3c67bc55b6165ed39266a1c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:28 +0000

    Add package twaddle/0.9/twaddle-0.9.tar.gz

---
+++ packages/twaddle/twaddle-0.9.tar.gz/twaddle-0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup(
+        name="twaddle",
+        version="0.9",
+        description="Python version of Rant v3",
+        author="Chris Hengler",
+        author_email="dr.c.hengler@gmail.com",
+        url="https://github.com/chrishengler/twaddle",
+        packages=["twaddle"],
+    )
