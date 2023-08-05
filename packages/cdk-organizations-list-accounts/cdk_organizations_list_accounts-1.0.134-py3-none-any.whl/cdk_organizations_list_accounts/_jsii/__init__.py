6dd1fef7b60373dd2aaf652f4a828d9f92ed4650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:48 +0000

    Add package cdk-organizations-list-accounts/1.0.134/cdk_organizations_list_accounts-1.0.134-py3-none-any.whl

---
+++ packages/cdk-organizations-list-accounts/cdk_organizations_list_accounts-1.0.134-py3-none-any.whl/cdk_organizations_list_accounts/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-organizations-list-accounts",
+    "1.0.134",
+    __name__[0:-6],
+    "cdk-organizations-list-accounts@1.0.134.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
