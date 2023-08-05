319c651e113a19d220f93ab1e3a794576ca88ea3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:42 +0000

    Add package pepperize.cdk-organizations/0.4.166/pepperize.cdk-organizations-0.4.166.tar.gz

---
+++ packages/pepperize.cdk-organizations/pepperize.cdk-organizations-0.4.166.tar.gz/pepperize.cdk-organizations-0.4.166/src/pepperize_cdk_organizations/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-organizations",
+    "0.4.166",
+    __name__[0:-6],
+    "cdk-organizations@0.4.166.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
