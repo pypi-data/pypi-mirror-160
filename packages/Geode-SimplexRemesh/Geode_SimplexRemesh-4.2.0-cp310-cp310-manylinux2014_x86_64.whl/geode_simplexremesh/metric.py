0b04b167b4ca6a52104c206e29f284c5ae41bdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:02 +0000

    Add package Geode-SimplexRemesh/4.2.0/Geode_SimplexRemesh-4.2.0-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/Geode-SimplexRemesh/Geode_SimplexRemesh-4.2.0-cp310-cp310-manylinux2014_x86_64.whl/geode_simplexremesh/metric.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2022 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+from geode_common import core, geometry, helpers, modifier_surface, modifier_edged_curve, modifier_model, cutter_surface
+
+from .geode_simplexremesh_py_metric import *
+
