d6e489bf611ad8a7fdcd98f2f590187414ef5ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:31 +0000

    Add package mypythontools/2.1.6/mypythontools-2.1.6.tar.gz

---
+++ packages/mypythontools/mypythontools-2.1.6.tar.gz/mypythontools-2.1.6/utils/push_script.py
@@ -0,0 +1,14 @@
+"""Push the CI pipeline. Format, create commit from all the changes, push and deploy to PyPi."""
+
+from mypythontools_cicd.cicd import cicd_pipeline, default_pipeline_config
+
+
+if __name__ == "__main__":
+    default_pipeline_config.deploy = True
+    default_pipeline_config.test.sync_test_requirements = ["tests.txt"]
+    default_pipeline_config.test.sync_test_requirements_path = "requirements"
+
+    default_pipeline_config.do_only = "deploy"
+
+    # All the parameters can be overwritten via CLI args
+    cicd_pipeline(config=default_pipeline_config)
