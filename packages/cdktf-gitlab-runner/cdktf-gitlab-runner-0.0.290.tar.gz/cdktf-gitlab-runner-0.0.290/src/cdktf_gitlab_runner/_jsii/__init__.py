92f2d52b479a74e5d693ef34bd85fd23f94bc558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:21 +0000

    Add package cdktf-gitlab-runner/0.0.290/cdktf-gitlab-runner-0.0.290.tar.gz

---
+++ packages/cdktf-gitlab-runner/cdktf-gitlab-runner-0.0.290.tar.gz/cdktf-gitlab-runner-0.0.290/src/cdktf_gitlab_runner/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdktf._jsii
+import cdktf_cdktf_provider_google._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdktf-gitlab-runner",
+    "0.0.290",
+    __name__[0:-6],
+    "cdktf-gitlab-runner@0.0.290.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
