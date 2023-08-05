03a61acf1be8f89169efdc88480096c7a471b3d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:45 +0000

    Add package LinAlgHelper/1.0.0/LinAlgHelper-1.0.0.tar.gz

---
+++ packages/LinAlgHelper/LinAlgHelper-1.0.0.tar.gz/LinAlgHelper-1.0.0/README.txt
@@ -0,0 +1,5 @@
+Linear Algebra Helper
+---------------------
+This is a simple linear algebra lib, basic functions for elementary LA operations are included.
+The point of this library is to alleviate tedious calculations that are encountered when 
+studying this topic.
---
+++ packages/LinAlgHelper/LinAlgHelper-1.0.0.tar.gz/LinAlgHelper-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='LinAlgHelper',
+  version='1.0.0',
+  description='A Linear Algebra library',
+  long_description=open('README.txt').read(),
+  url='https://github.com/NoahPinel/LinearAlgLib',  
+  author='Noah Pinel',
+  author_email='noah@binaryfox.ca',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='LinearAlgebra', 
+  install_requires=[''] 
+)
