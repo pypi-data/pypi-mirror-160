2a88e9e709b112400a41469e51c6744c5658b3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:09:42 +0000

    Add package cdk-lambda-subminute/2.0.41/cdk_lambda_subminute-2.0.41.tar.gz

---
+++ packages/cdk-lambda-subminute/cdk_lambda_subminute-2.0.41.tar.gz/cdk_lambda_subminute-2.0.41/src/cdk_lambda_subminute/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-lambda-subminute",
+    "2.0.41",
+    __name__[0:-6],
+    "cdk-lambda-subminute@2.0.41.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
