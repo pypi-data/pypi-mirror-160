2d172069dddcfbdb59237156f05273a9118f42fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:58 +0000

    Add package paketmutfak/1.0.30/paketmutfak-1.0.30-py3-none-any.whl

---
+++ packages/paketmutfak/paketmutfak-1.0.30-py3-none-any.whl/paketmutfak/utils/formats/applications.py
@@ -0,0 +1,34 @@
+def get_pm_all_applications_format():
+    all_pm_apps = {
+        "UYE_PANELI": {
+            "MARKA_URUN_YONETIMI": "-",
+            "UYELER": "-",
+            "UYE_OLUSTUR": "-"
+        },
+        "BINA_YONETIM_PANELI": {
+            "GENEL_OTM": "-",
+            "GENEL_RAPORLAR": "-",
+            "SIPARIS_AKISI": "-",
+            "MANUEL_SIPARIS": "-",
+            "BINA_OTM": "-",
+            "ARAMALAR": "-",
+            "BINA_AYARLARI": "-",
+            "BINA_MUTABAKAT": "-"
+        },
+        "LOJISTIK_PANELI": {
+            "KURYE_SEFI_EKRANI": "-",
+            "KURYE_KAYIT_LISTESI": "-",
+            "KURYE_PERFORMANS": "-",
+            "KURYE_HARITA": "-",
+            "KURYE_VARDIYALAR": "-",
+        },
+        "YONETICI_PANELI": {
+            "CALISAN_YONETIMI": "-"
+        },
+        "KDS": {
+            "VIEW": "-",
+            "GUNSONU": "-"
+        }
+    }
+
+    return all_pm_apps
