a3d984a916cc8aabbdbac550691a028ef489c6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:35 +0000

    Add package haibao/1.3.1/haibao-1.3.1.tar.gz

---
+++ packages/haibao/haibao-1.3.1.tar.gz/haibao-1.3.1/haibao/tools.py
@@ -0,0 +1,63 @@
+# -*- encoding: utf-8 -*-
+import time
+import json
+
+import signal
+import requests
+
+
+__author__ = u'seal'
+
+
+def getAddressByIp(ip):
+    """
+    通过ip地址获取归属地以及其他详细信息
+    """
+    if not isinstance(ip, str):
+        raise ValueError('ipaddress must be str')
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={}&resource_id=6006'.format(ip)
+    try:
+        html = requests.get(url=url, headers=headers).text
+        data = json.loads(html)
+    except Exception as e:
+        return 'time error'
+    return data
+
+
+def getSampleAddressByIp(ip):
+    """
+    通过ip地址获取归属地
+    """
+    data = getAddressByIp(ip)
+    if isinstance(data, dict):
+        try:
+            data = data['data'][0]['location']
+        except:
+            return 'time error'
+    return data
+
+
+class FunctionTimeOut(Exception):
+    pass
+
+
+def timeout_handler(signum, frame):
+    raise FunctionTimeOut
+
+
+def func_set_timeout(timeout):
+    def wraper(func):
+        def wrap(*args, **kwargs):
+            signal.signal(signal.SIGALRM, timeout_handler)
+            signal.alarm(timeout)
+            try:
+                return func(*args, **kwargs)
+            except FunctionTimeOut:
+                return None
+        return wrap
+    return wraper
+
+
+if __name__ == '__main__':
+    pass
