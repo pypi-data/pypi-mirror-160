54c1c15708fc887adc02ecefbe49b1acc64696da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:27 +0000

    Add package yourmove/0.0.0.1/yourmove-0.0.0.1.tar.gz

---
+++ packages/yourmove/yourmove-0.0.0.1.tar.gz/yourmove-0.0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='yourmove',
+    version='0.0.0.1',
+    description='YourMove',
+    author='jup014',
+    author_email='jup014@ucsd.edu',
+    url='https://cwphs.ucsd.edu',
+    license='MIT',
+    py_modules=['yourmove'],
+    python_requires='>=3',
+    install_requires=['pytz'],
+    packages=['yourmove']
+)
