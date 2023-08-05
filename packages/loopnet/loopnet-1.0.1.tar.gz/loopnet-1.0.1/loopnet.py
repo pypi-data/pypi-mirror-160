c533bc73236e3cc7b65f904365e24e1a0d307fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:55 +0000

    Add package loopnet/1.0.1/loopnet-1.0.1.tar.gz

---
+++ packages/loopnet/loopnet-1.0.1.tar.gz/loopnet-1.0.1/loopnet.py
@@ -0,0 +1,15 @@
+"""This is the "nester.py" module and it provides one function called print_lol()
+which prints lists that may or may not include nested lists."""
+
+def print_ll(l,level=0,indent=False):
+    """This function takes a positional argument called "the_list", which
+    is any Python list (of - possibly - nested lists). Each data item in the
+    provided list is (recursively) printed to the screen on it's own line."""
+    for i in l:
+        if isinstance(i,list):
+            print_ll(i,level+1,indent)
+        else:
+            if indent:
+                for tab in range(level):
+                    print('\t',end='')
+            print(i)
