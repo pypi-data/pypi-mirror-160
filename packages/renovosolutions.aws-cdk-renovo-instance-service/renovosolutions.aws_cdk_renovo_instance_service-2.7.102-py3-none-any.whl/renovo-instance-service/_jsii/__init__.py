f1069406c2e3aa92adbf4c51eddc5fff63e221a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:25 +0000

    Add package renovosolutions.aws-cdk-renovo-instance-service/2.7.102/renovosolutions.aws_cdk_renovo_instance_service-2.7.102-py3-none-any.whl

---
+++ packages/renovosolutions.aws-cdk-renovo-instance-service/renovosolutions.aws_cdk_renovo_instance_service-2.7.102-py3-none-any.whl/renovo-instance-service/_jsii/__init__.py
@@ -0,0 +1,27 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aspects-security-group._jsii
+import aws_cdk._jsii
+import constructs._jsii
+import managed_instance_role._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@renovosolutions/cdk-library-renovo-instance-service",
+    "2.7.102",
+    __name__[0:-6],
+    "cdk-library-renovo-instance-service@2.7.102.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
