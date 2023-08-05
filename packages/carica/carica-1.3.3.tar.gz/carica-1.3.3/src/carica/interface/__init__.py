60718b5ab243b7f8e56c50d10510bd683d1abe1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:00 +0000

    Add package carica/1.3.3/carica-1.3.3.tar.gz

---
+++ packages/carica/carica-1.3.3.tar.gz/carica-1.3.3/src/carica/interface/__init__.py
@@ -0,0 +1,3 @@
+from .Serializable import ISerializable, SerializableType, \
+                            PrimativeType, primativeTypes, primativeTypesTuple, serializableTypes, \
+                            serializableTypesTuple, primativeTypesTuple, SerializesToDict, SerializesToType
