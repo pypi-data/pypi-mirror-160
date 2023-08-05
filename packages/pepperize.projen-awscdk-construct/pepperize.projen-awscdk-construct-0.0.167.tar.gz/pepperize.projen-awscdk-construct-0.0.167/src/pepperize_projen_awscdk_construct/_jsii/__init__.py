41cbb4792006efd11a6f55afa50d2837f2f70b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:28 +0000

    Add package pepperize.projen-awscdk-construct/0.0.167/pepperize.projen-awscdk-construct-0.0.167.tar.gz

---
+++ packages/pepperize.projen-awscdk-construct/pepperize.projen-awscdk-construct-0.0.167.tar.gz/pepperize.projen-awscdk-construct-0.0.167/src/pepperize_projen_awscdk_construct/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import projen._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/projen-awscdk-construct",
+    "0.0.167",
+    __name__[0:-6],
+    "projen-awscdk-construct@0.0.167.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
