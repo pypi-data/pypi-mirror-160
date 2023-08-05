1b7fb9ee2728bb7321f957c3ba0a9c94e80e7570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:44 +0000

    Add package scipp/0.15.1/scipp-0.15.1-cp39-cp39-win_amd64.whl

---
+++ packages/scipp/scipp-0.15.1-cp39-cp39-win_amd64.whl/scipp/include/scipp/variable/arithmetic.h
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2022 Scipp contributors (https://github.com/scipp)
+/// @file
+/// @author Simon Heybrock
+#pragma once
+
+#include "scipp-variable_export.h"
+#include "scipp/variable/generated_arithmetic.h"
+#include "scipp/variable/multiply.h"
+#include "scipp/variable/variable.h"
+
+namespace scipp::units {
+template <typename T>
+std::enable_if_t<std::is_arithmetic_v<T> ||
+                     std::is_same_v<T, scipp::core::time_point>,
+                 Variable>
+operator*(T v, const units::Unit &unit) {
+  return makeVariable<T>(units::Unit{unit}, Values{v});
+}
+
+template <typename T>
+std::enable_if_t<std::is_arithmetic_v<T> ||
+                     std::is_same_v<T, scipp::core::time_point>,
+                 Variable>
+operator/(T v, const units::Unit &unit) {
+  return makeVariable<T>(units::one / unit, Values{v});
+}
+} // namespace scipp::units
