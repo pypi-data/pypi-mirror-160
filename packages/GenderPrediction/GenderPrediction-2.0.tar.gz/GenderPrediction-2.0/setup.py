9247f5e5c5f16655a32c6a339df7667c0f889eb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:35 +0000

    Add package GenderPrediction/2.0/GenderPrediction-2.0.tar.gz

---
+++ packages/GenderPrediction/GenderPrediction-2.0.tar.gz/GenderPrediction-2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='GenderPrediction',  # This is the name of your PyPI-package.
+      version='2.0',  # Update the version number for new releases
+      description='Classify the name based on given name',
+      author='Sounak-Mukherjee',
+      author_email='sounak@pratilipi.com',
+      license='MIT',
+      packages=['GenderPrediction'],
+      include_package_data=True,
+      install_requires=['numpy==1.19.2', 'tensorflow-cpu==2.5.0']
+      )
