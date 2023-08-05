7889d7c3b56dba4f72db356dbc9d77ca7524307f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:20 +0000

    Add package cdk-watchful/0.6.157/cdk_watchful-0.6.157-py3-none-any.whl

---
+++ packages/cdk-watchful/cdk_watchful-0.6.157-py3-none-any.whl/cdk_watchful/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-watchful", "0.6.157", __name__[0:-6], "cdk-watchful@0.6.157.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
