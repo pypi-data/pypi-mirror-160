ecee502d10a7af15364b08343aee2f989d07296c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:54 +0000

    Add package offline-model-builder/0.2.14/offline_model_builder-0.2.14-py3-none-any.whl

---
+++ packages/offline-model-builder/offline_model_builder-0.2.14-py3-none-any.whl/offline_model_builder/user_profile/clustering/config.py
@@ -0,0 +1,16 @@
+SPARSITY_THRESHOLD = 0.98
+K_NO_PAYTV = 30
+K_PAYTV = 26
+MINIBATCH_SIZE = 6
+MIN_MEMBER_COUNT = 15
+TENDENCY_CUTOFF = 3
+ENSEMBLE_SD_CUTOFF = 5
+TOP_N_MEMBERS_COUNT = 5
+
+GENDER_MAP = {
+    0: -1,
+    "na": -1,
+    "nan": -1,
+    "m": 0,
+    "f": 1
+}
