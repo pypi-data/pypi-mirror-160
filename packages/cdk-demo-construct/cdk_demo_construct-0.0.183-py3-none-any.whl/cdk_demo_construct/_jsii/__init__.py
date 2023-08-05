4a26574780b2575de23e7dbf2f6d16086f82c84f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:46 +0000

    Add package cdk-demo-construct/0.0.183/cdk_demo_construct-0.0.183-py3-none-any.whl

---
+++ packages/cdk-demo-construct/cdk_demo_construct-0.0.183-py3-none-any.whl/cdk_demo_construct/_jsii/__init__.py
@@ -0,0 +1,29 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_cloudwatch._jsii
+import aws_cdk.aws_cloudwatch_actions._jsii
+import aws_cdk.aws_ec2._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-demo-construct",
+    "0.0.183",
+    __name__[0:-6],
+    "cdk-demo-construct@0.0.183.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
