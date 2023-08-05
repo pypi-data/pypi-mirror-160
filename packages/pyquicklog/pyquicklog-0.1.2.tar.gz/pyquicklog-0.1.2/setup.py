2a45764acc39f677822471b62daa46db586a3d6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:00 +0000

    Add package pyquicklog/0.1.2/pyquicklog-0.1.2.tar.gz

---
+++ packages/pyquicklog/pyquicklog-0.1.2.tar.gz/pyquicklog-0.1.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+def readme_file(path: str = "README.md"):
+    with open(path, "r") as file:
+        content = file.read()
+    return content
+
+setup(
+    name="pyquicklog",
+    version="0.1.2",
+    description="A simple logger for python",
+    long_description=readme_file(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bbenouarets/quicklog.py",
+    author="Björn Benouarets",
+    author_email="kontakt@bbenouarets.site",
+    keywords="quicklog quick log logger logme logging",
+    classifiers=[
+        "Natural Language :: English"
+    ],
+    include_package_data=True,
+    install_requires=["os"],
+    extra_require={"test": ""},
+    packages=find_packages("pyquicklog", exclude=["docs", ".github"]),
+    # package_dir={"":"pyquicklog"},
+    py_modules=["pyquicklog"],
+    license="MIT license",
+    zip_safe=False
+)
