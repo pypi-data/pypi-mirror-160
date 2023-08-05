e3d41b664b39b6fae80866151e161bcf3f31c369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:47 +0000

    Add package google-recaptcha-flask/0.0.3/Google-reCaptcha-flask-0.0.3.tar.gz

---
+++ packages/google-recaptcha-flask/Google-reCaptcha-flask-0.0.3.tar.gz/Google-reCaptcha-flask-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name='Google-reCaptcha-flask',
+    version='0.0.3',
+    url='https://github.com/brunolimasp/Flask-Google-reCaptcha',
+    license='MIT License',
+    author='Bruno Augusto',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='augusto.han@gmail.com',
+    keywords=['flask', 'recaptcha', "google"],
+    description=u'Exemplo de pacote PyPI',
+    py_modules=['google_recaptcha_flask'],
+    include_package_data=True,
+    install_requires=['requests', 'MarkupSafe'],
+    )
