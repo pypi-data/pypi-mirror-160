81744d0789dd6df3a142cfd27eab3a52fbe18844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:36:06 +0000

    Add package liiatools/0.1.3/liiatools-0.1.3.tar.gz

---
+++ packages/liiatools/liiatools-0.1.3.tar.gz/liiatools-0.1.3/liiatools/datasets/shared_functions/common.py
@@ -0,0 +1,71 @@
+import re
+import logging
+
+from sfdata_stream_parser import events
+
+log = logging.getLogger(__name__)
+
+
+def flip_dict(some_dict):
+    """
+    Potentially a temporary function which switches keys and values in a dictionary.
+    May need to rewrite LA-codes YAML file to avoid this step
+
+    :param some_dict: A config dictionary
+    :return: a reversed dictionary with keys as values and vice versa
+    """
+    return {value: key for key, value in some_dict.items()}
+
+
+def check_postcode(postcode):
+    """
+    Checks that the postcodes are in the right format
+    :param postcode: A string with a UK-style post code
+    :return: a post code, or if incorrect a blank string
+    """
+    if postcode:
+        match = re.search(
+            r"^[A-Z]{1,2}\d[A-Z\d]? *\d[A-Z]{2}$", postcode.strip(), re.IGNORECASE
+        )
+        return match.group(0)
+    return ""
+
+
+def to_short_postcode(postcode):
+    """
+    Remove whitespace from the beginning and end of postcodes and the last two digits for anonymity
+    return blank if not in the right format
+    :param postcode: A string with a UK-style post code
+    :return: a shortened post code with the area, district, and sector. The units is removed
+    """
+    if postcode:
+        try:
+            match = re.search(
+                r"^[A-Z]{1,2}\d[A-Z\d]? *\d[A-Z]{2}$", postcode.strip(), re.IGNORECASE
+            )
+            return match.group(0)
+        except AttributeError:
+            return ""
+    return ""
+
+
+def inherit_property(stream, prop_name):
+    """
+    Reads a property from StartTable and sets that property (if it exists) on every event between this event
+    and the next EndTable event.
+
+    :param event: A filtered list of event objects of type StartTable
+    :param prop_name: The property name to inherit
+    :return: An updated list of event objects
+    """
+    prop_value = None
+    for event in stream:
+        if isinstance(event, events.StartTable):
+            prop_value = getattr(event, prop_name, None)
+        elif isinstance(event, events.EndTable):
+            prop_value = None
+
+        if prop_value and not hasattr(event, prop_name):
+            event = event.from_event(event, **{prop_name: prop_value})
+
+        yield event
