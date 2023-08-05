6bb5f0f3e95aca658f5416ead743b5a2e7c30b6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:28 +0000

    Add package standup-shows/1.0.0/standup-shows-1.0.0.tar.gz

---
+++ packages/standup-shows/standup-shows-1.0.0.tar.gz/standup-shows-1.0.0/README.md
@@ -0,0 +1,62 @@
+<!-- # CLI -->
+# Installation
+```shell
+$ pip install standup
+````
+
+# Basic Usage
+"Search for shows in a 50-mile radius from zipcode 75201 for comics listed in ./comics.txt"
+
+```
+$ standup 75201 50 comics.txt
+Ms. Pat:
+    Arlington Improv - Thursday, December 01 at 08:00 PM - https://www.dead-frog.com/live-comedy/event/881867
+    Arlington Improv - Friday, December 02 at 07:30 PM - https://www.dead-frog.com/live-comedy/event/881868
+    Arlington Improv - Friday, December 02 at 09:45 PM - https://www.dead-frog.com/live-comedy/event/881869
+    Arlington Improv - Saturday, December 03 at 07:00 PM - https://www.dead-frog.com/live-comedy/event/881870
+    Arlington Improv - Saturday, December 03 at 09:30 PM - https://www.dead-frog.com/live-comedy/event/881871
+    Arlington Improv - Sunday, December 04 at 07:00 PM - https://www.dead-frog.com/live-comedy/event/881872
+
+Mark Normand:
+    Dallas Comedy Club - Wednesday, July 13 at 10:00 PM - https://www.dead-frog.com/live-comedy/event/123455
+    Dallas Comedy Club - Wednesday, July 13 at 11:00 PM - https://www.dead-frog.com/live-comedy/event/123456
+```
+
+The script takes a few seconds to execute because it scrapes events from the web. You can specify the number of threads to create with the `--max_threads` flag.
+
+```
+$ standup --help
+NAME
+    standup.py - Find stand-up comedy shows that your favorite comedians are scheduled to perform near you.
+
+SYNOPSIS
+    standup.py COMEDIANS ZIPCODE <flags>
+
+DESCRIPTION
+    Find stand-up comedy shows that your favorite comedians are scheduled to perform near you.
+
+POSITIONAL ARGUMENTS
+    COMEDIANS
+        Type: str
+        A filepath containing a newline-delimited list of comedians in the form of <firstname lastname>.
+    ZIPCODE
+        Type: int
+        The zipcode to search near.
+
+FLAGS
+    --radius=RADIUS
+        Type: int
+        Default: 25
+        The radius in miles around the zipcode to search.
+    --max_threads=MAX_THREADS
+        Type: int
+        Default: 100
+        The maximum number of threads to create. A new thread is created for each URL to scrape.
+
+NOTES
+    You can also use flags syntax for POSITIONAL ARGUMENTS
+```
+
+# Further Development
+- Output to email
+- Find a dataset to add support for musical artists
---
+++ packages/standup-shows/standup-shows-1.0.0.tar.gz/standup-shows-1.0.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['standup_shows']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['beautifulsoup4', 'fire', 'requests']
+
+entry_points = \
+{'console_scripts': ['standup = standup_shows.standup:main']}
+
+setup_kwargs = {
+    'name': 'standup-shows',
+    'version': '1.0.0',
+    'description': 'Find stand-up comedy shows near you from the command line.',
+    'long_description': '<!-- # CLI -->\n# Installation\n```shell\n$ pip install standup\n````\n\n# Basic Usage\n"Search for shows in a 50-mile radius from zipcode 75201 for comics listed in ./comics.txt"\n\n```\n$ standup 75201 50 comics.txt\nMs. Pat:\n    Arlington Improv - Thursday, December 01 at 08:00 PM - https://www.dead-frog.com/live-comedy/event/881867\n    Arlington Improv - Friday, December 02 at 07:30 PM - https://www.dead-frog.com/live-comedy/event/881868\n    Arlington Improv - Friday, December 02 at 09:45 PM - https://www.dead-frog.com/live-comedy/event/881869\n    Arlington Improv - Saturday, December 03 at 07:00 PM - https://www.dead-frog.com/live-comedy/event/881870\n    Arlington Improv - Saturday, December 03 at 09:30 PM - https://www.dead-frog.com/live-comedy/event/881871\n    Arlington Improv - Sunday, December 04 at 07:00 PM - https://www.dead-frog.com/live-comedy/event/881872\n\nMark Normand:\n    Dallas Comedy Club - Wednesday, July 13 at 10:00 PM - https://www.dead-frog.com/live-comedy/event/123455\n    Dallas Comedy Club - Wednesday, July 13 at 11:00 PM - https://www.dead-frog.com/live-comedy/event/123456\n```\n\nThe script takes a few seconds to execute because it scrapes events from the web. You can specify the number of threads to create with the `--max_threads` flag.\n\n```\n$ standup --help\nNAME\n    standup.py - Find stand-up comedy shows that your favorite comedians are scheduled to perform near you.\n\nSYNOPSIS\n    standup.py COMEDIANS ZIPCODE <flags>\n\nDESCRIPTION\n    Find stand-up comedy shows that your favorite comedians are scheduled to perform near you.\n\nPOSITIONAL ARGUMENTS\n    COMEDIANS\n        Type: str\n        A filepath containing a newline-delimited list of comedians in the form of <firstname lastname>.\n    ZIPCODE\n        Type: int\n        The zipcode to search near.\n\nFLAGS\n    --radius=RADIUS\n        Type: int\n        Default: 25\n        The radius in miles around the zipcode to search.\n    --max_threads=MAX_THREADS\n        Type: int\n        Default: 100\n        The maximum number of threads to create. A new thread is created for each URL to scrape.\n\nNOTES\n    You can also use flags syntax for POSITIONAL ARGUMENTS\n```\n\n# Further Development\n- Output to email\n- Find a dataset to add support for musical artists\n',
+    'author': 'Cal Warhurst',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/calwarhurst/standup-shows',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
