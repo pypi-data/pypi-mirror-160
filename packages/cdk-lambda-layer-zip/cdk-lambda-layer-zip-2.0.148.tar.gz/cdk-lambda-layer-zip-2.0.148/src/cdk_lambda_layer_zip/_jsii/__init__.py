74118a2d6ac98c995f9bb7db71b8fb8bd871e5ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:28 +0000

    Add package cdk-lambda-layer-zip/2.0.148/cdk-lambda-layer-zip-2.0.148.tar.gz

---
+++ packages/cdk-lambda-layer-zip/cdk-lambda-layer-zip-2.0.148.tar.gz/cdk-lambda-layer-zip-2.0.148/src/cdk_lambda_layer_zip/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-lambda-layer-zip",
+    "2.0.148",
+    __name__[0:-6],
+    "cdk-lambda-layer-zip@2.0.148.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
