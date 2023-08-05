f977ef6dd116d5d5b888cf7465d7ee0fec8bc385
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:25 +0000

    Add package py-lief/0.9.0/py-lief-0.9.0.tar.gz

---
+++ packages/py-lief/py-lief-0.9.0.tar.gz/py-lief-0.9.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="py-lief",
+    version="0.9.0",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="py-lief of py-lief of py-lief",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
