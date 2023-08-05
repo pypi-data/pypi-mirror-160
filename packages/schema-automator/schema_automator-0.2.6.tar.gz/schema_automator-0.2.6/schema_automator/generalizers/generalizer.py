aaf0c7d827e5d677679eced0d5904839989e6f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:01 +0000

    Add package schema-automator/0.2.6/schema_automator-0.2.6.tar.gz

---
+++ packages/schema-automator/schema_automator-0.2.6.tar.gz/schema_automator-0.2.6/schema_automator/generalizers/generalizer.py
@@ -0,0 +1,20 @@
+from abc import ABC
+
+from linkml_runtime.linkml_model import SchemaDefinition, Prefix
+
+
+class Generalizer(ABC):
+    """
+    Abstract Base Class for all Generalization Engines.
+
+    Generalization Engines take example data in some format and generalizes to a new SchemaDefinition
+    """
+
+    def convert(self, file: str, **kwargs) -> SchemaDefinition:
+        raise NotImplementedError
+
+    def add_prefix(self, schema: SchemaDefinition, prefix: str, url: str):
+        schema.prefixes[prefix] = Prefix(prefix, url)
+
+    def add_default_prefixes(self, schema: SchemaDefinition):
+        self.add_prefix(schema, 'linkml', 'https://w3id.org/linkml/')
