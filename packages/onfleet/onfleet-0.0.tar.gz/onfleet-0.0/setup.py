ce96357dcf0d54c14e4aaa4e404a2d488565f9e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:26 +0000

    Add package onfleet/0.0/onfleet-0.0.tar.gz

---
+++ packages/onfleet/onfleet-0.0.tar.gz/onfleet-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='James Li',
+    author_email='support@onfleet.com',
+    description="Onfleet's Python API Wrapper Package",
+    install_requires=['pyonfleet'],
+    long_description='Use `pyonfleet <https://pypi.python.org/pypi/pyonfleet/>`_ instead.',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='onfleet',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/pyonfleet/',
+    version='0.0',
+    zip_safe=False,
+)
