3968273fe51d48940204afd4adc94d3383096ea7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:14 +0000

    Add package renovosolutions.aws-cdk-certbot/1.1.154/renovosolutions.aws_cdk_certbot-1.1.154-py3-none-any.whl

---
+++ packages/renovosolutions.aws-cdk-certbot/renovosolutions.aws_cdk_certbot-1.1.154-py3-none-any.whl/certbot/_jsii/__init__.py
@@ -0,0 +1,34 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_events._jsii
+import aws_cdk.aws_events_targets._jsii
+import aws_cdk.aws_iam._jsii
+import aws_cdk.aws_lambda._jsii
+import aws_cdk.aws_route53._jsii
+import aws_cdk.aws_s3._jsii
+import aws_cdk.aws_sns._jsii
+import aws_cdk.aws_sns_subscriptions._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+import one_time_event._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@renovosolutions/cdk-library-certbot",
+    "1.1.154",
+    __name__[0:-6],
+    "cdk-library-certbot@1.1.154.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
