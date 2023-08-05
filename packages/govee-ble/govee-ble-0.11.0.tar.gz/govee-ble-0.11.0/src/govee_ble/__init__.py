382c44adb5fd299aeedb22a2228d106a4e9dc3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:54 +0000

    Add package govee-ble/0.11.0/govee-ble-0.11.0.tar.gz

---
+++ packages/govee-ble/govee-ble-0.11.0.tar.gz/govee-ble-0.11.0/src/govee_ble/__init__.py
@@ -0,0 +1,47 @@
+"""Parser for Govee BLE advertisements.
+
+This file is shamelessly copied from the following repository:
+https://github.com/Ernst79/bleparser/blob/c42ae922e1abed2720c7fac993777e1bd59c0c93/package/bleparser/govee.py
+
+MIT License applies.
+"""
+from __future__ import annotations
+
+from bluetooth_sensor_state_data import SIGNAL_STRENGTH_KEY
+from sensor_state_data import (
+    DeviceClass,
+    DeviceKey,
+    SensorDescription,
+    SensorUpdate,
+    SensorValue,
+)
+from sensor_state_data.data import (
+    ATTR_HW_VERSION,
+    ATTR_MANUFACTURER,
+    ATTR_MODEL,
+    ATTR_NAME,
+    ATTR_SW_VERSION,
+    SensorDeviceInfo,
+)
+
+from .parser import GoveeBluetoothDeviceData
+
+__version__ = "0.11.0"
+
+__all__ = [
+    "GoveeBluetoothDeviceData",
+    "SIGNAL_STRENGTH_KEY",
+    "ATTR_HW_VERSION",
+    "ATTR_MANUFACTURER",
+    "ATTR_MODEL",
+    "ATTR_NAME",
+    "ATTR_SW_VERSION",
+    "SIGNAL_STRENGTH_KEY",
+    "SensorDescription",
+    "SensorDeviceInfo",
+    "DeviceClass",
+    "DeviceKey",
+    "SensorUpdate",
+    "SensorDeviceInfo",
+    "SensorValue",
+]
