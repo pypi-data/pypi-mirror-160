ec89ba8d4c4511a41d4ed4832d4cb3aed3261fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:09 +0000

    Add package planobs/0.4.3/planobs-0.4.3.tar.gz

---
+++ packages/planobs/planobs-0.4.3.tar.gz/planobs-0.4.3/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="planobs",
+    version="0.4.3",
+    author="Simeon Reusch",
+    author_email="simeon.reusch@desy.de",
+    description="Plan observations with the Zwicky Transient Facility",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="BSD (3-clause)",
+    keywords="astronomy astrophysics",
+    url="https://github.com/simeonreusch/planobs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    python_requires=">=3.8.0",
+    package_data={
+        "planobs": [
+            "data/*.csv",
+            "data/references/*.csv",
+        ]
+    },
+    install_requires=[
+        "astroplan>=0.7",
+        "astropy",
+        "astroquery",
+        "coveralls",
+        "flask",
+        "geopandas",
+        "html5lib",
+        "lxml",
+        "pandas",
+        "penquins",
+        "matplotlib",
+        "numpy",
+        "shapely",
+        "tqdm",
+        "ztfquery",
+    ],
+)
