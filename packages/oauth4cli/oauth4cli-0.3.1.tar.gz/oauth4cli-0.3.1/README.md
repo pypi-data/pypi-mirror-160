32cccb12e79761d17b212b8780373f0489547d85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:06 +0000

    Add package oauth4cli/0.3.1/oauth4cli-0.3.1.tar.gz

---
+++ packages/oauth4cli/oauth4cli-0.3.1.tar.gz/oauth4cli-0.3.1/README.md
@@ -0,0 +1,4 @@
+# oauth4cli
+
+This project was migrated to more generic OAuth2Tools. Please look at
+https://pypi.org/project/oauth2tools/
---
+++ packages/oauth4cli/oauth4cli-0.3.1.tar.gz/oauth4cli-0.3.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='oauth4cli',
+    version='0.3.1',
+    description='A solution for an authorization code flow via Browser for the CLI.',
+    url='https://github.com/aruehl/oauth4cli',
+    author='Andreas Rühl',
+    author_email='andreas.ruehl@gmail.com',
+    license='MIT',
+    packages=[
+        'oauth4cli'
+    ],
+    install_requires=[
+        'flask',
+        'requests',
+        'requests-oauthlib',
+        'werkzeug'
+    ],
+    zip_safe=False
+)
