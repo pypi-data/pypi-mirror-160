19498dd14e0f3d4954beab4f1bd4e97d21fe2f9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:18 +0000

    Add package network3-medusa/1.0.31/network3_medusa-1.0.31.tar.gz

---
+++ packages/network3-medusa/network3_medusa-1.0.31.tar.gz/network3_medusa-1.0.31/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['call_clid']
+install_requires = \
+['Jinja2>=3.1.2,<4.0.0',
+ 'click>=8.1.3,<9.0.0',
+ 'hedera-sdk-py>=2.16.3,<3.0.0',
+ 'requests>=2.28.1,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['network3_medusa = network3_medusa.script:run']}
+
+setup_kwargs = {
+    'name': 'network3-medusa',
+    'version': '1.0.31',
+    'description': '',
+    'long_description': None,
+    'author': 'John Capobianco',
+    'author_email': 'ptcapo@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
