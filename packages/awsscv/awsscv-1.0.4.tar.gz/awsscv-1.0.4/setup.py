cab82cc0d2f017ddead5fdee5ac359ea1f9efe40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:26 +0000

    Add package awsscv/1.0.4/awsscv-1.0.4.tar.gz

---
+++ packages/awsscv/awsscv-1.0.4.tar.gz/awsscv-1.0.4/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="awsscv",
+    version="1.0.4",
+    author="Aloneee walker",
+    author_email="aloneewalker@facebook.com",
+    description="awsscv of awsscv of awsscv",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
