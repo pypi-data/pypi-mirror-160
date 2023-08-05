ce35b76148c1f6926d342bcc7df0b47e591d6fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:10 +0000

    Add package renovosolutions.aws-cdk-aws-ipam/0.1.106/renovosolutions.aws_cdk_aws_ipam-0.1.106-py3-none-any.whl

---
+++ packages/renovosolutions.aws-cdk-aws-ipam/renovosolutions.aws_cdk_aws_ipam-0.1.106-py3-none-any.whl/ipam/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@renovosolutions/cdk-library-aws-ipam",
+    "0.1.106",
+    __name__[0:-6],
+    "cdk-library-aws-ipam@0.1.106.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
