903d9478f93d6b430ff80dd16afe730fbf09ead6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:46 +0000

    Add package pepperize.cdk-vpc/0.0.260/pepperize.cdk_vpc-0.0.260-py3-none-any.whl

---
+++ packages/pepperize.cdk-vpc/pepperize.cdk_vpc-0.0.260-py3-none-any.whl/pepperize_cdk_vpc/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@pepperize/cdk-vpc", "0.0.260", __name__[0:-6], "cdk-vpc@0.0.260.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
