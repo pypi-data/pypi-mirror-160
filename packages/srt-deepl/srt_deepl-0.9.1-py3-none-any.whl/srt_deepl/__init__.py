f253a69bb73b82ec433edd681c5940a6fd71a5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:34 +0000

    Add package srt-deepl/0.9.1/srt_deepl-0.9.1-py3-none-any.whl

---
+++ packages/srt-deepl/srt_deepl-0.9.1-py3-none-any.whl/srt_deepl/__init__.py
@@ -0,0 +1,4 @@
+import os
+from .main import translate, INPUT_LANG, OUTPUT_LANG
+
+os.environ["MOZ_HEADLESS"] = "1"
---
+++ packages/srt-deepl/srt_deepl-0.9.1-py3-none-any.whl/srt_deepl/main.py
@@ -0,0 +1,141 @@
+import os
+import glob
+import logging
+import random
+import geckodriver_autoinstaller
+
+from selenium import webdriver
+from selenium.webdriver.common.proxy import Proxy, ProxyType
+
+from .deepl import Translator
+from .srt_parser import wrap_line, save_srt
+from .utils import get_proxies
+
+# Check if the current version of geckodriver exists
+geckodriver_autoinstaller.install()
+
+INPUT_LANG = {
+    "auto": "Any language (detect)",
+    "bg": "Bulgarian",
+    "zh": "Chinese",
+    "cs": "Czech",
+    "da": "Danish",
+    "nl": "Dutch",
+    "en": "English",
+    "et": "Estonian",
+    "fi": "Finnish",
+    "fr": "French",
+    "de": "German",
+    "el": "Greek",
+    "hu": "Hungarian",
+    "it": "Italian",
+    "ja": "Japanese",
+    "lv": "Latvian",
+    "lt": "Lithuanian",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "ro": "Romanian",
+    "ru": "Russian",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "es": "Spanish",
+    "sv": "Swedish",
+}
+
+OUTPUT_LANG = {
+    "bg": "Bulgarian",
+    "zh": "Chinese (simplified)",
+    "cs": "Czech",
+    "da": "Danish",
+    "nl": "Dutch",
+    "en": "English",
+    "et": "Estonian",
+    "fi": "Finnish",
+    "fr": "French",
+    "de": "German",
+    "el": "Greek",
+    "hu": "Hungarian",
+    "it": "Italian",
+    "ja": "Japanese",
+    "lv": "Latvian",
+    "lt": "Lithuanian",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "br": "Portuguese (Brazilian)",
+    "ro": "Romanian",
+    "ru": "Russian",
+    "sk": "Slovak",
+    "sl": "Slovenian",
+    "es": "Spanish",
+    "sv": "Swedish",
+}
+
+
+def translate(
+    filepath,
+    lang_from,
+    lang_to,
+    wrap_limit=50,
+    delete_old=False,
+    driver=None,
+):
+
+    if driver is None:
+        proxies = get_proxies()
+        if len(proxies) == 0:
+            proxies = get_proxies(https=False)
+
+        my_proxy = random.choice(proxies)
+
+        proxy = Proxy(
+            {
+                "proxyType": ProxyType.MANUAL,
+                "httpProxy": my_proxy,
+                "ftpProxy": my_proxy,
+                "sslProxy": my_proxy,
+                "noProxy": "",  # set this value as desired
+            }
+        )
+
+        driver = webdriver.Firefox(proxy=proxy)
+        driver.maximize_window()
+
+    translator = Translator(driver)
+
+    lang_from = {
+        "lang": lang_from,
+        "description": INPUT_LANG[lang_from],
+    }
+    lang_to = {
+        "lang": lang_to,
+        "description": OUTPUT_LANG[lang_to],
+    }
+
+    if type(filepath) == str:
+        filepath = [filepath]
+    elif type(filepath) != list:
+        raise TypeError("Filepath must be str or list")
+
+    files = []
+    for fpath in filepath:
+        if os.path.isdir(fpath):
+            files += glob.glob(fpath + "/**/*.srt", recursive=True)
+        elif os.path.splitext(fpath)[-1].lower() == ".srt":
+            files.append(fpath)
+
+    for fpath in files:
+        subs = translator.translate(
+            fpath,
+            lang_from,
+            lang_to,
+            wrap_limit,
+        )
+
+        for sub in subs:
+            if len(sub.content) > wrap_limit:
+                sub.content = wrap_line(sub.content, wrap_limit)
+
+        fname = os.path.splitext(fpath)[0]
+        save_srt(fname, lang_to["lang"], subs)
+
+    translator.close()
