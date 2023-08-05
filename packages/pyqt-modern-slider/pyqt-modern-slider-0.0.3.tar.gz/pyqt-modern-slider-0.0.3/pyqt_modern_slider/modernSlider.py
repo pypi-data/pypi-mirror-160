be8066010671fd5bcb3ad8773f4ce2a773836d67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:08:04 +0000

    Add package pyqt-modern-slider/0.0.3/pyqt-modern-slider-0.0.3.tar.gz

---
+++ packages/pyqt-modern-slider/pyqt-modern-slider-0.0.3.tar.gz/pyqt-modern-slider-0.0.3/pyqt_modern_slider/modernSlider.py
@@ -0,0 +1,39 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QSlider
+
+
+class ModernSlider(QSlider):
+    def __init__(self):
+        super().__init__()
+        self.__initUi()
+
+    def __initUi(self):
+        self.setOrientation(Qt.Horizontal)
+        slider = '''
+        QSlider {
+            margin-top: 3px;
+            margin-bottom: 3px;
+        }
+
+        QSlider::groove:horizontal {
+            border: #000088;
+            height: 8px;
+            background: #6aa6ed;
+            margin: 2px 0;
+        }
+
+        QSlider::handle:horizontal {
+            background: white;
+
+            border: 8px solid #6aa6ed;
+            width: 24px;
+            margin: -16px 0;
+            border-radius: 20px;
+        }
+        QSlider::add-page:horizontal {
+            background: white;
+            height: 8px;
+            margin: 2px 0;
+        }
+        '''
+        self.setStyleSheet(slider)
