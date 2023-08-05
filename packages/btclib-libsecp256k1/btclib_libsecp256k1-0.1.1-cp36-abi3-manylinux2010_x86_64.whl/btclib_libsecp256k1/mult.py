f28f2f1b23e5564167f2d491b4e6198e265fe4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:28 +0000

    Add package btclib-libsecp256k1/0.1.1/btclib_libsecp256k1-0.1.1-cp36-abi3-manylinux2010_x86_64.whl

---
+++ packages/btclib-libsecp256k1/btclib_libsecp256k1-0.1.1-cp36-abi3-manylinux2010_x86_64.whl/btclib_libsecp256k1/mult.py
@@ -0,0 +1,30 @@
+"""
+Secp256k1 point multiplication
+"""
+
+from typing import Union, Tuple
+
+from . import ffi, lib
+
+ctx = lib.secp256k1_context_create(769)
+
+
+def mult(num: Union[bytes, int]) -> Tuple[int, int]:
+    "Multply the generator point"
+
+    if isinstance(num, int):
+        num_bytes = num.to_bytes(32, "big")
+    else:
+        num_bytes = num
+
+    point = ffi.new("secp256k1_pubkey *")
+    lib.secp256k1_ec_pubkey_create(ctx, point, num_bytes)
+
+    output = ffi.new("char[65]")
+    length = ffi.new("size_t *", 65)
+
+    lib.secp256k1_ec_pubkey_serialize(ctx, output, length, point, 2)
+
+    result = ffi.unpack(output, 65).hex()
+
+    return (int(result[2:66], 16), int(result[66:130], 16))
