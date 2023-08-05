9db83b5e12298e4fcabc22200451dc51dfded19d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:07:59 +0000

    Add package cdk-aurora-globaldatabase/2.0.186/cdk-aurora-globaldatabase-2.0.186.tar.gz

---
+++ packages/cdk-aurora-globaldatabase/cdk-aurora-globaldatabase-2.0.186.tar.gz/cdk-aurora-globaldatabase-2.0.186/src/cdk_aurora_globaldatabase/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-aurora-globaldatabase",
+    "2.0.186",
+    __name__[0:-6],
+    "cdk-aurora-globaldatabase@2.0.186.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
