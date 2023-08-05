e58d79074a97c2b05a55cc606aa02a25a76b503b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:37:03 +0000

    Add package userproperty/0.1.0/userproperty-0.1.0.tar.gz

---
+++ packages/userproperty/userproperty-0.1.0.tar.gz/userproperty-0.1.0/setup.py
@@ -0,0 +1,14 @@
+
+import io
+from setuptools import find_packages, setup
+
+setup(
+	name="userproperty",
+	version='0.1.0',
+	description='wrapper class for dictionary',
+	author='Yongsoo Hwang',
+	install_requires=[],
+	packages=find_packages(),
+	zip_safe=False,
+	python_requires='>=3'
+	)
