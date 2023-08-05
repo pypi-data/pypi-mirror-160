d657943bd153cd832247ab478a0bc5dbb7b7d08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:06:34 +0000

    Add package hello-robot-stretch-factory/0.2.5/hello_robot_stretch_factory-0.2.5-py3-none-any.whl

---
+++ packages/hello-robot-stretch-factory/hello_robot_stretch_factory-0.2.5-py3-none-any.whl/hello_robot_stretch_factory-0.2.5.data/scripts/RE1_dynamixel_id_scan.py
@@ -0,0 +1,48 @@
+#!python
+
+from stretch_body.dynamixel_XL430 import DynamixelXL430, DynamixelCommError
+import argparse
+
+import stretch_body.hello_utils as hu
+hu.print_stretch_re_use()
+
+import stretch_body.device
+d = stretch_body.device.Device(name='dummy_device',req_params=False) # to initialize logging config
+
+
+parser=argparse.ArgumentParser(description='Scan a dynamixel bus by ID for servos')
+parser.add_argument("usb_full_path", help="The full path to dynamixel USB bus e.g.: /dev/hello-dynamixel-head")
+parser.add_argument("--baud", help="Baud rate (57600, 115200, or 1000000) [57600]", type=int,default=0)
+args = parser.parse_args()
+
+
+if args.baud ==0:
+    bauds = [57600, 115200]
+else:
+    bauds=[args.baud]
+
+m=None
+nfind=0
+try:
+    print('Scanning bus %s' % (args.usb_full_path))
+    for id in range(25):
+        print('Checking ID %d'%id)
+        baud = DynamixelXL430.identify_baud_rate(id, args.usb_full_path)
+        if baud !=-1:
+            m = DynamixelXL430(id, args.usb_full_path,baud=baud)
+            if not m.hw_valid:
+                print('Unable to open bus %s. Exiting'%args.usb_full_path)
+                exit(1)
+            try:
+                m.startup()
+                nfind=nfind+1
+            except DynamixelCommError:
+                print("Ping failed for ID %d and baud %d: "%(str(id),b))
+                continue
+            m.do_ping()
+            m.stop()
+except (KeyboardInterrupt, SystemExit):
+    if m is not None:
+        m.stop()
+
+print('Found %d  servos on bus %s'%(nfind,args.usb_full_path))
