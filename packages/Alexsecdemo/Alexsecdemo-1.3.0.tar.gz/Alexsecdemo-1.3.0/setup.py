1c36e6467962a62e9b43ebba2542b78b71aea265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:59 +0000

    Add package Alexsecdemo/1.3.0/Alexsecdemo-1.3.0.tar.gz

---
+++ packages/Alexsecdemo/Alexsecdemo-1.3.0.tar.gz/Alexsecdemo-1.3.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+from setuptools.command.install import install
+import requests
+import socket
+import getpass
+import os
+
+class CustomInstall(install):
+    def run(self):
+        install.run(self)
+        print('run custominstall successfully!')
+
+
+setup(
+        name='Alexsecdemo', #package name
+        version='1.3.0',
+        description='A sample Python project, do not download it!',
+        author='Alex',
+        license='MIT',
+        packages=['mymod1','mymod2','zzzsecdemo'],
+        py_modules=['Alexsecdemoraw'],
+        cmdclass={'install': CustomInstall},
+        author_email='zhuzhuzhuzai@gmail.com',
+        install_requires=[
+        "Bfixsecdemo",
+        ],
+)
