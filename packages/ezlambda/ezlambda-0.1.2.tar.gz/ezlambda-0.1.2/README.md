efd4a7e40991bab7f308b20f569a5592798ab80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:06 +0000

    Add package ezlambda/0.1.2/ezlambda-0.1.2.tar.gz

---
+++ packages/ezlambda/ezlambda-0.1.2.tar.gz/ezlambda-0.1.2/README.md
@@ -0,0 +1,15 @@
+# EZ Lambda
+
+**Easy and intuitive notation to replace simple lambda functions.**
+
+&nbsp;
+
+```python
+from ezlambda import _
+
+m = map(_ * 4 + 1, range(10))
+print(list(m))
+```
+```python
+[1, 5, 9, 13, 17, 21, 25, 29, 33, 37]
+```
---
+++ packages/ezlambda/ezlambda-0.1.2.tar.gz/ezlambda-0.1.2/src/ezlambda/__init__.py
@@ -0,0 +1,36 @@
+from .variable import Variable, VariableOperationMarker
+
+__version__ = "0.1.2"
+
+Var = Variable
+VarMarker = VariableOperationMarker
+
+_ = Var("_")
+a = Var("a")
+b = Var("b")
+c = Var("c")
+d = Var("d")
+e = Var("e")
+f = Var("f")
+g = Var("g")
+h = Var("h")
+i = Var("i")
+j = Var("j")
+k = Var("k")
+l = Var("l")
+m = Var("m")
+n = Var("n")
+o = Var("o")
+p = Var("p")
+q = Var("q")
+r = Var("r")
+s = Var("s")
+t = Var("t")
+u = Var("u")
+v = Var("v")
+w = Var("w")
+x = Var("x")
+y = Var("y")
+z = Var("z")
+
+_m = VarMarker()
