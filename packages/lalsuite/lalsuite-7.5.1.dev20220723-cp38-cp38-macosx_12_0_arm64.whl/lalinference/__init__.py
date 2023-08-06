2762755b2f30c38b5c4a39d132d408195e6a125a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:10 +0000

    Add package lalsuite/7.5.1.dev20220723/lalsuite-7.5.1.dev20220723-cp38-cp38-macosx_12_0_arm64.whl

---
+++ packages/lalsuite/lalsuite-7.5.1.dev20220723-cp38-cp38-macosx_12_0_arm64.whl/lalinference/__init__.py
@@ -0,0 +1,7 @@
+# Import SWIG wrappings, if available
+from .lalinference import *
+
+__version__ = "3.0.3.1"
+
+## \addtogroup lalinference_python
+"""This package provides Python wrappings and extensions to LALInference"""
