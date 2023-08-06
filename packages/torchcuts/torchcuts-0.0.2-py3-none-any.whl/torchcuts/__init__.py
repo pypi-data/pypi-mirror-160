1f7f330e31ae7de305422bf0bdf8a3aff57c7509
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:59 +0000

    Add package torchcuts/0.0.2/torchcuts-0.0.2-py3-none-any.whl

---
+++ packages/torchcuts/torchcuts-0.0.2-py3-none-any.whl/torchcuts/__init__.py
@@ -0,0 +1,7 @@
+"""Use TorchCuts to have quick and easy access to central content from
+several relevant namespaces!"""
+
+#  Copyright (c) 2022. Asger Jon Vistisen, All Rights Reserved.
+from __future__ import annotations
+
+from ._tcuts import TorchCuts
