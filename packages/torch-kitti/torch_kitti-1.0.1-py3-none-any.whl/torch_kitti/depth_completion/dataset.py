23dedd3a712b453600322b0f15061dd4f73c8111
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:52:44 +0000

    Add package torch-kitti/1.0.1/torch_kitti-1.0.1-py3-none-any.whl

---
+++ packages/torch-kitti/torch_kitti-1.0.1-py3-none-any.whl/torch_kitti/depth_completion/dataset.py
@@ -0,0 +1,231 @@
+"""
+Dataset loading for Depth Completion KITTI Dataset
+:ref:`http://www.cvlibs.net/datasets/kitti/eval_depth.php?benchmark=depth_completion`
+"""
+
+import os
+from typing import Callable, Dict, List, Tuple, Union
+from torch_kitti._types import Literal
+
+from torch_kitti.depth_completion import download as kitti_depth_completion_download
+from torch_kitti.depth_completion import (
+    folders_check as kitti_depth_completion_folders_check,
+)
+from torch_kitti.raw.synced_rectified import check_drives as kitti_raw_check_drives
+from torch_kitti.raw.synced_rectified import download as kitti_raw_download
+from torch_kitti.common import DataElem, DataGroup, GenericDataset, _LoadPrev
+
+from pathlib import Path
+
+__all__ = ["KittiDepthCompletionDataset"]
+
+
+def _identity(x: Dict) -> Dict:
+    return x
+
+
+def _list_groundtruths(
+    kitti_completion_root: str,
+    subset: Literal["train", "val", "test", "all"],
+    load_stereo: bool = False,
+) -> List[DataElem]:
+
+    path = Path(kitti_completion_root)
+    if not load_stereo or subset == "test":
+        gt_glob = "**/groundtruth/**/*.png"
+    else:
+        gt_glob = "**/groundtruth/**/image_02/**/*.png"
+
+    gts = []
+    if subset in ["train", "all", "test"]:
+        used_path = path / "train"
+        gts.extend(DataElem("gt", "depth", path) for path in used_path.glob(gt_glob))
+    if subset in ["val", "all", "test"]:
+        used_path = path / "val"
+        gts.extend(DataElem("gt", "depth", path) for path in used_path.glob(gt_glob))
+
+    if subset == "test":
+        path = path / "val_selection_cropped/groundtruth_depth"
+        test_gts = [DataElem("gt", "depth", path) for path in path.glob("*")]
+        gts = [gt for gt in gts if gt in test_gts]
+
+    return gts
+
+
+def generate_examples(
+    kitti_raw_root: str,
+    kitti_completion_root: str,
+    subset: Literal["train", "val", "test", "all"] = "train",
+    load_stereo: bool = False,
+    load_previous: Union[Tuple[int, int], int] = 0,
+    load_sequence: int = 1,
+) -> List[DataGroup]:
+
+    if load_previous != 0 and load_sequence != 1:
+        assert ValueError("can't use load_previous and load_sequence together")
+
+    gts = _list_groundtruths(kitti_completion_root, subset, load_stereo)
+
+    def _load_stereo(elem: DataElem):
+        if load_stereo:
+            path_left = str(elem.path.as_posix()).replace(
+                f"image_{elem.cam:0>2}", f"image_02"
+            )
+            elem_left = DataElem(
+                elem.name + "_left", elem.type, path_left, 2, elem.drive, elem.idx
+            )
+            path_right = str(elem.path.as_posix()).replace(
+                f"image_{elem.cam:0>2}", f"image_03"
+            )
+            elem_right = DataElem(
+                elem.name + "_right", elem.type, path_right, 3, elem.drive, elem.idx
+            )
+            return [elem_left, elem_right]
+        return [elem]
+
+    # load elems for each gt elem
+
+    elems = []
+    for gt in gts:
+        ex_elems = []
+        ex_elems.extend(_load_stereo(gt))
+
+        data = "_".join(gt.drive.split("_")[:3])
+        path = (
+            Path(kitti_raw_root)
+            / data
+            / gt.drive
+            / f"image_{gt.cam:0>2}/data/{gt.idx:0>10}.png"
+        )
+        img = DataElem("image", "image", path)
+        ex_elems.extend(_load_stereo(img))
+
+        path = Path(str(gt.path).replace("groundtruth", "velodyne_raw"))
+        lidar = DataElem("lidar", "depth", path)
+        ex_elems.extend(_load_stereo(lidar))
+
+        path = Path(kitti_raw_root) / data / "calib_cam_to_cam.txt"
+        calib = DataElem("intrinsics", "intrinsics", path, gt.cam, gt.drive, idx=gt.idx)
+        ex_elems.extend(_load_stereo(calib))
+
+        _load_prev_ = _LoadPrev(gt)
+        ex_elems = [
+            _load_prev_(elem, load_previous, load_sequence) for elem in ex_elems
+        ]
+
+        elems.append(DataGroup(ex_elems))
+    return elems
+
+
+class KittiDepthCompletionDataset(GenericDataset):
+    """
+    2017 KITTI depth completion benchmarks
+    dataset, consisting of 93k training and 1.5k test images.
+
+    Ground truth has been acquired by accumulating 3D point clouds from a
+    360 degree Velodyne HDL-64 Laserscanner and a consistency check using
+    stereo camera pairs.
+
+    To load this dataset are needed:
+
+    KITTI Raw dataset (sync+rect)
+        can be found at :ref:`http://www.cvlibs.net/datasets/kitti/raw_data.php`,
+        automatically downloaded if required.
+
+    KITTI depth completion maps
+        can be found at :ref:`http://www.cvlibs.net/datasets/kitti/eval_depth.php?\
+        benchmark=depth_completion`, automatically downloaded if required
+
+    Parameters
+    ----------
+    kitti_raw_root: str
+        path to the root of the KITTI raw data sync+rect folder.
+    depth_completion_root: str
+        path to the root of the depth completion path.
+    subset: str, default train
+        If 'train' creates the dataset from training set, if 'val' creates
+        the dataset from validation set, if 'test' creates the
+        dataset from test set.
+    load_stereo: bool, default False
+        if True each batch provides left and right synchronized and rectified
+        cameras (the dataset size is halved). Not available on testing.
+    load_previous: Union[int, Tuple[int, int]], optional
+        if used a previous nth frame from the same sequence is provided, a random
+        previous frame in the range (n, m) is choosen if provided a tuple. if such
+        frame cannot be found the same frame is returned as previous
+    load_sequence: int, optional
+        if used loads a sequence of ``load_sequence`` frames, incompatible with
+        ``load_previous``, if enough previous frames are not available the last
+        one is used instead to fill the gap.
+    load_sequence: int, optional
+        It loads a sequence of frames, stacking them into a np.ndarray new dimension
+        or in a list.
+    transform: Callable[[Dict], Dict], optional
+        transformation applied to each output dictionary.
+    download: bool, default False
+        If true, downloads the dataset from the internet and puts
+        it in root directories. If dataset is already downloaded,
+        it is not downloaded again.
+    """
+
+    def __init__(
+        self,
+        kitti_raw_root: str,
+        kitti_completion_root: str,
+        subset: Literal["train", "val", "test", "all"] = "train",
+        load_stereo: bool = False,
+        load_previous: Union[Tuple[int, int], int] = 0,
+        load_sequence: int = 1,
+        transform: Callable[[Dict], Dict] = _identity,
+        download: bool = False,
+    ):
+        # checks
+        if subset not in ["train", "val", "test", "all"]:
+            raise ValueError(f"subset {subset} not in train, val, test, all")
+        if isinstance(load_previous, int) and load_previous < 0:
+            raise ValueError(f"load_previous negative not allowed")
+        if isinstance(load_previous, tuple):
+            if len(load_previous) != 2 or load_previous[0] < 0 or load_previous[1] < 0:
+                raise ValueError(
+                    f"load_previous must be a 2-tuple of positive ints or a single integer"
+                )
+        if load_sequence <= 0:
+            raise ValueError(f"load_sequence <= 0 not allowed")
+
+        # PARAMS
+        self._subset = subset
+        self._load_stereo = load_stereo
+        self._load_previous = load_previous
+
+        # DOWNLOAD
+        if download:
+            # download depth completion dataset
+            if not os.path.exists(kitti_completion_root):
+                kitti_depth_completion_download(kitti_completion_root)
+            elif os.path.isdir(kitti_completion_root) and not os.listdir(
+                kitti_completion_root
+            ):
+                kitti_depth_completion_download(kitti_completion_root)
+
+            # download kitti raw rect+sync dataset
+            if not os.path.exists(kitti_raw_root):
+                kitti_raw_download(kitti_raw_root)
+            elif os.path.isdir(kitti_raw_root) and not os.listdir(kitti_raw_root):
+                kitti_raw_download(kitti_raw_root)
+
+        # check folders
+        if not kitti_depth_completion_folders_check(kitti_completion_root):
+            raise ValueError(f"path {kitti_completion_root} contains wrong data")
+        if not kitti_raw_check_drives(kitti_raw_root):
+            raise ValueError(f"path {kitti_raw_root} contains wrong data")
+
+        # SET PATHS
+        elems = generate_examples(
+            kitti_raw_root,
+            kitti_completion_root,
+            subset,
+            load_stereo=load_stereo,
+            load_previous=load_previous,
+            load_sequence=load_sequence,
+        )
+        super().__init__(elems, transform)
---
+++ packages/torch-kitti/torch_kitti-1.0.1-py3-none-any.whl/torch_kitti/raw/dataset.py
@@ -0,0 +1,253 @@
+"""
+PyTorch Dataset to load KITTI Raw Data
+"""
+
+import os
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Tuple, Union
+
+from torch_kitti.common import DataElem, DataGroup, GenericDataset, _LoadPrev
+from torch_kitti._types import Literal
+
+from .synced_rectified import check_drives as sync_rect_check_drives
+from .synced_rectified import download as sync_rect_download
+
+__all__ = ["KittiRawDataset"]
+
+
+def _identity(x: Dict) -> Dict:
+    return x
+
+
+def generate_examples(
+    root: str,
+    select_cams: List[int] = [2],
+    imu_data: bool = False,
+    lidar_data: Optional[str] = "projective",
+    select_calibs: List[int] = [0, 2],
+    load_previous: Union[Tuple[int, int], int] = 0,
+    load_sequence: int = 1,
+) -> List[DataGroup]:
+
+    # find velodyne
+    path = Path(root)
+    all_velodyne = [
+        DataElem("lidar_pcd", "pcd", path, cam=False, pcd_format=lidar_data)
+        for path in path.glob("**/velodyne_points/data/*.bin")
+    ]
+
+    # add info required
+    elems = []
+    for vel in all_velodyne:
+        ex_elems = [vel]
+
+        ex_elems.append(
+            DataElem(
+                "lidar_to_cam_00",
+                "rt",
+                path / vel.date / "calib_velo_to_cam.txt",
+                cam=False,
+                drive=vel.drive,
+                idx=vel.idx,
+            )
+        )
+
+        ex_elems.append(
+            DataElem(
+                "imu_to_lidar",
+                "rt",
+                path / vel.date / "calib_imu_to_velo.txt",
+                cam=False,
+                drive=vel.drive,
+                idx=vel.idx,
+            )
+        )
+
+        for cam in select_cams:
+            ex_elems.append(
+                DataElem(
+                    f"cam_{cam:0>2}",
+                    "image",
+                    str(vel.path.as_posix())
+                    .replace("velodyne_points", f"image_{cam:0>2}")
+                    .replace(".bin", ".png"),
+                )
+            )
+
+        if imu_data:
+            ex_elems.append(
+                DataElem(
+                    f"imu_data",
+                    "imu",
+                    str(vel.path.as_posix())
+                    .replace("velodyne_points", "oxts")
+                    .replace(".bin", ".txt"),
+                    cam=False,
+                )
+            )
+
+        for calib in select_calibs:
+            ex_elems.append(
+                DataElem(
+                    f"cam_{calib:0>2}_calib",
+                    "calib",
+                    path / vel.date / "calib_cam_to_cam.txt",
+                    cam=calib,
+                    drive=vel.drive,
+                    idx=vel.idx,
+                )
+            )
+
+        _load_prev_ = _LoadPrev(vel)
+        ex_elems = [
+            _load_prev_(elem, load_previous, load_sequence) for elem in ex_elems
+        ]
+        elems.append(DataGroup(ex_elems))
+    return elems
+
+
+class KittiRawDataset(GenericDataset):
+    def __init__(
+        self,
+        root: str,
+        select_cams: List[int] = [2],
+        imu_data: bool = False,
+        lidar_data: Optional[str] = "projective",
+        select_calibs: List[int] = [0, 2],
+        load_previous: Union[Tuple[int, int], int] = 0,
+        load_sequence: int = 1,
+        transform: Callable[[Dict], Dict] = _identity,
+        download: Union[bool, Literal["sync+rect"]] = False,
+    ):
+        """
+        Dataset loading KITTI Raw Data available, KITTI raw data are organized by date
+        and drive and inside each drive are contained images taken by cameras,
+        for example::
+
+            .
+            └── 2011_09_26
+                ├── calib_cam_to_cam.txt
+                ├── calib_imu_to_velo.txt
+                ├── calib_velo_to_cam.txt
+                └── 2011_09_26_drive_0002_sync
+                    ├── image_00
+                    │   └── data
+                    │       └── ...
+                    ├── image_01
+                    │   └── data
+                    │       └── ...
+                    ├── image_02
+                    │   └── data
+                    │       └── ...
+                    ├── image_03
+                    │   └── data
+                    │       └── ...
+                    ├── oxts
+                    │   └── data
+                    │       └── ...
+                    └── velodyne_points
+                        └── data
+                            └── ...
+
+        Calibration data refers to the whole date.
+        Each example is a dictionary composed by many entries, such entries
+        can be selected at initialization time.
+
+        #. cam_0X: ndarray Image from camera X
+        #. lidar_data: ndarray Nx4 containing lidar \
+                       point cloud with respect to lidar coordinates
+        #. lidar_to_cam_00: [R|T] matrix from lidar to cam_00 coordinates in \
+                       projective space
+        #. cam_0X_calib: :class:`torch_kitti.raw.calibration.CamCalib` object \
+                       containing info about cam_0X calibration
+        #. imu_data: :class:`torch_kitti.raw.inertial_measurement_unit.IMUData` \
+                       object containing IMU data about the example
+        #. imu_to_lidar: [R|T] matrix from imu to lidar coordinates in projective \
+                       space
+
+        Parameters
+        ----------
+        root: str
+            path to the root of the dataset.
+        select_cams: Tuple[str], default ("cam_02",)
+            images to be loaded among cam_00, cam_01, cam_02, cam_03.
+        imu_data: bool, default False
+            if load IMU Data, when true examples will contain "imu_data" and also
+            "imu_to_lidar" fields
+        lidar_data: str, default "projective"
+            if "projective" Lidar Data are loaded in the projective space
+            (removing reflectance), if "reflectance" Lidar Data are loaded with
+            the reflectance, if None Lidar Data are not loaded. When enabled examples
+            will contain "lidar_data" and also "lidar_to_cam_00" fields
+        select_calibs: Tuple[str], default ("cam_00, "cam_02")
+            calibration objects to be loaded among cam_00, cam_01, cam_02, cam_03, they
+            are instances of :class:`torch_kitti.raw_calibration.CamCalib`
+        load_previous: Union[int, Tuple[int, int]], optional
+            if used a previous nth frame from the same sequence is provided, a random
+            previous frame in the range (n, m) is choosen if provided a tuple.
+        load_sequence: int, optional
+            It loads a sequence of frames, stacking them into a np.ndarray new dimension
+            or in a list.
+        transform: Callable[[Dict], Dict], optional
+            transformation applied to each output dictionary
+        download: bool or sync+rect, default False
+            if True downloads the sync+rect version, otherwise the version of
+            the dataset can be provided but by now only "sync+rect" is available
+        """
+
+        # PARAMS
+
+        self.select_cams = set(select_cams)
+        self.select_calibs = set(select_calibs)
+        self.imu_data = imu_data
+        self.transform = transform
+        self._load_previous = load_previous
+        self.lidar_data = lidar_data
+
+        # CHECK params
+
+        for cam in self.select_cams:
+            if cam not in range(4):
+                raise ValueError("each cam must be in range 0-3")
+
+        for calib in self.select_calibs:
+            if calib not in range(4):
+                raise ValueError("each calib must be in range 0-3")
+
+        if lidar_data not in ["projective", "reflectance", None]:
+            raise ValueError("lidar data must be 'projective', 'reflectance' or None")
+
+        if not isinstance(load_previous, (tuple, int)):
+            raise ValueError(
+                "load_previous must be and integer of a 2-tuple of integers"
+            )
+
+        if download not in [False, True, "sync+rect"]:
+            raise ValueError("download in True, False or sync+rect")
+
+        # DOWNLOAD
+
+        if download is True or download == "sync+rect":
+
+            # download sync+rect folders
+            if not os.path.exists(root):
+                sync_rect_download(root)
+            elif os.path.isdir(root) and not os.listdir(root):
+                sync_rect_download(root)
+
+        # check folders
+        if not sync_rect_check_drives(root):
+            raise ValueError(f"path {root} contains wrong data")
+
+        # PATHS
+
+        elems = generate_examples(
+            root,
+            select_cams,
+            imu_data,
+            lidar_data,
+            select_calibs,
+            load_previous,
+            load_sequence,
+        )
+        super().__init__(elems)
