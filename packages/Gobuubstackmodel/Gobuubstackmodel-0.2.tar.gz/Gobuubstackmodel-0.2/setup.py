049fcf56f6bb39bbb691985ddbf7220e961ebf0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:48 +0000

    Add package Gobuubstackmodel/0.2/Gobuubstackmodel-0.2.tar.gz

---
+++ packages/Gobuubstackmodel/Gobuubstackmodel-0.2.tar.gz/Gobuubstackmodel-0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Gobuubstackmodel",
+    version="0.2",
+    description="Personal Python lib for used for EDA's and train ML models",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Enrique Revuelta Garcia",
+    author_email="enrique.revuelta@enriquerevueltagarcia.com",
+    url="http://www.enriquerevueltagarcia.com",
+    packages=find_packages(),
+    scripts=[]
+)
