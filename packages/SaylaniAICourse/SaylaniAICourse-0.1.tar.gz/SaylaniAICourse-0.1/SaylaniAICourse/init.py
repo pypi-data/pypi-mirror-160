26a3af44cf0f6e27aedcea35989f7de94e72b11b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:38 +0000

    Add package SaylaniAICourse/0.1/SaylaniAICourse-0.1.tar.gz

---
+++ packages/SaylaniAICourse/SaylaniAICourse-0.1.tar.gz/SaylaniAICourse-0.1/SaylaniAICourse/init.py
@@ -0,0 +1,9 @@
+def mygenerator():
+    print('First item')
+    yield 10
+
+    print('Second item')
+    yield 20
+
+    print('Last item')
+    yield 30
---
+++ packages/SaylaniAICourse/SaylaniAICourse-0.1.tar.gz/SaylaniAICourse-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="SaylaniAICourse",
+version="0.1",
+description="this is a package",
+long_description="this is a very very long description",
+author = "Sir Qasim and Syed Muhammad Mooazam",
+packages = ["SaylaniAICourse"],
+install_packages = []
+)
