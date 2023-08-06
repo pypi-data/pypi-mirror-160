d5503dbe0b25a2311340acbedfe4f2e6245bc177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:00 +0000

    Add package nester-koisa/0.0.1/nester-koisa-0.0.1.tar.gz

---
+++ packages/nester-koisa/nester-koisa-0.0.1.tar.gz/nester-koisa-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A basic print list'
+
+# setting up
+setup(
+	name="nester-koisa",
+	version=VERSION,
+	py_modules=['nester-koisa'],
+	author='blackd000',
+	author_email='dongminhcho89@gmail.com',
+	description=DESCRIPTION,
+)
