f27de71300ed82aca2ea89349eb36329ef005de2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:21 +0000

    Add package cdk-create-ami/0.0.58/cdk-create-ami-0.0.58.tar.gz

---
+++ packages/cdk-create-ami/cdk-create-ami-0.0.58.tar.gz/cdk-create-ami-0.0.58/src/cdk_create_ami/_jsii/__init__.py
@@ -0,0 +1,22 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "cdk-create-ami", "0.0.58", __name__[0:-6], "cdk-create-ami@0.0.58.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
