7b1d9be913d79947205798fdc8a8fdec3a28c45f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:47 +0000

    Add package pyckaged/1.0/pyckaged-1.0.tar.gz

---
+++ packages/pyckaged/pyckaged-1.0.tar.gz/pyckaged-1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='pyckaged',
+    version='1.0',
+    entry_points={
+        #the only files are __main__.py and __init__.py so 'pyckaged:main' is the only entry point
+        #pyckaged:main means the main function in __main__.py
+        'console_scripts': [
+            'pyckaged = pyckaged.pyckaged:main',
+        ],
+    },
+    packages=['pyckaged'],
+    install_requires=[
+        'requests',
+        'colorama',
+        'termcolor',
+        'pyfiglet'
+    ],
+    author='lewolfyt',
+    author_email='ciblox3@gmail.com',
+    description='A package manager made with python! If you want your package to be added to the repository, please contact me!',
+    license='MIT',
+    keywords='package manager'
+    )
