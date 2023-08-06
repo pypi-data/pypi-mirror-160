eb2a66234863706853e84210fd26086351072a02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:42 +0000

    Add package acryl-executor/0.0.3rc3/acryl_executor-0.0.3rc3-py3-none-any.whl

---
+++ packages/acryl-executor/acryl_executor-0.0.3rc3-py3-none-any.whl/acryl_executor-0.0.3rc3.data/scripts/run_ingest.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# usage: ./run_ingest.sh <recipe-id> <datahub-version> <plugins-required> <tmp-dir>
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd "$DIR"
+
+# Create tmp file to store requirements using provided recipe id. 
+req_file="$4/requirements-$1.txt"
+touch $req_file
+echo "acryl-datahub[datahub-rest,datahub-kafka,$3]==$2" > $req_file
+
+venv_dir="$4/venv-$1"
+python3 -m venv $venv_dir
+source $venv_dir/bin/activate
+pip install --upgrade pip wheel setuptools
+pip install -r $req_file
+rm $req_file
+
+# Execute DataHub recipe, based on the recipe id. 
+if (python3 -m datahub ingest -c "$4/$1.yml"); then
+  rm -r $venv_dir
+  exit 0
+else
+  rm -r $venv_dir
+  exit 1
+fi
