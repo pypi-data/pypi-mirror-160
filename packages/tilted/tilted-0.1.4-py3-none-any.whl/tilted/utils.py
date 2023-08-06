2039404189d02d52c4bcf854dbbf22aec1d801a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:09 +0000

    Add package tilted/0.1.4/tilted-0.1.4-py3-none-any.whl

---
+++ packages/tilted/tilted-0.1.4-py3-none-any.whl/tilted/utils.py
@@ -0,0 +1,22 @@
+from typing import Any, Sequence
+
+from tilted.constants import NUM_BITS, UNUSED_BITS
+from tilted.enums import CardRank
+
+
+def get_combinations(items: Sequence[Any], n: int):
+    if n == 1:
+        for x in items:
+            yield [x]
+    for i in range(len(items)):
+        for x in get_combinations(items[:i], n - 1):
+            yield [items[i]] + x
+
+
+def bit_sequence_to_int(bit_list: Sequence[str]):
+    bits = "".join(bit_list)
+    return int(f"0b{bits}", 2)
+
+
+def get_binary_index_from_card_rank(rank: CardRank):
+    return NUM_BITS - rank.value - UNUSED_BITS
