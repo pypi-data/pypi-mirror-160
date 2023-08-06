0c666f1b7cb3723de577723cc2ee82a25703a225
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:17 +0000

    Add package kilroy-face-client-py-sdk/0.3.0/kilroy_face_client_py_sdk-0.3.0-py3-none-any.whl

---
+++ packages/kilroy-face-client-py-sdk/kilroy_face_client_py_sdk-0.3.0-py3-none-any.whl/kilroy_face_client_py_sdk/__init__.py
@@ -0,0 +1,27 @@
+from kilroy_face_client_py_sdk.client import FaceClient
+from kilroy_face_client_py_sdk.models import (
+    Config,
+    ConfigNotification,
+    ConfigSchema,
+    ConfigSetReply,
+    ConfigSetRequest,
+    FitPostsReply,
+    FitPostsRequest,
+    PostScore,
+    FitScoresReply,
+    FitScoresRequest,
+    GenerateReply,
+    GenerateRequest,
+    MetricsInfo,
+    MetricsNotification,
+    PostSchema,
+    Status,
+    StatusNotification,
+    StepReply,
+    StepRequest,
+)
+from kilroy_face_client_py_sdk.resources import (
+    resource,
+    resource_bytes,
+    resource_text,
+)
