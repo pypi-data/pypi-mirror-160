69f11d28fc068725355e339f9ada1ab278db3fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:53 +0000

    Add package student-46165/0.2/student_46165-0.2.tar.gz

---
+++ packages/student-46165/student_46165-0.2.tar.gz/student_46165-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup (name='student_46165',
+version='0.2',
+description='This is the Saylani DB Package',
+author = "Shahzad Ahmed Khan",
+packages = ['student_46165'],
+zip_safe=False)
