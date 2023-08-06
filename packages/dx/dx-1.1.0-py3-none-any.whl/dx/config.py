c9826667061260c6b4df49f26932a5a2952ec616
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:35 +0000

    Add package dx/1.1.0/dx-1.1.0-py3-none-any.whl

---
+++ packages/dx/dx-1.1.0-py3-none-any.whl/dx/config.py
@@ -0,0 +1,23 @@
+import os
+
+from IPython import get_ipython
+
+IN_IPYTHON_ENV = get_ipython() is not None
+DEFAULT_IPYTHON_DISPLAY_FORMATTER = None
+if IN_IPYTHON_ENV:
+    DEFAULT_IPYTHON_DISPLAY_FORMATTER = get_ipython().display_formatter
+
+
+def in_noteable_env() -> bool:
+    """
+    Check if we are running in a Noteable environment.
+
+    FUTURE: this will be used to determine whether IPython formatters
+    are automatically updated.
+    """
+    return "NTBL_USER_ID" in os.environ
+
+
+def in_nteract_env() -> bool:
+    # TODO: handle this?
+    return False
