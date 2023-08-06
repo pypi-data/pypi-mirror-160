d8baf54fc3619e8285e8c2e58d4d4a4af90667a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:19 +0000

    Add package deepModel/0.0.6/deepModel-0.0.6-py3-none-any.whl

---
+++ packages/deepModel/deepModel-0.0.6-py3-none-any.whl/deepmodel/core/__init__.py
@@ -0,0 +1,9 @@
+
+from .layers import fc_layer, pool_layer, attention_layer
+from .activation import dice
+from .sequence import SequencePoolingLayer
+from .sequence import WeightedPoolingLayer
+from .sequence import AttentionSequencePoolingLayer
+from .sequence import Transformer
+
+
