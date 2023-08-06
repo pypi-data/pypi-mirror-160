3a79ca530c0279502d780f16911121a9b9546665
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:13:14 +0000

    Add package py2sterimol/0.0.0/py2sterimol-0.0.0.tar.gz

---
+++ packages/py2sterimol/py2sterimol-0.0.0.tar.gz/py2sterimol-0.0.0/test/test_py2sterimol.py
@@ -0,0 +1,24 @@
+import os
+import sys
+import unittest
+
+# from py2sterimol import py2sterimol
+
+
+class Test_py2sterimol(unittest.TestCase):
+    """
+    Test the various functionalities of py2sterimol.
+    """
+
+    @classmethod
+    def setUpClass(self):
+        return
+
+    def test_placeholder(self):
+        """
+        """
+        self.assertTrue(1+1 == 2)
+
+
+if __name__ == '__main__':
+    unittest.main()
