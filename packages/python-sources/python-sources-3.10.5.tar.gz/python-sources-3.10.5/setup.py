18a3c7b37d1248456c6bc9c045ac784df518d3a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:25 +0000

    Add package python-sources/3.10.5/python-sources-3.10.5.tar.gz

---
+++ packages/python-sources/python-sources-3.10.5.tar.gz/python-sources-3.10.5/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+
+setup(
+    name='python-sources',
+    version='3.10.5',
+    setup_requires='setupmeta',
+    author='James Pic',
+    author_email='jamespic@gmail.com',
+    include_package_data=True,
+    license='MIT',
+)
