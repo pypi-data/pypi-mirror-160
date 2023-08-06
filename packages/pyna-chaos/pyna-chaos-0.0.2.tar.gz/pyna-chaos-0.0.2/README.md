13be98ee11fc26ae74c5e85dad9e45ceab08eb5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:19:36 +0000

    Add package pyna-chaos/0.0.2/pyna-chaos-0.0.2.tar.gz

---
+++ packages/pyna-chaos/pyna-chaos-0.0.2.tar.gz/pyna-chaos-0.0.2/README.md
@@ -0,0 +1,22 @@
+# pyna
+
+**P**ython package dedicated for research on **DYNA**mic system.
+
+```bash
+pip install pyna-chaos
+```
+
+```python
+import pyna
+```
+
+Plan to support:
+- One dimensional Map / Flow
+- Two dimensional Map / Flow
+    - Eigenvalue/vector
+    - Asymptotic Behaviour Analysis
+        - Topology Entropy (How bending are the curves after long-term evolution?)
+        - Lyapunov Exponent Spectrum
+    - Bifurcation diagram / Bifurcation curve 
+    - Stable/Unstable manifold drawing
+- Three dimensional Flow Visualization
---
+++ packages/pyna-chaos/pyna-chaos-0.0.2.tar.gz/pyna-chaos-0.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyna-chaos",
+    version="0.0.2",
+    author="Wenyin Wei",
+    author_email="wenyin.wei.ww@gmail.com",
+    description="Python package for DYNAmics (dynamical systems)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/WenyinWei/pyna",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
