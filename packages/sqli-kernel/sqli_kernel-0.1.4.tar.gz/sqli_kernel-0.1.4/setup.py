3d43c6bdec78cc6d05976acf5a1763f26390777c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:29 +0000

    Add package sqli-kernel/0.1.4/sqli_kernel-0.1.4.tar.gz

---
+++ packages/sqli-kernel/sqli_kernel-0.1.4.tar.gz/sqli_kernel-0.1.4/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="sqli_kernel",
+    version="0.1.4",
+    description="SQLite Kernel for Jupyter Notebook",
+    long_description=open("README.txt").read(),
+    long_description_content_type="text/plain",
+    url="https://github.com/mtatton/sqlok",
+    author="Michael Tatton",
+    license="",
+    packages=["sqli_kernel"],
+    install_requires=[
+        "tabulate",
+    ],
+    scripts={"sqli_kernel/sqlik_install"},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
