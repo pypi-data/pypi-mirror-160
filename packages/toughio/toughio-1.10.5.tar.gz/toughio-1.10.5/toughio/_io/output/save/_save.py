409f1d684aae61961aee6a8ca0e00cd2e3990e9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:07 +0000

    Add package toughio/1.10.5/toughio-1.10.5.tar.gz

---
+++ packages/toughio/toughio-1.10.5.tar.gz/toughio-1.10.5/toughio/_io/output/save/_save.py
@@ -0,0 +1,38 @@
+from __future__ import with_statement
+
+import numpy as np
+
+from ...input import tough
+from .._common import Output, reorder_labels
+
+__all__ = [
+    "read",
+]
+
+
+def read(filename, file_type, file_format, labels_order):
+    """Read SAVE."""
+    parameters = tough.read(filename)
+
+    labels = list(parameters["initial_conditions"].keys())
+    variables = [v["values"] for v in parameters["initial_conditions"].values()]
+
+    data = {"X{}".format(i + 1): x for i, x in enumerate(np.transpose(variables))}
+
+    data["porosity"] = np.array(
+        [v["porosity"] for v in parameters["initial_conditions"].values()]
+    )
+
+    userx = [
+        v["userx"]
+        for v in parameters["initial_conditions"].values()
+        if "userx" in v.keys()
+    ]
+    if userx:
+        data["userx"] = np.array(userx)
+
+    labels_order = (
+        labels_order if labels_order else parameters["initial_conditions_order"]
+    )
+    output = Output(file_type, file_format, None, np.array(labels), data)
+    return reorder_labels(output, labels_order)
---
+++ packages/toughio/toughio-1.10.5.tar.gz/toughio-1.10.5/toughio/_mesh/pickle/_pickle.py
@@ -0,0 +1,52 @@
+from __future__ import with_statement
+
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
+
+from ..._common import open_file
+
+__all__ = [
+    "read",
+    "write",
+]
+
+
+def read(filename):
+    """
+    Import pickled :class:`toughio.Mesh`.
+
+    Parameters
+    ----------
+    filename : str
+        Input file name.
+
+    Returns
+    -------
+    toughio.Mesh
+        Output mesh.
+
+    """
+    with open_file(filename, "rb") as f:
+        mesh = pickle.load(f)
+
+    return mesh
+
+
+def write(filename, mesh, protocol=pickle.HIGHEST_PROTOCOL):
+    """
+    Pickle :class:`toughio.Mesh`.
+
+    Parameters
+    ----------
+    filename : str
+        Output file name.
+    mesh : toughio.Mesh
+        Mesh to pickle.
+    protocol : integer, optional, default `pickle.HIGHEST_PROTOCOL`
+        :mod:`pickle` protocol version.
+
+    """
+    with open_file(filename, "wb") as f:
+        pickle.dump(mesh, f, protocol=protocol)
