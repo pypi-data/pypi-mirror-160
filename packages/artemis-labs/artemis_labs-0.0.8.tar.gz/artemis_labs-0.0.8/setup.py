294de1f0d73c51039710fa2cfe4b1cb6f91be383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:54 +0000

    Add package artemis-labs/0.0.8/artemis_labs-0.0.8.tar.gz

---
+++ packages/artemis-labs/artemis_labs-0.0.8.tar.gz/artemis_labs-0.0.8/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="artemis_labs",
+    version="0.0.8",
+    author="Artemis Labs",
+    author_email="austinmccoy@artemisar.com",
+    description="Artemis Labs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'imageio',
+        'websockets'
+    ]
+)
