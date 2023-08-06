e10c765a8f2d3e22b21ab6e03d464ef065c68865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:59 +0000

    Add package kirui/0.4.6/kirui-0.4.6.tar.gz

---
+++ packages/kirui/kirui-0.4.6.tar.gz/kirui-0.4.6/kirui_devserver/backend/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('index/', views.index, name='index'),
+    path('form/', views.FormView.as_view(), name='form'),
+    path('modal/', views.modal, name='modal'),
+    path('table/', views.table, name='table'),
+    path('table/filtered/', views.FilteredTable.as_view(), name='filtered-table'),
+    path('chart/', views.charts, name='charts'),
+]
+
+app_name = 'backend'
