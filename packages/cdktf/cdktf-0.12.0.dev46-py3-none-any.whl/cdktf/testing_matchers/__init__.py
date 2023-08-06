47df6141cbee6df7dad61d5e92e19dd856d769a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:01 +0000

    Add package cdktf/0.12.0.dev46/cdktf-0.12.0.dev46-py3-none-any.whl

---
+++ packages/cdktf/cdktf-0.12.0.dev46-py3-none-any.whl/cdktf/testing_matchers/__init__.py
@@ -0,0 +1,55 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+from .._jsii import *
+
+
+@jsii.data_type(
+    jsii_type="cdktf.testingMatchers.TerraformConstructor",
+    jsii_struct_bases=[],
+    name_mapping={"tf_resource_type": "tfResourceType"},
+)
+class TerraformConstructor:
+    def __init__(self, *, tf_resource_type: builtins.str) -> None:
+        '''
+        :param tf_resource_type: 
+
+        :stability: experimental
+        '''
+        self._values: typing.Dict[str, typing.Any] = {
+            "tf_resource_type": tf_resource_type,
+        }
+
+    @builtins.property
+    def tf_resource_type(self) -> builtins.str:
+        '''
+        :stability: experimental
+        '''
+        result = self._values.get("tf_resource_type")
+        assert result is not None, "Required property 'tf_resource_type' is missing"
+        return typing.cast(builtins.str, result)
+
+    def __eq__(self, rhs: typing.Any) -> builtins.bool:
+        return isinstance(rhs, self.__class__) and rhs._values == self._values
+
+    def __ne__(self, rhs: typing.Any) -> builtins.bool:
+        return not (rhs == self)
+
+    def __repr__(self) -> str:
+        return "TerraformConstructor(%s)" % ", ".join(
+            k + "=" + repr(v) for k, v in self._values.items()
+        )
+
+
+__all__ = [
+    "TerraformConstructor",
+]
+
+publication.publish()
