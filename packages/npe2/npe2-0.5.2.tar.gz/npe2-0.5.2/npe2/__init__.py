160905266227bb36181aae8debd40db2a281ae5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:04 +0000

    Add package npe2/0.5.2/npe2-0.5.2.tar.gz

---
+++ packages/npe2/npe2-0.5.2.tar.gz/npe2-0.5.2/npe2/__init__.py
@@ -0,0 +1,26 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+__author__ = "Talley Lambert"
+__email__ = "talley.lambert@gmail.com"
+
+from ._dynamic_plugin import DynamicPlugin
+from ._fetch import fetch_manifest
+from ._plugin_manager import PluginContext, PluginManager
+from .io_utils import read, read_get_reader, write, write_get_writer
+from .manifest import PackageMetadata, PluginManifest
+
+__all__ = [
+    "__version__",
+    "DynamicPlugin",
+    "fetch_manifest",
+    "PackageMetadata",
+    "PluginContext",
+    "PluginManager",
+    "PluginManifest",
+    "read_get_reader",
+    "read",
+    "write_get_writer",
+    "write",
+]
