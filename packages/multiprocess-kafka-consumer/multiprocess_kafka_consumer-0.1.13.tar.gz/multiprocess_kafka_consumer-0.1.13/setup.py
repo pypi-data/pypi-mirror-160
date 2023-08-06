3d47a8ead0b64998cf3defbf1b97aa6e5c7aa874
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:39 +0000

    Add package multiprocess-kafka-consumer/0.1.13/multiprocess_kafka_consumer-0.1.13.tar.gz

---
+++ packages/multiprocess-kafka-consumer/multiprocess_kafka_consumer-0.1.13.tar.gz/multiprocess_kafka_consumer-0.1.13/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name="multiprocess_kafka_consumer",
+    version="0.1.13",
+    description="",
+    author="",
+    install_requires=["requests", "kafka-python", "schedule"],
+    author_email="",
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
