0a7d5a42569a232c34b1022801738beec157e65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:22 +0000

    Add package astro-sdk-python/1.0.0b1/astro-sdk-python-1.0.0b1.tar.gz

---
+++ packages/astro-sdk-python/astro-sdk-python-1.0.0b1.tar.gz/astro-sdk-python-1.0.0b1/src/astro/sql/operators/drop.py
@@ -0,0 +1,33 @@
+from typing import Dict
+
+from airflow.decorators.base import get_unique_task_id
+from airflow.models import BaseOperator
+
+from astro.databases import create_database
+from astro.sql.table import Table
+
+
+class DropTableOperator(BaseOperator):
+    """Airflow Operator for dropping SQL tables."""
+
+    template_fields = ("table",)
+
+    def __init__(
+        self,
+        table: Table,
+        task_id: str = "",
+        **kwargs,
+    ):
+        self.table = table
+        task_id = task_id or get_unique_task_id("_drop")
+        super().__init__(
+            task_id=task_id,
+            **kwargs,
+        )
+
+    def execute(self, context: Dict) -> Table:  # skipcq: PYL-W0613
+        """Method run when the Airflow runner calls the operator."""
+        database = create_database(self.table.conn_id)
+        self.table = database.populate_table_metadata(self.table)
+        database.drop_table(self.table)
+        return self.table
