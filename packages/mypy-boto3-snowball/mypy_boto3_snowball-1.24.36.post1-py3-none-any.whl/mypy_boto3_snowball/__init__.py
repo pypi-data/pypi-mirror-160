c4c77a799afdfa76bdf4c940c443293ac8ab7f73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:54 +0000

    Add package mypy-boto3-snowball/1.24.36.post1/mypy_boto3_snowball-1.24.36.post1-py3-none-any.whl

---
+++ packages/mypy-boto3-snowball/mypy_boto3_snowball-1.24.36.post1-py3-none-any.whl/mypy_boto3_snowball/__init__.py
@@ -0,0 +1,48 @@
+"""
+Main interface for snowball service.
+
+Usage::
+
+    ```python
+    from boto3.session import Session
+    from mypy_boto3_snowball import (
+        Client,
+        DescribeAddressesPaginator,
+        ListClusterJobsPaginator,
+        ListClustersPaginator,
+        ListCompatibleImagesPaginator,
+        ListJobsPaginator,
+        SnowballClient,
+    )
+
+    session = Session()
+    client: SnowballClient = session.client("snowball")
+
+    describe_addresses_paginator: DescribeAddressesPaginator = client.get_paginator("describe_addresses")
+    list_cluster_jobs_paginator: ListClusterJobsPaginator = client.get_paginator("list_cluster_jobs")
+    list_clusters_paginator: ListClustersPaginator = client.get_paginator("list_clusters")
+    list_compatible_images_paginator: ListCompatibleImagesPaginator = client.get_paginator("list_compatible_images")
+    list_jobs_paginator: ListJobsPaginator = client.get_paginator("list_jobs")
+    ```
+"""
+from .client import SnowballClient
+from .paginator import (
+    DescribeAddressesPaginator,
+    ListClusterJobsPaginator,
+    ListClustersPaginator,
+    ListCompatibleImagesPaginator,
+    ListJobsPaginator,
+)
+
+Client = SnowballClient
+
+
+__all__ = (
+    "Client",
+    "DescribeAddressesPaginator",
+    "ListClusterJobsPaginator",
+    "ListClustersPaginator",
+    "ListCompatibleImagesPaginator",
+    "ListJobsPaginator",
+    "SnowballClient",
+)
