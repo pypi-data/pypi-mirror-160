f68973a7c075f0bd0161bfb398c1b9e25916e5da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:22 +0000

    Add package Ahmed-1/0.1/Ahmed-1-0.1.tar.gz

---
+++ packages/Ahmed-1/Ahmed-1-0.1.tar.gz/Ahmed-1-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="Ahmed-1",
+version="0.1",
+description="this is a package",
+long_description="this is a very very long description",
+author = "Sir Qasim and Ahmed Udiin Khan",
+packages = ["Ahmed-1"],
+install_packages = []
+)
