32e853f9ac2fe420883efcbeca5d88b7d5149ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:20 +0000

    Add package django-workflow-engine/0.0.21/django-workflow-engine-0.0.21.tar.gz

---
+++ packages/django-workflow-engine/django-workflow-engine-0.0.21.tar.gz/django-workflow-engine-0.0.21/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="django-workflow-engine",
+    version="0.0.21",
+    packages=find_packages(),
+    include_package_data=True,
+    author="DIT Live Service Team",
+    author_email="live.services@digital.trade.gov.uk",
+    url="https://github.com/uktrade/django-workflow-engine",
+    description="Lightweight, reusable workflow engine for Django applications",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "django>=3.2.8",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
