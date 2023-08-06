e3ce3e37cf4c45433c21faba3a12f1fc9d0f894a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:05 +0000

    Add package twitchAPI/2.5.7.1/twitchAPI-2.5.7.1.tar.gz

---
+++ packages/twitchAPI/twitchAPI-2.5.7.1.tar.gz/twitchAPI-2.5.7.1/twitchAPI/__init__.py
@@ -0,0 +1,7 @@
+from twitchAPI.twitch import Twitch
+from twitchAPI.oauth import UserAuthenticator, refresh_access_token, validate_token, revoke_token
+from twitchAPI.types import *
+from twitchAPI.pubsub import PubSub
+from twitchAPI.eventsub import EventSub
+
+VERSION = (2, 5, 7)
