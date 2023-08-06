340f236f8bea25a3a7c059996de252bcfb738d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:16:55 +0000

    Add package finflo/1.5.0/finflo-1.5.0.tar.gz

---
+++ packages/finflo/finflo-1.5.0.tar.gz/finflo-1.5.0/finflo/sample_code.py
@@ -0,0 +1,9 @@
+from transition import FinFlotransition
+from django.shortcuts import HttpResponse
+
+qs = FinFlotransition()
+
+def index(self):
+    # transition2(stage = 3)
+    qs.transition(type = "INVOICE" , action = "DELETE" , stage = 2)
+    return HttpResponse(str("data"))
