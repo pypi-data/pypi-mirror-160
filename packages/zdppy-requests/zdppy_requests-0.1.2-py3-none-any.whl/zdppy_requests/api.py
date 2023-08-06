56cbab00e88f3124ebbaf78ae095e8b8b6a9c7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:17 +0000

    Add package zdppy-requests/0.1.2/zdppy_requests-0.1.2-py3-none-any.whl

---
+++ packages/zdppy-requests/zdppy_requests-0.1.2-py3-none-any.whl/zdppy_requests/api.py
@@ -0,0 +1,121 @@
+from . import sessions
+from .models import Response
+
+
+def request(method, url, **kwargs):
+    """
+    构造请求对象
+    :param method: 请求方法
+    :param url: 请求地址
+    :param params: 查询参数
+    :param data: form表单参数
+    :param json: json参数
+    :param headers: 请求头
+    :param cookies: cookie
+    :param files: 文件列表
+    :param auth: 权限校验
+    :type timeout: 超时时间
+    :param allow_redirects: 重定向
+    :param proxies: 代理
+    :param verify: 认证
+    :param stream: 流
+    :param cert: SSL文件
+    """
+
+    # By using the 'with' statement we are sure the session is closed, thus we
+    # avoid leaving sockets open which can trigger a ResourceWarning in some
+    # cases, and look like a memory leak in others.
+    with sessions.Session() as session:
+        return session.request(method=method, url=url, **kwargs)
+
+
+def get(url: str, params: dict = None, **kwargs) -> Response:
+    r"""
+    发送GET请求
+
+    :param url: 请求的URL地址
+    :param params: 请求参数
+    :return: 响应对象
+    """
+
+    return request('get', url, params=params, **kwargs)
+
+
+def options(url, **kwargs):
+    r"""Sends an OPTIONS request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('options', url, **kwargs)
+
+
+def head(url, **kwargs):
+    r"""Sends a HEAD request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes. If
+        `allow_redirects` is not provided, it will be set to `False` (as
+        opposed to the default :meth:`request` behavior).
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    kwargs.setdefault('allow_redirects', False)
+    return request('head', url, **kwargs)
+
+
+def post(url, data=None, json=None, **kwargs):
+    r"""Sends a POST request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('post', url, data=data, json=json, **kwargs)
+
+
+def put(url, data=None, **kwargs):
+    r"""
+    发送PUT请求
+    :param url: 请求的URL地址
+    :param data: 请求的数据
+    :param json: JSON格式的数据
+    """
+
+    return request('put', url, data=data, **kwargs)
+
+
+def patch(url, data=None, **kwargs):
+    r"""Sends a PATCH request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of the :class:`Request`.
+    :param json: (optional) json data to send in the body of the :class:`Request`.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('patch', url, data=data, **kwargs)
+
+
+def delete(url, **kwargs):
+    r"""Sends a DELETE request.
+
+    :param url: URL for the new :class:`Request` object.
+    :param \*\*kwargs: Optional arguments that ``request`` takes.
+    :return: :class:`Response <Response>` object
+    :rtype: requests.Response
+    """
+
+    return request('delete', url, **kwargs)
