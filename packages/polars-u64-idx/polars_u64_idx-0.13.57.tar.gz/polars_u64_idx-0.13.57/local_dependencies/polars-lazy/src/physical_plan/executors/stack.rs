337a21d3fd1d19046404d4db103be0491d72be19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:55 +0000

    Add package polars-u64-idx/0.13.57/polars_u64_idx-0.13.57.tar.gz

---
+++ packages/polars-u64-idx/polars_u64_idx-0.13.57.tar.gz/polars_u64_idx-0.13.57/local_dependencies/polars-lazy/src/physical_plan/executors/stack.rs
@@ -0,0 +1,41 @@
+use crate::physical_plan::executors::execute_projection_cached_window_fns;
+use crate::physical_plan::state::ExecutionState;
+use crate::prelude::*;
+use polars_core::{prelude::*, POOL};
+use rayon::prelude::*;
+
+pub struct StackExec {
+    pub(crate) input: Box<dyn Executor>,
+    pub(crate) has_windows: bool,
+    pub(crate) expr: Vec<Arc<dyn PhysicalExpr>>,
+    pub(crate) input_schema: SchemaRef,
+}
+
+impl Executor for StackExec {
+    fn execute(&mut self, state: &mut ExecutionState) -> Result<DataFrame> {
+        let mut df = self.input.execute(state)?;
+
+        state.set_schema(self.input_schema.clone());
+        let res = if self.has_windows {
+            // we have a different run here
+            // to ensure the window functions run sequential and share caches
+            execute_projection_cached_window_fns(&df, &self.expr, state)?
+        } else {
+            POOL.install(|| {
+                self.expr
+                    .par_iter()
+                    .map(|expr| expr.evaluate(&df, state))
+                    .collect::<Result<Vec<_>>>()
+            })?
+        };
+        state.clear_schema_cache();
+        state.clear_expr_cache();
+
+        let schema = &*self.input_schema;
+        for s in res {
+            df.with_column_and_schema(s, schema)?;
+        }
+
+        Ok(df)
+    }
+}
