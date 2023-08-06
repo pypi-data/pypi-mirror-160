3aa5adbbe81d6a0848ac8ac4a6e4717296654e92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:19 +0000

    Add package ansible-generator/3.0.2/ansible-generator-3.0.2.tar.gz

---
+++ packages/ansible-generator/ansible-generator-3.0.2.tar.gz/ansible-generator-3.0.2/ansible_generator/utilities.py
@@ -0,0 +1,23 @@
+"""utilities are functions that need to be used by multiple files."""
+from pathlib import Path
+from os import PathLike
+
+from ansible_generator.log import setup_logger
+
+logger = setup_logger(name=__name__)
+
+StrPath = str | PathLike[str]
+
+
+def join_cwd_and_directory_path(dir_path: StrPath) -> Path:
+    """Join the current working directory with the provided path.
+
+    Args:
+        dir_path: The directory path.
+
+    Returns:
+        Path: The finalized path.
+    """
+    logger.debug("joining paths")
+    joined_path = Path.cwd().joinpath(dir_path).resolve()
+    return joined_path
