7db82c9467bab2ab4968a49073e2d6c09fd95baf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:38 +0000

    Add package stories/5.0.0/stories-5.0.0-py2.py3-none-any.whl

---
+++ packages/stories/stories-5.0.0-py2.py3-none-any.whl/stories/__init__.py
@@ -0,0 +1,11 @@
+"""Service objects designed with OOP in mind."""
+from _stories.argument import Argument
+from _stories.initiate import initiate
+from _stories.state import State
+from _stories.story import Story
+from _stories.stubs import I
+from _stories.union import Union
+from _stories.variable import Variable
+
+
+__all__ = ("Story", "I", "initiate", "State", "Union", "Argument", "Variable")
