69278aea08c161c5e62ed5376a3decb9731d421a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:48 +0000

    Add package pygitm/0.8.0/pygitm-0.8.0.tar.gz

---
+++ packages/pygitm/pygitm-0.8.0.tar.gz/pygitm-0.8.0/pygitm/git.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from functools import partial
+
+import pytcm
+
+from pygitm.options import (
+    AddOptions,
+    CheckoutOptions,
+    CloneOptions,
+    CommitOptions,
+    PullOptions,
+    PushOptions,
+)
+
+git_command = partial(pytcm.execute, "git")
+
+
+def clone(opts: CloneOptions, cwd: str = ...) -> pytcm.CommandResult:
+    return git_command(opts.to_list(), cwd)
+
+
+def add(opts: AddOptions, cwd: str = ...) -> pytcm.CommandResult:
+    return git_command(opts.to_list(), cwd)
+
+
+def push(opts: PushOptions, cwd: str = ...) -> pytcm.CommandResult:
+    return git_command(opts.to_list(), cwd)
+
+
+def pull(opts: PullOptions, cwd: str = ...) -> pytcm.CommandResult:
+    return git_command(opts.to_list(), cwd)
+
+
+def checkout(opts: CheckoutOptions, cwd: str = ...) -> pytcm.CommandResult:
+    return git_command(opts.to_list(), cwd)
+
+
+def commit(opts: CommitOptions, cwd: str = ...) -> pytcm.CommandResult:
+    return git_command(opts.to_list(), cwd)
+
+
+def is_git_repo(cwd: str = ...) -> bool:
+    c_result = git_command(
+        [pytcm.Positional("rev-parse"), pytcm.Flag("--is-inside-work-tree", True)], cwd
+    )
+
+    return c_result.out.strip() == 'true'
