5ae7b6ca76763be3c08afe0b57bf7321860388c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:56 +0000

    Add package mdml-client/1.2.15/mdml_client-1.2.15.tar.gz

---
+++ packages/mdml-client/mdml_client-1.2.15.tar.gz/mdml_client-1.2.15/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "mdml_client",
+    version = "1.2.15",
+    author = "Jakob Elias",
+    author_email = "jelias@anl.gov",
+    description = "Client to connect to the MDML",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/anl-mdml/MDML_Client",
+    packages = setuptools.find_packages(),
+    install_requires=[
+        "pandas",
+        "boto3",
+	    "confluent_kafka",
+	    "requests",
+        "jsonschema"
+    ],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
