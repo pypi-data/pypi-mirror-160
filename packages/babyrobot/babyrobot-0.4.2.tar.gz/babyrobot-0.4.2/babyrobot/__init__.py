d6b9313c5083853ff76c0a2ca9fa7d9ceb8142de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:37 +0000

    Add package babyrobot/0.4.2/babyrobot-0.4.2.tar.gz

---
+++ packages/babyrobot/babyrobot-0.4.2.tar.gz/babyrobot-0.4.2/babyrobot/__init__.py
@@ -0,0 +1,41 @@
+from gym.envs.registration import register
+
+register(
+    id='BabyRobotEnv-v0',
+    entry_point='babyrobot.envs:BabyRobotEnv_v0',
+)
+
+register(
+    id='BabyRobotEnv-v1',
+    entry_point='babyrobot.envs:BabyRobotEnv_v1',
+)
+
+register(
+    id='BabyRobotEnv-v2',
+    entry_point='babyrobot.envs:BabyRobotEnv_v2',
+)
+
+register(
+    id='BabyRobotEnv-v3',
+    entry_point='babyrobot.envs:BabyRobotEnv_v3',
+)
+
+register(
+    id='BabyRobotEnv-v4',
+    entry_point='babyrobot.envs:BabyRobotEnv_v4',
+)
+
+register(
+    id='BabyRobotEnv-v5',
+    entry_point='babyrobot.envs:BabyRobotEnv_v5',
+)
+
+register(
+    id='BabyRobotEnv-v6',
+    entry_point='babyrobot.envs:BabyRobotEnv_v6',
+)
+
+register(
+    id='BabyRobotEnv-v7',
+    entry_point='babyrobot.envs:BabyRobotEnv_v7',
+)
