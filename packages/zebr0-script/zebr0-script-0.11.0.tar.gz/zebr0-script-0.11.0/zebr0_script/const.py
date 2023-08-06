285023a9faa307ec92c05d1f9aff4479b9403399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:08 +0000

    Add package zebr0-script/0.11.0/zebr0-script-0.11.0.tar.gz

---
+++ packages/zebr0-script/zebr0-script-0.11.0.tar.gz/zebr0-script-0.11.0/zebr0_script/const.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+REPORTS_PATH_DEFAULT = Path("/var/zebr0/script/reports")
+KEY_DEFAULT = "script"
+ATTEMPTS_DEFAULT = 4
+PAUSE_DEFAULT = 10
+VARIANT_DEFAULT = ""
+
+INCLUDE = "include"
+COMMAND = "command"
+VARIANT = "variant"
+KEY = "key"
+TARGET = "target"
+CONTENT = "content"
+STATUS = "status"
+OUTPUT = "output"
