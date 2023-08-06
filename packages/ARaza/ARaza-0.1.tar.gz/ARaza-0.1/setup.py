e25cb90c21af234b04765e9d27e9ab907641cb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:39 +0000

    Add package ARaza/0.1/ARaza-0.1.tar.gz

---
+++ packages/ARaza/ARaza-0.1.tar.gz/ARaza-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="ARaza",
+version="0.1",
+description="this is a package",
+long_description="this is a very very long description",
+author = "Sir Qasim and Atiq Raza",
+packages = ["ARaza"],
+install_packages = []
+)
