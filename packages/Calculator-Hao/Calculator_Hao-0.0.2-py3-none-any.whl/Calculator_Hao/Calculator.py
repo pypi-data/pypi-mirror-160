862e78e8558c1bc6aa96d1f40f91776b98ef32f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:07 +0000

    Add package Calculator-Hao/0.0.2/Calculator_Hao-0.0.2-py3-none-any.whl

---
+++ packages/Calculator-Hao/Calculator_Hao-0.0.2-py3-none-any.whl/Calculator_Hao/Calculator.py
@@ -0,0 +1,14 @@
+def add_all(x,y):
+    return x+y
+
+def minus_all(x,y):
+    return x-y
+
+def multiply_all(x,y):
+    return x*y
+
+def divide_all(x,y):
+    return x/y
+
+def custom(x,y):
+    return x*y - (x+y)
