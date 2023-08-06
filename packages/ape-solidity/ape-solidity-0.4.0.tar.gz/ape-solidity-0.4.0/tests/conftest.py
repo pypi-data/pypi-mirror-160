d21cd476670ce131e54a948c2055a9b94c8268ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:12 +0000

    Add package ape-solidity/0.4.0/ape-solidity-0.4.0.tar.gz

---
+++ packages/ape-solidity/ape-solidity-0.4.0.tar.gz/ape-solidity-0.4.0/tests/conftest.py
@@ -0,0 +1,63 @@
+import shutil
+from distutils.dir_util import copy_tree
+from pathlib import Path
+from tempfile import mkdtemp
+
+import ape
+import pytest
+import solcx  # type: ignore
+
+# NOTE: Ensure that we don't use local paths for these
+ape.config.DATA_FOLDER = Path(mkdtemp()).resolve()
+ape.config.PROJECT_FOLDER = Path(mkdtemp()).resolve()
+
+
+@pytest.fixture()
+def temp_solcx_path(monkeypatch):
+    solcx_install_path = mkdtemp()
+
+    monkeypatch.setenv(
+        solcx.install.SOLCX_BINARY_PATH_VARIABLE,
+        solcx_install_path,
+    )
+
+    yield solcx_install_path
+
+    if Path(solcx_install_path).is_dir():
+        shutil.rmtree(solcx_install_path, ignore_errors=True)
+
+
+@pytest.fixture
+def config():
+    return ape.config
+
+
+@pytest.fixture(autouse=True)
+def project(config):
+    project_source_dir = Path(__file__).parent
+    project_dest_dir = config.PROJECT_FOLDER / project_source_dir.name
+
+    # Delete build / .cache that may exist pre-copy
+    project_path = Path(__file__).parent
+    for path in (
+        project_path,
+        project_path / "BrownieProject",
+        project_path / "Dependency",
+        project_path / "DependencyOfDependency",
+        project_path / "ProjectWithinProject",
+        project_path / "VersionSpecifiedInConfig",
+    ):
+        for cache in (path / ".build", path / "contracts" / ".cache"):
+            if cache.is_dir():
+                shutil.rmtree(cache)
+
+    copy_tree(project_source_dir.as_posix(), project_dest_dir.as_posix())
+    with config.using_project(project_dest_dir) as project:
+        yield project
+        if project._project._cache_folder.is_dir():
+            shutil.rmtree(project._project._cache_folder)
+
+
+@pytest.fixture
+def compiler():
+    return ape.compilers.registered_compilers[".sol"]
