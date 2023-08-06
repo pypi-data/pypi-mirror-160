67cb2e26beddcfbd284da29659a022a91d762819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:24 +0000

    Add package reskin-sensor/2.0.1/reskin_sensor-2.0.1.tar.gz

---
+++ packages/reskin-sensor/reskin_sensor-2.0.1.tar.gz/reskin_sensor-2.0.1/setup.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="reskin_sensor",
+    version="2.0.1",
+    author="Raunaq Bhirangi",
+    author_email="rbhirang@andrew.cmu.edu",
+    description="Data acquisition library for a ReSkin sensor",
+    long_description=read('README.md'),
+    packages=find_packages(),
+    install_requires=["numpy>=1.21.3", "pyserial>=3.5"],
+    python_requires=">=3.6",
+    url="https://github.com/raunaqbhirangi/reskin_sensor.git",
+)
