f912eef83329a37b3ca571df3755d8cc6c996e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:00 +0000

    Add package xedocs/0.1.16/xedocs-0.1.16.tar.gz

---
+++ packages/xedocs/xedocs-0.1.16.tar.gz/xedocs-0.1.16/xedocs/schemas/operations_reports/anode_ramp.py
@@ -0,0 +1,15 @@
+
+from typing import Literal
+from .base_report import BaseOperationsReport
+
+
+
+class AnodeRampReport(BaseOperationsReport):
+    """Anode ramp - report
+    """
+
+    _ALIAS = "anode_ramps"
+    
+    direction: Literal['up','down']
+    starting_voltage: float
+    final_voltage: float
