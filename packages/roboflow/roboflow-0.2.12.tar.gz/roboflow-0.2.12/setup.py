e80defe3cd773e7d96c982ce160c0e18d3f0659a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:24 +0000

    Add package roboflow/0.2.12/roboflow-0.2.12.tar.gz

---
+++ packages/roboflow/roboflow-0.2.12.tar.gz/roboflow-0.2.12/setup.py
@@ -0,0 +1,47 @@
+import setuptools
+from setuptools import find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="roboflow",  # Replace with your own username
+    version="0.2.12",
+    author="Roboflow",
+    author_email="jacob@roboflow.com",
+    description="python client for the Roboflow application",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roboflow-ai/roboflow-python",
+    install_requires=[
+        "certifi==2021.5.30",
+        "chardet==4.0.0",
+        "cycler==0.10.0",
+        "glob2",
+        "idna==2.10",
+        "Pillow==9.0.1",
+        "kiwisolver==1.3.1",
+        "matplotlib",
+        "numpy>=1.18.5",
+        "opencv-python-headless==4.2.0.32",
+        "Pillow>=7.1.2",
+        "pyparsing==2.4.7",
+        "python-dateutil",
+        "python-dotenv",
+        "requests",
+        "requests_toolbelt",
+        "six",
+        "urllib3==1.26.6",
+        "wget",
+        "tqdm>=4.41.0",
+        "PyYAML>=5.3.1",
+        "wget"
+    ],
+    packages=find_packages(exclude=('tests',)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
