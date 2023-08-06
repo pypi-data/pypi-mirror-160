3602401e040a4f01aa779d42ace5104b33d1837b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:19:26 +0000

    Add package fortranformat/1.2.1/fortranformat-1.2.1.tar.gz

---
+++ packages/fortranformat/fortranformat-1.2.1.tar.gz/fortranformat-1.2.1/fortranformat/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '1.2.1'
+
+from .FortranRecordReader import FortranRecordReader
+from .FortranRecordWriter import FortranRecordWriter
+from ._exceptions import InvalidFormat
+from . import config
