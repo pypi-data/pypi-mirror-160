6696f9825561df7c354d506cd398c2a04220b21d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:42 +0000

    Add package real-deepfakes-detect/0.0.5/real-deepfakes-detect-0.0.5.tar.gz

---
+++ packages/real-deepfakes-detect/real-deepfakes-detect-0.0.5.tar.gz/real-deepfakes-detect-0.0.5/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "real-deepfakes-detect",
+    version = "0.0.5",
+    description='Realword DeepFakes Detection',
+    long_description='Realword DeepFakes Detection',
+    license = "MIT Licence",
+
+    # author='vg450',
+    # author_email='gward@python.net',
+    url='https://github.com/ThreeCatsLoveFish/RDD',
+
+    packages = find_packages(),
+    include_package_data = False,
+    data_files = [('configs', ['real_deepfakes_detect/configs/ffpp_2s_inference.yaml'])],
+    install_requires = [
+        'pytorchvideo==0.1.5',
+        'decord==0.6.0',
+        'omegaconf==2.2.2',
+        'torch==1.8.1',
+        'torchvision==0.9.1',
+        'opencv-python==4.5.5.64',
+        'scikit-learn==1.0.2',
+        'easydict',
+        'einops',
+        'requests',
+        'thop',
+        'seaborn',
+        'pandas',
+    ],
+
+    scripts = [],
+    entry_points = {
+        'console_scripts': [
+            'deepfakes_detect = real_deepfakes_detect:main'
+        ]
+    }
+)
