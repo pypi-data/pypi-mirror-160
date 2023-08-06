6c9959226a55a5e0c8f934a3454b8a58c17e317a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:24 +0000

    Add package colourfulprint/7.0/colourfulprint-7.0.tar.gz

---
+++ packages/colourfulprint/colourfulprint-7.0.tar.gz/colourfulprint-7.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+long_description = open("README.md").read()
+
+setup(name="colourfulprint",
+version="7.0",
+description="Colourfulprint wfor Rainbow print without extra newline character(\n).",
+long_description=long_description,
+long_description_content_type='text/markdown',
+author="NISHANT",
+url="https://github.com/Nishant2009/colourfulprint",
+scripts=["colourfulprint"],
+install_requires= [''],
+classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+], )
