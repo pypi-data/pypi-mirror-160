c93bb0e7fdd97fe3b41fe6dc4d2acbbc110b7f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:33 +0000

    Add package radicalpy/0.1rc0/radicalpy-0.1rc0.tar.gz

---
+++ packages/radicalpy/radicalpy-0.1rc0.tar.gz/radicalpy-0.1rc0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(
+    name="radicalpy",
+    version="0.1c",
+    license="MIT",
+    author="Lewis M. Antill",
+    author_email="lewismantill@gmail.com",
+    maintainer="Emil Vatai",
+    maintainer_email="emil.vatai@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/Spin-Chemistry-Labs/radicalpy",
+    keywords="quantum spin chemistry",
+    install_requires=["numpy", "scipy"],
+    include_package_data=True,
+)
+
+# Build with:
+# python setup.py sdist
+#
+# Local install with:
+# pip install dist/*.tar.gz --user
+#
+# Upload:
+# twine upload dist/*
+#
+# Create tag/release to upload from github
