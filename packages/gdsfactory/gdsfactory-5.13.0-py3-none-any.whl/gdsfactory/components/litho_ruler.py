95a51266730e1827ba608702b090609f90c922f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:41 +0000

    Add package gdsfactory/5.13.0/gdsfactory-5.13.0-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-5.13.0-py3-none-any.whl/gdsfactory/components/litho_ruler.py
@@ -0,0 +1,44 @@
+from typing import Tuple
+
+import gdsfactory as gf
+from gdsfactory.types import LayerSpec
+
+
+@gf.cell
+def litho_ruler(
+    height: float = 2,
+    width: float = 0.5,
+    spacing: float = 2.0,
+    scale: Tuple[float] = (3, 1, 1, 1, 1, 2, 1, 1, 1, 1),
+    num_marks: int = 21,
+    layer: LayerSpec = "WG",
+) -> gf.Component:
+    """Creates a ruler structure for lithographic measurement with marks of
+    varying scales to allow for easy reading by eye.
+
+    adapted from phidl.geometry
+
+    Args:
+        height: Height of the ruling marks.
+        width: Width of the ruling marks.
+        spacing: Center-to-center spacing of the ruling marks.
+        scale: Height scale pattern of marks.
+        num_marks: Total number of marks to generate.
+        layer: Specific layer to put the ruler geometry on.
+
+    """
+
+    D = gf.Component("litho_ruler")
+    for n in range(num_marks):
+        h = height * scale[n % len(scale)]
+        D << gf.components.rectangle(size=(width, h), layer=layer)
+
+    D.distribute(direction="x", spacing=spacing, separation=False, edge="x")
+    D.align(alignment="ymin")
+    D.flatten()
+    return D
+
+
+if __name__ == "__main__":
+    c = litho_ruler()
+    c.show(show_ports=True)
