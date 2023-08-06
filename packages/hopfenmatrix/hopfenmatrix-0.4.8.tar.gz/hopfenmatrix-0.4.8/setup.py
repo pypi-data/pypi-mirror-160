f642eb6a256e215b6a63f644025661025561a4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:23:37 +0000

    Add package hopfenmatrix/0.4.8/hopfenmatrix-0.4.8.tar.gz

---
+++ packages/hopfenmatrix/hopfenmatrix-0.4.8.tar.gz/hopfenmatrix-0.4.8/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="hopfenmatrix",
+    version="0.4.8",
+    author="Wolfgang Fischer, Niklas Pfister",
+    author_email="kontakt@omikron.dev",
+    description="A library to make matrix-nio easier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hopfenspace/hopfenmatrix/",
+    packages=setuptools.find_packages(include=("hopfenmatrix",)),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "matrix-nio[e2e]~=0.18.7",
+        "Pillow~=9.0.0",
+        "python-magic~=0.4.18",
+        "aiofiles~=0.6.0",
+        "mutagen~=1.45.1",
+        "aiohttp~=3.8.1"
+    ]
+)
