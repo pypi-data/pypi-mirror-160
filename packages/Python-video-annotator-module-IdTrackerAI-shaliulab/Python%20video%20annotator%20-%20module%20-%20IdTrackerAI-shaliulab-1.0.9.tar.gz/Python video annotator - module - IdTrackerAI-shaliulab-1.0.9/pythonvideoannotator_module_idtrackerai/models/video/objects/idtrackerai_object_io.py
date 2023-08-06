3041826500dbe8059f4519cf99492a2fa43e778c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:05 +0000

    Add package Python-video-annotator-module-IdTrackerAI-shaliulab/1.0.9/Python%20video%20annotator%20-%20module%20-%20IdTrackerAI-shaliulab-1.0.9.tar.gz

---
+++ packages/Python-video-annotator-module-IdTrackerAI-shaliulab/Python%20video%20annotator%20-%20module%20-%20IdTrackerAI-shaliulab-1.0.9.tar.gz/Python video annotator - module - IdTrackerAI-shaliulab-1.0.9/pythonvideoannotator_module_idtrackerai/models/video/objects/idtrackerai_object_io.py
@@ -0,0 +1,206 @@
+import time, copy, numpy as np, os, logging
+import warnings
+from confapp import conf, load_config
+import tqdm
+try:
+    import sys
+
+    sys.path.append(os.getcwd())
+    import local_settings # type: ignore
+
+    conf += local_settings
+except Exception as e:
+    print(e)
+    pass
+
+from datetime import datetime
+from idtrackerai.utils.py_utils import get_spaced_colors_util
+from idtrackerai.tracker.get_trajectories import produce_output_dict
+from idtrackerai.trajectories import save_trajectories
+
+from idtrackerai.tracker.trajectories_to_csv import (
+    convert_trajectories_file_to_csv_and_json,
+)
+from idtrackerai.groundtruth_utils.generate_groundtruth import (
+    generate_groundtruth,
+)
+from idtrackerai.groundtruth_utils.compute_groundtruth_statistics_general import (
+    compute_and_save_session_accuracy_wrt_groundtruth,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class IdtrackeraiObjectIO(object):
+
+    FACTORY_FUNCTION = "create_idtrackerai_object"
+
+    def save(self, data={}, obj_path=None):
+        idtrackerai_prj_path = os.path.relpath(
+            self.idtrackerai_prj_path, obj_path
+        )
+        data["idtrackerai-project-path"] = idtrackerai_prj_path
+        return super().save(data, obj_path)
+
+    def save_updated_identities(self):
+        # logger.info("Disconnecting list of blobs...")
+        # self.list_of_blobs.disconnect()
+        self.list_of_blobs.dealign(self.video_object)
+
+        logger.info("Saving list of blobs...")
+        path = os.path.join(
+            self.idtrackerai_prj_path,
+            "preprocessing",
+            "blobs_collection_no_gaps.npy",
+        )
+        np.save(path, self.list_of_blobs)
+        logger.info("List of blobs saved")
+
+        timestamp_str = datetime.fromtimestamp(time.time()).strftime(
+            "%Y-%m-%d_%H%M%S"
+        )
+
+        trajectories_wo_gaps_file = os.path.join(
+            self.idtrackerai_prj_path,
+            "trajectories_wo_gaps",
+            "trajectories_wo_gaps_{}.npy".format(timestamp_str),
+        )
+        logger.info("Producing trajectories without gaps ...")
+        trajectories_wo_gaps = produce_output_dict(
+            self.list_of_blobs.blobs_in_video, self.video_object
+        )
+        logger.info("Saving trajectories without gaps...")
+        os.makedirs(os.path.dirname(trajectories_wo_gaps_file), exist_ok=True)
+        # NOTE
+        # trajectories are saved here
+        # np.save(trajectories_wo_gaps_file, trajectories_wo_gaps)          
+        save_trajectories(
+            trajectories_wo_gaps_file,
+            trajectories_wo_gaps,
+            start=self.list_of_blobs._start_end_with_blobs[0],
+            end=self.list_of_blobs._start_end_with_blobs[1]+1,
+        )
+
+        if conf.CONVERT_TRAJECTORIES_DICT_TO_CSV_AND_JSON:
+            logger.info("Saving trajectories in csv format...")
+            convert_trajectories_file_to_csv_and_json(
+                trajectories_wo_gaps_file
+            )
+        logger.info("Trajectories without gaps saved")
+
+        trajectories_file = os.path.join(
+            self.idtrackerai_prj_path,
+            "trajectories",
+            "trajectories_{}.npy".format(timestamp_str),
+        )
+        logger.info("Producing trajectories")
+        trajectories = produce_output_dict(
+            self.list_of_blobs.blobs_in_video, self.video_object
+        )
+        logger.info("Saving trajectories...")
+        # NOTE
+        # trajectories are saved here
+        # np.save(trajectories_file, trajectories)
+        save_trajectories(
+            trajectories_file, trajectories,
+            start=self.list_of_blobs._start_end_with_blobs[0],
+            end=self.list_of_blobs._start_end_with_blobs[1]+1,
+        )
+
+
+        self.list_of_blobs.align(self.video_object)
+
+        if conf.CONVERT_TRAJECTORIES_DICT_TO_CSV_AND_JSON:
+            logger.info("Saving trajectories in csv format...")
+            convert_trajectories_file_to_csv_and_json(trajectories_file)
+        logger.info("Trajectories saved")
+        logger.info("Saving video object...")
+        self.video_object.save()
+        logger.info("Video saved")
+
+    def compute_gt_accuracy(self, generate=True, start=0, end=-1):
+        if generate:
+            generate_groundtruth(
+                self.video_object,
+                blobs_in_video=self.list_of_blobs.blobs_in_video,
+                start=start,
+                end=end,
+                save_gt=True,
+            )
+
+        compute_and_save_session_accuracy_wrt_groundtruth(
+            self.video_object, "normal"
+        )
+        logger.info(f"{self.video_object.gt_accuracy}")
+
+    def load(self, data, obj_path):
+        path = data.get("idtrackerai-project-path", None)
+        if path is None:
+            return
+
+        idtrackerai_prj_path = os.path.join(obj_path, path)
+
+        logger.info("Loading video object...")
+        vidobj_path = os.path.join(idtrackerai_prj_path, "video_object.npy")
+        videoobj = np.load(vidobj_path, allow_pickle=True).item()
+        videoobj.update_paths(vidobj_path)
+        logger.info("Video object loaded")
+
+        self.load_from_idtrackerai(idtrackerai_prj_path, videoobj)
+
+    def load_from_idtrackerai(self, project_path, video_object=None):
+
+        vidobj_path = os.path.join(project_path, "video_object.npy")
+        if video_object is None:
+            video_object = np.load(vidobj_path, allow_pickle=True).item()
+            video_object.update_paths(vidobj_path)
+
+        self.idtrackerai_prj_path = project_path
+
+        logger.info("Updating paths in video object")
+        self.video_object = video_object
+        video_object.update_paths(vidobj_path)
+        logger.info("Paths updated")
+        
+        if conf.TRY_TO_LOAD_FROM_NO_GAPS:
+            path = os.path.join(
+                project_path, "preprocessing", "blobs_collection_no_gaps.npy"
+            )
+
+            if not os.path.exists(path):
+                warnings.warn(f"{path} does not exist")
+                path = os.path.join(
+                    project_path, "preprocessing", "blobs_collection.npy"
+                )
+        else:
+                path = os.path.join(
+                    project_path, "preprocessing", "blobs_collection.npy"
+                )
+
+
+        logger.info("Loading list of blobs...")
+        self.list_of_blobs = np.load(path, allow_pickle=True).item()
+        logger.info("List of blobs loaded")
+        logger.info("Connecting list of blobs...")
+        if conf.RECONNECT_BLOBS_FROM_CACHE:
+            self.list_of_blobs.reconnect_from_cache()
+        else:
+            self.list_of_blobs.compute_overlapping_between_subsequent_frames()        
+        logger.info("List of blobs connected")
+        self.list_of_blobs.align(self.video_object)
+        logger.info("Loading fragments...")
+        path = os.path.join(project_path, "preprocessing", "fragments.npy")
+        if (
+            not os.path.exists(path)
+            and self.video_object.user_defined_parameters["number_of_animals"]
+            == 1
+        ):
+            self.list_of_framents = None
+            logger.info("Fragments did not exist")
+        else:
+            self.list_of_framents = np.load(path, allow_pickle=True).item()
+            logger.info("Loading fragments...")
+        self.colors = get_spaced_colors_util(
+            self.video_object.user_defined_parameters["number_of_animals"],
+            black=True,
+        )
---
+++ packages/Python-video-annotator-module-IdTrackerAI-shaliulab/Python%20video%20annotator%20-%20module%20-%20IdTrackerAI-shaliulab-1.0.9.tar.gz/Python video annotator - module - IdTrackerAI-shaliulab-1.0.9/pythonvideoannotator_module_idtrackerai/models/video/objects/idtrackerai_object_mouse_events.py
@@ -0,0 +1,179 @@
+import math, logging
+
+logger = logging.getLogger(__name__)
+from confapp import load_config
+import pythonvideoannotator_module_idtrackerai.constants
+from pythonvideoannotator_module_idtrackerai.utils import notify_propagation
+config=load_config(pythonvideoannotator_module_idtrackerai.constants)
+
+
+
+class SelectedBlob(object):
+    """
+    Class to store information about the selected blob.
+    """
+    def __init__(self, blob, blob_id, blob_pos):
+        """
+        :param int blob: Blob object.
+        :param int blob_id: Id of the blob object.
+        :param int blob_pos: Blob position.
+        """
+        self.blob     = blob
+        self.position = blob_pos[0], blob_pos[1]
+        self.identity = blob_id
+
+
+
+
+class IdtrackeraiObjectMouseEvents(object):
+
+
+    def __init__(self):
+
+        self._tmp_object_pos = None # used to store the temporary object position on drag
+        self.selected        = None # Store information about the the selected blob.
+        self._drag_active = True
+
+
+    def on_click(self, event, x, y):
+        """
+        Event on click, it is used to select a blob.
+        :param event: Click event.
+        :param int x: X coordinate.
+        :param int y: Y coordinate.
+        """
+
+        if config.FRAMES_ARE_ZERO_INDEXED:
+            frame_index = self.mainwindow.timeline.value+1
+        else:
+            frame_index = self.mainwindow.timeline.value
+
+        if not self._add_centroidchk.value:
+
+            selected = False
+
+            p0          = x, y
+
+
+            # no blobs to select, exit the function
+            if not self.list_of_blobs.blobs_in_video[frame_index]:
+                self.selected = None
+                return
+
+            # blobs in the current frame
+            blobs = self.list_of_blobs.blobs_in_video[frame_index]
+
+            for blob in blobs:
+
+                for identity, p1 in zip(blob.final_identities, blob.final_centroids_full_resolution):
+
+                    # check if which blob was selected
+                    if math.sqrt((p0[0] - p1[0])**2 + (p0[1] - p1[1])**2)< config.RADIUS_TO_SELECT_BLOB:
+
+                        self.mainwindow.player.stop()
+
+                        self.selected = SelectedBlob(blob, identity, p1)
+                        selected = True
+                        break
+
+            if not selected:
+                self.selected = None
+
+        elif self.selected and self.selected.blob.frame_number == frame_index:
+
+            # ask the new blob identity
+            identity = self.input_int(
+                'Type in the centroid identity',
+                title='Identity',
+                default=1
+            )
+
+            if not( identity in ['', None]):
+                try:
+                    self.selected.blob.add_centroid(self.video_object, (x,y), identity )
+                except Exception as e:
+                    logger.debug(str(e), exc_info=True)
+                    self.warning(str(e), 'Error')
+
+            self._add_centroidchk.value = False
+            self._tmp_object_pos = None
+            self._drag_active = False
+
+        elif self.selected and self.selected.blob.frame_number != frame_index:
+            self.selected = None
+            self._add_centroidchk.value = False
+
+
+
+    def on_double_click(self, event, x, y):
+
+        p0          = x, y
+        frame_index = self.mainwindow.timeline.value
+
+        # if one object is selected
+        if self.selected is not None:
+
+            identity = self.selected.identity
+            blob     = self.selected.blob
+            centroid = self.selected.position
+
+            # ask the new blob identity
+            new_blob_identity_blocked = self.input_text(
+                'Type in the new identity',
+                title='New identity',
+                default=str(identity) if identity is not None else "0"
+            )
+
+            full_set = set([str(e) for e in list(range(10))] + [";"])
+
+            # Update only if the new identity is different from the old one.
+            if new_blob_identity_blocked is not None:
+                assert set(new_blob_identity_blocked).union(full_set) == full_set
+                try:
+                    new_blob_identity = int(new_blob_identity_blocked.replace(";", ""))
+                    blob.update_identity(identity, new_blob_identity, centroid)
+                    blob._is_directly_annotated=True
+                    most_past_blob, most_future_blob = blob.propagate_identity(identity, new_blob_identity_blocked, centroid)
+                    n_past = blob.frame_number  - most_past_blob.frame_number
+                    n_future = most_future_blob.frame_number -  blob.frame_number
+                    
+                    if config.EXTRA_FUNCTIONS:
+                        notify_propagation(n_past, n_future)
+
+
+                except Exception as e:
+                    logger.debug(str(e), exc_info=True)
+                    self.warning(str(e), 'Error')
+
+        elif self._add_blobchk.value:
+            new_blob_identity = self.input_int(
+                'Type the identity for the new blob',
+                title='New identity',
+                default=0
+            )
+            if not( new_blob_identity in ['', None]):
+                try:
+                    self.list_of_blobs.add_blob(self.video_object, frame_index,
+                                                (x, y), new_blob_identity)
+                except Exception as e:
+                    logger.debug(str(e), exc_info=True)
+                    self.warning(str(e), 'Error')
+
+
+    def on_drag(self, p1, p2):
+
+        if self.selected and \
+           self.selected.identity and \
+           math.sqrt((p1[0] - p2[0])**2 + (p1[1] - p2[1])**2)>5:
+
+            self._tmp_object_pos = int(round(p2[0],0)), int(round(p2[1],0))
+
+
+    def on_end_drag(self, p1, p2):
+
+        if self._tmp_object_pos and self.selected and self.selected.blob:
+
+            self.selected.blob.update_centroid(self.video_object, self.selected.position, p2, self.selected.identity)
+            self.selected.position = p2
+
+        self._tmp_object_pos = None
