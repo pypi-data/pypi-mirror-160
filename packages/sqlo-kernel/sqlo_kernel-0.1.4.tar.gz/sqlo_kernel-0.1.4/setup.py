da54f4e0009e42a08e1de80ebc7fa8c74945b314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:29 +0000

    Add package sqlo-kernel/0.1.4/sqlo_kernel-0.1.4.tar.gz

---
+++ packages/sqlo-kernel/sqlo_kernel-0.1.4.tar.gz/sqlo_kernel-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="sqlo_kernel",
+    version="0.1.4",
+    description="Oracle Kernel for Jupyter Notebook",
+    long_description=open("README.txt").read(),
+    long_description_content_type="text/plain",
+    url="https://github.com/mtatton/sqlok",
+    author="Michael Tatton",
+    license="",
+    packages=["sqlo_kernel"],
+    install_requires=[
+        "tabulate",
+        "cx_Oracle",
+    ],
+    scripts={"sqlo_kernel/sqlok_install"},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
