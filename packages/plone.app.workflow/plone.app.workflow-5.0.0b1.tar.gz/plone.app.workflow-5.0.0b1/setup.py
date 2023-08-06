c09b82c2460489af8a89d3485fde6620ba8628e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:03 +0000

    Add package plone.app.workflow/5.0.0b1/plone.app.workflow-5.0.0b1.tar.gz

---
+++ packages/plone.app.workflow/plone.app.workflow-5.0.0b1.tar.gz/plone.app.workflow-5.0.0b1/setup.py
@@ -0,0 +1,46 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+version = "5.0.0b1"
+
+setup(
+    name="plone.app.workflow",
+    version=version,
+    description="workflow and security settings for Plone",
+    long_description=(open("README.rst").read() + "\n" + open("CHANGES.rst").read()),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Plone :: 6.0",
+        "Framework :: Plone :: Core",
+        "Framework :: Zope :: 5",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="workflow sharing plone",
+    author="Plone Foundation",
+    author_email="plone-developers@lists.sourceforge.net",
+    url="https://github.com/plone/plone.app.workflow",
+    license="GPL version 2",
+    packages=find_packages(),
+    namespace_packages=["plone", "plone.app"],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require=dict(
+        test=[
+            "plone.app.testing",
+        ]
+    ),
+    install_requires=[
+        "plone.base",
+        "plone.memoize",
+        "Products.statusmessages",
+        "setuptools",
+    ],
+)
