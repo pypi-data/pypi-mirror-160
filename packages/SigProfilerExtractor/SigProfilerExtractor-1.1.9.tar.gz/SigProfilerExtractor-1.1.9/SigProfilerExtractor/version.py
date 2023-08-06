53d8aa885f8c0e1de02ffab1c9ef1c8b8d29d034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:29 +0000

    Add package SigProfilerExtractor/1.1.9/SigProfilerExtractor-1.1.9.tar.gz

---
+++ packages/SigProfilerExtractor/SigProfilerExtractor-1.1.9.tar.gz/SigProfilerExtractor-1.1.9/SigProfilerExtractor/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEREXTRACTOR SETUP.PY
+short_version = '1.1.9'
+version = '1.1.9'
+Update = 'Fix typos, requirements, and remove legacy decomposition code'
+    
+    
