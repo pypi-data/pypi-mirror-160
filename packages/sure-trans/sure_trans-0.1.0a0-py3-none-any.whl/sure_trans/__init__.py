4c99531b6c0fcd1c7d9009413f1c7b765c5ab761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:54 +0000

    Add package sure-trans/0.1.0a0/sure_trans-0.1.0a0-py3-none-any.whl

---
+++ packages/sure-trans/sure_trans-0.1.0a0-py3-none-any.whl/sure_trans/__init__.py
@@ -0,0 +1,5 @@
+"""Init."""
+__version__ = "0.1.0a0"
+from .sure_trans import sure_trans
+
+__all__ = ("sure_trans",)
---
+++ packages/sure-trans/sure_trans-0.1.0a0-py3-none-any.whl/sure_trans/sure_trans.py
@@ -0,0 +1,7 @@
+"""Define sure_trans."""
+from logzero import logger
+
+
+def sure_trans():
+    """Define sure_trans."""
+    logger.debug(" entry ")
