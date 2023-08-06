cfb7438fda012adf23e25865db0b7b73099b636d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:24 +0000

    Add package matialvarezs-handlers-easy/0.1.68/matialvarezs_handlers_easy-0.1.68.tar.gz

---
+++ packages/matialvarezs-handlers-easy/matialvarezs_handlers_easy-0.1.68.tar.gz/matialvarezs_handlers_easy-0.1.68/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+# long_description = (here / "README.md").read_text(encoding="utf-8")
+
+setup(
+    name='matialvarezs_handlers_easy',
+    version='0.1.68',
+    description='Easy handler functions',
+    # include_package_data=True,
+    author='Matias Alvarez Sabate',
+    author_email='matialvarezs@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+    ],
+    packages=find_packages(where="src"),
+    # packages=['matialvarezs_handlers_easy'],  # this must be the same as the name above
+    python_requires=">=3.7, <4",
+    install_requires=[
+        'django-json-response',
+        'celery',
+        'django-celery-results',
+        'arrow',
+        'python-dateutil',
+        'pytz',
+        'python-crontab',
+        'paho-mqtt'
+    ],
+)
+
+
+# from distutils.core import setup
+# import setuptools
+#
+# setuptools.setup(
+#     name='matialvarezs_handlers_easy',
+#     packages=setuptools.find_packages(),  # this must be the same as the name above
+#     version='0.1.40',
+#     install_requires=[
+#         'django-json-response==1.1.3',
+#         'celery==4.1.0',
+#         'django-celery-results==1.0.1',
+#         'arrow==0.13.1',
+#         'python-dateutil==2.8.0',
+#         'pytz',
+#         'python-crontab'
+#     ],
+#     include_package_data=True,
+#     description='Easy handler',
+#     author='Matias Alvarez Sabate',
+#     author_email='matialvarezs@gmail.com',
+#     classifiers=[
+#         'Programming Language :: Python :: 3.5',
+#     ],
+# )
