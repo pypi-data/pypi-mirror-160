43b22c5c6b6f7aa477ea671cdf5b195c5699ec3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:33:53 +0000

    Add package pyfrigel-report-tool/0.3/pyfrigel-report-tool-0.3.tar.gz

---
+++ packages/pyfrigel-report-tool/pyfrigel-report-tool-0.3.tar.gz/pyfrigel-report-tool-0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='pyfrigel-report-tool',
+      version='0.3',
+      description='Package for the creation of Frigel reports',
+      packages=find_packages(),
+      package_data={'pyfrigel_report_tool': ['assets/*']},
+      include_package_data=True,
+      license='LICENSE.txt',
+   # TODO
+    url='http://pypi.python.org/pypi/pyfrigel-report-tool/',
+    install_requires=[
+       "reportlab",
+       'numpy'
+   ],
+      zip_safe=False)
