1a11dc79970210577718d3de8325882bf52ce94f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:11 +0000

    Add package easyidp/2.0.0.dev2/easyidp-2.0.0.dev2-py3-none-any.whl

---
+++ packages/easyidp/easyidp-2.0.0.dev2-py3-none-any.whl/easyidp/data.py
@@ -0,0 +1,327 @@
+import os
+import sys
+import pathlib
+import zipfile
+import gdown
+import requests
+import subprocess
+import webbrowser
+
+def user_data_dir(file_name=""):
+    r"""Get OS specific data directory path for EasyIDP.
+    
+    Parameters
+    ----------
+    file_name : str
+        file to be fetched from the data dir
+
+    Returns
+    -------
+    str
+        full path to the user-specific data dir
+
+    Notes
+    -----
+    Typical user data directories are:
+
+    .. code-block:: text
+
+        macOS:    ~/Library/Application Support/easyidp.data
+        Unix:     ~/.local/share/easyidp.data   # or in $XDG_DATA_HOME, if defined
+        Win 10:   C:\Users\<username>\AppData\Local\easyidp.data
+
+    For Unix, we follow the XDG spec and support ``$XDG_DATA_HOME`` if defined.
+
+    Referenced from stackoverflow [1]_ then get github [2]_ .
+
+    References
+    ----------
+    .. [1] Python: Getting AppData folder in a cross-platform way https://stackoverflow.com/questions/19078969/python-getting-appdata-folder-in-a-cross-platform-way
+    .. [2] SwagLyrics-For-Spotify/swaglyrics/__init__.py https://github.com/SwagLyrics/SwagLyrics-For-Spotify/blob/master/swaglyrics/__init__.py#L8-L32
+
+    """
+    # get os specific path
+    if sys.platform.startswith("win"):
+        os_path = os.getenv("LOCALAPPDATA")
+    elif sys.platform.startswith("darwin"):
+        os_path = "~/Library/Application Support"
+    else:
+        # linux
+        os_path = os.getenv("XDG_DATA_HOME", "~/.local/share")
+
+    # join with easyidp.data dir
+    path = pathlib.Path(os_path) / "easyidp.data"
+
+    add_usr = path.expanduser()
+
+    if not os.path.exists(str(add_usr)):
+        os.mkdir(str(add_usr))
+
+    return add_usr / file_name
+
+def show_data_dir():
+    """open the cached data files in cross-platform system default viewer.
+
+    It modified from this [1]_ webpage.
+
+    References
+    ----------
+    .. [1] Python: Opening a folder in Explorer/Nautilus/Finder https://stackoverflow.com/questions/6631299/python-opening-a-folder-in-explorer-nautilus-finder
+
+    """
+    path = user_data_dir()
+
+    if sys.platform.startswith("win"):
+        os.startfile(path)
+    elif sys.platform.startswith("darwin"):
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])
+
+
+def url_checker(url):
+    r"""Check if download url is accessable or not.
+    
+    Modified from this [1]_ link.
+
+    Parameters
+    ----------
+    url : str
+        The url need to be checked
+
+    References
+    ----------
+    .. [1] https://pytutorial.com/check-url-is-reachable
+    """
+    try:
+        #Get Url
+        get = requests.get(url, timeout=3)
+        # if the request succeeds 
+        if get.status_code == 200:
+            return True
+        else:
+            return False
+
+    #Exception
+    except requests.exceptions.RequestException as e:
+        # print URL with Errs
+        return False
+
+
+def download_all():
+    """download all datasets
+    """
+    lotus = Lotus()
+    gd = GDownTest()
+
+
+class EasyidpDataSet():
+
+    def __init__(self, name="", url_list=[], size="",):
+        """The dataset has the following properties (almost in string type)
+
+        name
+            The dataset name
+        url_list
+            The possible download urls
+        size
+            Ths size of zipped file
+        data_dir
+            The final dataset directory
+        zip_file
+            The *temporary* downloaded zip file path (won't be used in)
+        shp
+            The path to plot ROI shapefile (\*.shp)
+        photo
+            The folder path to the raw photos
+
+        pix4d.proj
+            The pix4d project folder
+        pix4d.param
+            The parameter folder of pix4d porject
+        pix4d.dom
+            The generated DOM path of plot map
+        pix4d.dsm
+            The generated DSM path of plot map
+        pix4d.pcd
+            The generated pointcloud path of plot map
+
+        metashape.proj
+            The metashape project file
+        metashape.param
+            The parameter folder of metashape porject
+        metashape.dom
+            The generated DOM path of plot map
+        metashape.dsm
+            The generated DSM path of plot map
+        metashape.pcd
+            The generated pointcloud path of plot map
+        
+
+        """
+        self.name = name
+        self.url_list = url_list
+        self.size = size
+        self.data_dir = user_data_dir(self.name)
+        self.zip_file = user_data_dir(self.name + ".zip")
+
+        self.pix4d = self.ReconsProj()
+        self.metashape = self.ReconsProj()
+
+    def load_data(self):
+        r"""Download dataset from Google Drive to user AppData folder
+
+        Caution
+        -------
+        For users in China mainland, please either find a way to access google drive,
+        or download from `CowTransfer Link <https://cowtransfer.com/s/4cc3d3f199824b>`_ 
+
+        Save and extract all \*.zip file to folder ``idp.data.show_data_dir()``.
+
+        The data structure should like this:
+
+        
+        .. tab:: Windows
+
+            .. code-block:: text
+
+                . C:\Users\<user>\AppData\Local\easyidp.data
+                |-- 2017_tanashi_lotus
+                |-- gdown_test
+                |-- ...
+
+        .. tab:: MacOS
+
+            .. code-block:: text
+
+                . ~/Library/Application Support/easyidp.data
+                |-- 2017_tanashi_lotus
+                |-- gdown_test
+                |-- ...
+
+        .. tab:: Linux/BSD
+
+            .. code-block:: text
+
+                . ~/.local/share/easyidp.data   # or in $XDG_DATA_HOME, if defined
+                |-- 2017_tanashi_lotus
+                |-- gdown_test
+                |-- ...
+
+        """
+
+        if not os.path.exists(self.data_dir):
+            out = self._download_data()
+
+            if os.path.exists(self.zip_file):
+                self._unzip_data()
+            else:
+                raise FileNotFoundError(
+                    f"Could not find the downloaded file [{self.zip_file}], "
+                    f"please call ().load_data() to download again.\n"
+                    f"Tips: ensure you can access Google Drive url."
+                )
+        
+    def _download_data(self):
+        """using gdown to download dataset from Google Drive to user AppData folder
+        """
+        # Download; extract data to disk.
+        # Raise an exception if the link is bad, or we can't connect, etc.
+
+        if url_checker(self.url_list[0]):   # google drive 
+            output = gdown.download(url=self.url_list[0], output=str(self.zip_file), quiet=False, fuzzy=True)
+        elif url_checker(self.url_list[1]):  # cowtransfer
+            print(
+                f"Could not access to default google drive download link <{self.url_list[1]}>."
+                f"Please download the file in browser and unzip to the popup folder "
+                f"[{str(user_data_dir())}]"
+            )
+            # open url
+            webbrowser.open(self.url_list[1], new=0, autoraise=True)
+            # open folder in file explorer
+            show_data_dir()
+        else:
+            raise ConnectionError("Could not find any downloadable link. Please contact the maintainer via github.")
+
+        return output
+
+    def _unzip_data(self):
+        """Unzip downloaded zip data and remove after decompression
+        """
+        with zipfile.ZipFile(self.zip_file, 'r') as zip_ref:
+            zip_ref.extractall(self.data_dir)
+
+        # already extracted
+        if os.path.exists(self.data_dir):
+            os.remove(self.zip_file)
+        else:
+            raise FileNotFoundError("Seems fail to unzip, please check whether the zip file is fully downloaded.")
+
+    class ReconsProj():
+
+
+        def __init__(self) -> None:
+            self.project = ""
+            self.param = ""
+            self.dom = ""
+            self.dsm = ""
+            self.pcd = ""
+
+
+class Lotus(EasyidpDataSet):
+
+    def __init__(self):
+        url_list = [
+            "https://drive.google.com/file/d/1SJmp-bG5SZrwdeJL-RnnljM2XmMNMF0j/view?usp=sharing",
+            "https://cowtransfer.com/s/03355b0b684442"
+        ]
+        super().__init__(name="2017_tanashi_lotus", url_list=url_list, size="3.6GB")
+
+        super().load_data()
+
+        self.photo = str(self.data_dir / "20170531" / "photos")
+        self.shp = str(self.data_dir / "plots.shp")
+
+        self.pix4d.project = str(self.data_dir / "20170531")
+        self.pix4d.param = str(self.data_dir / "20170531" / "params")
+        
+        self.pix4d.dom = str(
+            self.data_dir / "20170531" / "hasu_tanashi_20170531_Ins1RGB_30m_transparent_mosaic_group1.tif"
+        )
+        self.pix4d.dsm = str(
+            self.data_dir / "20170531" / "hasu_tanashi_20170531_Ins1RGB_30m_dsm.tif"
+        )
+        self.pix4d.pcd = str(
+            self.data_dir / "20170531" / "hasu_tanashi_20170531_Ins1RGB_30m_group1_densified_point_cloud.ply"
+        )
+
+        self.metashape.project = str(self.data_dir / "170531.Lotus.psx")
+        self.metashape.param = str(self.data_dir / "170531.Lotus.files")
+        self.metashape.dom = str(self.data_dir / "170531.Lotus.outputs" / "170531.Lotus_dom.tif")
+        self.metashape.dsm = str(self.data_dir / "170531.Lotus.outputs" / "170531.Lotus_dsm.tif")
+        self.metashape.pcd = str(self.data_dir / "170531.Lotus.outputs" / "170531.Lotus.laz")
+
+    def load_data(self):
+        return super().load_data()
+
+        
+class GDownTest(EasyidpDataSet):
+
+    def __init__(self):
+        url_list = [
+            "https://drive.google.com/file/d/1yWvIOYJ1ML-UGleh3gT5b7dxXzBuSPgQ/view?usp=sharing",
+            "https://cowtransfer.com/s/20fe3984fb9a47"
+        ]
+        super().__init__("gdown_test", url_list, "0.2KB")
+        super().load_data()
+
+        self.pix4d.proj = str(self.data_dir / "file1.txt")
+        self.metashape.param = str(self.data_dir / "folder1")
+
+    def load_data(self):
+        return super().load_data()
+
+
+
+
+    
---
+++ packages/easyidp/easyidp-2.0.0.dev2-py3-none-any.whl/tests/test_shp.py
@@ -0,0 +1,188 @@
+import os
+import pytest
+import pyproj
+import re
+import numpy as np
+import easyidp as idp
+
+
+data_path =  "./tests/data/shp_test"
+
+########################
+# test show shp fields #
+########################
+
+complex_shp_review_num5 = '| [0] ID                      | [1] MASSIFID        | [2] CROPTYPE   | [3] CROPDATE   | [4] CROPAREA   | [5] ATTID   |\n|-----------------------------|---------------------|----------------|----------------|----------------|-------------|\n| 230104112201809010000000000 | 2301041120000000000 | 小麦           | 2018-09-01     | 61525.26302    |             |\n| 230104112201809010000000012 | 2301041120000000012 | 蔬菜           | 2018-09-01     | 2802.33512     |             |\n| 230104112201809010000000014 | 2301041120000000014 | 玉米           | 2018-09-01     | 6960.7745      |             |\n| 230104112201809010000000061 | 2301041120000000061 | 牧草           | 2018-09-01     | 25349.08639    |             |\n| 230104112201809010000000062 | 2301041120000000062 | 玉米           | 2018-09-01     | 71463.27666    |             |\n| ...                         | ...                 | ...            | ...            | ...            | ...         |\n| 230104112201809010000000582 | 2301041120000000582 | 胡萝卜         | 2018-09-01     | 288.23876      |             |\n| 230104112201809010000000577 | 2301041120000000577 | 杂豆           | 2018-09-01     | 2001.80384     |             |\n| 230104112201809010000000583 | 2301041120000000583 | 大豆           | 2018-09-01     | 380.41704      |             |\n| 230104112201809010000000584 | 2301041120000000584 | 其它           | 2018-09-01     | 9133.25998     |             |\n| 230104112201809010000000585 | 2301041120000000585 | 其它           | 2018-09-01     | 1704.27193     |             |'
+
+roi_shp_preview = '|   [0] id |\n|----------|\n|        0 |\n|        1 |\n|        2 |'
+
+plots_shp_preview5 = '| [0] plot_id   |\n|---------------|\n| N1W1          |\n| N1W2          |\n| N1W3          |\n| N1W4          |\n| N1W5          |\n| ...           |\n| S4E3          |\n| S4E4          |\n| S4E5          |\n| S4E6          |\n| S4E7          |'
+
+
+plots_shp_preview3 = '| [0] plot_id   |\n|---------------|\n| N1W1          |\n| N1W2          |\n| N1W3          |\n| ...           |\n| S4E5          |\n| S4E6          |\n| S4E7          |'
+
+def test_show_shp_fields_complex(capfd):
+    test_shp = os.path.join(data_path, "complex_shp_review.shp")
+
+    idp.shp.show_shp_fields(test_shp, encoding="GBK") 
+    out, err = capfd.readouterr()
+
+    # mac & windows give different space number
+    assert out.replace(' ', '') ==  complex_shp_review_num5.replace(' ', '') + '\n'
+
+def test_show_shp_fields_roi(capfd):
+    test_shp = os.path.join(data_path, "roi.shp")
+
+    idp.shp.show_shp_fields(test_shp) 
+    out, err = capfd.readouterr()
+    assert out ==  roi_shp_preview + '\n'
+
+def test_show_shp_fields_plots_5(capfd):
+    test_shp = r"./tests/data/pix4d/lotus_tanashi_full/plots.shp"
+
+    idp.shp.show_shp_fields(test_shp) 
+    out, err = capfd.readouterr()
+    assert out ==  plots_shp_preview5 + '\n'
+
+def test_show_shp_fields_plots_3(capfd):
+    test_shp = r"./tests/data/pix4d/lotus_tanashi_full/plots.shp"
+
+    idp.shp.show_shp_fields(test_shp, show_num=3) 
+    out, err = capfd.readouterr()
+    assert out ==  plots_shp_preview3 + '\n'
+
+
+###########################
+# test read_proj function #
+###########################
+def test_read_prj():
+    prj_path = os.path.join(data_path, r'roi.prj')
+    out_proj = idp.shp.read_proj(prj_path)
+
+    check_proj = pyproj.CRS.from_string('WGS84 / UTM Zone 54N')
+    assert out_proj.name == check_proj.name
+    assert out_proj.coordinate_system == check_proj.coordinate_system
+
+###########################
+# test read_shp function #
+###########################
+def test_read_shp_without_target():
+    # the degree unit (lat & lon) shp file using lon, lat order
+    shp_path = os.path.join(data_path, "lon_lat.shp")
+
+    # will raise error if shp_proj not given
+    with pytest.raises(ValueError, match=re.escape("Unable to find the proj coordinate info [")):
+        lonlat_shp = idp.shp.read_shp(shp_path)
+
+    # continue if shp_proj is given
+    lonlat_shp = idp.shp.read_shp(shp_path, shp_proj=pyproj.CRS.from_epsg(4326))
+    # flipped for pyproj which input is (lat, lon)
+    wanted_np = np.asarray([[34.90284972, 134.8312376],
+                            [34.90285097, 134.8312399],
+                            [34.90285516, 134.8312371],
+                            [34.90285426, 134.8312349],
+                            [34.90284972, 134.8312376]])
+
+    np.testing.assert_almost_equal(lonlat_shp['0'], wanted_np)
+
+def test_read_shp_proj_success_print(capfd):
+    shp_path = os.path.join(data_path, "test_utm.shp")
+    test_utm_shp = idp.shp.read_shp(shp_path)
+    out, err = capfd.readouterr()
+    assert out == '[shp][proj] Use projection [WGS 84 / UTM zone 53N] for loaded shapefile [test_utm.shp]\n'
+
+    wanted_np = np.asarray([[ 484593.62443893, 3862259.85857523],
+                            [ 484593.71826623, 3862259.85888695],
+                            [ 484593.71795452, 3862259.76537136],
+                            [ 484593.62319206, 3862259.76537136],
+                            [ 484593.62443893, 3862259.85857523]])
+
+    np.testing.assert_almost_equal(test_utm_shp['0'], wanted_np)
+
+def test_read_shp_key_names():
+    shp_path = os.path.join(data_path, "lon_lat_utm53n.shp")
+
+    str_no_name_field_title_false = idp.shp.read_shp(shp_path)
+    assert "1" in str_no_name_field_title_false.keys()
+
+    str_no_name_field_title_true= idp.shp.read_shp(shp_path, include_title=True)
+    assert "line_1" in str_no_name_field_title_true.keys()
+
+    str_name_field_title_false = idp.shp.read_shp(shp_path, name_field="Attr")
+    assert "1_02" in str_name_field_title_false.keys()
+
+    int_name_field_title_false = idp.shp.read_shp(shp_path, name_field=0)
+    assert "1_02" in int_name_field_title_false.keys()
+
+    str_name_field_title_true = idp.shp.read_shp(shp_path, name_field="Attr", include_title=True)
+    assert "Attr_1_02" in str_name_field_title_true.keys()
+
+    int_name_field_title_true = idp.shp.read_shp(shp_path, name_field=0, include_title=True)
+    assert "Attr_1_02" in int_name_field_title_true.keys()
+
+def test_read_shp_key_names_merge():
+    # merge several columns
+    shp_path = os.path.join(data_path, "complex_shp_review.shp")
+
+    str_name_field_list_title_false = idp.shp.read_shp(shp_path, name_field=["CROPTYPE", "MASSIFID"], encoding='gbk')
+    assert "小麦_2301041120000000000" in str_name_field_list_title_false.keys()
+
+    str_name_field_list_title_true = idp.shp.read_shp(shp_path, name_field=["CROPTYPE", "MASSIFID"], include_title=True, encoding='gbk')
+    assert "CROPTYPE_小麦_MASSIFID_2301041120000000000" in str_name_field_list_title_true.keys()
+
+    int_name_field_list_title_false = idp.shp.read_shp(shp_path, name_field=[2, 1], encoding='gbk')
+    assert "小麦_2301041120000000000" in int_name_field_list_title_false.keys()
+
+    int_name_field_list_title_true = idp.shp.read_shp(shp_path, name_field=[2, 1], include_title=True, encoding='gbk')
+    assert "CROPTYPE_小麦_MASSIFID_2301041120000000000" in int_name_field_list_title_true.keys()
+
+def test_read_shp_duplicate_key_name_error():
+    shp_path = os.path.join(data_path, "complex_shp_review.shp")
+    with pytest.raises(KeyError, match=re.escape("Meet with duplicated key")):
+        lonlat_shp = idp.shp.read_shp(shp_path, name_field="CROPTYPE", encoding='gbk')
+
+def test_read_shp_non_exist_key_name_error():
+    shp_path = os.path.join(data_path, "complex_shp_review.shp")
+
+    # wrong key
+    with pytest.raises(KeyError, match=re.escape("Can not find key")):
+        lonlat_shp = idp.shp.read_shp(shp_path, name_field="AAAA", encoding='gbk')
+
+    # wrong key in lists
+    with pytest.raises(KeyError, match=re.escape("Can not find key")):
+        lonlat_shp = idp.shp.read_shp(shp_path, name_field=["AAAA", "BBBB"], encoding='gbk')
+
+    # key out of field
+    with pytest.raises(IndexError, match=re.escape("Int key [6] is outside the number of fields")):
+        lonlat_shp = idp.shp.read_shp(shp_path, name_field=6, encoding='gbk')
+
+    # list key out of field
+    with pytest.raises(IndexError, match=re.escape("Int key [6] is outside the number of fields")):
+        lonlat_shp = idp.shp.read_shp(shp_path, name_field=[1,6], encoding='gbk')
+
+def test_convert_shp():
+    lonlat_path = os.path.join(data_path, "lon_lat.shp")
+    utm_path = os.path.join(data_path, "lon_lat_utm53n.shp")
+
+    lonlat_shp, lonlat_proj = idp.shp.read_shp(lonlat_path, shp_proj=pyproj.CRS.from_epsg(4326), return_proj=True)
+
+    utm_shp, utm_proj = idp.shp.read_shp(utm_path, return_proj=True)
+
+    utm_cvt_shp = idp.shp.convert_proj(lonlat_shp, lonlat_proj, utm_proj)
+
+    a = utm_cvt_shp['0']
+    b = utm_shp['0']
+
+    #  A         B
+    #   o-------o
+    #   |       |
+    #   |       |
+    #   |       |
+    #   |       |
+    #   o-------o
+    #  C         D
+
+    # lon_lat order: [A, B, C, D, A]
+    # lonlat_utm order: [A, D. C. B, A]
+
+    np.testing.assert_almost_equal(a, np.flip(b, axis=0), decimal=6)
