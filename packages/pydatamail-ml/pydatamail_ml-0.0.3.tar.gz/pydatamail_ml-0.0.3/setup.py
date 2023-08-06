8c45788f664e526e9918bec7601fc6153bad6571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:29 +0000

    Add package pydatamail-ml/0.0.3/pydatamail_ml-0.0.3.tar.gz

---
+++ packages/pydatamail-ml/pydatamail_ml-0.0.3.tar.gz/pydatamail_ml-0.0.3/setup.py
@@ -0,0 +1,32 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+from pathlib import Path
+import versioneer
+
+
+setup(
+    name='pydatamail_ml',
+    version=versioneer.get_version(),
+    description='pydatamail_ml - Machine Learning extension for pydatamail',
+    long_description=Path("README.md").read_text(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/pyscioffice/pydatamail_ml',
+    author='Jan Janssen',
+    author_email='jan.janssen@outlook.com',
+    license='BSD',
+    packages=find_packages(exclude=["*tests*"]),
+    install_requires=[
+        'bleach>=5.0.1',
+        'cssutils>=2.5.0',
+        'langdetect>=1.0.9',
+        'numpy>=1.23.1',
+        'pandas>=1.4.3',
+        'pydatamail>=0.0.9',
+        'scikit-learn>=1.1.1',
+        'sqlalchemy>=1.4.39',
+        'tqdm>=4.64.0',
+    ],
+    cmdclass=versioneer.get_cmdclass()
+)
