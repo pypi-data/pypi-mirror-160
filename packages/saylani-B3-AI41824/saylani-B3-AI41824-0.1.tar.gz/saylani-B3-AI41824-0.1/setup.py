c28bf22fc0d5af735d168d98588a09ef282cc95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:07 +0000

    Add package saylani-B3-AI41824/0.1/saylani-B3-AI41824-0.1.tar.gz

---
+++ packages/saylani-B3-AI41824/saylani-B3-AI41824-0.1.tar.gz/saylani-B3-AI41824-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="saylani-B3-AI41824",
+version="0.1",
+description="This is a package",
+long_description="this is a very very long description",
+author = "Sir Qasim and Mehmood Suleman",
+packages = ["saylani-B3-AI41824"],
+install_packages = []
+)
