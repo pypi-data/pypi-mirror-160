d2afaa792ed297c21114da2c14218a39e390777d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:56 +0000

    Add package PRTGAPI/1.0/PRTGAPI-1.0.tar.gz

---
+++ packages/PRTGAPI/PRTGAPI-1.0.tar.gz/PRTGAPI-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PRTGAPI',
+    version='1.0',
+    packages=['PRTGAPI'],
+    url='',
+    license='',
+    author='The-Captain8',
+    author_email='',
+    description='A package meant to make interfacing with the PRTG API more simple.'
+)
