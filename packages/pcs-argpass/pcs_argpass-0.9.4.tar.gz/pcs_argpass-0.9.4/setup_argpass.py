417126c69636785facc3fe5eeb04b5d2143a0d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:39 +0000

    Add package pcs-argpass/0.9.4/pcs_argpass-0.9.4.tar.gz

---
+++ packages/pcs-argpass/pcs_argpass-0.9.4.tar.gz/pcs_argpass-0.9.4/setup_argpass.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README_Argpass.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pcs_argpass",
+    version="0.9.4",
+    python_requires='>=3.6',
+    author="Rainer Pietsch",
+    author_email="r.pietsch@pcs-at.com",
+    description="Argument parser and checker.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rpietsch1953/pcs-pylibs",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+	"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ],
+)
