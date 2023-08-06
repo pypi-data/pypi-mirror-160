5049279dc6a02eb4d17b23a83ece4e6f2b8614b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:34 +0000

    Add package hapiclient/0.2.5/hapiclient-0.2.5.tar.gz

---
+++ packages/hapiclient/hapiclient-0.2.5.tar.gz/hapiclient-0.2.5/hapiclient/__init__.py
@@ -0,0 +1,17 @@
+# Allow "from hapiclient import hapi"
+from hapiclient.hapi import hapi
+
+# Allow "from hapiclient import hapitime2datetime"
+from hapiclient.hapitime import hapitime2datetime
+
+# Allow "from hapiclient import HAPIError"
+from hapiclient.util import HAPIError
+
+__version__ = '0.2.5'
+
+import sys
+if sys.version_info[0] < 3:
+    # Python 2.7
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
