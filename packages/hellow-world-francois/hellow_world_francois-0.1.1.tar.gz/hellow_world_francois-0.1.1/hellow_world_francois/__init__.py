40d3daa3d0c80393f0ef78665d4738f7c37e45bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:00 +0000

    Add package hellow-world-francois/0.1.1/hellow_world_francois-0.1.1.tar.gz

---
+++ packages/hellow-world-francois/hellow_world_francois-0.1.1.tar.gz/hellow_world_francois-0.1.1/hellow_world_francois/__init__.py
@@ -0,0 +1,2 @@
+__version__ = '0.1.0'
+from .hellow import pretty_text
---
+++ packages/hellow-world-francois/hellow_world_francois-0.1.1.tar.gz/hellow_world_francois-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['hellow_world_francois']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyfiglet>=0.8.post1,<0.9', 'termcolor>=1.1.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'hellow-world-francois',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'francois_saigen',
+    'author_email': 'francois@saigen.co.za',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
