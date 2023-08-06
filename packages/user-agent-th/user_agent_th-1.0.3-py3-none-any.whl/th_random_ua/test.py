e2ab23e1c52676f8b3dd5284f262a52563c920c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:19:05 +0000

    Add package user-agent-th/1.0.3/user_agent_th-1.0.3-py3-none-any.whl

---
+++ packages/user-agent-th/user_agent_th-1.0.3-py3-none-any.whl/th_random_ua/test.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# from random_ua import RandomUA
+#
+# d = RandomUA()
+# for i in range(100000):
+#     print(d.random_android())
+from random_ua import RandomUA
+d = RandomUA()
+print(d.random_android())
