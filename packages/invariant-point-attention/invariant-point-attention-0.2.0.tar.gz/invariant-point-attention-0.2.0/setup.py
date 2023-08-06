e418a0d62e5137eb456dc06eed49a1233a69e2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:18 +0000

    Add package invariant-point-attention/0.2.0/invariant-point-attention-0.2.0.tar.gz

---
+++ packages/invariant-point-attention/invariant-point-attention-0.2.0.tar.gz/invariant-point-attention-0.2.0/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'invariant-point-attention',
+  packages = find_packages(),
+  version = '0.2.0',
+  license='MIT',
+  description = 'Invariant Point Attention',
+  long_description_content_type = 'text/markdown',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/invariant-point-attention',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'protein folding'
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.7'
+  ],
+  setup_requires=[
+    'pytest-runner',
+  ],
+  tests_require=[
+    'pytest'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
