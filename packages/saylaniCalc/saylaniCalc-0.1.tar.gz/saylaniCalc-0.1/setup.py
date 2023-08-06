102e6725716915223b363320e495e9143ef6b37d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:51 +0000

    Add package saylaniCalc/0.1/saylaniCalc-0.1.tar.gz

---
+++ packages/saylaniCalc/saylaniCalc-0.1.tar.gz/saylaniCalc-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="saylaniCalc",
+version="0.1",
+description="this is a package",
+long_description="this is a very very long description",
+author = "Sir Qasim and Syed Muhammad Mooazam",
+packages = ["saylaniCalc"],
+install_packages = []
+)
