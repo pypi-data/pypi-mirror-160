5c54c23b8b6e6b7d2a67f7bf2378d652dd507b9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:32 +0000

    Add package lamin_logger/0.1.1/lamin_logger-0.1.1-py2.py3-none-any.whl

---
+++ packages/lamin_logger/lamin_logger-0.1.1-py2.py3-none-any.whl/lamin_logger/__init__.py
@@ -0,0 +1,17 @@
+"""Logging setup.
+
+Import the package::
+
+   import lamin_logger
+
+This is the complete API reference:
+
+.. autosummary::
+   :toctree: .
+
+   colors
+"""
+
+__version__ = "0.1.1"
+
+from ._core import colors, logger  # noqa
