38c5fbc5791f351c94438fcbca5749e9d42ddbf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:39 +0000

    Add package logitorch/0.0.0/logitorch-0.0.0-py3-none-any.whl

---
+++ packages/logitorch/logitorch-0.0.0-py3-none-any.whl/data_collators/ruletaker_collator.py
@@ -0,0 +1,52 @@
+from typing import Dict, Tuple
+
+import torch
+from transformers import AutoTokenizer, RobertaTokenizer
+
+from logitorch.datasets.proof_qa.proofwriter_dataset import PROOFWRITER_LABEL_TO_ID
+
+
+class RuleTakerCollator:
+    def __init__(self) -> None:
+        self.tokenizer = RobertaTokenizer.from_pretrained(
+            "LIAMF-USP/roberta-large-finetuned-race"
+        )
+
+    def __call__(self, batch) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
+        contexts = []
+        questions = []
+        batch_y = []
+
+        for context, question, label in batch:
+            contexts.append(context)
+            questions.append(question)
+            batch_y.append(label)
+
+        batch_x = self.tokenizer(contexts, questions, padding=True, return_tensors="pt")
+        return batch_x, torch.tensor(batch_y, dtype=torch.int64)
+
+
+class RuleTakerProofWriterCollator:
+    def __init__(self) -> None:
+        self.tokenizer = RobertaTokenizer.from_pretrained(
+            "LIAMF-USP/roberta-large-finetuned-race"
+        )
+
+    def __call__(self, batch) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
+        contexts = []
+        questions = []
+        labels = []
+
+        for i in batch:
+            sentences = []
+            for k, v in i[0].items():
+                sentences.append(f"{k}: {v}")
+            for k, v in i[1].items():
+                sentences.append(f"{k}: {v}")
+
+            contexts.append("".join(sentences))
+            questions.append(i[2])
+            labels.append(PROOFWRITER_LABEL_TO_ID[str(i[3])])
+
+        batch_x = self.tokenizer(contexts, questions, padding=True, return_tensors="pt")
+        return batch_x, torch.tensor(labels, dtype=torch.int64)
