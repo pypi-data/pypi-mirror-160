14f9fa0222b5e863e5e7940d2f627d0d013a9a51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:50 +0000

    Add package zamtelsms/0.0.3/zamtelsms-0.0.3.tar.gz

---
+++ packages/zamtelsms/zamtelsms-0.0.3.tar.gz/zamtelsms-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='zamtelsms',
+    packages = ['zamtelsms'],
+    version='0.0.3',
+    description='A small python Package for Zamtel bulk SMS API',
+    author='Mathews Musukuma',
+    author_email='sikaili99@gmail.com',
+    url='https://github.com/Mathewsmusukuma/zamtel-sms',
+    py_modules=['zamtelsms'],
+    install_requires=['requests','python-decouple'],
+    license='MIT License',
+    zip_safe=False,
+    keywords='zamtel sms package',
+      classifiers=[
+    'Development Status :: 3 - Alpha',     
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ]
+    )
