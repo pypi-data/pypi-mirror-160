22227c31b7478be77c1673322ecaa57d7b19792d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:57 +0000

    Add package python-fsa/0.0.2/python-fsa-0.0.2.tar.gz

---
+++ packages/python-fsa/python-fsa-0.0.2.tar.gz/python-fsa-0.0.2/src/python_fsa/dfa.py
@@ -0,0 +1,43 @@
+from collections.abc import Mapping
+from typing import Generic, TypeVar
+
+import pygraphviz as pgv
+
+from .utils.dot_parsing import *
+
+T = TypeVar("T")
+S = TypeVar("S")
+
+
+class DFA(Generic[T, S]):
+    def __init__(
+            self,
+            *,
+            alphabet: frozenset[T],
+            states: frozenset[S],
+            initial: S,
+            transition: Mapping[tuple[S, T], S],
+            final_states: frozenset[S],
+    ):
+        self.alphabet = alphabet
+        self.states = states
+        self.initial = initial
+        self.transition = transition
+        self.final_states = final_states
+
+    def accepts(self, *seq: T) -> bool:
+        current = self.initial
+        for elt in seq:
+            current = self.transition.get((current, elt), None)
+        return current in self.final_states
+
+    @classmethod
+    def from_dot(cls, data: str):
+        g = pgv.AGraph(string=data)
+        return cls(
+            alphabet=alphabet_of(g),
+            states=states_of(g),
+            initial=initial_state_of(g),
+            transition=deterministic_transitions_of(g),
+            final_states=finial_states_of(g),
+        )
