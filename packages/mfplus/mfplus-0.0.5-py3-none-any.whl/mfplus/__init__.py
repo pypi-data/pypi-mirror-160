df18dfed811acd27ecf061b38b668b2ef49c03a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:19 +0000

    Add package mfplus/0.0.5/mfplus-0.0.5-py3-none-any.whl

---
+++ packages/mfplus/mfplus-0.0.5-py3-none-any.whl/mfplus/__init__.py
@@ -0,0 +1,6 @@
+"""Provides access to MicroFile+ image conversion function
+
+   The convert function provided by this package is flexible - it can be used to convert any number of files, with optional arguments.
+"""
+
+from . mfpconvert import convert
