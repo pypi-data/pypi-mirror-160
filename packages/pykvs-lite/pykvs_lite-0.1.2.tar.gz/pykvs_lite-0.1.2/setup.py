1ca6b610271068f5bef33479c5aab7b3a884b229
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:26 +0000

    Add package pykvs-lite/0.1.2/pykvs_lite-0.1.2.tar.gz

---
+++ packages/pykvs-lite/pykvs_lite-0.1.2.tar.gz/pykvs_lite-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# get version
+with open("./pykvs_lite/version.py") as f:
+    exec(f.read())
+
+# get readme
+with open("./README.md") as f:
+    long_desc = f.read().split("## Sample\n")[0]
+
+# setup
+setup(
+    name="pykvs_lite",
+    version=__version__,
+    url="https://github.com/kujirahand/pykvs_lite",
+    author="kujirahand",
+    author_email="web@kujirahand.com",
+    maintainer="kujirahand",
+    maintainer_email="web@kujirahand.com",
+    description="Simple and convenient Key-value Store Library",
+    packages=find_packages(where='.'),
+    install_requires=[],
+    long_description=long_desc,
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ]
+)
