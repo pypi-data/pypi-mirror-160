107206438610d6c7fef2b2c8894e22681bdd4108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:36 +0000

    Add package imio/0.2.3/imio-0.2.3.tar.gz

---
+++ packages/imio/imio-0.2.3.tar.gz/imio-0.2.3/setup.py
@@ -0,0 +1,51 @@
+from setuptools import find_packages, setup
+
+requirements = [
+    "numpy",
+    "scikit-image",
+    "tifffile",
+    "pynrrd",
+    "nibabel",
+    "tqdm",
+    "natsort",
+    "psutil",
+    "slurmio >= 0.0.5",
+    "imlib",
+    "nibabel >= 2.1.0",
+]
+
+setup(
+    name="imio",
+    version="0.2.3",
+    description="Loading and saving of image data.",
+    install_requires=requirements,
+    extras_require={
+        "dev": [
+            "black",
+            "pytest-cov",
+            "pytest",
+            "coverage",
+            "bump2version",
+            "pre-commit",
+            "flake8",
+        ]
+    },
+    python_requires=">=3.7",
+    packages=find_packages(exclude=("tests", "tests.*")),
+    include_package_data=True,
+    url="https://github.com/brainglobe/imio",
+    author="Charly Rousseau, Adam Tyson",
+    author_email="code@adamltyson.com",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    zip_safe=False,
+)
