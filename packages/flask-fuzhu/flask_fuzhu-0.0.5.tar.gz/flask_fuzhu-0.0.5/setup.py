aad76ce1b7b3b45fba2b4a6c61a1453322f2b50f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:56 +0000

    Add package flask-fuzhu/0.0.5/flask_fuzhu-0.0.5.tar.gz

---
+++ packages/flask-fuzhu/flask_fuzhu-0.0.5.tar.gz/flask_fuzhu-0.0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="flask_fuzhu",
+    version="0.0.5",
+    author="littlx",
+    author_email="littlx@qq.com",
+    description="practice build package",
+    long_description=long_description,
+    long_description_contet_type="text/markdown",
+    # url="github.com/littlx/flask_fuzhu",
+    packages=setuptools.find_packages(),
+    install_requires=["flask", "python-dotenv"],
+    python_requires=">=3.9",
+)
