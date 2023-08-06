b07fa075527db3a36be6510b46dc01870e3c08b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:20 +0000

    Add package netkeiber/1.0.24/netkeiber-1.0.24-py3-none-any.whl

---
+++ packages/netkeiber/netkeiber-1.0.24-py3-none-any.whl/netkeiber/Library/netkeiberController.py
@@ -0,0 +1,49 @@
+import re
+import LibHanger.Library.uwGetter as CmnGetter
+from LibHanger.Models.fields import *
+from Scrapinger.Library.browserContainer import browserContainer
+from Scrapinger.Library.webDriverController import webDriverController
+from netkeiber.Library.netkeiberGlobals import *
+
+class netkeibaBrowserController(browserContainer):
+    
+    """
+    netkeiberブラウザコントローラー
+    """
+    
+    def __init__(self) -> None:
+        
+        """
+        コンストラクタ
+        """
+        
+        # 基底側コンストラクタ
+        super().__init__()
+                
+        # WebDriverController
+        self.wdc = webDriverController(gv.netkeiberConfig, self)
+    
+    def getData(self, *args, **kwargs):
+        
+        """
+        データ取得
+        """
+        
+        pass
+    
+    def getUpdInfo(self):
+
+        """
+        更新情報取得
+        """
+
+        return CmnGetter.getNow().strftime('%Y/%m/%d %H:%M:%S')
+    
+    def isdigitEx(self, targetString:str) -> bool:
+        
+        """
+        数値判定
+        """
+        
+        return re.compile("^\d+\.?\d*\Z").match(targetString)
+    
