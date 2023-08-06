4ad64b07982975f4f9026d1ce93f399efeb064fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:27 +0000

    Add package JsMacrosAC/1.8.0.8844812/JsMacrosAC-1.8.0.8844812-py3-none-any.whl

---
+++ packages/JsMacrosAC/JsMacrosAC-1.8.0.8844812-py3-none-any.whl/JsMacrosAC/ChatHudLineHelper.py
@@ -0,0 +1,33 @@
+from typing import overload
+from typing import TypeVar
+from .BaseHelper import BaseHelper
+from .TextHelper import TextHelper
+
+ChatHudLine = TypeVar["net.minecraft.client.gui.hud.ChatHudLine_net.minecraft.text.Text_"]
+ChatHud = TypeVar["net.minecraft.client.gui.hud.ChatHud"]
+
+class ChatHudLineHelper(BaseHelper):
+
+	@overload
+	def __init__(self, base: ChatHudLine, hud: ChatHud) -> None:
+		pass
+
+	@overload
+	def getText(self) -> TextHelper:
+		pass
+
+	@overload
+	def getId(self) -> int:
+		pass
+
+	@overload
+	def getCreationTick(self) -> int:
+		pass
+
+	@overload
+	def deleteById(self) -> None:
+		pass
+
+	pass
+
+
