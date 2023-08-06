94ae7d3cf66f5deea364c76cc434659a5bfee239
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:54 +0000

    Add package acoustics-hardware/0.2.0/acoustics-hardware-0.2.0.tar.gz

---
+++ packages/acoustics-hardware/acoustics-hardware-0.2.0.tar.gz/acoustics-hardware-0.2.0/acoustics_hardware/_version.py
@@ -0,0 +1,11 @@
+"""File generated while packaging."""
+import contextlib
+
+version_info = {'short': '0.2.0', 'full': '0.2.0', 'release': True, 'clean': True, 'git revision': 'd905a22352b1f7a74600a910b7908f8521a603e3'}
+version = '0.2.0'
+
+
+@contextlib.contextmanager
+def version_manager():
+    """Dummy context manager, returns the version."""
+    yield version
