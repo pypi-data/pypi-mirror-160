68ba0b7bab8c973f39e161ded9668dfbbe815420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:38 +0000

    Add package streamlit-pagnation/0.0.2/streamlit_pagnation-0.0.2-py3-none-any.whl

---
+++ packages/streamlit-pagnation/streamlit_pagnation-0.0.2-py3-none-any.whl/streamlit_pagnation/frontend/build/precache-manifest.43bb459d0959ee104ce8f6b3d63a4806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a0d1796c53fc36748fb13a67bf1808",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f77fdf2cdebdf5a14c5c",
+    "url": "./static/css/main.004acf31.chunk.css"
+  },
+  {
+    "revision": "26a6ffc990b97973e550",
+    "url": "./static/js/2.cbebb380.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.cbebb380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f77fdf2cdebdf5a14c5c",
+    "url": "./static/js/main.3ac2dffb.chunk.js"
+  },
+  {
+    "revision": "10588f3b7c5854f6521f",
+    "url": "./static/js/runtime-main.1367036c.js"
+  }
+]);
---
+++ packages/streamlit-pagnation/streamlit_pagnation-0.0.2-py3-none-any.whl/streamlit_pagnation/frontend/build/precache-manifest.ccfe45cbd33f5f7cd344f7605d8ca472.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0655bb1141f62d7d46645c94dcc67c26",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90569ee471f5e18ba64b",
+    "url": "./static/css/main.ccd4e4ae.chunk.css"
+  },
+  {
+    "revision": "26a6ffc990b97973e550",
+    "url": "./static/js/2.cbebb380.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.cbebb380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90569ee471f5e18ba64b",
+    "url": "./static/js/main.acfd91d8.chunk.js"
+  },
+  {
+    "revision": "10588f3b7c5854f6521f",
+    "url": "./static/js/runtime-main.1367036c.js"
+  }
+]);
