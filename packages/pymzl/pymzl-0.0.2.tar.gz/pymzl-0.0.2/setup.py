f92d817fd086d9106311c4098344a433b87fef99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:40 +0000

    Add package pymzl/0.0.2/pymzl-0.0.2.tar.gz

---
+++ packages/pymzl/pymzl-0.0.2.tar.gz/pymzl-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="pymzl",
+      version="0.0.2",
+      description="calculation",
+      py_modules={"pymzl"},
+      package_dir={'': "pymzl"},
+      author="Zilu Meng",
+      author_email="mzll1202@163.com",
+      license="MIT")
