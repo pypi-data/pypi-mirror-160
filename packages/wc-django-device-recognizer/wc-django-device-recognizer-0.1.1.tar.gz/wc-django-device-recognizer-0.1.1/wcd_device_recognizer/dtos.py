410e5336161dc1834c05a34888a46169dd5defad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:10 +0000

    Add package wc-django-device-recognizer/0.1.1/wc-django-device-recognizer-0.1.1.tar.gz

---
+++ packages/wc-django-device-recognizer/wc-django-device-recognizer-0.1.1.tar.gz/wc-django-device-recognizer-0.1.1/wcd_device_recognizer/dtos.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from ipaddress import IPv4Address
+from decimal import Decimal
+from typing import Dict, Optional, Tuple
+from user_agents.parsers import UserAgent
+
+from .const import DEFAULT_DPR
+
+
+__all__ = 'AppDTO', 'OSDTO', 'DeviceDTO', 'InterlocutorDTO',
+
+
+DEFAULT_DPR = Decimal(1)
+
+
+@dataclass(frozen=True, eq=True)
+class AppDTO:
+    family: str
+    version: Tuple[int, ...]
+    version_string: str
+
+
+@dataclass(frozen=True, eq=True)
+class OSDTO:
+    family: str
+    version: Tuple[int, ...]
+    version_string: str
+    arch: Optional[str] = None
+
+
+@dataclass(frozen=True, eq=True)
+class DeviceDTO:
+    id: Optional[str] = None
+    family: str = 'Other'
+    brand: Optional[str] = None
+    model: Optional[str] = None
+    bitness: Optional[str] = None
+    memory: Optional[Decimal] = None
+    dpr: Decimal = DEFAULT_DPR
+    viewport_width: Optional[int] = None
+
+
+@dataclass(frozen=True, eq=True)
+class InterlocutorDTO:
+    os: OSDTO
+    app: AppDTO
+    device: DeviceDTO
+
+    user_agent: UserAgent
+    client_hints: Dict[str, str] = field(default_factory=dict)
+    ip: Optional[IPv4Address] = None
