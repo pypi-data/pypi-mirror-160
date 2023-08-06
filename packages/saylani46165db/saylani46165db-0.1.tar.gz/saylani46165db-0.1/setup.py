8d570e1eaf6dc0cc0100ea35278fd84f8e1f067e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:38 +0000

    Add package saylani46165db/0.1/saylani46165db-0.1.tar.gz

---
+++ packages/saylani46165db/saylani46165db-0.1.tar.gz/saylani46165db-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name='saylani46165db',
+version='0.1',
+description='This is the Saylani DB Package',
+author = "Shahzad Ahmed Khan",
+packages = ['saylani46165db'],
+zip_safe=False,
+install_packages = []
+)
