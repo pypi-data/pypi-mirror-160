37dae68c5bd99f1a9d612d26662f4673d005d048
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:10 +0000

    Add package prettyformatter/1.0.4/prettyformatter-1.0.4.tar.gz

---
+++ packages/prettyformatter/prettyformatter-1.0.4.tar.gz/prettyformatter-1.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    description=
+        "Pretty formatter enables pretty formatting using hanging "
+        "indents, dataclasses, ellipses, and simple customizability "
+        "by registering formatters.",
+    homepage=
+        "https://github.com/SimpleArt/prettyformatter",
+    repository=
+        "https://github.com/SimpleArt/prettyformatter",
+    summary=
+        "Pretty formatter for prettier formatting.",
+)
