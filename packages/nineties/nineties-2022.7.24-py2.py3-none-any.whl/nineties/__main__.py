b0fa86d0606c40cf25a32e74e46e815e5b3a98a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:49 +0000

    Add package nineties/2022.7.24/nineties-2022.7.24-py2.py3-none-any.whl

---
+++ packages/nineties/nineties-2022.7.24-py2.py3-none-any.whl/nineties/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import sys
+
+from nineties.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))  # pragma: no cover
