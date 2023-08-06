23b0fb60da61d402701a70a0ca4538bb000de1ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:23 +0000

    Add package drake/1.5.0/drake-1.5.0-cp39-cp39-macosx_11_0_x86_64.whl

---
+++ packages/drake/drake-1.5.0-cp39-cp39-macosx_11_0_x86_64.whl/pydrake/common/pybind11_version.py
@@ -0,0 +1,8 @@
+
+"""
+Provides information on the external fork of `pybind11` used by `pydrake`.
+"""
+
+repository = "RobotLocomotion/pybind11"
+commit = "25eac331ce222f7cd6cca5f3e023daa0ab1cb475"
+sha256 = "2736e9407efb1243c76f32261fdb29625657ff53f74469505e47d6340dc60a9f"
