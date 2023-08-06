3d6e9d3e38bbcb331599c094fa6b8345c1a32340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:05 +0000

    Add package PyForms-GUI-shaliulab/4.905.157/PyForms_GUI_shaliulab-4.905.157-py3-none-any.whl

---
+++ packages/PyForms-GUI-shaliulab/PyForms_GUI_shaliulab-4.905.157-py3-none-any.whl/pyforms_gui/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__author__      = "Ricardo Ribeiro"
+__credits__     = ["Ricardo Ribeiro"]
+__license__     = "MIT"
+__version__ = "4.905.157"
+__maintainer__  = "Ricardo Ribeiro"
+__email__       = "ricardojvr@gmail.com"
+__status__      = "Development"
+
+from confapp import conf
+import sys
+import os
+
+sys.path.insert(0, os.getcwd())
+
+try:
+    import local_settings
+    conf += local_settings
+except:
+    pass
+
+from . import settings as pyforms_settings
+conf += pyforms_settings
+
+import logging
+
+logging.basicConfig(
+    level=conf.PYFORMS_LOG_HANDLER_LEVEL,
+    format=conf.PYFORMS_LOG_FORMAT,
+    handlers=[logging.StreamHandler()]
+)
