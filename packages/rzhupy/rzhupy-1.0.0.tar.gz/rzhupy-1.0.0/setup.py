d30fdcdd589b4a17affd67094350000a7177acdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:10 +0000

    Add package rzhupy/1.0.0/rzhupy-1.0.0.tar.gz

---
+++ packages/rzhupy/rzhupy-1.0.0.tar.gz/rzhupy-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Jokes, stories etc.'
+
+# Setting up
+setup(
+	name="rzhupy",
+	version=VERSION,
+	author="mqchinee",
+	description=DESCRIPTION,
+	packages=find_packages(),
+	install_requires=['requests', 'bs4', 'deep_translator'],
+	keywords=['random', 'python', 'eightball'],
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Unix",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: Microsoft :: Windows",
+	]
+)
