e52397bcd323a8ad61baed26a0ff264c510241a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:47:22 +0000

    Add package pyrosimple/2.0.0rc3/pyrosimple-2.0.0rc3.tar.gz

---
+++ packages/pyrosimple/pyrosimple-2.0.0rc3.tar.gz/pyrosimple-2.0.0rc3/README.md
@@ -0,0 +1,35 @@
+# pyrosimple
+
+[![GitHub Workflow Status](https://img.shields.io/github/workflow/status/kannibalox/pyrosimple/Pylint)](https://github.com/kannibalox/pyrosimple/actions/workflows/pylint.yml)
+[![PyPI](https://img.shields.io/pypi/v/pyrosimple)](https://pypi.org/project/pyrosimple/)
+![PyPI -	Python Version](https://img.shields.io/pypi/pyversions/pyrosimple)
+
+A overhauled Python 3 fork  of the [pyrocore tools](https://github.com/pyroscope/pyrocore), for working with the [rTorrent client](https://github.com/rakshasa/rtorrent).
+
+## Installation
+
+```bash
+pip install pyrosimple
+```
+
+See the [documentation for usage](https://kannibalox.github.io/pyrosimple/). If you've used rtcontrol/rtxmlrpc before, you should feel right at home.
+
+## What's the point of this?
+
+The pyrocore tools are great, but being stuck on python 2, along with the complicated install procedure made integrating both the tools and the code into other processes very painful.
+
+## Differences from pyrocore
+
+The following lists are not exhaustive, and don't cover many of the internal improvements and refactoring.
+
+- Only supports python 3 and rtorrent 0.9.8+ (0.9.6/0.9.7 should still work just fine, but aren't officially supported)
+- Simpler poetry-based build/install system
+- Everything in one package (no separate pyrobase)
+  - Use external lib for bencode
+
+### New features
+- Multi-instance support for rtcontrol/rtxmlrpc
+- Replaced Tempita with jinja2
+- Support for JSON-RPC (only implemented by https://github.com/jesec/rtorrent)
+- pyrotorque job to move torrents between hosts
+- pyrotorque job to move torrent paths
---
+++ packages/pyrosimple/pyrosimple-2.0.0rc3.tar.gz/pyrosimple-2.0.0rc3/src/pyrosimple/job/move_path.py
@@ -0,0 +1,81 @@
+"""Move torrents to a new path.
+Primarily intended for move-on-completion, but is intended to be generic.
+"""
+import concurrent.futures
+import shutil
+
+from pathlib import Path
+
+import pyrosimple
+
+from pyrosimple import error
+from pyrosimple.torrent import formatting, rtorrent
+from pyrosimple.util import matching, pymagic, rpc
+
+
+def move(i: rtorrent.RtorrentItem, target: str):
+    """Move a torrent path"""
+    i.stop()
+    shutil.move(str(i.datapath()), target)
+    i._engine.rpc.d.directory.set(i.hash, target)
+    i.start()
+
+
+class PathMover:
+    """Conditionally move torrent paths.
+    Currently it just relies on the current datapath"""
+
+    def __init__(self, config=None):
+        """Set up statistics logger."""
+        self.config = config or {}
+        self.config.setdefault("max_workers", 1)
+        self.config.setdefault("dry_run", False)
+        if not self.config["target"]:
+            raise Exception("'target' not defined!")
+        self.engine = None
+        self.LOG = pymagic.get_class_logger(self)
+        self.LOG.debug("Path mover created with config %r", self.config)
+
+    def check_and_move(self, i: rtorrent.RtorrentItem):
+        """Conditionally move data"""
+        target = formatting.format_item_str(self.config["target"], i)
+        if not target:
+            self.LOG.debug("Empty target for %s", i.hash)
+            return
+        if (Path(i.fetch("directory")) == Path(target)) or (
+            i.rpc_call("d.is_multi_file")
+            and Path(i.fetch("directory")).parent == Path(target)
+        ):
+            self.LOG.debug("%s already moved, skipping", i.hash)
+            return
+        if self.config["dry_run"]:
+            self.LOG.info(
+                "Would move %s from '%s' to '%s'", i.hash, i.datapath(), target
+            )
+            return
+        self.LOG.info(
+            "Moving path for %s from '%s' to '%s'", i.hash, i.datapath(), target
+        )
+        move(i, target)
+        i.flush()
+
+    def run(self):
+        """Check if any torrents need to be moved"""
+        try:
+            self.engine = pyrosimple.connect()
+            matcher = matching.create_matcher(self.config["matcher"])
+            futures = []
+            with concurrent.futures.ThreadPoolExecutor(
+                max_workers=self.config["max_workers"]
+            ) as executor:
+                # Submit tasks
+                for i in self.engine.view("default", matcher):
+                    if matcher.match(i):
+                        futures.append(executor.submit(self.check_and_move, i))
+                # Wait and check for exceptions
+                for future in concurrent.futures.as_completed(futures):
+                    exc = future.result()
+                    if exc is not None:
+                        self.LOG.error("Could not move: %s: %s", future, exc)
+        except (error.LoggableError, *rpc.ERRORS) as exc:
+            self.LOG.warning(str(exc))
