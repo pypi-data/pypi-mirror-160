8fe8d7d57b105853b833272f1694a272c2f77064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:51 +0000

    Add package sourmash/4.4.3/sourmash-4.4.3-py2.py3-none-manylinux_2_17_s390x.manylinux2014_s390x.whl

---
+++ packages/sourmash/sourmash-4.4.3-py2.py3-none-manylinux_2_17_s390x.manylinux2014_s390x.whl/sourmash/cli/sig/subtract.py
@@ -0,0 +1,32 @@
+"""subtract one or more signatures"""
+
+from sourmash.cli.utils import (add_moltype_args, add_ksize_arg)
+
+
+def subparser(subparsers):
+    subparser = subparsers.add_parser('subtract')
+    subparser.add_argument('signature_from')
+    subparser.add_argument('subtraction_sigs', nargs='+')
+    subparser.add_argument(
+        '-q', '--quiet', action='store_true',
+        help='suppress non-error output'
+    )
+    subparser.add_argument(
+        '-o', '--output', metavar='FILE',
+        help='output signature to this file (default stdout)'
+    )
+    subparser.add_argument(
+        '--flatten', action='store_true',
+        help='remove abundance from signatures before subtracting'
+    )
+    subparser.add_argument(
+        '-A', '--abundances-from', metavar='FILE',
+        help='intersect with & take abundances from this signature'
+    )
+    add_ksize_arg(subparser, 31)
+    add_moltype_args(subparser)
+
+
+def main(args):
+    import sourmash
+    return sourmash.sig.__main__.subtract(args)
