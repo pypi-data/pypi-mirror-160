70c86e166f94c4517fe0aab965479feba361fc81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:33 +0000

    Add package ccprocesspool/2.0/ccprocesspool-2.0.tar.gz

---
+++ packages/ccprocesspool/ccprocesspool-2.0.tar.gz/ccprocesspool-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ccprocesspool',
+      version='2.0',
+      description='Process pool to add/check/stop all processes in pool',
+      url='',
+      author='OpenFibers',
+      author_email='openfibers@gmail.com',
+      license='MIT',
+      packages=['ccprocesspool'],
+      zip_safe=False)
