dffa7688cb21b7fe3c84fbf27cb342b14f108bc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:26 +0000

    Add package pack-legolas-lee-package0/1.0/pack_legolas_lee_package0-1.0.tar.gz

---
+++ packages/pack-legolas-lee-package0/pack_legolas_lee_package0-1.0.tar.gz/pack_legolas_lee_package0-1.0/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='pack_legolas_lee_package0',
+    version='1.0',
+    description='My First Package',
+    author="Legolas",
+    author_email="jingui.ljg@alibaba-inc.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
