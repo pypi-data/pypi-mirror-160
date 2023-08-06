23c11d52c47ae4556a97cc37ff04d250dda6b70c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:44 +0000

    Add package artemis-labs/0.0.11/artemis_labs-0.0.11.tar.gz

---
+++ packages/artemis-labs/artemis_labs-0.0.11.tar.gz/artemis_labs-0.0.11/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="artemis_labs",
+    version="0.0.11",
+    author="Artemis Labs",
+    author_email="austinmccoy@artemisar.com",
+    description="Artemis Labs",
+    packages= setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'imageio',
+        'websockets'
+    ]
+)
