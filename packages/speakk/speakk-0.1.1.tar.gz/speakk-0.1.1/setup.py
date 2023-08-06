c68bef9a505c44caabadba854e380c2e753c46f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:21:09 +0000

    Add package speakk/0.1.1/speakk-0.1.1.tar.gz

---
+++ packages/speakk/speakk-0.1.1.tar.gz/speakk-0.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = 'This code is help you to replace print() function and make your code more attractive.'
+LONG_DESCRIPTION = 'long descriptions'
+
+# Setting up
+setup(
+    name="speakk",
+    version=VERSION,
+    author="Jerry4539",
+    author_email="<shanayaagraval@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pyttsx3','colorama'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/speakk/speakk-0.1.1.tar.gz/speakk-0.1.1/speakk/__init__.py
@@ -0,0 +1,7 @@
+from speak import display
+from speak import speak
+from speak import alert
+from speak import terminal
+from speak import command
+
+
