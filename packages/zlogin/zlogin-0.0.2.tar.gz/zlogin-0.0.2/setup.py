2714ef5f3a0ee45b558e529832775e1225e4b284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:16 +0000

    Add package zlogin/0.0.2/zlogin-0.0.2.tar.gz

---
+++ packages/zlogin/zlogin-0.0.2.tar.gz/zlogin-0.0.2/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Zerodha Login Automation - Selenium Based'
+with open('README.md', 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+# Setting up
+setup(
+    name="zlogin",
+    version=VERSION,
+    author="Prabhat Rastogi",
+    author_email="prabhatrastogik@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    license="MIT",
+    install_requires=['boto3', 'undetected_chromedriver',
+                      'selenium'],  # add any additional packages that
+    keywords=['selenium login', 'zerodha', 'automate zerodha'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
