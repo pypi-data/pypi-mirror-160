1e3b22b96b0704dc5e21f80f29924cd47b971bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:16:42 +0000

    Add package iqsylani46214/0.1/iqsylani46214-0.1.tar.gz

---
+++ packages/iqsylani46214/iqsylani46214-0.1.tar.gz/iqsylani46214-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="iqsylani46214",
+version="0.1",
+description="this is a package created by iqbal",
+long_description="this is a very very long description created by iqbal",
+author = "Iqbal Ahmed",
+packages = ["iqsylani46214"],
+install_packages = []
+)
