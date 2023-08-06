0d0415412c2f96d61278c06976ea7094aa03c124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:00 +0000

    Add package shijie/1.2/shijie-1.2.tar.gz

---
+++ packages/shijie/shijie-1.2.tar.gz/shijie-1.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+with open("README.md", "r",encoding='UTF-8') as f:
+  long_desc = f.read()
+
+setup(name='shijie',
+      version='1.2',
+      description='Secuer: ultrafast, scalable and accurate clustering of single-cell RNA-seq data',
+      author='nnwei',
+      long_description=long_desc,
+      url='https://github.com/nanawei11/Secuer',
+      packages=['secuer'],
+      license='MIT',
+      entry_points={
+            'console_scripts': ['secuer = secuer_console.console:main']}# linux
+      )
+
