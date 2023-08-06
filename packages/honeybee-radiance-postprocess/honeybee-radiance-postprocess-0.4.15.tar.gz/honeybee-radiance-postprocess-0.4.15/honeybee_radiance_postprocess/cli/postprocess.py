313119ffc410aee6ef7c38de378f0e023451276b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:33 +0000

    Add package honeybee-radiance-postprocess/0.4.15/honeybee-radiance-postprocess-0.4.15.tar.gz

---
+++ packages/honeybee-radiance-postprocess/honeybee-radiance-postprocess-0.4.15.tar.gz/honeybee-radiance-postprocess-0.4.15/honeybee_radiance_postprocess/cli/postprocess.py
@@ -0,0 +1,92 @@
+"""honeybee radiance daylight postprocessing commands."""
+import click
+import sys
+import os
+import logging
+
+from honeybee_radiance_postprocess.results import Results
+
+_logger = logging.getLogger(__name__)
+
+
+# we will import this from inside honeybee-radiance and expose it from honeybee-radiance
+# cli
+@click.group(help='Commands to post-process Radiance results.')
+def post_process():
+    pass
+
+
+@post_process.command('annual-daylight')
+@click.argument(
+    'folder',
+    type=click.Path(exists=True, file_okay=False, dir_okay=True, resolve_path=True)
+)
+@click.option(
+    '--schedule', '-sch', help='Path to an annual schedule file. Values should be 0-1 '
+    'separated by new line. If not provided an 8-5 annual schedule will be created.',
+    type=click.Path(exists=False, file_okay=True, dir_okay=False, resolve_path=True)
+)
+@click.option(
+    '--threshold', '-t', help='Threshold illuminance level for daylight autonomy.',
+    default=300, type=int, show_default=True
+)
+@click.option(
+    '--lower-threshold', '-lt',
+    help='Minimum threshold for useful daylight illuminance.', default=100, type=int,
+    show_default=True
+)
+@click.option(
+    '--upper-threshold', '-ut',
+    help='Maximum threshold for useful daylight illuminance.', default=3000, type=int,
+    show_default=True
+)
+@click.option(
+    '--states', '-st', help='A dictionary of states.', default=None, type=dict,
+    show_default=True
+)
+@click.option(
+    '--grids-filter', '-gf', help='A pattern to filter the grids.', default='*',
+    show_default=True
+)
+@click.option(
+    '--sub_folder', '-sf', help='Optional relative path for subfolder to write output '
+    'metric files.', default='metrics'
+)
+def annual_metrics(
+    folder, schedule, threshold, lower_threshold, upper_threshold, states,
+    grids_filter, sub_folder
+):
+    """Compute annual metrics in a folder and write them in a subfolder.
+
+    \b
+    This command generates 5 files for each input grid.
+        da/{grid-name}.da -> Daylight Autonomy
+        cda/{grid-name}.cda -> Continuos Daylight Autonomy
+        udi/{grid-name}.udi -> Useful Daylight Illuminance
+        udi_lower/{grid-name}_upper.udi -> Upper Useful Daylight Illuminance
+        udi_upper/{grid-name}_lower.udi -> Lower Useful Daylight Illuminance
+
+    \b
+    Args:
+        folder: Results folder. This folder is an output folder of annual
+        daylight recipe. Folder should include grids_info.json and sun-up-hours.txt.
+        The command uses the list in grids_info.json to find the result files for each
+        sensor grid.
+    """
+    # optional input - only check if the file exist otherwise ignore
+    if schedule and os.path.isfile(schedule):
+        with open(schedule) as hourly_schedule:
+            schedule = [int(float(v)) for v in hourly_schedule]
+    else:
+        schedule = None
+    try:
+        results = Results(folder, schedule=schedule)
+        results.annual_metrics_to_folder(
+            sub_folder, threshold=threshold, min_t=lower_threshold,
+            max_t=upper_threshold, states=states, grids_filter=grids_filter
+            )
+    except Exception:
+        _logger.exception('Failed to calculate annual metrics.')
+        sys.exit(1)
+    else:
+        sys.exit(0)
