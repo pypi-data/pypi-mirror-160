57ab8a61109291af14968688cb3dbfcc3ea811ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:47:20 +0000

    Add package astrosat/1.0.1/astrosat-1.0.1.tar.gz

---
+++ packages/astrosat/astrosat-1.0.1.tar.gz/astrosat-1.0.1/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(
+    name='astrosat',
+    author='James Osborn',
+    author_email='james.osborn@durham.ac.uk, matthew.townson@durham.ac.uk',
+    url='https://github.com/james-m-osborn/astrosat',
+    packages=['astrosat',
+              'astrosat.data',
+              ],
+    package_dir={'astrosat': 'astrosat'},
+    package_data={'astrosat': ['data/bsc.dat']},
+    description='A tool for forecasting satellite transits in astronomical observations',
+    version='1.0.1',
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+    ],
+)
