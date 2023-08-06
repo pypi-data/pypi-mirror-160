40c3e93f52a5f82d1ca6efafc50153e37f411336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:29 +0000

    Add package link-slot-gacor/0.0.1/link-slot-gacor-0.0.1.tar.gz

---
+++ packages/link-slot-gacor/link-slot-gacor-0.0.1.tar.gz/link-slot-gacor-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Link Slot Gacor'
+long_description = open('README.md').read()
+
+# Setting up
+setup(
+    name="link-slot-gacor",
+    version=VERSION,
+    author="HIGGSGO",
+    author_email="<higgsgo@bonanzabon.click>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['higgsgo','link slot gacor','slot gacor','slot online','slot gacor ternama','judi online','togel online','agen slot online','bandar slot online','judi slot online','slot online terbaik','slot online terpercaya'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
