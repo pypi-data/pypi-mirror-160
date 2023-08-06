c6920594f1be6708aeae5bb0cba4534b854f8084
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:54:01 +0000

    Add package cbl-tools/0.0.13/cbl_tools-0.0.13.tar.gz

---
+++ packages/cbl-tools/cbl_tools-0.0.13.tar.gz/cbl_tools-0.0.13/tests/test_tools.py
@@ -0,0 +1,24 @@
+# info@codebasedlearning.dev, a.voss@fh-aachen.de
+import importlib
+import unittest
+from importlib.metadata import version
+
+import cbl_tools
+#from cbl_tools import semver, __version__
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        cbl_tools_version = version('cbl_tools')
+        #self.assertEqual(f"{cbl_tools_version}", __version__.__version__)
+
+        #self.assertEqual(f"{cbl_tools_version}", semver.make_stage_version())
+
+        stage = "rc"
+        #self.assertEqual(f"{cbl_tools_version}-{stage}", semver.make_stage_version(stage))
+        #print(__version__.__version__)
+        #s = importlib.metadata.version()
+        s=cbl_tools. make_stage_version()
+
+
+if __name__ == '__main__':
+    unittest.main()
