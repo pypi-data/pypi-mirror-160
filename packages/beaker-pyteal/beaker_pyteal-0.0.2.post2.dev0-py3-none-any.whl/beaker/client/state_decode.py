b4d594db47ec0cd021d82e374c99c03a3660a257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:55 +0000

    Add package beaker-pyteal/0.0.2.post2.dev0/beaker_pyteal-0.0.2.post2.dev0-py3-none-any.whl

---
+++ packages/beaker-pyteal/beaker_pyteal-0.0.2.post2.dev0-py3-none-any.whl/beaker/client/state_decode.py
@@ -0,0 +1,36 @@
+from typing import Any
+from base64 import b64decode
+
+
+def str_or_hex(v: bytes) -> str:
+    decoded: str = ""
+    try:
+        decoded = v.decode("utf-8")
+    except Exception:
+        decoded = f"0x{v.hex()}"
+
+    return decoded
+
+
+def decode_state(
+    state: list[dict[str, Any]], force_str=False
+) -> dict[str | bytes, bytes | str | int]:
+
+    decoded_state: dict[str | bytes, bytes | str | int] = {}
+
+    for sv in state:
+
+        raw_key = b64decode(sv["key"])
+
+        key: str | bytes = str_or_hex(raw_key) if force_str else raw_key
+        val: str | bytes | int
+
+        match sv["value"]["type"]:
+            case 1:
+                raw_val = b64decode(sv["value"]["bytes"])
+                val = str_or_hex(raw_val) if force_str else raw_val
+            case 2:
+                val = sv["value"]["uint"]
+
+        decoded_state[key] = val
+    return decoded_state
