50681da16cc7faa4bd83d15f089605e1bd41622a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:51 +0000

    Add package SparkStream/1.1.2/SparkStream-1.1.2.tar.gz

---
+++ packages/SparkStream/SparkStream-1.1.2.tar.gz/SparkStream-1.1.2/SparkStream/__init__.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import findspark
+
+# env variables for spark and kafka
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.0,com.datastax.spark:spark-cassandra-connector_2.12:3.2.0 --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions pyspark-shell'
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+findspark.init()
+
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
