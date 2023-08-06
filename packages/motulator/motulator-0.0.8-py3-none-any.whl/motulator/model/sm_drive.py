9b549f31e1b5b2432c97e30394c6b8b292d3b01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:22 +0000

    Add package motulator/0.0.8/motulator-0.0.8-py3-none-any.whl

---
+++ packages/motulator/motulator-0.0.8-py3-none-any.whl/motulator/model/sm_drive.py
@@ -0,0 +1,153 @@
+# pylint: disable=invalid-name
+"""
+Continuous-time models for synchronous motor drives.
+
+Peak-valued complex space vectors are used. The default values correspond to a
+2.2-kW permanent-magnet synchronous motor.
+
+"""
+from __future__ import annotations
+import numpy as np
+
+from motulator.helpers import Bunch
+from motulator.model.sm import SynchronousMotor
+from motulator.model.mech import Mechanics
+from motulator.model.converter import Inverter
+
+
+# %%
+class SynchronousMotorDrive:
+    """
+    Continuous-time model for a synchronous motor drive.
+
+    This interconnects the subsystems of a synchronous motor drive and provides
+    an interface to the solver. More complicated systems could be modeled using
+    a similar template.
+
+    Parameters
+    ----------
+    motor : SynchronousMotor
+        Synchronous motor model.
+    mech : Mechanics
+        Mechanics model.
+    conv : Inverter
+        Inverter model.
+
+    """
+
+    def __init__(self, motor=SynchronousMotor(), mech=Mechanics(),
+                 conv=Inverter()):
+
+        self.motor = motor
+        self.motor._mech = mech
+        self.mech = mech
+        self.conv = conv
+
+        # Initial time
+        self.t0 = 0
+
+        # Store the solution in these lists
+        self.data = Bunch()
+        self.data.t, self.data.q = [], []
+        self.data.psi_s, self.data.theta_M, self.data.w_M = [], [], []
+
+    def get_initial_values(self):
+        """
+        Get the initial values.
+
+        Returns
+        -------
+        x0 : complex list, length 2
+            Initial values of the state variables.
+
+        """
+        x0 = [self.motor.psi_s0, self.mech.w_M0, self.mech.theta_M0]
+        return x0
+
+    def set_initial_values(self, t0, x0):
+        """
+        Set the initial values.
+
+        Parameters
+        ----------
+        x0 : complex ndarray
+            Initial values of the state variables.
+
+        """
+        self.t0 = t0
+        self.motor.psi_s0 = x0[0]
+        # x0[1].imag and x0[2].imag are always zero
+        self.mech.w_M0 = x0[1].real
+        self.mech.theta_M0 = x0[2].real
+        # Limit the angle [0, 2*pi]
+        self.mech.theta_M0 = np.mod(self.mech.theta_M0, 2*np.pi)
+
+    def f(self, t, x):
+        """
+        Compute the complete state derivative list for the solver.
+
+        Parameters
+        ----------
+        t : float
+            Time.
+        x : complex ndarray
+            State vector.
+
+        Returns
+        -------
+        complex list
+            State derivatives.
+
+        """
+        # Unpack the states
+        psi_s, w_M, theta_M = x
+        theta_m = self.motor.p*theta_M
+
+        # Interconnections: outputs for computing the state derivatives
+        u_ss = self.conv.ac_voltage(self.conv.q, self.conv.u_dc0)
+        u_s = np.exp(-1j*theta_m)*u_ss  # Stator voltage in rotor coordinates
+        i_s = self.motor.current(psi_s)
+        tau_M = self.motor.torque(psi_s, i_s)
+
+        # State derivatives
+        motor_f = self.motor.f(psi_s, i_s, u_s, w_M)
+        mech_f = self.mech.f(t, w_M, tau_M)
+
+        # List of state derivatives
+        return motor_f + mech_f
+
+    def save(self, sol):
+        """
+        Save the solution.
+
+        Parameters
+        ----------
+        sol : Bunch object
+            Solution from the solver.
+
+        """
+        self.data.t.extend(sol.t)
+        self.data.q.extend(sol.q)
+        self.data.psi_s.extend(sol.y[0])
+        self.data.w_M.extend(sol.y[1].real)
+        self.data.theta_M.extend(sol.y[2].real)
+
+    def post_process(self):
+        """
+        Transform the lists to the ndarray format and post-process them.
+
+        """
+        # From lists to the ndarray
+        for key in self.data:
+            self.data[key] = np.asarray(self.data[key])
+
+        # Compute some useful quantities
+        self.data.i_s = self.motor.current(self.data.psi_s)
+        self.data.w_m = self.motor.p*self.data.w_M
+        self.data.tau_M = self.motor.torque(self.data.psi_s, self.data.i_s)
+        self.data.tau_L = (self.mech.tau_L_ext(self.data.t)
+                           + self.mech.B*self.data.w_M)
+        self.data.u_ss = self.conv.ac_voltage(self.data.q,
+                                              self.conv.u_dc0)
+        self.data.theta_m = self.motor.p*self.data.theta_M
+        self.data.theta_m = np.mod(self.data.theta_m, 2*np.pi)
---
+++ packages/motulator/motulator-0.0.8-py3-none-any.whl/motulator/simulation.py
@@ -0,0 +1,254 @@
+# pylint: disable=invalid-name
+"""
+Simulation environment.
+
+"""
+import numpy as np
+from scipy.integrate import solve_ivp
+from scipy.io import savemat
+
+from motulator.helpers import abc2complex
+
+
+# %%
+class Delay:
+    """
+    Computational delay.
+
+    This models the computational delay as a ring buffer.
+
+    """
+
+    # pylint: disable=R0903
+    def __init__(self, length=1, elem=3):
+        """
+        Parameters
+        ----------
+        length : int, optional
+            Length of the buffer in samples. The default is 1.
+
+        """
+        self.data = length*[elem*[0]]  # Creates a zero list
+
+    def __call__(self, u):
+        """
+        Parameters
+        ----------
+        u : array_like, shape (elem,)
+            Input array.
+
+        Returns
+        -------
+        array_like, shape (elem,)
+            Output array.
+
+        """
+        # Add the latest value to the end of the list
+        self.data.append(u)
+        # Pop the first element and return it
+        return self.data.pop(0)
+
+
+# %%
+class CarrierCmp:
+    """
+    Carrier comparison.
+
+    This computes the the switching states and their durations based on the
+    duty ratios. Instead of searching for zero crossings, the switching
+    instants are explicilty computed in the begininning of each sampling
+    period, allowing faster simulations.
+
+    """
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self, N=2**12):
+        """
+        Parameters
+        ----------
+        N : int, optional
+            Amount of the counter quantization levels. The default is 2**12.
+
+        """
+        self.N = N
+        self.rising_edge = True  # Stores the carrier direction
+
+    def __call__(self, T_s, d_abc):
+        """
+        Carrier comparison.
+
+        Parameters
+        ----------
+        T_s : float
+            Sampling period (either half or full carrier period).
+        d_abc : array_like of floats, shape (3,)
+            Duty ratios in the range [0, 1].
+
+        Returns
+        -------
+        t_steps : ndarray, shape (4,)
+            Switching state durations, `[t0, t1, t2, t3]`.
+        q : complex ndarray, shape (4,)
+            Switching state vectors, `[0, q1, q2, 0]`, where `q1` and `q2` are
+            active vectors.
+
+        """
+        # Quantize the duty ratios to N levels
+        d_abc = np.round(self.N*np.asarray(d_abc))/self.N
+
+        # Normalized switching instants and switching states
+        if self.rising_edge:
+            # Normalized switching instants: t_n = [0, t_n1, t_n2, t_n3]
+            t_n = np.append(0, np.sort(1 - d_abc))
+            # States: q_abc = [[0, 0, 0], [q_abc1], [q_abc2], [1, 1, 1]]
+            q_abc = (t_n[:, np.newaxis] >= 1 - d_abc).astype(int)
+        else:
+            t_n = np.append(0, np.sort(d_abc))
+            q_abc = (t_n[:, np.newaxis] < d_abc).astype(int)
+
+        # Durations of switching states: t_steps = [t0, t1, t2, t3]
+        t_steps = T_s*np.diff(t_n, append=1)
+        # Note: No switching (e.g. d_a == 0 or d_a == 1) or simultaneous
+        # switchings (e.g d_a == d_b) lead to some zero steps.
+
+        # Array of the switching state space vectors: q = [0, q1, q2, 0]
+        q = abc2complex(q_abc.T)
+
+        # Change the carrier direction for the next call
+        self.rising_edge = not self.rising_edge
+
+        # If needed, alternatively q_abc could be returned
+        return t_steps, q
+
+
+# %%
+def zoh(T_s, d_abc):
+    """
+    Zero-order hold of the duty ratios over the sampling period.
+
+    Parameters
+    ----------
+    T_s : float
+        Sampling period.
+    d_abc : array_like of floats, shape (3,)
+        Duty ratios in the range [0, 1].
+
+    Returns
+    -------
+    t_steps : ndarray, shape (1,)
+        Sampling period as an array compatible with the solver.
+    q : complex ndarray, shape (1,)
+        Duty ratio vector as an array compatible with the solver.
+
+    """
+    # Shape the output arrays to be compatible with the solver
+    t_steps = np.array([T_s])
+    q = np.array([abc2complex(d_abc)])
+    return t_steps, q
+
+
+# %%
+class Simulation:
+    """
+    Simulation environment.
+
+    Each simulation object has a system model object and a controller object.
+
+    """
+
+    def __init__(self, mdl=None, ctrl=None, delay=1, enable_pwm=False,
+                 base=None, t_stop=1):
+        """
+        Parameters
+        ----------
+        mdl : InductionMotorDrive | SynchronousMotorDrive
+            Continuous-time system model.
+        ctrl : SynchronousMotorVectorCtrl | InductionMotorVectorCtrl |
+            InductionMotorVHzCtrl
+            Discrete-time controller.
+        delay : int, optional
+            Amount of computational delays. The default is 1.
+        enable_pwm : bool, optional
+            Enable carrier comparison. The default is False.
+        base : BaseValues, optional
+            Base values for plotting figures.
+        t_stop : float, optional
+            Simulation stop time. The default is 1.
+
+        """
+        # pylint: disable=too-many-arguments
+        self.mdl = mdl
+        self.ctrl = ctrl
+        self.delay = Delay(delay)
+        self.base = base
+        self.t_stop = t_stop
+        if enable_pwm:
+            self.pwm = CarrierCmp()
+        else:
+            self.pwm = zoh
+
+    def simulate(self, max_step=np.inf):
+        """
+        Solve the continuous-time model and call the discrete-time controller.
+
+        Parameters
+        ----------
+        max_step : float, optional
+            Max step size of the solver. The default is inf.
+
+        Notes
+        -----
+        Other options of solve_ivp could be easily changed if needed, but, for
+        simplicity, only max_step is included as an option of this method.
+
+        """
+        # Simulation loop
+        while self.mdl.t0 <= self.t_stop:
+
+            # Run the digital controller
+            T_s, d_abc_ref = self.ctrl(self.mdl)
+
+            # Computational delay model
+            d_abc = self.delay(d_abc_ref)
+
+            # Carrier comparison
+            t_steps, q = self.pwm(T_s, d_abc)
+
+            # Loop over the sampling period T_s
+            for i, t_step in enumerate(t_steps):
+
+                if t_step > 0:
+                    # Update the switching state
+                    self.mdl.conv.q = q[i]
+
+                    # Get initial values
+                    x0 = self.mdl.get_initial_values()
+
+                    # Integrate over t_span
+                    t_span = (self.mdl.t0, self.mdl.t0+t_step)
+                    sol = solve_ivp(self.mdl.f, t_span, x0, max_step=max_step)
+
+                    # Set the new initial values (last points of the solution)
+                    t0_new, x0_new = t_span[-1], sol.y[:, -1]
+                    self.mdl.set_initial_values(t0_new, x0_new)
+
+                    # Save the solution
+                    sol.q = len(sol.t)*[self.mdl.conv.q]
+                    self.mdl.save(sol)
+
+        # Call the post-processing functions
+        self.mdl.post_process()
+        self.ctrl.post_process()
+
+    def save_mat(self, name='sim'):
+        """
+        Save the simulation data into MATLAB .mat files.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name for the simulation instance. The default is 'sim'.
+
+        """
+        savemat(name+'_mdl_data'+'.mat', self.mdl.data)
+        savemat(name+'_ctrl_data'+'.mat', self.ctrl.data)
