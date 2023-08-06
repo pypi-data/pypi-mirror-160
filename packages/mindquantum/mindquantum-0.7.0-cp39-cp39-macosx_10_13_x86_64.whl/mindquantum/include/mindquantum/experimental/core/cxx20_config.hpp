562d0715d35abbb4700c53d21d2eb144fbd2169e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:25 +0000

    Add package mindquantum/0.7.0/mindquantum-0.7.0-cp39-cp39-macosx_10_13_x86_64.whl

---
+++ packages/mindquantum/mindquantum-0.7.0-cp39-cp39-macosx_10_13_x86_64.whl/mindquantum/include/mindquantum/experimental/core/cxx20_config.hpp
@@ -0,0 +1,42 @@
+//   Copyright 2022 <Huawei Technologies Co., Ltd>
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+
+#ifndef CXX20_CONFIG_HPP
+#define CXX20_CONFIG_HPP
+
+#define MQ_HAS_CLASS_NON_TYPE_TEMPLATE_ARGS 0
+
+#define MQ_HAS_CONCEPTS 1
+
+#define MQ_HAS_CONCEPT_LIBRARY 1
+
+#define MQ_HAS_CONCEPT_DESTRUCTIBLE 1
+
+#define MQ_HAS_IMPLICIT_TEMPLATE_DEDUCTION_GUIDES 0
+
+#define MQ_HAS_DETECTED_TS2 1
+
+#define MQ_HAS_MAP_CONTAINS 1
+
+#define MQ_HAS_OPERATOR_NOT_EQUAL_SYNTHESIS 1
+
+#define MQ_HAS_REMOVE_CVREF_T 1
+
+#define MQ_HAS_STD_FILESYSTEM 1
+
+#define MQ_HAS_STD_LAUNDER 1
+
+#define MQ_HAS_CONSTEXPR_STD_VECTOR 0
+
+#endif /* CXX20_CONFIG_HPP */
---
+++ packages/mindquantum/mindquantum-0.7.0-cp39-cp39-macosx_10_13_x86_64.whl/mindquantum/io/display/circuit_text_drawer.py
@@ -0,0 +1,144 @@
+# Copyright 2021 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+
+"""Text draw a circuit."""
+
+import numpy as np
+
+from ._config import _text_drawer_config
+
+
+def _get_qubit_range(gate):
+    """Get qubit range."""
+    out = []
+    out.extend(gate.obj_qubits)
+    out.extend(gate.ctrl_qubits)
+    if not out:
+        raise ValueError(f"{gate.name} gate should apply to certain qubit first.")
+    return out
+
+
+def brick_model(circ, qubits_name=None):  # pylint: disable=too-many-locals
+    """Split a circuit into layers."""
+    from mindquantum import (  # pylint: disable=import-outside-toplevel,cyclic-import
+        gates,
+    )
+
+    n = circ.n_qubits
+    if qubits_name is None:
+        qubits_name = list(range(n))
+    v_n = _text_drawer_config['v_n']
+    blocks = []
+    qubit_hight = np.zeros(n, dtype=int)
+    for gate in circ:
+        if isinstance(gate, gates.BarrierGate):
+            qrange = range(n)
+        else:
+            qrange = _get_qubit_range(gate)
+        max_hight = np.max(qubit_hight[range(min(qrange), max(qrange) + 1)])
+        if len(blocks) <= max_hight:
+            blocks.append([])
+        blocks[max_hight].append(gate)
+        qubit_hight[range(min(qrange), max(qrange) + 1)] = max_hight + 1
+    blocks = [_single_block_drawer(i, n) for i in blocks]
+    res = {}
+    max_q = 0
+    for i in range(n):
+        string = f'q{qubits_name[i]}: '
+        res[i * (v_n + 1)] = string
+        max_q = max(max_q, len(string))
+    for i in range(n):
+        res[i * (v_n + 1)] = res[i * (v_n + 1)].ljust(max_q, ' ')
+        if i != n - 1:
+            for j in range(v_n):
+                res[i * (v_n + 1) + j + 1] = ' ' * max_q
+    for block in blocks:
+        for k, v in block.items():
+            res[k] += v
+    return [res[i].rstrip() for i in range((n - 1) * (v_n + 1) + 1)]
+
+
+def _single_gate_drawer(gate):
+    """Single gate drawer."""
+    from mindquantum import (  # pylint: disable=import-outside-toplevel,cyclic-import
+        gates,
+    )
+
+    if isinstance(gate, gates.CNOTGate):
+        gate = gates.X.on(*gate.obj_qubits)
+    main_text = gate.__str_in_circ__()
+    if isinstance(gate, gates.SWAPGate):
+        main_text = _text_drawer_config['swap_mask'][0]
+    main_text = _text_drawer_config['edge'] + main_text + _text_drawer_config['edge']
+    res = {}
+    for i in gate.obj_qubits:
+        res[i] = main_text
+    if isinstance(gate, gates.SWAPGate):
+        max_idx = max(gate.obj_qubits)
+        lower_txt = _text_drawer_config['swap_mask'][1]
+        res[max_idx] = _text_drawer_config['edge'] + lower_txt + _text_drawer_config['edge']
+    for i in gate.ctrl_qubits:
+        res[i] = _text_drawer_config['ctrl_mask']
+        res[i] = res[i].center(len(main_text), _text_drawer_config['circ_line'])
+    res['len'] = len(main_text)
+    return res
+
+
+def _single_block_drawer(block, n_qubits):  # pylint: disable=too-many-branches,too-many-locals
+    """Single block drawer."""
+    from mindquantum import (  # pylint: disable=import-outside-toplevel,cyclic-import
+        gates,
+    )
+
+    v_n = _text_drawer_config['v_n']
+    text_gates = {}
+    if isinstance(block[0], gates.BarrierGate):
+        if not block[0].show:
+            tmp = ''
+        else:
+            tmp = _text_drawer_config['barrier']
+        for i in range((n_qubits - 1) * v_n + n_qubits):
+            text_gates[i] = tmp
+        return text_gates
+    for gate in block:
+        text_gate = _single_gate_drawer(gate)
+        qrange = _get_qubit_range(gate)
+        for qubit in range(min(qrange), max(qrange) + 1):
+            ind = qubit * (v_n + 1)
+            if qubit in qrange:
+                text_gates[ind] = text_gate[qubit]
+            else:
+                text_gates[ind] = _text_drawer_config['cross_mask']
+                text_gates[ind] = text_gates[ind].center(text_gate['len'], _text_drawer_config['circ_line'])
+        for qubit in range(min(qrange), max(qrange)):
+            for i in range(v_n):
+                ind = qubit * (v_n + 1) + i + 1
+                text = _text_drawer_config['ctrl_line']
+                text_gates[ind] = text
+                text_gates[ind] = text.center(text_gate.get('len', 0), ' ')
+    max_l = max(len(j) for j in text_gates.values())
+    for k, v in text_gates.items():
+        if len(v) != max_l:
+            if k % (v_n + 1) == 0:
+                text_gates[k] = text_gates[k].center(max_l, _text_drawer_config['circ_line'])
+            else:
+                text_gates[k] = text_gates[k].center(max_l, ' ')
+    for i in range((n_qubits - 1) * v_n + n_qubits):
+        if i not in text_gates:
+            if i % (v_n + 1) == 0:
+                text_gates[i] = _text_drawer_config['circ_line'] * max_l
+            else:
+                text_gates[i] = ' ' * max_l
+    return text_gates
