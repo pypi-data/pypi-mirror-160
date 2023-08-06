5c7317e834e5c2b24327cdd27cbaf7af93b03442
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:16 +0000

    Add package bchaiker/0.0.1/bchaiker-0.0.1.tar.gz

---
+++ packages/bchaiker/bchaiker-0.0.1.tar.gz/bchaiker-0.0.1/bchaiker/__init__.py
@@ -0,0 +1,15 @@
+
+def amine():
+    import pandas as pd
+    print('wait..\n')
+    df = pd.read_csv('ml2.csv')
+    return pd
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divise_numbers(num1, num2):
+    return num1 / num2
