bb2778e7312d3f34863ba3a16962b8118415c4a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:38 +0000

    Add package tfae/0.1.0/tfae-0.1.0.tar.gz

---
+++ packages/tfae/tfae-0.1.0.tar.gz/tfae-0.1.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tfae',
+ 'tfae.bottlenecks',
+ 'tfae.models',
+ 'tfae.regularizers',
+ 'tfae.schedulers']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['tensorflow>=2.9.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'tfae',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Artem Legotin',
+    'author_email': 'hello@artemlegotin.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<=3.10',
+}
+
+
+setup(**setup_kwargs)
