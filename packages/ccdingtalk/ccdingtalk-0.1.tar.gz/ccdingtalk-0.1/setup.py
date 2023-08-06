52669df869a43388b432c8c6e51aa5051b1da609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:05 +0000

    Add package ccdingtalk/0.1/ccdingtalk-0.1.tar.gz

---
+++ packages/ccdingtalk/ccdingtalk-0.1.tar.gz/ccdingtalk-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ccdingtalk',
+      version='0.1',
+      description='Send dingtalk webhook with simple interface and load balance',
+      url='https://github.com/OpenFibers/CCDingTalk',
+      author='OpenFibers',
+      author_email='openfibers@gmail.com',
+      license='MIT',
+      packages=['ccdingtalk'],
+      zip_safe=False)
