0dbc2b0edb637621d5ce03af28e0666231132486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:38 +0000

    Add package carter-probability/0.1/carter_probability-0.1.tar.gz

---
+++ packages/carter-probability/carter_probability-0.1.tar.gz/carter_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='carter_probability',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['carter_probability'],
+      author = 'Carter Lam',
+      author_email = 'carterlamtlc@gmail.com',
+      zip_safe=False)
