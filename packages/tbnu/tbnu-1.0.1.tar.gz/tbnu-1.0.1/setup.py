71038ce2a43f3db55fbdb4ab585fcd8dcfba2550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:41:01 +0000

    Add package tbnu/1.0.1/tbnu-1.0.1.tar.gz

---
+++ packages/tbnu/tbnu-1.0.1.tar.gz/tbnu-1.0.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tbnu', 'tbnu.aesthetics']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['appdirs>=1.4.4,<2.0.0', 'art>=5.6,<6.0']
+
+entry_points = \
+{'console_scripts': ['tbnu = tbnu:main']}
+
+setup_kwargs = {
+    'name': 'tbnu',
+    'version': '1.0.1',
+    'description': 'TBNU is a super simple and easy to use CLI notes application',
+    'long_description': None,
+    'author': 'Abdullah Mohammed',
+    'author_email': 'aamohammed4556@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
