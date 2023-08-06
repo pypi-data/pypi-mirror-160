b5bbe4ad8c0ffb4585bf60cc3e688f8500019d96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:04 +0000

    Add package rebotics-sdk/0.17.5/rebotics_sdk-0.17.5.tar.gz

---
+++ packages/rebotics-sdk/rebotics_sdk-0.17.5.tar.gz/rebotics_sdk-0.17.5/rebotics_sdk/providers/__init__.py
@@ -0,0 +1,5 @@
+from .base import *  # NOQA
+from .admin import AdminProvider  # NOQA
+from .dataset import DatasetProvider  # NOQA
+from .retailer import RetailerProvider  # NOQA
+from .fvm import FVMProvider # NOQA
