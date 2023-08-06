9521f03db985a04254127f922b24939b98a3ed5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:12:03 +0000

    Add package Saylani-DS-3346/0.1/Saylani_DS_3346-0.1.tar.gz

---
+++ packages/Saylani-DS-3346/Saylani_DS_3346-0.1.tar.gz/Saylani_DS_3346-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup 
+
+setup (name = "Saylani_DS_3346", 
+version = "0.1", 
+description = "This is My First Ever Package", 
+long_description = "this is a very very long description of my first package", 
+author = "Sir Qasim & Hafiz Junaid Jahangir Qazi", 
+packages = ["Saylani_DS_3346"], 
+install_package = []
+)
