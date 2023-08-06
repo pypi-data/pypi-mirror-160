bf06c8012c195d1fdc7cbfa5a808fca80fcae42b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:29 +0000

    Add package keras-tweaks/0.2.3/keras-tweaks-0.2.3.tar.gz

---
+++ packages/keras-tweaks/keras-tweaks-0.2.3.tar.gz/keras-tweaks-0.2.3/keras_tweaks/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.2.3'
+
+from .idseqs_to_mask import idseqs_to_mask
+from .dense_sparse_matmul import dense_sparse_matmul
+from .get_sparsity_pattern import get_sparsity_pattern
---
+++ packages/keras-tweaks/keras-tweaks-0.2.3.tar.gz/keras-tweaks-0.2.3/keras_tweaks/dense_sparse_matmul.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+
+
+def dense_sparse_matmul(denV: tf.Tensor, spW: tf.SparseTensor) -> tf.Tensor:
+    """Multiply row vector with sparse matrix
+
+    Parameters:
+        denV (tf.Tensor): Dense BxN or BxTxN dense tensor.
+        spW (tf.SparseTensor): Sparse NxM matrix.
+
+    Returns:
+        tf.Tensor: Dense BxM or BxTxM dense tensor
+
+    Motivation:
+        TF only supports multiplying a sparse matrix with a column vector.
+        By transposing, we can use `tf.sparse.sparse_dense_matmul` to
+        multiply a row vector with a sparse matrix. The later multiplication
+        is commonly used in context of neural networks in TF/Keras.
+    """
+    # reshape to list of row vectors if neccessary
+    if denV.shape.ndims == 1:
+        denV = tf.reshape(denV, (1, -1))
+
+    # transpose -> multiply -> transpose back
+    if denV.shape.ndims == 2:
+        # W * V_(batch, dim)
+        return tf.transpose(tf.sparse.sparse_dense_matmul(
+            tf.sparse.transpose(spW, perm=[1, 0]),
+            tf.transpose(denV, perm=[1, 0])))
+
+    if denV.shape.ndims == 3:
+        # W * V_(batch, seqlen, dim)
+        Wt = tf.sparse.transpose(spW, perm=[1, 0])
+        ht = tf.transpose(denV, perm=[0, 2, 1])
+        out = []
+        for ex in ht:
+            out.append(tf.sparse.sparse_dense_matmul(Wt, ex))
+        return tf.transpose(tf.stack(out), perm=[0, 2, 1])
+
+    raise ValueError("Invalid shape: {}".format(denV.shape))
