4de1d465ceed02670114dcaf243d1932e8cc2ed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:42 +0000

    Add package ezbootstrap/0.0.2a0/ezbootstrap-0.0.2a0-py3-none-any.whl

---
+++ packages/ezbootstrap/ezbootstrap-0.0.2a0-py3-none-any.whl/ezbootstrap/__init__.py
@@ -0,0 +1,16 @@
+from ezbootstrap.core import (
+    bs_1sample,
+    bs_2sample,
+    bs_pairs,
+    permutation_2sample,
+    permutation_2sample_independent,
+)
+
+
+__all__ = [
+    "bs_1sample",
+    "bs_2sample",
+    "bs_pairs",
+    "permutation_2sample",
+    "permutation_2sample_independent",
+]
