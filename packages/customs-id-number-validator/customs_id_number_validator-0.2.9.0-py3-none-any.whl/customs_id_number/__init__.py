905e6048ff70536552760a44b408aea3733ecdc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:56 +0000

    Add package customs-id-number-validator/0.2.9.0/customs_id_number_validator-0.2.9.0-py3-none-any.whl

---
+++ packages/customs-id-number-validator/customs_id_number_validator-0.2.9.0-py3-none-any.whl/customs_id_number/__init__.py
@@ -0,0 +1,14 @@
+# /package/__init__.py
+
+""" 
+Description for Package 
+"""
+
+from customs_id_number.customs_id_number import *
+# from package import * 로 써도 된다.
+
+__all__ = ['customs_id_number']
+
+# 패키지 버전 정의
+
+__version__ = '0.2.1'
