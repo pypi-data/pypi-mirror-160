548038186cf59d3657246b0d4def3c4726a94b7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:55:58 +0000

    Add package mosaicml/0.8.1/mosaicml-0.8.1.tar.gz

---
+++ packages/mosaicml/mosaicml-0.8.1.tar.gz/mosaicml-0.8.1/composer/__init__.py
@@ -0,0 +1,27 @@
+# Copyright 2022 MosaicML Composer authors
+# SPDX-License-Identifier: Apache-2.0
+
+"""Composer."""
+
+from composer.core import Algorithm, Callback, DataSpec, Engine, Evaluator, Event, State, Time, Timestamp, TimeUnit
+from composer.loggers import Logger
+from composer.models import ComposerModel
+from composer.trainer import Trainer
+
+__all__ = [
+    'Algorithm',
+    'Callback',
+    'DataSpec',
+    'Engine',
+    'Evaluator',
+    'Event',
+    'State',
+    'Time',
+    'Timestamp',
+    'TimeUnit',
+    'Logger',
+    'ComposerModel',
+    'Trainer',
+]
+
+__version__ = '0.8.1'
