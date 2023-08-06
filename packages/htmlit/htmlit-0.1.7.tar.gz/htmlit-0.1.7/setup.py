9e1098d5eec171de760755bcfc2aa7577448c639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:53:24 +0000

    Add package htmlit/0.1.7/htmlit-0.1.7.tar.gz

---
+++ packages/htmlit/htmlit-0.1.7.tar.gz/htmlit-0.1.7/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="htmlit",
+    version="0.1.007",
+    author="Gaetan Desrues",
+    author_email="gdesrues@gmail.com",
+    url="https://github.com/GaetanDesrues/htmlgen",
+    description="Description",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["htmlit2"],  # setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
