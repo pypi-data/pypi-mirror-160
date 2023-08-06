29c5ce6ccb41c27be10ed4cd0bb948188387e300
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:37 +0000

    Add package thursday/0.1a0/thursday-0.1a0-py3-none-any.whl

---
+++ packages/thursday/thursday-0.1a0-py3-none-any.whl/thursday/__init__.py
@@ -0,0 +1,9 @@
+from .chrome import ChromeDriver
+from .exceptions import BrowserNotInstalledException
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import TimeoutException
