7ee39d72669afcb9ef356966e1b1558c7cbcbde3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:47:22 +0000

    Add package sensor-state-data/2.0.2/sensor-state-data-2.0.2.tar.gz

---
+++ packages/sensor-state-data/sensor-state-data-2.0.2.tar.gz/sensor-state-data-2.0.2/src/sensor_state_data/value.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from datetime import date, datetime
+from decimal import Decimal
+
+from .device import DeviceKey
+
+
+@dataclass(frozen=True)
+class SensorValue:
+    """A class that describes sensor values."""
+
+    device_key: DeviceKey
+    name: str
+    native_value: None | str | int | float | date | datetime | Decimal
