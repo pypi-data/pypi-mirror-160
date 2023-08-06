f132a1f1c9e6d9f76148c91f75ec701778381c6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:39 +0000

    Add package csv-detective/0.4.5/csv_detective-0.4.5.tar.gz

---
+++ packages/csv-detective/csv_detective-0.4.5.tar.gz/csv_detective-0.4.5/csv_detective/detect_fields/FR/geo/adresse/__init__.py
@@ -0,0 +1,76 @@
+from csv_detective.process_text import _process_text
+
+PROPORTION = 0.55
+
+def _is(val):
+    '''Repere des adresses'''
+    voies = [
+        'Aire',
+        'Allée',
+        'Avenue',
+        'Base',
+        'Boulevard',
+        'Cami',
+        'Carrefour',
+        'Chemin',
+        'Cheminement',
+        'Chaussée',
+        'Cité',
+        'Clos',
+        'Coin',
+        'Corniche',
+        'Cote',
+        'Cour',
+        'Cours',
+        'Domaine',
+        'Descente',
+        'Ecart',
+        'Esplanade',
+        'Faubourg',
+        'Gare',
+        'Grande Rue',
+        'Hameau',
+        'Halle',
+        'Ilôt',
+        'Impasse',
+        'Lieu dit',
+        'Lotissement',
+        'Marché',
+        'Montée',
+        'Parc',
+        'Passage',
+        'Place',
+        'Plan',
+        'Plaine',
+        'Plateau',
+        'Pont',
+        'Port',
+        'Promenade',
+        'Parvis',
+        'Quartier',
+        'Quai',
+        'Résidence',
+        'Ruelle',
+        'Rocade',
+        'Rond Point',
+        'Route',
+        'Rue',
+        'Sente - Sentier',
+        'Square',
+        'Tour',
+        'Terre-plein',
+        'Traverse',
+        'Villa',
+        'Village',
+        'Voie',
+        'Zone artisanale',
+        'Zone d’aménagement concerté',
+        'Zone d’aménagement différé',
+        'Zone industrielle',
+        'Zone'
+    ]
+
+    val = _process_text(val)
+    # Since 'true' contains 'rue', we need to exclude it.
+    a = any([x.lower() in val for x in voies]) and val != 'true'
+    return a
