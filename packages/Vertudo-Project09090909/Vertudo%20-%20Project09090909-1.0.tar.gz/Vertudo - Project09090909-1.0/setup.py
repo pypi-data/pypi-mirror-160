4bd9e31ec41ca18f468e3c53c71fc9d413357575
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:45 +0000

    Add package Vertudo-Project09090909/1.0/Vertudo%20-%20Project09090909-1.0.tar.gz

---
+++ packages/Vertudo-Project09090909/Vertudo%20-%20Project09090909-1.0.tar.gz/Vertudo - Project09090909-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Vertudo - Project09090909',
+      version='1.0',
+      description='Tmp project',
+      author='Vertudo-X',
+      author_email='justgamee92@gmail.com',
+      url='https://www.google.com/',
+      packages=['module1', 'module2'],
+     )
