7c6c5c09858f225a7819a05df5e78bcf610777cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:30 +0000

    Add package capylang/0.10.1/capylang-0.10.1.tar.gz

---
+++ packages/capylang/capylang-0.10.1.tar.gz/capylang-0.10.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['capylang', 'capylang.src.capylang']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['numpy>=1.22.2,<2.0.0', 'requests>=2.28.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'capylang',
+    'version': '0.10.1',
+    'description': "Python's little programming language.",
+    'long_description': None,
+    'author': 'Kia Kazemi',
+    'author_email': 'kia@anistick.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8.0,<3.9',
+}
+
+
+setup(**setup_kwargs)
