1c7b64bf9cffc53ddabe2dd44c2a8e5c448067e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:08 +0000

    Add package toolky/0.1.1.1/toolky-0.1.1.1.tar.gz

---
+++ packages/toolky/toolky-0.1.1.1.tar.gz/toolky-0.1.1.1/setup.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2022 Keyu Tian. All rights reserved.
+import setuptools
+from toolky import __version__
+
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='toolky',
+    version=__version__,
+    author='Keyu Tian',
+    author_email='tiankeyu.00@gmail.com',
+    description='tools',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/keyu-research/toolky',
+    packages=setuptools.find_packages(),
+    platforms=['all'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        # 'pathos'
+    ],
+    entry_points={
+        'console_scripts': [
+            'sea = toolky.cli.search:main'
+        ]
+    }
+)
+
+
