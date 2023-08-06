32834eefef2e3edf26b633117f00b2f975456231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:06 +0000

    Add package hspylib/0.11.92/hspylib-0.11.92.tar.gz

---
+++ packages/hspylib/hspylib-0.11.92.tar.gz/hspylib-0.11.92/hspylib/addons/appman/appman_enums.py
@@ -0,0 +1,14 @@
+from hspylib.core.enums.enumeration import Enumeration
+
+
+class Extension(Enumeration):
+    """TODO"""
+    GRADLE = 'gradle'
+    GIT = 'git'
+
+
+class AppType(Enumeration):
+    """TODO"""
+    APP = 'app'
+    QT_APP = 'qt-app'
+    WIDGET = 'widget'
