0c50a5ac003cb578dd9dfb6374b5695d321be76f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:13 +0000

    Add package SurveyHumpbackWhale/2020.1.0/SurveyHumpbackWhale-2020.1.0.tar.gz

---
+++ packages/SurveyHumpbackWhale/SurveyHumpbackWhale-2020.1.0.tar.gz/SurveyHumpbackWhale-2020.1.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='SurveyHumpbackWhale',
+    version='2020.1.0',
+    description='Survey utils module to make transforms and proccess this sources of data easily',
+    py_modules=["SurveyHumpbackWhale"],
+    )
+
