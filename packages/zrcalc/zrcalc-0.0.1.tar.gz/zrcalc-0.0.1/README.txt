aece51e9119f33ef3ac32ee846e96ab97a832a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:57 +0000

    Add package zrcalc/0.0.1/zrcalc-0.0.1.tar.gz

---
+++ packages/zrcalc/zrcalc-0.0.1.tar.gz/zrcalc-0.0.1/README.txt
@@ -0,0 +1,2 @@
+zrcalc is a simple calculator.Let's say you want to add the numbers.Then do it like this: zrcalc.add(1, 1)
+i used 1, 1 but you can use every number if you want.
---
+++ packages/zrcalc/zrcalc-0.0.1.tar.gz/zrcalc-0.0.1/zrcalc/__init__.py
@@ -0,0 +1,13 @@
+class zrcalc:
+    def add(num1, num2):
+        num= num1 + num2
+        print(num)
+    def sub(num1, num2):
+        num= num1 - num2
+        print(num)
+    def mul(num1, num2):
+        num= num1 * num2
+        print(num)
+    def div(num1, num2):
+        num = num1 / num2
+        print(num)
