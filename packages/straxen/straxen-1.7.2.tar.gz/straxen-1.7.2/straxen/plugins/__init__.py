100786ed6a1b188b0161dc449dd44da17a23902c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:20 +0000

    Add package straxen/1.7.2/straxen-1.7.2.tar.gz

---
+++ packages/straxen/straxen-1.7.2.tar.gz/straxen-1.7.2/straxen/plugins/__init__.py
@@ -0,0 +1,64 @@
+from . import daqreader
+from .daqreader import *
+
+from . import pax_interface
+from .pax_interface import *
+
+from . import pulse_processing
+from .pulse_processing import *
+
+from . import peaklet_processing
+from .peaklet_processing import *
+
+from . import bayes_peak_classification
+from .bayes_peak_classification import *
+
+from . import peak_processing
+from .peak_processing import *
+
+from .position_reconstruction import *
+from . import position_reconstruction
+
+from . import event_area_per_channel
+from .event_area_per_channel import *
+
+from . import event_info
+from .event_info import *
+
+from . import event_patternfit
+from .event_patternfit import *
+
+from . import event_processing
+from .event_processing import *
+
+from .event_w_bayes_class import *
+from . import event_w_bayes_class
+
+from . import afterpulse_processing
+from .afterpulse_processing import *
+
+from . import double_scatter
+from .double_scatter import *
+
+from . import led_calibration
+from .led_calibration import *
+
+from . import x1t_cuts
+
+from . import nveto_recorder
+from .nveto_recorder import *
+
+from . import veto_pulse_processing
+from .veto_pulse_processing import *
+
+from . import veto_hitlets
+from .veto_hitlets import *
+
+from . import veto_events
+from .veto_events import *
+
+from . import acqmon_processing
+from .acqmon_processing import *
+
+from . import online_monitor
+from .online_monitor import *
