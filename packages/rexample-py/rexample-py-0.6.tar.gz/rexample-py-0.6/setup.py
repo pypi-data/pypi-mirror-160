301378ce497a396db2e3156b39d18ba4b116549a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:48 +0000

    Add package rexample-py/0.6/rexample-py-0.6.tar.gz

---
+++ packages/rexample-py/rexample-py-0.6.tar.gz/rexample-py-0.6/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='rexample-py',
+    version='0.6',
+    license='MIT',
+    author="Ramez Yousri",
+    author_email='email@example.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='',
+    keywords='example project',
+    install_requires=[
+          
+      ],
+
+)
