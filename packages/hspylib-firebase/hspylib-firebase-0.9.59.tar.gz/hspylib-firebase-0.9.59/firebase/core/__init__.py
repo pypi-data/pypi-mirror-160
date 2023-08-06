c36a5897cb505811f3e6d846817d6c016b666e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:06 +0000

    Add package hspylib-firebase/0.9.59/hspylib-firebase-0.9.59.tar.gz

---
+++ packages/hspylib-firebase/hspylib-firebase-0.9.59.tar.gz/hspylib-firebase-0.9.59/firebase/core/__init__.py
@@ -0,0 +1,13 @@
+# _*_ coding: utf-8 _*_
+#
+# hspylib-firebase v0.9.59
+#
+# Package: main.firebase.core
+"""Package initialization."""
+
+__all__ = [
+  'agent_config', 
+  'file_processor', 
+  'firebase'
+]
+__version__ = '0.9.59'
