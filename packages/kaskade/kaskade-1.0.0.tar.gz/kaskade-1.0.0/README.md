28622c00a70270294b19c7bca288046d2bd8f9c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:12 +0000

    Add package kaskade/1.0.0/kaskade-1.0.0.tar.gz

---
+++ packages/kaskade/kaskade-1.0.0.tar.gz/kaskade-1.0.0/README.md
@@ -0,0 +1,315 @@
+<p align="center">
+<a href="https://github.com/sauljabin/kaskade"><img alt="kaskade" src="https://raw.githubusercontent.com/sauljabin/kaskade/main/screenshots/banner.png"></a>
+</p>
+<a href="https://www.python.org/"><img alt="Python" width="60" height="20" src="https://img.shields.io/badge/-python-brightgreen?logo=python&logoColor=white"></a>
+<a href="https://pypi.org/project/kaskade"><img alt="Version" src="https://img.shields.io/pypi/v/kaskade"></a>
+<a href="https://pypi.org/project/kaskade"><img alt="Python Versions" src="https://img.shields.io/pypi/pyversions/kaskade"></a>
+<a href="https://libraries.io/pypi/kaskade"><img alt="Dependencies" src="https://img.shields.io/librariesio/release/pypi/kaskade"></a>
+<a href="https://pypi.org/project/kaskade"><img alt="Platform" src="https://img.shields.io/badge/platform-linux%20%7C%20osx-0da5e0"></a>
+<br>
+<a href="https://kafka.apache.org/"><img alt="Kafka" width="60" height="20" src="https://img.shields.io/badge/-kafka-e3e3e3?logo=apache-kafka&logoColor=202020"></a>
+<a href="https://kafka.apache.org/"><img alt="Kafka" src="https://img.shields.io/badge/kafka-2.8%20%7C%203.0%20%7C%203.1%20%7C%203.2-blue"/></a>
+<a href="https://pypi.org/project/confluent-kafka/"><img alt="Kafka Client" src="https://img.shields.io/pypi/v/confluent-kafka?label=kafka%20client"></a>
+<br>
+<a href="https://github.com"><img alt="GitHub" width="60" height="20" src="https://img.shields.io/badge/-github-blueviolet?logo=github&logoColor=white"></a>
+<a href="https://github.com/sauljabin/kaskade/blob/main/LICENSE"><img alt="MIT License" src="https://img.shields.io/github/license/sauljabin/kaskade"></a>
+<a href="https://github.com/sauljabin/kaskade/actions"><img alt="GitHub Workflow Status" src="https://img.shields.io/github/workflow/status/sauljabin/kaskade/CI?label=tests"></a>
+<a href="https://app.codecov.io/gh/sauljabin/kaskade"><img alt="Codecov" src="https://img.shields.io/codecov/c/github/sauljabin/kaskade"></a>
+<br>
+<a href="https://www.docker.com/"><img alt="Docker" width="60" height="20" src="https://img.shields.io/badge/-docker-blue?logo=docker&logoColor=white"></a>
+<a href="https://hub.docker.com/r/sauljabin/kaskade"><img alt="Docker Image Version (latest by date)" src="https://img.shields.io/docker/v/sauljabin/kaskade?label=tag"></a>
+<a href="https://hub.docker.com/r/sauljabin/kaskade"><img alt="Docker Image Size (latest by date)" src="https://img.shields.io/docker/image-size/sauljabin/kaskade"></a>
+
+**kaskade** is a read-only tui (text user interface) for [kafka](https://kafka.apache.org/).
+
+:rocket: This project is powered by [textual](https://github.com/willmcgugan/textual)
+and [rich](https://github.com/willmcgugan/rich)!.
+
+For a local kafka environment go to https://github.com/sauljabin/kafka-docker.
+
+# Table of Contents
+
+- [Table of Contents](#table-of-contents)
+- [Installation and Usage](#installation-and-usage)
+- [Running with Docker](#running-with-docker)
+- [Configuration](#configuration)
+    - [Kafka](#kafka)
+    - [Schema Registry](#schema-registry)
+    - [Kaskade](#kaskade)
+    - [Other Examples](#other-examples)
+- [Screenshots](#screenshots)
+- [Alternatives](#alternatives)
+- [Development](#development)
+    - [Scripts](#scripts)
+    - [Docker](#docker)
+    - [Bumping Version](#bumping-version)
+
+# Installation and Usage
+
+Install with pip:
+
+```shell
+pip install kaskade
+```
+
+> `pip` will install `kaskade` and `kskd` aliases.
+
+Upgrade with pip:
+
+```shell
+pip install --upgrade kaskade
+```
+
+Help:
+
+```shell
+kaskade --help
+```
+
+Version:
+
+```shell
+kaskade --version
+```
+
+Information, it shows app information:
+
+```shell
+kaskade --info
+```
+
+Configurations, it shows config examples:
+
+```shell
+kaskade --configs
+```
+
+Generating a default config yml file:
+
+```shell
+kaskade --yml
+```
+
+Run without config file (it'll take any of `kaskade.yml`, `kaskade.yaml`, `config.yml` or `config.yaml`):
+
+```shell
+kaskade
+```
+
+Run with config file:
+
+```shell
+kaskade my-config.yml
+```
+
+# Running with Docker
+
+Using docker (remember to set a `network` and `volume`):
+
+```shell
+docker run --rm -it --network kafka \
+--volume $(pwd):/kaskade \
+sauljabin/kaskade:latest
+```
+
+Aliases:
+
+```shell
+alias kaskade='docker run --rm -it --network kafka \
+--volume $(pwd):/kaskade \
+sauljabin/kaskade:latest'
+alias kskd=kaskade
+```
+
+> These aliases will mount the current directory as a volume.
+
+# Configuration
+
+A [yaml](https://yaml.org/spec/1.2/spec.html) configuration file
+(check [Installation and Usage](#installation-and-usage) section for more information).
+
+### Kafka
+
+Simple connection example:
+
+```yaml
+kafka:
+  bootstrap.servers: localhost:9092
+```
+
+SSL encryption example:
+
+```yaml
+kafka:
+  bootstrap.servers: localhost:9092
+  security.protocol: SSL
+```
+
+> For more information about SSL encryption and SSL authentication go
+> to [confluent-kafka](https://github.com/confluentinc/confluent-kafka-python#ssl-certificates)
+> and [librdkafka](https://github.com/edenhill/librdkafka/wiki/Using-SSL-with-librdkafka#configure-librdkafka-client).
+
+Support for env variables:
+
+```yaml
+kafka:
+  bootstrap.servers: ${BOOTSTRAP_SERVERS}
+```
+
+### Schema Registry
+
+Simple connection example:
+
+```yaml
+schema.registry:
+  url: http://localhost:8081
+```
+
+### Kaskade
+
+Next settings are optional:
+
+```yaml
+kaskade:
+  debug: off # enable debug mode, default off
+  refresh: on # enable auto-refresh, default on
+  refresh-rate: 5 # auto-refresh rate, default 5 secs
+```
+
+> `debug` enabled will generate logs into a specific log file, execute `kaskade --info` to get the log path.
+
+### Other Examples
+
+Confluent Cloud:
+
+```yaml
+kafka:
+  bootstrap.servers: ${BOOTSTRAP_SERVERS}
+  security.protocol: SASL_SSL
+  sasl.mechanism: PLAIN
+  sasl.username: ${CLUSTER_API_KEY}
+  sasl.password: ${CLUSTER_API_SECRET}
+
+schema.registry:
+  url: ${SCHEMA_REGISTRY_URL}
+  basic.auth.credentials.source: USER_INFO
+  basic.auth.user.info: ${SR_API_KEY}:${SR_API_SECRET}
+```
+
+# Screenshots
+
+<p align="center">
+<img alt="kaskade" src="https://raw.githubusercontent.com/sauljabin/kaskade/main/screenshots/dashboard.png">
+</p>
+
+<p align="center">
+<img alt="kaskade" src="https://raw.githubusercontent.com/sauljabin/kaskade/main/screenshots/consumer.png">
+</p>
+
+# Alternatives
+
+- cli: [[kcat](https://github.com/edenhill/kcat), [zoe](https://github.com/adevinta/zoe), [kaf](https://github.com/birdayz/kaf)]
+- wui: [[akhq](https://github.com/tchiotludo/akhq)]
+- tui: [[kcli](https://github.com/cswank/kcli)]
+
+# Development
+
+Python supported versions:
+
+<a href="https://pypi.org/project/kaskade"><img alt="Python Versions" src="https://img.shields.io/pypi/pyversions/kaskade?label="></a>
+
+Installing poetry:
+
+```shell
+pip install poetry
+```
+
+Installing development dependencies:
+
+```shell
+poetry install
+```
+
+Installing pre-commit hooks:
+
+```shell
+poetry run pre-commit install
+```
+
+Running kaskade:
+
+```shell
+poetry run kaskade
+```
+
+### Scripts
+
+Running unit tests:
+
+```shell
+poetry run python -m scripts.tests
+```
+
+Applying code styles:
+
+```shell
+poetry run python -m scripts.styles
+```
+
+Running code analysis:
+
+```shell
+poetry run python -m scripts.analyze
+```
+
+Running code coverage:
+
+```shell
+poetry run python -m scripts.tests-coverage
+```
+
+Running pre-commit hooks:
+
+```shell
+poetry run python -m scripts.pre-commit
+```
+
+Generate readme banner:
+
+```shell
+poetry run python -m scripts.banner
+```
+
+### Docker
+
+Build docker:
+
+```shell
+poetry run python -m scripts.docker-build
+```
+
+> Image tag `sauljabin/kaskade:latest`.
+
+Run with docker (create a `config.yml` file):
+
+```shell
+docker run --rm -it --network kafka \
+--volume $(pwd):/kaskade \
+sauljabin/kaskade:latest
+```
+
+### Bumping Version
+
+Help:
+
+```shell
+poetry run python -m scripts.release --help
+```
+
+> More info at https://python-poetry.org/docs/cli/#version and https://semver.org/.
+
+Upgrade (`major.minor.patch`):
+
+```shell
+poetry run python -m scripts.release patch
+```
---
+++ packages/kaskade/kaskade-1.0.0.tar.gz/kaskade-1.0.0/kaskade/tui.py
@@ -0,0 +1,259 @@
+from threading import Event, Thread
+from typing import Any, Optional, Type
+
+from confluent_kafka import KafkaException
+from textual.app import App
+from textual.driver import Driver
+from textual.keys import Keys
+from textual.reactive import Reactive
+from textual.widget import Widget
+
+from kaskade import logger
+from kaskade.config import Config
+from kaskade.kafka.cluster_service import ClusterService
+from kaskade.kafka.models import Topic
+from kaskade.kafka.topic_service import TopicService
+from kaskade.utils.circular_list import CircularList
+from kaskade.widgets.consumer_mode import ConsumerMode
+from kaskade.widgets.describer_mode import DescriberMode
+from kaskade.widgets.error import Error
+from kaskade.widgets.footer import Footer
+from kaskade.widgets.header import Header
+from kaskade.widgets.help import Help
+from kaskade.widgets.topic_header import TopicHeader
+from kaskade.widgets.topic_list import TopicList
+
+
+class Tui(App):
+    __topic: Optional[Topic] = None
+    show_help = Reactive(False)
+    error = Reactive("")
+    background_lock: Optional[Event] = None
+
+    def __init__(
+        self,
+        config: Config,
+        screen: bool = True,
+        driver_class: Optional[Type[Driver]] = None,
+        log_verbosity: int = 1,
+    ) -> None:
+        super().__init__(
+            screen=screen,
+            driver_class=driver_class,
+            log_verbosity=log_verbosity,
+        )
+        self.config = config
+
+        self.topic_service = TopicService(self.config)
+        self.cluster_service = ClusterService(self.config)
+
+        self.cluster = self.cluster_service.current()
+        self.topics = self.topic_service.list()
+
+        self.footer_widget = Footer()
+        self.header_widget = Header()
+
+        self.help_widget = Help()
+        self.error_widget = Error()
+
+        self.topic_list_widget = TopicList()
+        self.topic_header_widget = TopicHeader()
+
+        self.describer_mode_widget = DescriberMode()
+        self.consumer_mode_widget = ConsumerMode()
+
+        self.focusables = CircularList(
+            [
+                self.topic_list_widget,
+                self.describer_mode_widget,
+                self.consumer_mode_widget,
+            ]
+        )
+
+    def background_execution(self, refresh_rate: float) -> None:
+        self.background_lock = Event()
+        while self.is_running and not self.background_lock.wait(refresh_rate):
+            logger.debug("Background thread is running")
+            self.reload_content()
+        logger.debug("Closing background thread")
+
+    def log(self, *args: Any, verbosity: int = 1, **kwargs: Any) -> None:
+        if verbosity > self.log_verbosity:
+            return
+
+        message = " ".join(str(arg) for arg in args)
+        if kwargs:
+            key_values = " ".join(f"{key}={value}" for key, value in kwargs.items())
+            message = " ".join((message, key_values))
+
+        logger.debug(message)
+
+    async def on_mount(self) -> None:
+        self.help_widget.visible = False
+        self.error_widget.visible = False
+        self.consumer_mode_widget.visible = False
+
+        await self.view.dock(self.header_widget, edge="top")
+        await self.view.dock(self.footer_widget, edge="bottom")
+        await self.view.dock(self.topic_list_widget, edge="left", size=40)
+        await self.view.dock(
+            self.topic_header_widget,
+            self.describer_mode_widget,
+            self.consumer_mode_widget,
+            edge="top",
+            size=1000,
+        )
+        await self.view.dock(self.error_widget, edge="right", size=40, z=1)
+        await self.view.dock(self.help_widget, edge="right", size=40, z=1)
+
+        refresh_config = self.config.kaskade.get("refresh")
+
+        if refresh_config is None:
+            refresh = True
+        else:
+            refresh = bool(refresh_config)
+
+        if refresh:
+            refresh_rate_config = self.config.kaskade.get("refresh-rate")
+
+            if refresh_rate_config is None:
+                refresh_rate = 5.0
+            else:
+                refresh_rate = float(refresh_rate_config)
+
+            logger.debug("Refresh enable with %.1f secs", refresh_rate)
+            background_thread = Thread(
+                target=self.background_execution, args=(refresh_rate,)
+            )
+            background_thread.start()
+            self.set_interval(refresh_rate, self.topic_list_widget.refresh)
+            self.set_interval(refresh_rate, self.topic_header_widget.refresh)
+            self.set_interval(refresh_rate, self.describer_mode_widget.refresh)
+        else:
+            logger.debug("Auto-refresh disabled")
+
+    async def on_load(self) -> None:
+        await self.bind(Keys.ControlC, "quit")
+        await self.bind(Keys.ControlR, "toggle_consumer_mode")
+        await self.bind(Keys.ControlD, "toggle_describer_mode")
+        await self.bind("?", "toggle_help")
+        await self.bind(Keys.Escape, "back")
+        await self.bind(Keys.F5, "reload_content")
+        await self.bind(Keys.Left, "change_focus('{}')".format(Keys.Left))
+        await self.bind(Keys.Right, "change_focus('{}')".format(Keys.Right))
+
+    async def action_toggle_consumer_mode(self) -> None:
+        if not self.topic_list_widget.has_focus:
+            self.focusables.current = self.consumer_mode_widget
+            await self.set_focus(self.consumer_mode_widget)
+            self.view.refresh()
+        self.enable_consumer_mode()
+
+    def enable_consumer_mode(self) -> None:
+        self.footer_widget.mode = "consumer"
+        self.consumer_mode_widget.visible = True
+        self.describer_mode_widget.visible = False
+        self.consumer_mode_widget.refresh()
+        self.consumer_mode_widget.consume_topic()
+        self.consumer_mode_widget.load_messages()
+
+    async def action_toggle_describer_mode(self) -> None:
+        if not self.topic_list_widget.has_focus:
+            self.focusables.current = self.describer_mode_widget
+            await self.set_focus(self.describer_mode_widget)
+            self.view.refresh()
+        self.enable_describer_mode()
+
+    def enable_describer_mode(self) -> None:
+        self.footer_widget.mode = "describer"
+        self.consumer_mode_widget.visible = False
+        self.describer_mode_widget.visible = True
+        self.describer_mode_widget.reset()
+        self.describer_mode_widget.refresh()
+
+    async def action_quit(self) -> None:
+        if self.background_lock is not None:
+            logger.debug("Shutdown received")
+            self.background_lock.set()
+        await self.shutdown()
+
+    async def shutdown(self) -> None:
+        driver = self._driver
+        if driver is None:
+            raise Exception("Driver is None")
+        driver.disable_input()
+        await self.close_messages()
+
+    async def watch_error(self, error: str) -> None:
+        show_error = bool(error)
+        self.error_widget.message = error
+        self.error_widget.visible = show_error
+
+    async def watch_show_help(self, show_help: bool) -> None:
+        self.help_widget.visible = show_help
+
+    async def action_toggle_help(self) -> None:
+        self.show_help = not self.show_help
+
+    async def action_back(self) -> None:
+        self.show_help = False
+        self.error = ""
+
+    def handle_exception(self, exception: Exception) -> None:
+        message = str(exception)
+
+        if isinstance(exception, KafkaException):
+            message = exception.args[0].str()
+
+        self.error = message
+
+        logger.error("Error in runtime: %s", message)
+        logger.exception(exception)
+
+    async def action_reload_content(self) -> None:
+        self.reload_content()
+        self.topic_list_widget.refresh()
+        self.topic_header_widget.refresh()
+        self.describer_mode_widget.refresh()
+
+    def reload_content(self) -> None:
+        selected_topic: Optional[Topic] = None
+
+        if self.topic is not None:
+            selected_topic = self.topic
+
+        logger.debug("Reloading content - started")
+        try:
+            self.topics = self.topic_service.list()
+        except Exception as ex:
+            self.topics = []
+            self.handle_exception(ex)
+        logger.debug("Reloading content - finished")
+
+        try:
+            if self.topic is not None:
+                self.topic = self.topics[self.topics.index(self.topic)]
+        except Exception as ex:
+            self.error = f"Selected topic [yellow bold]{selected_topic}[/] not found"
+            logger.exception(ex)
+            self.topic = None
+
+    @property
+    def topic(self) -> Optional[Topic]:
+        return self.__topic
+
+    @topic.setter
+    def topic(self, topic: Optional[Topic]) -> None:
+        self.__topic = topic
+        self.describer_mode_widget.table = None
+        self.describer_mode_widget.refresh()
+        self.topic_header_widget.refresh()
+
+    async def action_change_focus(self, key: Keys) -> None:
+        focused: Widget = (
+            self.focusables.next() if Keys.Right == key else self.focusables.previous()
+        )
+        if focused.visible:
+            await focused.focus()
+        else:
+            await self.action_change_focus(key)
