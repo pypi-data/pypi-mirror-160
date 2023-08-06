4ab7100000773556b5c5d0ba26ca38a90b8123b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:03:38 +0000

    Add package seams/0.1.68/seams-0.1.68.tar.gz

---
+++ packages/seams/seams-0.1.68.tar.gz/seams-0.1.68/seams/exceptions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from seams.severity import Severity
+class SeamsException(Exception):
+    '''
+    An exception raised when the SDK is used incorrectly
+    '''
+
+    @property
+    def message(self):
+        return self.__dict__.get('message', None) or getattr(self, 'args')[0]
+
+
+class SeamsAPIException(Exception):
+    '''
+    An exception raised when the API response is an error
+    '''
+
+    @property
+    def message(self):
+        return self.__dict__.get('message', None) or getattr(self, 'args')[0]
