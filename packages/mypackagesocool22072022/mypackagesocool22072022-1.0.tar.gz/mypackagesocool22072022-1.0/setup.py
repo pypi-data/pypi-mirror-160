e16fbde96e6c757396ad7d691e9c89c8c84a45cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:40 +0000

    Add package mypackagesocool22072022/1.0/mypackagesocool22072022-1.0.tar.gz

---
+++ packages/mypackagesocool22072022/mypackagesocool22072022-1.0.tar.gz/mypackagesocool22072022-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_namespace_packages, find_packages
+
+setup(
+    name="mypackagesocool22072022",
+    version="1.0",
+    description='this package really cool',
+    packages=find_namespace_packages(include=['russkikorablidinahui*']) + find_packages()
+)
