bcaaca5f2d552f414c1c67328d625ed3e2cc3f7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:22 +0000

    Add package MegaAI/1.0.0/MegaAI-1.0.0.tar.gz

---
+++ packages/MegaAI/MegaAI-1.0.0.tar.gz/MegaAI-1.0.0/README.rst
@@ -0,0 +1,15 @@
+MegaAI
+======
+
+Installing
+~~~~~~~~~~
+
+**Python 3.9 or higher is required**
+
+.. code:: sh
+
+    # Linux/macOS
+    pip3 install -U mariocard.py
+
+    # Windows
+    pip install -U mariocard.py
---
+++ packages/MegaAI/MegaAI-1.0.0.tar.gz/MegaAI-1.0.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MegaAI',
+    version='1.0.0',
+    description='',
+    long_description=open('README.rst').read(),
+    url='',
+    author='Lubix88',
+    author_email='',
+    license='MIT',
+    keywords='MegaAI',
+    packages=find_packages(),
+    install_requires=['tensorflow']
+)
