77817d735e01b6a3eaf13ed6a1d1bbdc0d0e4b5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:10 +0000

    Add package getdeck/0.8.4/getdeck-0.8.4-py3-none-any.whl

---
+++ packages/getdeck/getdeck-0.8.4-py3-none-any.whl/getdeck/provider/types.py
@@ -0,0 +1,15 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic.main import BaseModel
+
+
+class K8sProviderType(Enum):
+    k3d = "k3d"
+    kubectlctx = "KubectlCtx"
+    kind = "kind"
+
+
+class K8sProviderData(BaseModel):
+    id: str
+    name: Optional[str] = None
