fb9f0d721f8cd1db115ed9bbc6985c73ca4521c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:18 +0000

    Add package random-utilites/1.0.1/random_utilites-1.0.1-py3-none-any.whl

---
+++ packages/random-utilites/random_utilites-1.0.1-py3-none-any.whl/random_utilities/__init__.py
@@ -0,0 +1,70 @@
+import os
+import random
+from console import fx, fg
+import flask
+
+def log(msg: str, is_error=False, is_success=False):
+    """ Logs verbose information about runtime. """
+    if bool(os.environ.get("VERBOSE", False)) == True:
+        msg = fx.bold(str(msg))
+        print(f'{fx.dim("* |")} [{fg.red(msg) if is_error else (fg.white(msg) if not is_success else fg.green(msg))}]')
+log("Verbose mode: " + os.environ.get("VERBOSE", "False"), "True")
+
+
+def query_to_dict(query_string: str) -> dict:
+    """ Convert a URL query string to a dictionary """
+    query_string_split_pairs = query_string.split("&")
+    dict_query_string = { }
+    for query in query_string_split_pairs:
+        query_split = query.split("=") # name=value === ["name", "value"]
+        dict_query_string[query_split[0]] = query_split[1]
+        if query_split[1] in ("true", "false"):
+            dict_query_string[query_split[0]] = bool(query_split[1]) # Parse boolean values
+        elif query_split[1].isdecimal():
+            dict_query_string[query_split[0]] = float(query_split[1])
+    return dict_query_string
+
+
+def random_sort(_list: list) -> list:
+    """ Returns a randomly sorted list. """
+    random_sorted_indices = []
+    for _ in _list:
+        b = len(_list) - 1
+        index = random.randint(a=0, b=b)
+        while index in random_sorted_indices:
+            index = random.randint(a=0, b=b)
+            if index not in random_sorted_indices:
+                break
+        random_sorted_indices.append(index)
+    
+    """ Re-structure the list with the randomness. """
+    random_list = [0 for i in range(len(_list))]
+    for index, random_index in enumerate(random_sorted_indices):
+        random_list[index] = _list[random_index]
+    return random_list
+    
+
+def read_request_body() -> dict:
+    """Reads the binary data sent in the request."""
+    request_data = flask.request.get_data()
+
+    """Proof check if the data is readable as normal text or not."""
+    if request_data != b'' or request_data.isascii():
+        request_body = flask.request.get_json()
+        if request_body != None:
+            return request_body
+        else:
+            """Else return an empty dictionary."""
+            return dict()
+    else:
+        """Else return an empty dictionary."""
+        return dict()
+
+
+# Checks if a dictionary contains all fields in a provided array, returns missing fields if any
+def fields_all_check(dct: dict, lst: list) -> tuple:
+    for field in dct:
+        if field in lst:
+            lst.remove(field)
+    is_all_check = True if len(lst) == 0 else False
+    return is_all_check, lst
