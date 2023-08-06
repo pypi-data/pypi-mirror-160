03a674ccbca6e315dcd8eabe5ff6988b2aecabd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:07 +0000

    Add package nft-market/1.28/nft_market-1.28.tar.gz

---
+++ packages/nft-market/nft_market-1.28.tar.gz/nft_market-1.28/nft_market/market.py
@@ -0,0 +1,28 @@
+from enum import Enum, auto
+
+
+class Market(Enum):
+    OpenSea = auto()
+    tofuNFT = auto()
+    PancakeSwap = auto()
+    Rarible = auto()
+    GhostMarket = auto()
+    Cryptocom = auto()
+    Gem = auto()
+    NFTrade = auto()
+    Solanart = auto()
+    MagicEden = auto()
+    XANALIA = auto()
+    Coinbase = auto()
+    NiftyGateway = auto()
+    YUMI = auto()
+    # deprecated
+    Entrepot = auto()
+    CetoSwap = auto()
+    CCC = auto()
+    Jelly = auto()
+
+
+class Explorer(Enum):
+    NFTgeek = auto()
+    ICScan = auto()
