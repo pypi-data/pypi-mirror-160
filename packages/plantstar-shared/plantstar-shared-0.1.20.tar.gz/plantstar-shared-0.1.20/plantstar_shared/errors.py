b9ad0a640dc4a057b12824c5c50638347f82a646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:44:04 +0000

    Add package plantstar-shared/0.1.20/plantstar-shared-0.1.20.tar.gz

---
+++ packages/plantstar-shared/plantstar-shared-0.1.20.tar.gz/plantstar-shared-0.1.20/plantstar_shared/errors.py
@@ -0,0 +1,10 @@
+class InvalidApiRequest(Exception):
+    pass
+
+
+class SysconProgrammingError(Exception):
+    pass
+
+    @staticmethod
+    def get_invalid_type_error(the_type, invalid_type_supplied):
+        return SysconProgrammingError(f"Invalid \"{the_type.__name__}\" type: \"{invalid_type_supplied}\"")
