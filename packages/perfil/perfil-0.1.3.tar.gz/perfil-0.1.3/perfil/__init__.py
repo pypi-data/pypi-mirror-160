a89f96041653f29766eadef710a700f292b41bbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:35 +0000

    Add package perfil/0.1.3/perfil-0.1.3.tar.gz

---
+++ packages/perfil/perfil-0.1.3.tar.gz/perfil-0.1.3/perfil/__init__.py
@@ -0,0 +1,5 @@
+__version__ = "0.1.3"
+
+from perfil.profiling import TimeSubject
+from perfil.timing import Timers
+from perfil.info import FunctionInfo, SubjectInfo
---
+++ packages/perfil/perfil-0.1.3.tar.gz/perfil-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name="perfil",
+    version="0.1.3",
+    description="Profile Python functions.",
+    url="https://github.com/JCPedroza/perfil",
+    author="JCPedroza",
+    author_email="<nemesis4go10@hotmail.com>",
+    license="MIT",
+    packages=["perfil"],
+    install_requires=[
+        "setuptools",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+)
