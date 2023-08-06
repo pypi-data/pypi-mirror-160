de1d58697244f4b6e9d1abbfccba137fc5ca90b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:40 +0000

    Add package aiooss2/0.1.2/aiooss2-0.1.2-py2.py3-none-any.whl

---
+++ packages/aiooss2/aiooss2-0.1.2-py2.py3-none-any.whl/aiooss2/__init__.py
@@ -0,0 +1,9 @@
+"""
+Imports from this file follows the contents in `oss2``
+"""
+
+# flake8: noqa
+from oss2 import AnonymousAuth, Auth, StsAuth
+
+from .api import AioBucket, AioService
+from .iterators import AioObjectIterator
