dc8af792cfe8d80ff3511b8b56f21d840fdf0cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:26:25 +0000

    Add package types-aioboto3-lite/9.6.0.post2/types-aioboto3-lite-9.6.0.post2.tar.gz

---
+++ packages/types-aioboto3-lite/types-aioboto3-lite-9.6.0.post2.tar.gz/types-aioboto3-lite-9.6.0.post2/aioboto3-stubs/__main__.py
@@ -0,0 +1,37 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for boto3 9.6.0\n"
+        "Version:         9.6.0.post2\n"
+        "Builder version: 7.10.0\n"
+        "Docs:            https://pypi.org/project/types-aioboto3-lite/\n"
+        "Changelog:       https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("9.6.0.post2")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
