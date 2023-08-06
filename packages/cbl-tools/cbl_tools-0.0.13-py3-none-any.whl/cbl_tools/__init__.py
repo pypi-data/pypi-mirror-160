05c0b7e3b179b82e61c53de00c3e405ff1a75fa0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:54:00 +0000

    Add package cbl-tools/0.0.13/cbl_tools-0.0.13-py3-none-any.whl

---
+++ packages/cbl-tools/cbl_tools-0.0.13-py3-none-any.whl/cbl_tools/__init__.py
@@ -0,0 +1,5 @@
+# info@codebasedlearning.dev, a.voss@fh-aachen.de
+
+#import os
+#import sys
+#sys.path.append(os.path.dirname(os.path.realpath(__file__)))  # enables relative local imports
---
+++ packages/cbl-tools/cbl_tools-0.0.13-py3-none-any.whl/cbl_tools/semver/__version__.py
@@ -0,0 +1,4 @@
+# info@codebasedlearning.dev, a.voss@fh-aachen.de
+
+__version__ = "0.0.13"  # package version, also dynamically used by pyproject.toml
+
