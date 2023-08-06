c0eba97fad4db1c3693aef09bcd8a96442fd235c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:50:41 +0000

    Add package cbl-tools/0.0.7/cbl_tools-0.0.7.tar.gz

---
+++ packages/cbl-tools/cbl_tools-0.0.7.tar.gz/cbl_tools-0.0.7/src/cbl_tools/tools.py
@@ -0,0 +1,37 @@
+# a.voss@fh-aachen.de
+
+#import __version__
+#from __version__ import __version__
+
+import version.__version__
+
+
+def version_n(n: int) -> str:
+    return f"{version.__version__.__version__}-{n}"
+
+
+if __name__ == "__main__":
+    print(f"Version: {version_n(25)}")
+
+
+"""
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+https://choosealicense.com/licenses/mit/
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cbl_test_package==1.2.3
+
+
+python3 -m build  
+python3 -m twine upload --repository testpypi dist/* 
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cbl_test_package@1.2.3 
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+python3 -m build  
+python3 -m twine upload dist/* 
+python3 -m pip install cbl_test_package==1.2.3 
+pip install cbl-test-package==1.2.3
+pip install cbl-test-package==2.3.4
+
+pip install cbl_tools
+"""
