aa85eff91f64967b146f4d2dc6027b4e2830cfff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:55 +0000

    Add package perfil/0.0.0/perfil-0.0.0.tar.gz

---
+++ packages/perfil/perfil-0.0.0.tar.gz/perfil-0.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="perfil",
+    description="Profile Python functions.",
+    url="https://github.com/JCPedroza/perfil",
+    author="JCPedroza",
+    author_email="<nemesis4go10@hotmail.com>",
+    license="MIT",
+    packages=["perfil"],
+    install_requires=[
+        "setuptools",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+    ],
+)
