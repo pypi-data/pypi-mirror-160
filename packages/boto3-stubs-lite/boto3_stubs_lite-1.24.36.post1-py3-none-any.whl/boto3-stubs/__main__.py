f23ee536a3a0f3b4a2b89d6fa46b556f55c17828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:56 +0000

    Add package boto3-stubs-lite/1.24.36.post1/boto3_stubs_lite-1.24.36.post1-py3-none-any.whl

---
+++ packages/boto3-stubs-lite/boto3_stubs_lite-1.24.36.post1-py3-none-any.whl/boto3-stubs/__main__.py
@@ -0,0 +1,37 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for boto3 1.24.36\n"
+        "Version:         1.24.36.post1\n"
+        "Builder version: 7.10.0\n"
+        "Docs:            https://pypi.org/project/boto3-stubs-lite/\n"
+        "Changelog:       https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("1.24.36.post1")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
