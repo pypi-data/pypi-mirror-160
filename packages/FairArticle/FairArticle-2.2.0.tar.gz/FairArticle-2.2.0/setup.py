5c0cb4882c2e63b52368fa382f55085b83bf73ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:37 +0000

    Add package FairArticle/2.2.0/FairArticle-2.2.0.tar.gz

---
+++ packages/FairArticle/FairArticle-2.2.0.tar.gz/FairArticle-2.2.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import os
+
+current = os.getcwd()
+
+setup(
+    name='FairArticle',
+    version='2.2.0',
+    description='Article Provider for Sources, Categorizer, Utils and more.',
+    url='https://github.com/chazzcoin/FairArticle',
+    author='ChazzCoin',
+    author_email='chazzcoin@gmail.com',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    package_data={
+        'ArticleResources': ['*.txt', '*.csv']
+    },
+    install_requires=['fairweb>=3.1.1', 'fairresources>=1.0.0'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ]
+)
