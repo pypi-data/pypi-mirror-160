dafc282c7e6d8eeacc83ad3f9435d2c45fe7b2cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:32 +0000

    Add package statistica123/0.2/statistica123-0.2.tar.gz

---
+++ packages/statistica123/statistica123-0.2.tar.gz/statistica123-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup (name="statistica123",
+version="0.2",
+description="this is a package",
+long_description="this is a very very long description",
+author = "Abdul Sami",
+packages = ["statistica123"],
+install_packages = []
+)
