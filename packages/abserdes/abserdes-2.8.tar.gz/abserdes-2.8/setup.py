79a573b2f4b78012763091b3217152c99a01a190
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:46 +0000

    Add package abserdes/2.8/abserdes-2.8.tar.gz

---
+++ packages/abserdes/abserdes-2.8.tar.gz/abserdes-2.8/setup.py
@@ -0,0 +1,29 @@
+"""
+abserdes
+A Basic Serializer and Deserializer that makes XML serialization and
+deserialization in Python absurdly easy.
+"""
+
+import sys
+import os
+from setuptools import setup, find_packages, Extension
+
+
+setup(
+    name='abserdes',
+    author='Andy Stokely',
+    author_email='amstokely@ucsd.edu',
+    license='MIT',
+    description="A Basic Serializer and Deserializer that makes XML"
+        + "serialization and deserialization in Python absurdly easy.",
+    keywords='xml, serialize, deserialize, serializer',
+    url='https://github.com/astokely/abserdes',
+    packages=find_packages(),
+    install_requires=[
+		"numpy", "pytest", 
+		"data-science-types", "cython"
+	],              
+    platforms=['Linux',
+                'Unix',],
+    python_requires=">=3.8",          
+)
