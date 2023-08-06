8da16428a8b19cc848760e41080135d53d8078e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:39:51 +0000

    Add package explainaboard/0.10.8/explainaboard-0.10.8-py2.py3-none-any.whl

---
+++ packages/explainaboard/explainaboard-0.10.8-py2.py3-none-any.whl/explainaboard/loaders/__init__.py
@@ -0,0 +1,41 @@
+from explainaboard.loaders import (
+    aspect_based_sentiment_classification,
+    cloze_generative,
+    cloze_multiple_choice,
+    conditional_generation,
+    extractive_qa,
+    file_loader,
+    grammatical_error_correction,
+    kg_link_tail_prediction,
+    language_modeling,
+    loader,
+    loader_registry,
+    qa_multiple_choice,
+    sequence_labeling,
+    tabular_classification,
+    tabular_regression,
+    text_classification,
+    text_pair_classification,
+)
+
+get_datalab_loader = loader_registry.get_datalab_loader
+get_custom_dataset_loader = loader_registry.get_custom_dataset_loader
+DatalabLoaderOption = file_loader.DatalabLoaderOption
+
+__all__ = [
+    'aspect_based_sentiment_classification',
+    'cloze_multiple_choice',
+    'conditional_generation',
+    'extractive_qa',
+    'kg_link_tail_prediction',
+    'language_modeling',
+    'loader',
+    'sequence_labeling',
+    'qa_multiple_choice',
+    'tabular_classification',
+    'tabular_regression',
+    'text_classification',
+    'text_pair_classification',
+    'cloze_generative',
+    'grammatical_error_correction',
+]
