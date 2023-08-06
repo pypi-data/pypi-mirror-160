0fb383c29ea5e92d15eb6c71ca98965c57095ffe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:40 +0000

    Add package bifeatureanalysis/1.0.2/bifeatureanalysis-1.0.2.tar.gz

---
+++ packages/bifeatureanalysis/bifeatureanalysis-1.0.2.tar.gz/bifeatureanalysis-1.0.2/setup.py
@@ -0,0 +1,39 @@
+'''
+Author:  BDFD
+Date: 2021-10-27 18:39:19
+LastEditTime: 2022-07-26 11:00:44
+LastEditors: BDFD
+Description: In User Settings Edit
+FilePath: \Section5.3-PyPi_Feature_Engineering\setup.py
+'''
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '1.0.2'
+DESCRIPTION = 'common usage in feature engineering analysis, make analysis more easily'
+PACKAGE_NAME = 'bifeatureanalysis'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author="BDFD",
+    author_email="bdfd2005@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdfd",
+    project_urls={
+        "Bug Tracker": "https://github.com/bdfd",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
