2ef8853c5c3f1b74166b3b5831b8729f94a58aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:46 +0000

    Add package TRDstest/0.1.2/TRDstest-0.1.2.tar.gz

---
+++ packages/TRDstest/TRDstest-0.1.2.tar.gz/TRDstest-0.1.2/trdstest/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+from decomposition import TRSVD
+from decomposition import TRALS
+from decomposition import TRALSAR
+
+
+
+T = np.random.rand(3,4,5,6)
+ep = 1e-5
+tau = 1e-4
+epoch = 20
+
+node1, r1 = TRSVD(T, ep=1e-6)
+node2, r2 = TRALS(T, tol=1e-6)
+node3, r3 = TRALSAR(T, tol=1e-6)
