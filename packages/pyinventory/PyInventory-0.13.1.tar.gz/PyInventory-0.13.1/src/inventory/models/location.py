dc2f743a8a26e9d9e4c064331b36ff908ecbccae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:09 +0000

    Add package pyinventory/0.13.1/PyInventory-0.13.1.tar.gz

---
+++ packages/pyinventory/PyInventory-0.13.1.tar.gz/PyInventory-0.13.1/src/inventory/models/location.py
@@ -0,0 +1,35 @@
+from ckeditor_uploader.fields import RichTextUploadingField
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_tools.model_version_protect.models import VersionProtectBaseModel
+
+from inventory.models.base import BaseModel
+
+
+class LocationModel(BaseModel, VersionProtectBaseModel):
+    """
+    A Storage for items.
+    """
+    description = RichTextUploadingField(
+        blank=True, null=True,
+        config_name='LocationModel.description',
+        verbose_name=_('LocationModel.description.verbose_name'),
+        help_text=_('LocationModel.description.help_text')
+    )
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        blank=True, null=True,
+        verbose_name=_('LocationModel.parent.verbose_name'),
+        help_text=_('LocationModel.parent.help_text')
+    )
+
+    def __str__(self):
+        if self.parent_id is None:
+            return self.name
+        else:
+            return f'{self.name} › {self.parent}'
+
+    class Meta:
+        verbose_name = _('LocationModel.verbose_name')
+        verbose_name_plural = _('LocationModel.verbose_name_plural')
