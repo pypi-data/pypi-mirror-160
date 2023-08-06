aad52b40a31931ec9a6c86c32b4ebc33d9f9351e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:21 +0000

    Add package py-xlsx-textconv/0.0.5/py-xlsx-textconv-0.0.5.tar.gz

---
+++ packages/py-xlsx-textconv/py-xlsx-textconv-0.0.5.tar.gz/py-xlsx-textconv-0.0.5/py_xlsx_textconv/__main__.py
@@ -0,0 +1,22 @@
+import argparse
+import os
+from . import __version__
+from .convert import convert
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='excel filename')
+    parser.add_argument('-v',
+                        '--version',
+                        action='version',
+                        version=__version__)
+    args = parser.parse_args()
+
+    filename = args.filename
+    if os.path.isfile(filename):
+        convert(filename)
+
+
+if __name__ == '__main__':
+    main()
