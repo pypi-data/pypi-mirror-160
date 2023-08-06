d92128a8e5312bfd2b4599e371f80ee700dcd96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:41 +0000

    Add package sambhawithawa/0.1/sambhawithawa-0.1.tar.gz

---
+++ packages/sambhawithawa/sambhawithawa-0.1.tar.gz/sambhawithawa-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='sambhawithawa',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['sambhawithawa'],
+      author = 'Hasarindu Perera',
+      zip_safe=False)
