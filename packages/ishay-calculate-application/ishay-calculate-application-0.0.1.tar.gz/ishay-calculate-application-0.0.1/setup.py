615ea47d12e1e7bd704d3476d6e642c3144a1ddf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:17 +0000

    Add package ishay-calculate-application/0.0.1/ishay-calculate-application-0.0.1.tar.gz

---
+++ packages/ishay-calculate-application/ishay-calculate-application-0.0.1.tar.gz/ishay-calculate-application-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="ishay-calculate-application",
+    version="0.0.1",
+    description="A sample Python project for calculations",
+    url="https://github.com/naturalett/getting-started",
+    install_requires=['xmlrunner'],
+    packages=['calculate']
+)
