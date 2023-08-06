89e4c4a5b1cef9816ba750c0b1fc9f7168561e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:57 +0000

    Add package ernest-nkunzimana-distributions/0.1/ernest_nkunzimana_distributions-0.1.tar.gz

---
+++ packages/ernest-nkunzimana-distributions/ernest_nkunzimana_distributions-0.1.tar.gz/ernest_nkunzimana_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='ernest_nkunzimana_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['ernest_nkunzimana_distributions'],
+      zip_safe=False)
