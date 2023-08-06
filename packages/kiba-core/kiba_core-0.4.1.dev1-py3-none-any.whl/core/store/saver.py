c29a49c14519a2da5a06d374d4c742cf0e5bd0fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:13 +0000

    Add package kiba-core/0.4.1.dev1/kiba_core-0.4.1.dev1-py3-none-any.whl

---
+++ packages/kiba-core/kiba_core-0.4.1.dev1-py3-none-any.whl/core/store/saver.py
@@ -0,0 +1,34 @@
+import contextlib
+from typing import Optional
+
+import asyncpg
+from sqlalchemy.sql import ClauseElement
+
+from core import logging
+from core.exceptions import DuplicateValueException
+from core.exceptions import InternalServerErrorException
+from core.store.database import Database
+from core.store.database import DatabaseConnection
+
+
+class Saver:
+
+    def __init__(self, database: Database):
+        self.database = database
+
+    @contextlib.asynccontextmanager
+    async def create_transaction(self):
+        async with self.database.create_transaction() as connection:
+            yield connection
+
+    async def _execute(self, query: ClauseElement, connection: Optional[DatabaseConnection] = None):
+        try:
+            if connection:
+                return await self.database.execute(query=query, connection=connection)
+            async with self.create_transaction() as connection:
+                return await self.database.execute(query=query, connection=connection)
+        except asyncpg.exceptions.UniqueViolationError as exception:
+            raise DuplicateValueException(message=str(exception)) from exception
+        except Exception as exception:
+            logging.error(exception)
+            raise InternalServerErrorException(message='Error running save operation') from exception
