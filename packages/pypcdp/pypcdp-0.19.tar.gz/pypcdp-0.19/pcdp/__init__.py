2ed9073792739a02f8d6303fe94e3b5379a1856f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:39 +0000

    Add package pypcdp/0.19/pypcdp-0.19.tar.gz

---
+++ packages/pypcdp/pypcdp-0.19.tar.gz/pypcdp-0.19/pcdp/__init__.py
@@ -0,0 +1,3 @@
+from .RDFM import RDFM, GenCtypesStruct, OPERATOR, ParsingStruct
+from .ALARM import Log
+from .utils import filetime_to_str, str_to_filetime, dump_to_dict
---
+++ packages/pypcdp/pypcdp-0.19.tar.gz/pypcdp-0.19/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+setuptools.setup(
+    name='pypcdp',
+    version='0.19',
+    author='xgzh',
+    author_email="",
+    description="PCDP api",
+    long_description="PCDP Api",
+    #platforms='win32',
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.8',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ]
+
+)
