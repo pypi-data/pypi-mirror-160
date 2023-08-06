2beb8672a3d76fec4cb94c6987fc570b152bc7da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:29 +0000

    Add package sqlp-kernel/0.1.4/sqlp_kernel-0.1.4.tar.gz

---
+++ packages/sqlp-kernel/sqlp_kernel-0.1.4.tar.gz/sqlp_kernel-0.1.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name="sqlp_kernel",
+    version="0.1.4",
+    description="Postgres Kernel for Jupyter Notebook",
+    long_description=open("README.txt").read(),
+    long_description_content_type="text/plain",
+    url="https://github.com/mtatton/sqlok",
+    author="Michael Tatton",
+    license="",
+    packages=["sqlp_kernel"],
+    install_requires=[
+        "tabulate",
+        "psycopg2",
+    ],
+    scripts={"sqlp_kernel/sqlpk_install"},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
