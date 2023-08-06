7fb3b75fdbf33f4cfa4945915aa9a5ab50f2685f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:38 +0000

    Add package babbab/0.1.1/babbab-0.1.1-py2.py3-none-any.whl

---
+++ packages/babbab/babbab-0.1.1-py2.py3-none-any.whl/babbab/__init__.py
@@ -0,0 +1,6 @@
+"""BAyesian Beta-Binomial AB testing (BABBAB), is an implementation of the Beta-Binomial model with some fancy visualizations on top of Pymc"""
+
+__version__ = "0.1.1"
+
+
+from .core import abtest_plot
