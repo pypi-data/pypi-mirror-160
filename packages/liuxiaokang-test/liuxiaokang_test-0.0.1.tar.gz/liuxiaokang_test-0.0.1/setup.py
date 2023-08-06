a2f9cb89d51ca09e7ad3b8c8cc55a1c33f686104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:02 +0000

    Add package liuxiaokang-test/0.0.1/liuxiaokang_test-0.0.1.tar.gz

---
+++ packages/liuxiaokang-test/liuxiaokang_test-0.0.1.tar.gz/liuxiaokang_test-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name='liuxiaokang_test',
+      version='0.0.1',
+      description='this is a test packages',
+      author='liuxiaokang',
+      author_email='1099301992@qq.com',
+      packages=['liuxiaokang_test'],
+
+   )
