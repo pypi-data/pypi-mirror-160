e604d5952daa7a909a5206af20969c6995069ca2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:51 +0000

    Add package seaflow/0.0.1b6/seaflow-0.0.1b6.tar.gz

---
+++ packages/seaflow/seaflow-0.0.1b6.tar.gz/seaflow-0.0.1b6/examples/main.py
@@ -0,0 +1,19 @@
+import os
+import sys
+sys.path.append('/Users/chenjian/repos/github.com/seaflow')
+import django
+from celery import Celery
+import seaflow
+
+
+# django settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+django.setup()
+
+# celery app definition
+app = Celery()
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# set celery app for seaflow
+seaflow.set_celery_app(app)
+seaflow.autodiscover_actions(packages=['examples'], related_name='actions', force=True)
