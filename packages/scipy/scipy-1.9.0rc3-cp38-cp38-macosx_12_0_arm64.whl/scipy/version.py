7cdbfb4286dfded5e06d26ce7dbf4c902646443b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:27:22 +0000

    Add package scipy/1.9.0rc3/scipy-1.9.0rc3-cp38-cp38-macosx_12_0_arm64.whl

---
+++ packages/scipy/scipy-1.9.0rc3-cp38-cp38-macosx_12_0_arm64.whl/scipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE SCIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.9.0rc3'
+version = '1.9.0rc3'
+full_version = '1.9.0rc3'
+git_revision = 'c4c9348'
+commit_count = '2303'
+release = True
+
+if not release:
+    version = full_version
