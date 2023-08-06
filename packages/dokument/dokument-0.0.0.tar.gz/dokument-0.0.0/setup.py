5c26033131711d2483125087b68038c95ecd22b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:15 +0000

    Add package dokument/0.0.0/dokument-0.0.0.tar.gz

---
+++ packages/dokument/dokument-0.0.0.tar.gz/dokument-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="dokument",
+    version="0.0.0",
+    author="rdbende",
+    author_email="rdbende@gmail.com",
+    license="MIT license",
+    python_requires=">=3.7",
+    classifiers=[
+        "Development Status :: 1 - Planning",
+    ],
+)
