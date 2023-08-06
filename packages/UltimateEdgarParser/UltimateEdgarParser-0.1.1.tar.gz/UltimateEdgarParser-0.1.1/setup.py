a398f44a7f4a8546bc16c1e22b082233f1f553b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:18 +0000

    Add package UltimateEdgarParser/0.1.1/UltimateEdgarParser-0.1.1.tar.gz

---
+++ packages/UltimateEdgarParser/UltimateEdgarParser-0.1.1.tar.gz/UltimateEdgarParser-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Pulling Edgar 10Q Files'
+long_description = 'Pulling Edgar 10Q Files'
+# Setting up
+setup(
+    name="UltimateEdgarParser",
+    version=VERSION,
+    author="Saeed Shadkam",
+    author_email="<shadkam@ualberta.ca",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pandas'],
+    keywords=['python', 'Finance', 'EDGAR', 'SEC'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
