cf3141f9f1d907d3785ae3369822237ae7f79286
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:45 +0000

    Add package aph/0.1/aph-0.1.tar.gz

---
+++ packages/aph/aph-0.1.tar.gz/aph-0.1/aph/main.py
@@ -0,0 +1,13 @@
+class Test:
+
+    def __init__(self):
+        pass
+
+    def use(self):
+        return ["test1", "test2", "test3"]
+
+
+
+
+
+
---
+++ packages/aph/aph-0.1.tar.gz/aph-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='aph',
+      version='0.1',
+      description='check nexus beeline',
+      packages=['aph'],
+      author_email='alexey.pha@gmail.com',
+      zip_safe=False)
+
