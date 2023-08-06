1f1d6f7ca288183db7fea85a0995501d7c8f54ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:46 +0000

    Add package st-parsetree/0.0.2/st-parsetree-0.0.2.tar.gz

---
+++ packages/st-parsetree/st-parsetree-0.0.2.tar.gz/st-parsetree-0.0.2/parse_tree/frontend/build/precache-manifest.860702dd11d39912206728a266996559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b35892758527fde504ad52fbe569e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4b58ec4035634e54b1c4",
+    "url": "./static/css/main.33d8ae6b.chunk.css"
+  },
+  {
+    "revision": "30466942019d00b33004",
+    "url": "./static/js/2.3558ddb7.chunk.js"
+  },
+  {
+    "revision": "ed343a8f514c3f6dd33f8ad021bb94fa",
+    "url": "./static/js/2.3558ddb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b58ec4035634e54b1c4",
+    "url": "./static/js/main.53bd2766.chunk.js"
+  },
+  {
+    "revision": "645b2152038c93cbe093",
+    "url": "./static/js/runtime-main.79f62daa.js"
+  }
+]);
