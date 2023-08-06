b3832a63e262528d1dbb852c39cc67d831003d69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:34 +0000

    Add package deploifai/0.3.0/deploifai-0.3.0-py3-none-any.whl

---
+++ packages/deploifai/deploifai-0.3.0-py3-none-any.whl/deploifai/project/__init__.py
@@ -0,0 +1,17 @@
+import click
+
+from .create import create
+from .browse import browse
+from .list import list_project
+
+
+@click.group()
+def project():
+    """
+    Initialise, manage, and deploy an ML project
+    """
+
+
+project.add_command(create)
+project.add_command(browse)
+project.add_command(list_project)
