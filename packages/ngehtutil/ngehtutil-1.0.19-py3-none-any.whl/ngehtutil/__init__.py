b4f4b82726f69b55e81c0c2545f494364f7414e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:16 +0000

    Add package ngehtutil/1.0.19/ngehtutil-1.0.19-py3-none-any.whl

---
+++ packages/ngehtutil/ngehtutil-1.0.19-py3-none-any.whl/ngehtutil/__init__.py
@@ -0,0 +1,16 @@
+"""
+Copyright (c) 2022 Center for Astrophysics | Harvard & Smithsonian
+
+This software is licensed for use as described in the LICENSE file in
+the root directory of this distribution.
+
+Originator: Aaron Oppenheimer March 2020
+"""
+from .target import *
+from .source import *
+from .cost import *
+from .campaign import *
+from .station import *
+from .array import *
+from .program import *
+from .version import VERSION
