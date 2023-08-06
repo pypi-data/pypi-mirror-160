74644c60c6fcb90efd8b7b067329cb95e54b0246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:53:27 +0000

    Add package fastapialchemycollector/0.1.13/fastapialchemycollector-0.1.13.tar.gz

---
+++ packages/fastapialchemycollector/fastapialchemycollector-0.1.13.tar.gz/fastapialchemycollector-0.1.13/fastapialchemycollector/__init__.py
@@ -0,0 +1,3 @@
+from .instruments import MetisInstrumentor
+from .instruments import setup
+from .plan_collect_type import PlanCollectType
---
+++ packages/fastapialchemycollector/fastapialchemycollector-0.1.13.tar.gz/fastapialchemycollector-0.1.13/fastapialchemycollector/metis_formater.py
@@ -0,0 +1,6 @@
+class MetisFormatter:
+    def __init__(self, exporter):
+        self.exporter = exporter
+
+    def format(self):
+        return self.data
