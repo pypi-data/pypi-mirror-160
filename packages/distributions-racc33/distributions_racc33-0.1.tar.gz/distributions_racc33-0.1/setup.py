01ebf601fc722e8bfad80d879f0eb7348ee66152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:17 +0000

    Add package distributions-racc33/0.1/distributions_racc33-0.1.tar.gz

---
+++ packages/distributions-racc33/distributions_racc33-0.1.tar.gz/distributions_racc33-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_racc33',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_racc33'],
+      zip_safe=False)
