e0ecaa9a1d0ad18b25e19adc85a2fb58eb6dd435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:19 +0000

    Add package lib310/0.1.16/lib310-0.1.16-py3-none-any.whl

---
+++ packages/lib310/lib310-0.1.16-py3-none-any.whl/lib310/database/_visualize.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import math
+from .charts.bubble_chart import BubbleChart
+
+
+def visualize_all_datasets(df):
+    bubble_chart = BubbleChart(area=[math.log(x) for x in df['num_rows']],
+                               bubble_spacing=0.1)
+    bubble_chart.collapse()
+
+    fig, ax = plt.subplots(subplot_kw=dict(aspect="equal"))
+    bubble_chart.plot(ax, list(df.index), sns.color_palette("Spectral", len(list(df.index))).as_hex())
+    ax.axis("off")
+    ax.relim()
+    ax.autoscale_view()
+    ax.set_title('Dataset (All)')
+
+    plt.show()
+
+
+def visualize_dataset(df, name):
+    fig, ax = plt.subplots()
+    ax.bar(df['table'], df['num_rows'], color=sns.color_palette("Spectral", len(df['table'])).as_hex())
+    ax.axis("on")
+    ax.relim()
+    ax.autoscale_view()
+    ax.set_title('Dataset (' + name + ')')
+    ax.set_yscale('log')
+    plt.show()
