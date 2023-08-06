06f4b09666286147356218ccb884985cc7ec5efc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:13 +0000

    Add package jmtor-probdistributions/0.1/jmtor_probdistributions-0.1.tar.gz

---
+++ packages/jmtor-probdistributions/jmtor_probdistributions-0.1.tar.gz/jmtor_probdistributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='jmtor_probdistributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['jmtor_probdistributions'],
+      zip_safe=False)
