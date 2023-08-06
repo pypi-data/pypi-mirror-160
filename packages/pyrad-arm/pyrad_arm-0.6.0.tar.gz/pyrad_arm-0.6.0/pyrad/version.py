b5c5f5ced809c83f40eb3d7b7ede3c57b98ab545
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package pyrad-arm/0.6.0/pyrad_arm-0.6.0.tar.gz

---
+++ packages/pyrad-arm/pyrad_arm-0.6.0.tar.gz/pyrad_arm-0.6.0/pyrad/version.py
@@ -0,0 +1,12 @@
+
+# THIS FILE IS GENERATED FROM PYRAD_PROC SETUP.PY
+short_version = '0.6.0'
+version = '0.6.0'
+full_version = '0.6.0'
+git_revision = '9980c97309789657bddbc6ebb7d9d1b22b798e36'
+compile_date_time = '2022-07-25 14:41'
+username = 'wolfensb'
+release = True
+
+if not release:
+    version = full_version
