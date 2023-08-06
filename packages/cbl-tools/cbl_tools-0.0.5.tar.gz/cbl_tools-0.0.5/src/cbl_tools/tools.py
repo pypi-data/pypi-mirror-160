f663eedf01fd42ae2828fbf9d4de494d1d2a9d2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:55:48 +0000

    Add package cbl-tools/0.0.5/cbl_tools-0.0.5.tar.gz

---
+++ packages/cbl-tools/cbl_tools-0.0.5.tar.gz/cbl_tools-0.0.5/src/cbl_tools/tools.py
@@ -0,0 +1,32 @@
+# a.voss@fh-aachen.de
+
+import __version__
+
+
+def version(n: int) -> str:
+    return f"{__version__.__version__}-{n}"
+
+
+if __name__ == "__main__":
+    print(f"Version: {version(25)}")
+
+
+"""
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+https://choosealicense.com/licenses/mit/
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cbl_test_package==1.2.3
+
+
+python3 -m build  
+python3 -m twine upload --repository testpypi dist/* 
+python3 -m pip install --index-url https://test.pypi.org/simple/ --no-deps cbl_test_package@1.2.3 
+
+https://packaging.python.org/en/latest/tutorials/packaging-projects/
+
+python3 -m build  
+python3 -m twine upload dist/* 
+python3 -m pip install cbl_test_package==1.2.3 
+pip install cbl-test-package==1.2.3
+pip install cbl-test-package==2.3.4
+"""
---
+++ packages/cbl-tools/cbl_tools-0.0.5.tar.gz/cbl_tools-0.0.5/tests/test_tools.py
@@ -0,0 +1,13 @@
+import unittest
+
+from cbl_tools import tools
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        #self.assertEqual(True, False)  # add assertion here
+        print(tools.version(12))
+
+
+if __name__ == '__main__':
+    unittest.main()
