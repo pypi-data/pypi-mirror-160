553144288ee6eeadb6506434f95a2b5fea0faaec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:22:31 +0000

    Add package keyword-module/1.0.7/keyword_module-1.0.7.tar.gz

---
+++ packages/keyword-module/keyword_module-1.0.7.tar.gz/keyword_module-1.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='keyword_module',
+    version='1.0.7',
+    description='keyword_module',
+    author='birdcrane',
+    author_email='hsblhs2808@gmail.com',
+    url=None,
+    py_modules=['text2keyword'],
+    packages=find_packages(),
+    install_requires=[
+        'sentence_transformers',
+        'numpy',
+        'sklearn',
+        'konlpy'
+    ],
+
+)
