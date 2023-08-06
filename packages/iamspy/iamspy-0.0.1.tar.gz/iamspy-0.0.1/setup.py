e117845cb74471f0454d023bfbd509414511b755
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:37 +0000

    Add package iamspy/0.0.1/iamspy-0.0.1.tar.gz

---
+++ packages/iamspy/iamspy-0.0.1.tar.gz/iamspy-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="iamspy",
+    version="0.0.1",
+    description="",
+    long_description="",
+    url="",
+    author="Mohit Gupta",
+    author_email="mohit.gupta@withsecure.com",
+    packages=[],
+    classifiers=["Development Status :: 1 - Planning"],
+)
