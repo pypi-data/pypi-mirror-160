5d394831ea7645d0eaf7c4aa286f3e24fc72754f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:25 +0000

    Add package databutton/0.15.5/databutton-0.15.5-py3-none-any.whl

---
+++ packages/databutton/databutton-0.15.5-py3-none-any.whl/databutton/version.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: databutton.version
+.. moduleauthor:: Databutton <support@databutton.com>
+"""
+
+__version__ = "0.15.5"
