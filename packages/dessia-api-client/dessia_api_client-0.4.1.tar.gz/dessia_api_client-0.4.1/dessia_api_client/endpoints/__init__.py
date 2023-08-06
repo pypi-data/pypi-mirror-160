a23fe61285ec6a38e4671a88d185ac32111ba37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:38 +0000

    Add package dessia-api-client/0.4.1/dessia_api_client-0.4.1.tar.gz

---
+++ packages/dessia-api-client/dessia_api_client-0.4.1.tar.gz/dessia_api_client-0.4.1/dessia_api_client/endpoints/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 12:51:49 2021
+
+@author: steven
+"""
+
---
+++ packages/dessia-api-client/dessia_api_client-0.4.1.tar.gz/dessia_api_client-0.4.1/dessia_api_client/utils/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  1 12:52:21 2021
+
+@author: steven
+"""
+
