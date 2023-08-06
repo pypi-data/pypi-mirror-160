4787c8a113d87df1ea841ef56a2febcf54c12f71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:07 +0000

    Add package lazyearth/0.0.9/lazyearth-0.0.9.tar.gz

---
+++ packages/lazyearth/lazyearth-0.0.9.tar.gz/lazyearth-0.0.9/lazyearth/mainfile.py
@@ -0,0 +1,53 @@
+import numpy as np
+import xarray
+import matplotlib.pyplot as plt
+class earthobj:
+    def __init__(self,DataArray):
+        self.DataArray = DataArray
+    def testprint(self):
+        print('Done')
+    def plotshow(self,lst=True):
+        self.lst=lst
+        if type(self.DataArray) == xarray.core.dataarray.DataArray:
+            if self.lst==True:
+                ymax = 0 ; ymin = self.DataArray.shape[0]
+                xmin = 0 ; xmax = self.DataArray.shape[1] 
+            else:
+                ymax = self.lst[0] ; ymin = self.lst[1]
+                xmin = self.lst[2] ; xmax = self.lst[3]
+            lon  =  self.DataArray.longitude.to_numpy()[xmin:xmax]
+            lon0 =  lon[0] ; lon1 =  lon[-1]
+            lat  =  self.DataArray.latitude.to_numpy()[ymax:ymin]
+            lat0 = -lat[-1] ; lat1 = -lat[0]
+            def longitude(lon):
+                return [lon0,lon1]
+            def latitude(lat):
+                return [lat0,lat1]
+            def axis(x=0):
+                return x
+            fig,ax = plt.subplots(constrained_layout=True)
+            fig.set_size_inches(7,7)
+            ax.set_xlabel('x axis size')
+            ax.set_ylabel('y axis size')
+            ax.imshow(self.DataArray[ymax:ymin,xmin:xmax],extent=[xmin,xmax,ymin,ymax])
+            secax_x = ax.secondary_xaxis('top',functions=(longitude,axis))
+            secax_x.set_xlabel('longitude')
+            secax_x = ax.secondary_xaxis('top',functions=(longitude,axis))
+            secax_x.set_xlabel('longitude')
+            secax_y = ax.secondary_yaxis('right',functions=(latitude,axis))
+            secax_y.set_ylabel('latitute')
+            plt.grid(color='w', linestyle='-', linewidth=0.1)
+            plt.show()
+        
+        elif type(self.DataArray) == numpy.ndarray:
+            if self.lst==True:
+                ymax = 0 ; ymin = self.DataArray.shape[0]
+                xmin = 0 ; xmax = self.DataArray.shape[1]
+            else:
+                ymax = self.lst[0] ; ymin = self.lst[1]
+                xmin = self.lst[2] ; xmax = self.lst[3]
+            plt.figure(figsize=(10,10))
+            plt.imshow(self.DataArray[ymax:ymin,xmin:xmax],extent=[xmin,xmax,ymin,ymax])
+        
+        else:
+            print("Nonetype :",type(self.DataArray))
