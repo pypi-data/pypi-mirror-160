543eb3bec983f239cd3c1da241749e89e2057a7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:20:26 +0000

    Add package nionctl/0.2.2.7/nionctl-0.2.2.7.tar.gz

---
+++ packages/nionctl/nionctl-0.2.2.7.tar.gz/nionctl-0.2.2.7/nionctl/main.py
@@ -0,0 +1,22 @@
+import typer
+
+import os
+pwd = ".".join(__file__.split("/")[:-1])
+os.system("export PYTHONPATH="+pwd)
+
+import wifi as wifi
+import bt as bt
+import util as util
+import archive as archive
+
+
+main_app = typer.Typer()
+
+main_app.add_typer(wifi.wifi_app,       name="wifi"   )
+main_app.add_typer(bt.bt_app,           name="bt"     )
+main_app.add_typer(util.util_app,       name="util"   )
+main_app.add_typer(archive.archive_app, name="archive")
+
+
+if __name__ == "__main__":
+    main_app()
