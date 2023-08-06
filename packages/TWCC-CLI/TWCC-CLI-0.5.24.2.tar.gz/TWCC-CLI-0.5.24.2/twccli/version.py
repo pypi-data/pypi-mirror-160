b4d3fea10454e31b4872429a19d30b610ffe7b85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:16:01 +0000

    Add package TWCC-CLI/0.5.24.2/TWCC-CLI-0.5.24.2.tar.gz

---
+++ packages/TWCC-CLI/TWCC-CLI-0.5.24.2.tar.gz/TWCC-CLI-0.5.24.2/twccli/version.py
@@ -0,0 +1,8 @@
+__TWCC_CLI_MAJOR__ = "0"
+__TWCC_CLI_MINOR__ = "5"
+__TWCC_CLI_BATCH__ = "24.2"
+__version__ = "{}.{}.{}".format(__TWCC_CLI_MAJOR__, __TWCC_CLI_MINOR__, __TWCC_CLI_BATCH__)
+
+
+if __name__ == "__main__":
+    print(__version__)
