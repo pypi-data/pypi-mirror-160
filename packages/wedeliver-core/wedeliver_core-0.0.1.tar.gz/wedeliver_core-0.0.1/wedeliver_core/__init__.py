8b217da8bcba3ec5f0a569ee25c711b1cceb713b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:20 +0000

    Add package wedeliver-core/0.0.1/wedeliver_core-0.0.1.tar.gz

---
+++ packages/wedeliver-core/wedeliver_core-0.0.1.tar.gz/wedeliver_core-0.0.1/wedeliver_core/__init__.py
@@ -0,0 +1,19 @@
+# from flask import current_app, _app_ctx_stack
+
+
+class WeDeliverCore:
+    __app = None
+
+    @staticmethod
+    def getApp():
+        """ Static access method. """
+        if WeDeliverCore.__app == None:
+            WeDeliverCore()
+        return WeDeliverCore.__app
+
+    def __init__(self, app=None):
+        """ Virtually private constructor. """
+        if WeDeliverCore.__app != None:
+            raise Exception("This class is a singleton!")
+        else:
+            WeDeliverCore.__app = app
