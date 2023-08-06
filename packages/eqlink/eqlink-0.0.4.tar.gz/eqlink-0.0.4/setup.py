7bdf8c3803d3f2be64534f91445946f9bdfc437f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:17 +0000

    Add package eqlink/0.0.4/eqlink-0.0.4.tar.gz

---
+++ packages/eqlink/eqlink-0.0.4.tar.gz/eqlink-0.0.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eqlink",
+    version="0.0.4",
+    keywords=("eqlink", "Registration Center", "eqsmart"),
+    description="注册中心",
+    long_description="Provider列表持久化，失效服务移除",
+    license="GPL-2.0",
+
+    url="https://gitee.com/jingenqiang/eqlink.git",
+    author="eq",
+    author_email="eq_enqiang@163.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=['PyYAML==6.0']
+)
+
+"""
+项目打包
+python setup.py bdist_egg     # 生成类似 eqlink-0.0.1-py2.7.egg，支持 easy_install 
+# 使用此方式
+python setup.py sdist         # 生成类似 eqlink-0.0.1.tar.gz，支持 pip
+# twine 需要安装
+twine upload dist/eqlink-0.0.4.tar.gz
+"""
