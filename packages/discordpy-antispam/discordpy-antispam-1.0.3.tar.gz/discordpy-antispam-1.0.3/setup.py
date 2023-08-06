203ef0ef0498e0bc8e4a60648eec1ca9be9f7e2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:52 +0000

    Add package discordpy-antispam/1.0.3/discordpy-antispam-1.0.3.tar.gz

---
+++ packages/discordpy-antispam/discordpy-antispam-1.0.3.tar.gz/discordpy-antispam-1.0.3/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="discordpy-antispam",
+    version="1.0.3",
+    description="An efficient and simple anti-spam system made for your discord bots.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=['SpamFilter'],
+    url="https://github.com/DorianAarno/SpamFilter",
+    author="Aarno Dorian",
+    author_email="aarnodorian56@gmail.com",
+    download_url = "https://github.com/DorianAarno/SpamFilter/archive/refs/tags/v1.0.2.tar.gz",
+    license = "MIT",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'Typing :: Typed',
+    ],
+      install_requires=[
+          'nltk'
+      ]
+)
