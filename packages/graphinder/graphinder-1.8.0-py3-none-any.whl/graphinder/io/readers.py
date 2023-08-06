160d206400c860a72f1b92bdcca75535fadf7e22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:02 +0000

    Add package graphinder/1.8.0/graphinder-1.8.0-py3-none-any.whl

---
+++ packages/graphinder/graphinder-1.8.0-py3-none-any.whl/graphinder/io/readers.py
@@ -0,0 +1,35 @@
+"""I/O readers."""
+
+from io import TextIOWrapper
+from typing import List, Optional
+
+from graphinder.pool.domain import Domain
+from graphinder.utils.filters import transform_url_in_domain
+from graphinder.utils.logger import get_logger
+
+
+def read_domains(
+    file: Optional[TextIOWrapper],
+    domain: Optional[str],
+    precision_mode: bool = False,
+) -> List[Domain]:
+    """Read domains from file."""
+
+    if domain is not None:
+        if (clean := transform_url_in_domain(domain)) is not None:
+            return [Domain(clean, precision_mode)]
+        return []
+
+    if file is None:
+        get_logger().warning('no input file specified, skipping reading domains..')
+        return []
+
+    urls: List[str] = list(set(file.read().splitlines()))
+    domains: List[Domain] = []
+    for url in urls:
+        if (clean := transform_url_in_domain(url)) is not None:
+            domains.append(Domain(clean, precision_mode))
+
+    get_logger().info(f'found { len(domains) } domains.')
+
+    return domains
