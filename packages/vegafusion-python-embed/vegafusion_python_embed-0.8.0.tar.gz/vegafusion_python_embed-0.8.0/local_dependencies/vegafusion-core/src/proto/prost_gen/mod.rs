f91e3c2b9d98a719563f93143a8e1f230fc3c581
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:46 +0000

    Add package vegafusion-python-embed/0.8.0/vegafusion_python_embed-0.8.0.tar.gz

---
+++ packages/vegafusion-python-embed/vegafusion_python_embed-0.8.0.tar.gz/vegafusion_python_embed-0.8.0/local_dependencies/vegafusion-core/src/proto/prost_gen/mod.rs
@@ -0,0 +1,26 @@
+/*
+ * VegaFusion
+ * Copyright (C) 2022 VegaFusion Technologies LLC
+ *
+ * This program is distributed under multiple licenses.
+ * Please consult the license documentation provided alongside
+ * this program the details of the active license.
+ */
+#[rustfmt::skip]
+#[allow(clippy::module_inception)]
+pub mod expression;
+
+#[rustfmt::skip]
+pub mod transforms;
+
+#[rustfmt::skip]
+pub mod tasks;
+
+#[rustfmt::skip]
+pub mod errors;
+
+#[rustfmt::skip]
+pub mod pretransform;
+
+#[rustfmt::skip]
+pub mod services;
