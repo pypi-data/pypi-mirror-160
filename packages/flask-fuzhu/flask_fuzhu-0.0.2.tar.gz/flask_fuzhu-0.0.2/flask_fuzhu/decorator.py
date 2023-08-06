9e0af6548671c6ded705314245f61ff95984a003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:55 +0000

    Add package flask-fuzhu/0.0.2/flask_fuzhu-0.0.2.tar.gz

---
+++ packages/flask-fuzhu/flask_fuzhu-0.0.2.tar.gz/flask_fuzhu-0.0.2/flask_fuzhu/decorator.py
@@ -0,0 +1,36 @@
+from functools import wraps
+
+from flask import request
+
+from flask_fuzhu.exception import BadRequest
+from flask_fuzhu.exception import Unauthorized
+
+
+def need_token(verify_func, token_key="TOKEN"):
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            token = request.headers.get(token_key, None)
+            if token is None:
+                raise BadRequest(f"Header未包含{token_key}")
+            if not verify_func(token):
+                raise Unauthorized()
+            return func(*args, *kwargs)
+
+        return wrapper
+
+    return inner
+
+
+def need_args(args):
+    def inner(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            miss_args = [a for a in args if request.args.get(a, None) is None]
+            if len(miss_args):
+                raise BadRequest(f"Url参数缺少{','.join(miss_args)}")
+            return func(*args, *kwargs)
+
+        return wrapper
+
+    return inner
