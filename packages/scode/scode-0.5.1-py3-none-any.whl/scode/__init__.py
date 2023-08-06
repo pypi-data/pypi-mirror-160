74b6840b9e7d7026bdf0de0f781813dddcf22e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:15 +0000

    Add package scode/0.5.1/scode-0.5.1-py3-none-any.whl

---
+++ packages/scode/scode-0.5.1-py3-none-any.whl/scode/__init__.py
@@ -0,0 +1,7 @@
+__all__ = ['selenium', 'paramiko', 'telegram', 'dropbox', 'util', 'schedule']
+
+__version__ = '0.5.1'
+
+from . import selenium, paramiko, telegram, dropbox, util, schedule
+
+from .util import *
