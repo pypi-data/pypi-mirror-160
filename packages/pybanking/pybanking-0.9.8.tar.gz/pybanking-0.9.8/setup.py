e983bd7363c40b3f60bb721259741e31cb7d05e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:25 +0000

    Add package pybanking/0.9.8/pybanking-0.9.8.tar.gz

---
+++ packages/pybanking/pybanking-0.9.8.tar.gz/pybanking-0.9.8/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "READMELIB.rst").read_text()
+
+setup(
+    name='pybanking',
+    description = "Machine learning module for banking",
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    version='0.9.8',
+    license='MIT',
+    author="Shorthills Tech",
+    author_email='apurv@shorthillstech.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/shorthillstech/pybanking',
+    keywords='banking project',
+    install_requires=[
+          'scikit-learn',
+          'pandas',
+          'numpy',
+          'bayesian-optimization'
+      ],
+
+)
