5a59821933d2842bad48e51ecf487e4b3030bbdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:14 +0000

    Add package akr-probability/0.1/akr_probability-0.1.tar.gz

---
+++ packages/akr-probability/akr_probability-0.1.tar.gz/akr_probability-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='akr_probability',
+      version='0.1',
+      description='Gaussian akr_probability',
+      packages=['akr_probability'],
+      author='Arishwin Kishaan',
+      author_email='arishwinkishaan3@gmail.com',
+      zip_safe=False)
