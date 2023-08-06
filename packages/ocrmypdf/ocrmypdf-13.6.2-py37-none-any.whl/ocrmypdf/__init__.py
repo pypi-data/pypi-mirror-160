7037e8a9a5d30b0388b1b6abd9e4f71d5c5df40c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:15 +0000

    Add package ocrmypdf/13.6.2/ocrmypdf-13.6.2-py37-none-any.whl

---
+++ packages/ocrmypdf/ocrmypdf-13.6.2-py37-none-any.whl/ocrmypdf/__init__.py
@@ -0,0 +1,35 @@
+# © 2017 James R. Barlow: github.com/jbarlow83
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""Adds OCR layer to PDFs."""
+
+from __future__ import annotations
+
+from pluggy import HookimplMarker as _HookimplMarker
+
+from ocrmypdf import helpers, hocrtransform, pdfa, pdfinfo
+from ocrmypdf._concurrent import Executor
+from ocrmypdf._jobcontext import PageContext, PdfContext
+from ocrmypdf._version import PROGRAM_NAME, __version__
+from ocrmypdf.api import Verbosity, configure_logging, ocr
+from ocrmypdf.exceptions import (
+    BadArgsError,
+    DpiError,
+    EncryptedPdfError,
+    ExitCode,
+    ExitCodeException,
+    InputFileError,
+    MissingDependencyError,
+    OutputFileAccessError,
+    PdfMergeFailedError,
+    PriorOcrFoundError,
+    SubprocessOutputError,
+    TesseractConfigError,
+    UnsupportedImageFormatError,
+)
+from ocrmypdf.pluginspec import OcrEngine, OrientationConfidence
+
+hookimpl = _HookimplMarker('ocrmypdf')
