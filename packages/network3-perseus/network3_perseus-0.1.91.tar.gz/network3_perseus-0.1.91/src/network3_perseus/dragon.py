53e16e2fc895660cc8a16ad3e6ef4b829cce0972
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:53:56 +0000

    Add package network3-perseus/0.1.91/network3_perseus-0.1.91.tar.gz

---
+++ packages/network3-perseus/network3_perseus-0.1.91.tar.gz/network3_perseus-0.1.91/src/network3_perseus/dragon.py
@@ -0,0 +1,14 @@
+import requests
+import json 
+
+url = "https://api-testnet.dragonglass.me/hedera/api/accounts/0.0.34961353/files"
+
+payload = {}
+headers = {
+    'X-API-KEY': '7c68843c-bcc4-3b7b-92d9-ad1937294a91',
+    'accept': 'application/json'
+}
+
+response = requests.request("GET", url, headers=headers, data = payload, verify=False)
+json_response = response.json()
+print(json_response)
