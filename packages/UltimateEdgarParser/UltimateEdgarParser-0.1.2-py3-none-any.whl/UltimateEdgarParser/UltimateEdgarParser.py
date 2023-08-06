b64d457eee62006c06d9426c20cd88a099dadaa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:19 +0000

    Add package UltimateEdgarParser/0.1.2/UltimateEdgarParser-0.1.2-py3-none-any.whl

---
+++ packages/UltimateEdgarParser/UltimateEdgarParser-0.1.2-py3-none-any.whl/UltimateEdgarParser/UltimateEdgarParser.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+data = pd.read_csv("UltimateEdgarParser/data/After2005.csv")
+data = data.drop(['Unnamed: 0'], axis=1)
+
+
+
+
+def LookUp(CIK, DateFiled, Type = '10Q'):
+    prefix = 'https://www.sec.gov/Archives/edgar/'
+    return prefix + data.loc[(data.CIK == CIK) & (data.DateFiled == DateFiled), :].TXTAddress
+
+
