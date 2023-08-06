beee1d154c6c3ab862244474c075797b6946e2e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:51 +0000

    Add package python-ics/908.12/python_ics-908.12-cp37-cp37m-win32.whl

---
+++ packages/python-ics/python_ics-908.12-cp37-cp37m-win32.whl/ics/structures/ics_fire2_device_status.py
@@ -0,0 +1,20 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+from ics.structures.ethernet_network_status_t import *
+
+
+class ics_fire2_device_status(ctypes.Structure):
+    _pack_ = 4
+    _fields_ = [
+        ('backupPowerGood', ctypes.c_uint8),
+        ('backupPowerEnabled', ctypes.c_uint8),
+        ('usbHostPowerEnabled', ctypes.c_uint8),
+        ('ethernetActivationLineEnabled', ctypes.c_uint8),
+        ('ethernetStatus', ethernetNetworkStatus_t),
+    ]
+
+
+icsFire2DeviceStatus = ics_fire2_device_status
+
