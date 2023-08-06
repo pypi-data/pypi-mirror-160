fe1ebcca54ca80ff624720833b19305f70e7c8d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:01:16 +0000

    Add package regexr/0.0.1/regexr-0.0.1.tar.gz

---
+++ packages/regexr/regexr-0.0.1.tar.gz/regexr-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['regexr']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'regexr',
+    'version': '0.0.1',
+    'description': 'Regular expressions for humans',
+    'long_description': None,
+    'author': 'pwwang',
+    'author_email': 'pwwang@pwwang.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
