b071804a6f2d8e4d18c5ae5b2ccd11d4d88290ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:11:32 +0000

    Add package datapoints/0.0.1/datapoints-0.0.1.tar.gz

---
+++ packages/datapoints/datapoints-0.0.1.tar.gz/datapoints-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['datapoints', 'datapoints.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['asyncua==0.9.94']
+
+setup_kwargs = {
+    'name': 'datapoints',
+    'version': '0.0.1',
+    'description': 'Управление данными',
+    'long_description': '# data_connector\n\n## Описание\n\n- signal - элементарный тип данных. Кроме значения, хранит также время последнего чтения / записи, код качества, единицу измерения, пределы измерения.\n\n- channel - объект для связи signal с периферийными значениями.\n\n- driver - управляет объектами channel.\n\n- datapoint - объект для храненения данных. Связывает signal и channel.\n\n## Разработка\n\nУстановить виртуальное окружение\n\n```sh\npoetry install\n```\n\nОпубликовать пакет\n\n```sh\npoetry build && poetry publish\n```\n',
+    'author': 'Konstantin-Dudersky',
+    'author_email': 'Konstantin.Dudersky@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Konstantin-Dudersky/data_connector',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10.0,<3.11.0',
+}
+
+
+setup(**setup_kwargs)
