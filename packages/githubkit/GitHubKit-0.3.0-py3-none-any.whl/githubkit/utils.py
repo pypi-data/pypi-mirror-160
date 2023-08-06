8212bcfe2a800f5b7d8304b3a974f8632059491d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:48 +0000

    Add package githubkit/0.3.0/GitHubKit-0.3.0-py3-none-any.whl

---
+++ packages/githubkit/GitHubKit-0.3.0-py3-none-any.whl/githubkit/utils.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+
+class Unset:
+    def __str__(self) -> str:
+        return "<UNSET>"
+
+    def __bool__(self) -> bool:
+        return False
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls._validate
+
+    @classmethod
+    def _validate(cls, value: Any):
+        if value is not UNSET:
+            raise ValueError(f"{value!r} is not UNSET")
+        return value
+
+
+UNSET = Unset()
+
+
+def exclude_unset(data: Any) -> Any:
+    if isinstance(data, dict):
+        return data.__class__(
+            (k, exclude_unset(v)) for k, v in data.items() if v is not UNSET
+        )
+    elif data is UNSET:
+        return None
+    return data
