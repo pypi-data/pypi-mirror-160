1684d02a0857db73f557de33139d986b7649f1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:45:53 +0000

    Add package paperviz/0.0.6/paperviz-0.0.6.tar.gz

---
+++ packages/paperviz/paperviz-0.0.6.tar.gz/paperviz-0.0.6/paperviz/bubble/__init__.py
@@ -0,0 +1,6 @@
+from bubble import Bubble
+path = r'C:\Users\87139\Desktop\Paper-Viz-master\Example_Data\Bubble'
+path_img = r'C:\Users\87139\Desktop\Paper-Viz-master\Example_Data\Bubble'
+file_name = "bubble_gdp.xlsx"
+b = Bubble(file_name)
+b.bubble_plot(file_name)
---
+++ packages/paperviz/paperviz-0.0.6.tar.gz/paperviz-0.0.6/paperviz/time_series/__init__.py
@@ -0,0 +1,6 @@
+from time_series import TimeSeries
+
+file_name = "mortality.csv"
+
+ts = TimeSeries(file_name)
+ts.plot()
