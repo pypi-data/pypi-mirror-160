9b1e1ce4e98936024f2e08f67bf4fee71619aada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:53:28 +0000

    Add package loghelper/1.0.3/LogHelper-1.0.3-py3-none-any.whl

---
+++ packages/loghelper/LogHelper-1.0.3-py3-none-any.whl/loghelper/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = "ITXiaoPang"
+__mtime__ = "2021/07/10"
+__project__ = "LogHelper"
+__file__ = "__init__.py"
+__IDE__ = "PyCharm"
+
+from loghelper.loghelper import (
+    LogHelper,
+    TimedRotatingFileHandler,
+    WatchedTimedRotatingFileHandler,
+    ConcurrentTimedRotatingFileHandler,
+)
+
+from loghelper.ver import __version__
+
+__all__ = [
+    'LogHelper',
+    'TimedRotatingFileHandler',
+    'WatchedTimedRotatingFileHandler',
+    'ConcurrentTimedRotatingFileHandler',
+    '__version__'
+]
