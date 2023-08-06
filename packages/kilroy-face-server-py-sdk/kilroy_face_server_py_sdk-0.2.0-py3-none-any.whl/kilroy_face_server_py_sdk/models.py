5b73fcca2c0b6ac0ecfe4af09c221b0c4aa16336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:23 +0000

    Add package kilroy-face-server-py-sdk/0.2.0/kilroy_face_server_py_sdk-0.2.0-py3-none-any.whl

---
+++ packages/kilroy-face-server-py-sdk/kilroy_face_server_py_sdk-0.2.0-py3-none-any.whl/kilroy_face_server_py_sdk/models.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional
+from uuid import UUID
+
+from jsonschema.exceptions import SchemaError
+from jsonschema.validators import Draft202012Validator
+from kilroy_ws_server_py_sdk import JSON
+from pydantic import BaseModel
+
+
+class JSONSchema(dict):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, schema: JSON) -> JSON:
+        try:
+            Draft202012Validator.check_schema(schema)
+        except SchemaError as e:
+            raise ValueError(
+                "Schema is not a valid JSON Schema 2020-12."
+            ) from e
+        if "type" not in schema:
+            raise ValueError("Schema should have a type field.")
+        elif schema["type"] != "object":
+            raise ValueError("Only object types are allowed.")
+        return schema
+
+
+class PostSchema(BaseModel):
+    postSchema: JSONSchema
+
+
+class StatusEnum(str, Enum):
+    loading = "loading"
+    ready = "ready"
+
+
+class Status(BaseModel):
+    status: StatusEnum
+
+
+class StatusNotification(BaseModel):
+    old: Status
+    new: Status
+
+
+class Config(BaseModel):
+    config: JSON
+
+
+class ConfigSchema(BaseModel):
+    configSchema: JSONSchema
+
+
+class ConfigNotification(BaseModel):
+    old: Config
+    new: Config
+
+
+class ConfigSetRequest(BaseModel):
+    set: Config
+
+
+class ConfigSetReply(BaseModel):
+    old: Config
+    new: Config
+
+
+class PostRequest(BaseModel):
+    post: JSON
+
+
+class PostReply(BaseModel):
+    postId: UUID
+
+
+class ScoreRequest(BaseModel):
+    postId: UUID
+
+
+class ScoreReply(BaseModel):
+    score: float
+
+
+class ScrapRequest(BaseModel):
+    limit: Optional[int] = None
+    before: Optional[datetime] = None
+    after: Optional[datetime] = None
+
+
+class ScrapReply(BaseModel):
+    postId: UUID
+    post: JSON
