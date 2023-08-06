32f8c6070ab69d3d497daf567da2543a01260c56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:52 +0000

    Add package sourmash/4.4.3/sourmash-4.4.3-py2.py3-none-musllinux_1_1_aarch64.whl

---
+++ packages/sourmash/sourmash-4.4.3-py2.py3-none-musllinux_1_1_aarch64.whl/sourmash/cli/sig/cat.py
@@ -0,0 +1,38 @@
+"""concatenate signature files"""
+
+from sourmash.cli.utils import (add_moltype_args, add_ksize_arg,
+                                add_picklist_args, add_pattern_args)
+
+
+def subparser(subparsers):
+    subparser = subparsers.add_parser('cat')
+    subparser.add_argument('signatures', nargs='*')
+    subparser.add_argument(
+        '--from-file',
+        help='a text file containing a list of files to load signatures from'
+    )
+    subparser.add_argument(
+        '-q', '--quiet', action='store_true',
+        help='suppress non-error output'
+    )
+    subparser.add_argument(
+        '-o', '--output', metavar='FILE', default='-',
+        help='output signature to this file (default stdout)'
+    )
+    subparser.add_argument(
+        '-u', '--unique', action='store_true',
+        help='keep only distinct signatures, removing duplicates (based on md5sum)'
+    )
+    subparser.add_argument(
+        '-f', '--force', action='store_true',
+        help='try to load all files as signatures'
+    )
+    add_ksize_arg(subparser, 31)
+    add_moltype_args(subparser)
+    add_pattern_args(subparser)
+    add_picklist_args(subparser)
+
+
+def main(args):
+    import sourmash
+    return sourmash.sig.__main__.cat(args)
