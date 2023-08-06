fa3a7c02791789533943fd3fd3dd0dc877ca3f66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:26 +0000

    Add package renovosolutions.aws-cdk-renovo-s3-bucket/1.0.180/renovosolutions.aws-cdk-renovo-s3-bucket-1.0.180.tar.gz

---
+++ packages/renovosolutions.aws-cdk-renovo-s3-bucket/renovosolutions.aws-cdk-renovo-s3-bucket-1.0.180.tar.gz/renovosolutions.aws-cdk-renovo-s3-bucket-1.0.180/src/renovo_s3_bucket/_jsii/__init__.py
@@ -0,0 +1,26 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_s3._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@renovosolutions/cdk-library-renovo-s3-bucket",
+    "1.0.180",
+    __name__[0:-6],
+    "cdk-library-renovo-s3-bucket@1.0.180.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
