06e3de6d1538290d0f3190b7aab120f4fb9b5eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:54:45 +0000

    Add package types-pyOpenSSL/22.0.6/types-pyOpenSSL-22.0.6.tar.gz

---
+++ packages/types-pyOpenSSL/types-pyOpenSSL-22.0.6.tar.gz/types-pyOpenSSL-22.0.6/CHANGELOG.md
@@ -0,0 +1,60 @@
+## 22.0.6 (2022-07-22)
+
+Add `ocsp` callback setters to `OpenSSL.SSL` (#8367)
+
+## 22.0.5 (2022-07-22)
+
+Add `proto_version` setters to `OpenSSL.SSL` (#8363)
+
+## 22.0.4 (2022-06-26)
+
+Third-party stubs: audit `Callable[<parameters>, None]` annotations (#8175)
+
+## 22.0.3 (2022-04-16)
+
+Third-party stubs: import from `collections.abc` where possible (#7637)
+
+## 22.0.2 (2022-04-16)
+
+Use `TypeAlias` where possible for type aliases (#7630)
+
+## 22.0.1 (2022-03-16)
+
+Use PEP 604 syntax wherever possible (#7493)
+
+## 22.0.0 (2022-01-30)
+
+pyOpenSSL: Adapt to changes in 22.0.0 (#7080)
+
+## 21.0.2 (2021-12-28)
+
+Use PEP 585 syntax wherever possible (#6717)
+
+## 21.0.1 (2021-11-23)
+
+Reduce use of deprecated `typing` aliases (#6358)
+
+## 21.0.0 (2021-11-10)
+
+Add pyOpenSSL 21 constants (#6273)
+
+## 20.0.9 (2021-10-15)
+
+Use lowercase tuple where possible (#6170)
+
+## 20.0.8 (2021-10-12)
+
+Add star to all non-0.1 versions (#6146)
+
+## 20.0.7 (2021-09-30)
+
+Unbreak CI (#6093)
+
+* Temporarily remove `cachetools` stubs. They will be added back soon, and meanwhile they will continue to be available on pypi.
+
+* Remove `filelock.logger()` (no longer exists in filelock 3.2)
+
+* Delete `OpenSSL.crypto` from stubtest whitelist
+
+Co-authored-by: Akuli <akuviljanen17@gmail.com>
+
---
+++ packages/types-pyOpenSSL/types-pyOpenSSL-22.0.6.tar.gz/types-pyOpenSSL-22.0.6/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+
+name = "types-pyOpenSSL"
+description = "Typing stubs for pyOpenSSL"
+long_description = '''
+## Typing stubs for pyOpenSSL
+
+This is a PEP 561 type stub package for the `pyOpenSSL` package.
+It can be used by type-checking tools like mypy, PyCharm, pytype etc. to check code
+that uses `pyOpenSSL`. The source for this package can be found at
+https://github.com/python/typeshed/tree/master/stubs/pyOpenSSL. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/master/README.md for more details.
+This package was generated from typeshed commit `60e5d960436c4bb16a7af284909436c3333376ac`.
+'''.lstrip()
+
+setup(name=name,
+      version="22.0.6",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/pyOpenSSL.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=['types-cryptography'],
+      packages=['OpenSSL-stubs'],
+      package_data={'OpenSSL-stubs': ['SSL.pyi', '__init__.pyi', 'crypto.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
