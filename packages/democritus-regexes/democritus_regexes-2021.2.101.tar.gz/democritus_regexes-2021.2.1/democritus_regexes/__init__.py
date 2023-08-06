3e4a5ac18568a15ff6590fd2d39356ceef8ce943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:11 +0000

    Add package democritus-regexes/2021.2.1/democritus_regexes-2021.2.101.tar.gz

---
+++ packages/democritus-regexes/democritus_regexes-2021.2.101.tar.gz/democritus_regexes-2021.2.1/democritus_regexes/__init__.py
@@ -0,0 +1,15 @@
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version('democritus_regexes')
+except PackageNotFoundError:
+    message = 'Unable to find a version number for "democritus_regexes". This likely means the library was not installed properly. Please re-install it and, if the problem persists, raise an issue here: https://github.com/democritus-project/democritus-regexes/issues.'
+    print(message)
+
+__author__ = '''Floyd Hightower'''
+__email__ = 'floyd.hightower27@gmail.com'
+
+from .regexes import *
