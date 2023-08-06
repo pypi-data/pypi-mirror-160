0cb9231e4778558beefb4e1bd532db1caed246d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:34 +0000

    Add package pychoosealicense/2022.7.25/pychoosealicense-2022.7.25-py3-none-any.whl

---
+++ packages/pychoosealicense/pychoosealicense-2022.7.25-py3-none-any.whl/pychoosealicense/_licenses/__init__.py
@@ -0,0 +1,17 @@
+"""
+License metadata used by ``pychoosealicense``.
+"""
+
+CHOOSEALICENSE_COMMIT_INFO = """
+commit 62dfebe236ac92b5627f4f7cf96650801c5bdb89
+Merge: 695f9ca cf8f0d7
+Author: Mike Linksvayer <mlinksva@github.com>
+Date:   Tue Jul 19 10:24:57 2022 -0700
+
+    Merge pull request #1014 from c0nspiracy/patch-1
+
+    Fix typo in non-software licenses page
+"""
+
+CHOOSEALICENSE_COMMIT_HASH = "62dfebe236ac92b5627f4f7cf96650801c5bdb89"
+CHOOSEALICENSE_COMMIT_SHORTHASH = "62dfebe"
