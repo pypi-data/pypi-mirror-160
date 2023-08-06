85482d666d180e1978c2dafee85f6e51adc7d5dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:23 +0000

    Add package one-py-sdk/0.0.8/one_py_sdk-0.0.8-py3-none-any.whl

---
+++ packages/one-py-sdk/one_py_sdk-0.0.8-py3-none-any.whl/one_py_sdk/historian/data.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import requests
+import json
+from one_py_sdk.enterprise.authentication import AuthenticationApi
+from one_py_sdk.shared.helpers.protobufhelper import DeserializeResponse
+
+
+class HistorianApi:
+    def __init__(self, env, auth:AuthenticationApi):
+        self.Environment =env
+        self.Authentication = auth
+        self.AppUrl = "/historian/data/v1/"
+        
+    def GetHistorianData(self, twinRefId, date:datetime):
+        url=f'{self.Environment}{self.AppUrl}{twinRefId}/{date}'
+        headers = {'Authorization': self.Authentication.Token.access_token, "Accept":"application/x-protobuf"}
+        print(url)
+        response = DeserializeResponse(requests.get(url, headers=headers))
+        if response.errors:            
+            return response
+        return response.content.historianDatas.items
+    
+    def GetHistorianDataRange(self, twinRefId, startTime:datetime, endTime: datetime):        
+        url=f'{self.Environment}{self.AppUrl}{twinRefId}?startTime={startTime}&endTime={endTime}'
+        print(url)
+        headers = {'Authorization': self.Authentication.Token.access_token, "Accept":"application/x-protobuf"}
+        response = DeserializeResponse(requests.get(url, headers=headers))
+        if response.errors:            
+            return response
+        return response.content.historianDatas.items
+    
+        
