49129b876d9280f612f0bfec3e8592f89db6856e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:25:50 +0000

    Add package jf-tokenize-package/1.0/jf_tokenize_package-1.0.tar.gz

---
+++ packages/jf-tokenize-package/jf_tokenize_package-1.0.tar.gz/jf_tokenize_package-1.0/jf_tokenize_package/tokenize.py
@@ -0,0 +1,39 @@
+import re
+
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger','omw-1.4',
+              'stopwords'])
+
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize, sent_tokenize, pos_tag
+
+def tokenize(text):
+    """
+    Create lemmatized tokens from words in a string and remove stopwords.
+
+    Input:
+    Text as a string.
+
+    Output:
+    A list of tokenised and lemmatized words.
+    """
+    # Replacing urls in text with placeholder
+    from nltk.corpus import stopwords
+
+    detected_urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'\
+                               '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+    for url in detected_urls:
+        text = text.replace(url,"urlplaceholder")
+
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+    sw_nltk = stopwords.words('english')
+
+    clean_tokens = []
+    for tok in tokens:
+        if tok not in sw_nltk:
+            clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+            clean_tokens.append(clean_tok)
+
+    return clean_tokens
---
+++ packages/jf-tokenize-package/jf_tokenize_package-1.0.tar.gz/jf_tokenize_package-1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='jf_tokenize_package',
+    version='1.0',
+    license='MIT',
+    author="Julien Flandre",
+    author_email='julien.flandre@gmail.com',
+    description='A simple tokenizer function for NLP',
+    long_description='This tokenizer function turns text into lowercase word \
+                      tokens, removes English stopwords, lemmatize the tokens \
+                      and replaces URLs with a placeholder.',
+    long_description_content_type='text/x-rst',
+    packages=['jf_tokenize_package'],
+    url='https://github.com/Granju/tokenize-package',
+    keywords='tokenizer function',
+    install_requires=[
+          're',
+          'nltk'
+      ],
+
+)
