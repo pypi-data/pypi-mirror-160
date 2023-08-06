1f0cb77a5af28435d849bac587832e4d3bca9a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:17 +0000

    Add package spackage/0.1/spackage-0.1.tar.gz

---
+++ packages/spackage/spackage-0.1.tar.gz/spackage-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name = "spackage",
+version = "0.1",
+description = "This is a Description",
+long_description = "This is a very very long description",
+author = "Sir Qasim and Shahzad Khan",
+packages = ["spackage"],
+install_packages = []
+)
