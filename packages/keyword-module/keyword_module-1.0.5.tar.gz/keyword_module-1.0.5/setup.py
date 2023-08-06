590c1dd4780e0ccecb8c7679fb1fca4025678fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:45 +0000

    Add package keyword-module/1.0.5/keyword_module-1.0.5.tar.gz

---
+++ packages/keyword-module/keyword_module-1.0.5.tar.gz/keyword_module-1.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+
+setup(
+    name='keyword_module',
+    version='1.0.5',
+    description='keyword_module',
+    author='birdcrane',
+    author_email='hsblhs2808@gmail.com',
+    url=None,
+    py_modules=['text2keyword'],
+    packages=find_packages(),
+)
