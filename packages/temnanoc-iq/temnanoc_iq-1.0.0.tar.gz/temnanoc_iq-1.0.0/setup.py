567163acb62a530b1db2fb5c2b11116c62f7514f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:35 +0000

    Add package temnanoc-iq/1.0.0/temnanoc_iq-1.0.0.tar.gz

---
+++ packages/temnanoc-iq/temnanoc_iq-1.0.0.tar.gz/temnanoc_iq-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '1.0.0'
+DESCRIPTION = "Temnanoc's iq."
+
+# Setting up
+setup(
+	name="temnanoc_iq",
+	version=VERSION,
+	author="mqchinee",
+	description=DESCRIPTION,
+	packages=find_packages(),
+	install_requires=[],
+	keywords=['random', 'python', 'eightball'],
+	classifiers=[
+		"Development Status :: 1 - Planning",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python :: 3",
+		"Operating System :: Unix",
+		"Operating System :: MacOS :: MacOS X",
+		"Operating System :: Microsoft :: Windows",
+	]
+)
