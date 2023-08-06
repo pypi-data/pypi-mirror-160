e009688ed1d761d343af59b1fcd70b1499a45299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:20 +0000

    Add package logistics/0.0.11/logistics-0.0.11.tar.gz

---
+++ packages/logistics/logistics-0.0.11.tar.gz/logistics-0.0.11/logistics/plugins/__init__.py
@@ -0,0 +1,37 @@
+# imports relevant contents.
+from logistics.plugins.metaclass import Meta
+from logistics.plugins.types import *
+
+# all relevant contents.
+__all__ = [
+    'VandalType',
+    'IntegerType',
+    'FloatType',
+    'NumberType',
+    'ReturnType',
+    'PrintType',
+    'GraphType',
+    'StringType',
+    'ListType',
+    'TupleType',
+    'DictionaryType',
+    'BooleanType',
+    'FilePathType',
+    'SpecialType',
+    'NumberVector',
+    'StringVector',
+    'StringDictionary',
+    'DictionaryVector',
+    'NumberVectorAlike',
+    'NumberArrayAlike',
+    'AnyArrayAlike',
+    'AnyVectorAlike',
+    'AnyType',
+    'VandalTypes',
+    'Meta',
+]
+
+# metaclass.
+metaclass = [
+    'Meta',
+]
