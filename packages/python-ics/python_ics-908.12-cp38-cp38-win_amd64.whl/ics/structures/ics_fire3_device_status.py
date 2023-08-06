03a24d050497d1b7b22f835731664a64ce7dee55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:51 +0000

    Add package python-ics/908.12/python_ics-908.12-cp38-cp38-win_amd64.whl

---
+++ packages/python-ics/python_ics-908.12-cp38-cp38-win_amd64.whl/ics/structures/ics_fire3_device_status.py
@@ -0,0 +1,18 @@
+# This file was auto generated; Do not modify, if you value your sanity!
+import ctypes
+import enum
+
+from ics.structures.ethernet_network_status_t import *
+
+
+class ics_fire3_device_status(ctypes.Structure):
+    _pack_ = 4
+    _fields_ = [
+        ('ethernetActivationLineEnabled', ctypes.c_uint8),
+        ('ethernetStatus', ethernetNetworkStatus_t),
+        ('unused', ctypes.c_uint8),
+    ]
+
+
+icsFire3DeviceStatus = ics_fire3_device_status
+
