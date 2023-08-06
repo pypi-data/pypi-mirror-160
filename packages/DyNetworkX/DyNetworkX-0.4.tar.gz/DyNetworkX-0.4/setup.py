21582e863713b9ce8a99e9694afcc8115bc02e26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:15:22 +0000

    Add package DyNetworkX/0.4/DyNetworkX-0.4.tar.gz

---
+++ packages/DyNetworkX/DyNetworkX-0.4.tar.gz/DyNetworkX-0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='DyNetworkX',
+    version='0.4',
+    author='Makan Arastuie',
+    author_email='Makan.Arastuie@rockets.utoledo.edu',
+    packages=['dynetworkx',
+              'dynetworkx.algorithms',
+              'dynetworkx.classes',
+              'dynetworkx.tests',
+              ],
+    license='Released under the 3-Clause BSD license.',
+    url='https://github.com/IdeasLabUT/dynetworkx',
+    description='DyNetworkX is a Python package for the study of dynamic network analysis.',
+    long_description=open('README.rst').read(),
+    install_requires=['networkx',
+                      'sortedcontainers',
+                      'numpy',
+                      'timeit',
+                      'sklearn'
+                      ],
+    python_requires='>=3.4',
+)
