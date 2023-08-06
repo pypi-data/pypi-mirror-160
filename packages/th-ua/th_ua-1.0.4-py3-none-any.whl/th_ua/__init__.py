31b753c98d6825e740dce9e12affe24e7b7a2340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:49 +0000

    Add package th-ua/1.0.4/th_ua-1.0.4-py3-none-any.whl

---
+++ packages/th-ua/th_ua-1.0.4-py3-none-any.whl/th_ua/__init__.py
@@ -0,0 +1,8 @@
+# import sys
+# import os
+# import json
+#
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#
+# with open('random_ua.json', 'r', encoding='utf-8') as p:
+#     list_user_agent = json.loads(p.read())
---
+++ packages/th-ua/th_ua-1.0.4-py3-none-any.whl/th_ua/th_random_ua.py
@@ -0,0 +1,67 @@
+import json
+import random
+# import sys
+# import os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#
+import os
+# filepath = os.path.join(os.path.dirname(os.path.realpath(__file__)), "random_ua.json")
+# print(filepath)
+THIS_DIR = os.path.dirname(os.path.realpath(__file__))
+SPINNERS_PATH = os.path.join(THIS_DIR, "random_ua.json")
+# print(SPINNERS_PATH)
+with open(SPINNERS_PATH, 'r', encoding='utf-8') as p:
+    list_user_agent = json.loads(p.read())
+
+
+class RandomUA:
+
+    def __init__(self):
+        self.list_user_agent = list_user_agent
+        self.uc_index = 0
+        self.index_dict = {
+            "Chrome": 0,
+            "UC": 0,
+            'Android': 0,
+            'iPhone': 0,
+            'XiaoMi': 0
+
+        }
+
+    def random_chrome(self):
+        return random.choice(self.list_user_agent['Chrome'])
+
+    def random_uc(self):
+        return random.choice(self.list_user_agent['UC'])
+
+    def random_android(self):
+        return random.choice(self.list_user_agent['Android'])
+
+    def random_iphone(self):
+        return random.choice(self.list_user_agent['iPhone'])
+
+    def random_xiaomi(self):
+        return random.choice(self.list_user_agent['XiaoMi'])
+
+    def order_chrome(self):
+        return self.order_index_ua('Chrome')
+
+    def order_uc(self):
+        return self.order_index_ua('UC')
+
+    def order_android(self):
+        return self.order_index_ua('Android')
+
+    def order_iphone(self):
+        return self.order_index_ua('iPhone')
+
+    def order_xiaomi(self):
+        return self.order_index_ua('XiaoMi')
+
+    def order_index_ua(self, index_name):
+        if self.index_dict[index_name] < len(self.list_user_agent[index_name]) - 1:
+            self.index_dict[index_name] += 1
+        else:
+            self.index_dict[index_name] = 0
+
+        return self.list_user_agent[index_name][self.index_dict[index_name]]
