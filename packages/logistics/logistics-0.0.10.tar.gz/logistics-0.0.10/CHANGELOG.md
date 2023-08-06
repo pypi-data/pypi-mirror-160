2728db54940998fc596f00d488bd183b6ef5f506
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:43 +0000

    Add package logistics/0.0.10/logistics-0.0.10.tar.gz

---
+++ packages/logistics/logistics-0.0.10.tar.gz/logistics-0.0.10/CHANGELOG.md
@@ -0,0 +1,36 @@
+CHANGELOG
+==========
+
+0.0.2 (20/12/2021)
+- initial release.
+
+0.0.2 (20/12/2021)
+- initial change.
+
+0.0.3 (07/06/2022)
+- transformed into Vandal framework logistics package.
+
+0.0.4 (07/06/2022)
+- bug fix.
+
+0.0.5 (11/06/2022)
+- added FilePathType and SpecialType.
+- added multiple folders to aid testing and development.
+
+0.0.6 (13/06/2022)
+- Manifesto added.
+- special methods redefined.
+- PEP8 alignments.
+
+0.0.7 (13/06/2022)
+- added meta data.
+
+0.0.8 (28/07/2022)
+- redefined the package structure.
+- added VandalTypes function to print all the types.
+
+0.0.9 (28/07/2022)
+- quick bug fix.
+
+0.0.10 (28/07/2022)
+- resolved VandalTypes issues.
---
+++ packages/logistics/logistics-0.0.10.tar.gz/logistics-0.0.10/logistics/plugins/__init__.py
@@ -0,0 +1,42 @@
+# imports relevant contents.
+from logistics.plugins.metaclass import Meta
+from logistics.plugins.types import *
+
+# all relevant contents.
+__all__ = [
+    'VandalType',
+    'IntegerType',
+    'FloatType',
+    'NumberType',
+    'ReturnType',
+    'PrintType',
+    'GraphType',
+    'StringType',
+    'ListType',
+    'TupleType',
+    'DictionaryType',
+    'BooleanType',
+    'FilePathType',
+    'SpecialType',
+    'NumberVector',
+    'StringVector',
+    'StringDictionary',
+    'DictionaryVector',
+    'NumberVectorAlike',
+    'NumberArrayAlike',
+    'AnyArrayAlike',
+    'AnyVectorAlike',
+    'AnyType',
+    'VandalTypes',
+    'Meta',
+]
+
+# all available types.
+VandalTypes = [
+    'VandalTypes',
+]
+
+# metaclass.
+metaclass = [
+    'Meta',
+]
