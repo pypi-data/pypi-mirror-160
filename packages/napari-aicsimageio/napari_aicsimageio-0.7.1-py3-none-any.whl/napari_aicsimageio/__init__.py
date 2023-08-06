a7502d82ea32be4ff5388e6f58b8108e08e394f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:27:49 +0000

    Add package napari-aicsimageio/0.7.1/napari_aicsimageio-0.7.1-py3-none-any.whl

---
+++ packages/napari-aicsimageio/napari_aicsimageio-0.7.1-py3-none-any.whl/napari_aicsimageio/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for napari-aicsimageio."""
+
+__author__ = "Eva Maxfield Brown"
+__email__ = "evamaxfieldbrown@gmail.com"
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.7.1"
+
+
+def get_module_version() -> str:
+    return __version__
