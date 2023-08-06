0e6ce1dd49fdbd25006b1ecf7f5ad700e06f9c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:46 +0000

    Add package arena-robot/2.1.0/arena_robot-2.1.0-pp38-pypy38_pp73-win_amd64.whl

---
+++ packages/arena-robot/arena_robot-2.1.0-pp38-pypy38_pp73-win_amd64.whl/licosa_py/__init__.py
@@ -0,0 +1,11 @@
+"""
+__init__.py: Definitions for LiCosa.
+
+Created by Perry Naseck on 2/22/22.
+
+Copyright (c) 2022, The CONIX Research Center
+All rights reserved.
+
+This source code is licensed under the BSD-3-Clause license found in the
+LICENSE file in the root directory of this source tree.
+"""
