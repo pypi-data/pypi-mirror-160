eb46ff03890b74de98418c497488f56c832c7fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:24 +0000

    Add package monitor-logs/0.1.1/monitor_logs-0.1.1.tar.gz

---
+++ packages/monitor-logs/monitor_logs-0.1.1.tar.gz/monitor_logs-0.1.1/setup.py
@@ -0,0 +1,9 @@
+##setup.py
+
+from setuptools import setup
+
+setup(name = 'monitor_logs', 
+    version = '0.1.1', 
+    description = 'Monitor your logs from anywhere', 
+    packages = ['monitor_logs'], 
+    zip_safe = False)
