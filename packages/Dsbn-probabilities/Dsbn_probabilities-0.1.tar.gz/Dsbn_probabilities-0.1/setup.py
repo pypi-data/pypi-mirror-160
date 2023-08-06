fcc76a71eed9e26afe719798cc20b6bbaab73a43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:20 +0000

    Add package Dsbn-probabilities/0.1/Dsbn_probabilities-0.1.tar.gz

---
+++ packages/Dsbn-probabilities/Dsbn_probabilities-0.1.tar.gz/Dsbn_probabilities-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Dsbn_probabilities',
+      version='0.1',
+      description='Dsbn_probabilities',
+      packages=['Dsbn_probabilities'],
+      zip_safe=False)
