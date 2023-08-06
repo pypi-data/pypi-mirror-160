5a72c0b9e133fa00909f2d6f50cfa4bf7c591b23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:29:53 +0000

    Add package handyscikit/0.0.4.8/handyscikit-0.0.4.8.tar.gz

---
+++ packages/handyscikit/handyscikit-0.0.4.8.tar.gz/handyscikit-0.0.4.8/setup.py
@@ -0,0 +1,17 @@
+"""
+Type this in terminal to upgrade the package. (Write here for remember)
+>> python setup.py sdist bdist_wheel
+>> twine upload dist/*
+"""
+from setuptools import find_packages, setup
+
+setup(name="handyscikit",
+      version="0.0.4.8",
+      description="Add extract data interface to class SquareMesh. Little revised.",
+      author="Hong Peng",
+      python_requires=">=3.7.0",
+      url="https://github.com/minho-hong/handyscikit.git",
+      package_data={"handyscikit.plot":["*.ttc"]},
+      packages=find_packages(),
+      install_requires=["numpy", "vtk"],
+      license="GPL")
