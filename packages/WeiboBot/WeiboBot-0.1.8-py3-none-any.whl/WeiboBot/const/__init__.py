d165886f027f6039da4444b5ceb3f99b103ccd71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:20:45 +0000

    Add package WeiboBot/0.1.8/WeiboBot-0.1.8-py3-none-any.whl

---
+++ packages/WeiboBot/WeiboBot-0.1.8-py3-none-any.whl/WeiboBot/const/__init__.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+__all__ = ["VISIBLE", "MSG", "ACTION", "WEIBOERR", "WEIBO_WARNING"]
+
+
+class VISIBLE(Enum):
+    ALL = 0  # 全部
+    ONLY_ME = 1  # 仅自己可见
+    FRIENDS = 6  # 仅好友可见
+    FOLLOWERS = 10  # 仅关注人可见
+
+
+class MSG(Enum):
+    NORMAL = 1  # 普通消息
+    SUBSCRIPTION = 4  # 订阅消息
+
+
+class ACTION(Enum):
+    UNDONE = 0  # 未完成
+    RUNNING = 1  # 运行中
+    DONE = 2  # 已完成
+    FAILED = 3  # 失败
+    MAX_TRY = 4  # 超过最大尝试次数
+
+
+class WEIBOERR(Enum):
+    NO_DATA = 100011  # 没有数据
+
+
+WEIBO_WARNING = [WEIBOERR.NO_DATA.value]
