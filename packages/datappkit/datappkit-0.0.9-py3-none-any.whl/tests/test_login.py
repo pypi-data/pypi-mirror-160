d0e620c8834b611656a2bfb88e2f5f12c6263710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:08 +0000

    Add package datappkit/0.0.9/datappkit-0.0.9-py3-none-any.whl

---
+++ packages/datappkit/datappkit-0.0.9-py3-none-any.whl/tests/test_login.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append("/Users/garry/PycharmProjects/data-service-sdk")
+
+from datappkit.datapp import Datapp
+
+
+if __name__ == '__main__':
+    datapp = Datapp()
+    # test account
+    # res, data = datapp.login("13333330003", "test123")
+    res, data = datapp.login("17610188166", "test123")
+
+    # res, data = datapp.login("18610928883", "21151091193273")
+    # res, data = datapp.login("15185055472", "megviiHEXIN02000000")
+    # res, data = datapp.login("18612481928", "dwh123456000000")
+    print(res, data)
