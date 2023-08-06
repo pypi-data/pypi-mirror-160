e9c0f4f4fc8e3f26e05922344dfbb7223955da10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:36 +0000

    Add package conversion-vitesse/0.0.2/conversion_vitesse-0.0.2.tar.gz

---
+++ packages/conversion-vitesse/conversion_vitesse-0.0.2.tar.gz/conversion_vitesse-0.0.2/conversion_vitesse/Conversion.py
@@ -0,0 +1,12 @@
+def Conversion_Vitesse_kmH(vitesse):
+    initial_vitesse = vitesse
+    final_vitesse = initial_vitesse * 3.6
+    float(final_vitesse)
+    return final_vitesse
+
+def Conversion_Vitesse_mS(vitesse):
+    initial_vitesse = vitesse
+    final_vitesse = initial_vitesse / 3.6
+    float(final_vitesse)
+    return final_vitesse
+
