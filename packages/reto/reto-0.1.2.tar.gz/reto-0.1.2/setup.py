fcfd98777f499ff39510b35310fbf16ce1948b8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:36 +0000

    Add package reto/0.1.2/reto-0.1.2.tar.gz

---
+++ packages/reto/reto-0.1.2.tar.gz/reto-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['reto']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=3.1.2,<4.0.0',
+ 'click>=8.1.3,<9.0.0',
+ 'mdweek>=1.2.4,<2.0.0',
+ 'networkx>=2.8,<3.0',
+ 'tqdm>=4.64.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'reto',
+    'version': '0.1.2',
+    'description': 'A python package for programming in declarative style.',
+    'long_description': None,
+    'author': 'Yasunori Horikoshi',
+    'author_email': 'hotoku@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
