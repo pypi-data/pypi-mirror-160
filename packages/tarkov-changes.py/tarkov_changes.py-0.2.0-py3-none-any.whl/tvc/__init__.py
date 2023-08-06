56be829388c88bbccafd229d618db240e4aceb5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:26 +0000

    Add package tarkov-changes.py/0.2.0/tarkov_changes.py-0.2.0-py3-none-any.whl

---
+++ packages/tarkov-changes.py/tarkov_changes.py-0.2.0-py3-none-any.whl/tvc/__init__.py
@@ -0,0 +1,19 @@
+"""
+tvc.types
+~~~~~~~~~~~~~~
+Wrapper for the Tarkov Changes API
+:copyright: (c) 2022-present Hostagen
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = 'v0.2.0'
+
+from .ammunition import *
+from .armor import *
+from .backpack import *
+from .barter import *
+from .client import *
+from .food import *
+from .grenade import *
+from .item import *
+from . import utils
