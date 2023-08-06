8e45e696bc58158d8e321662b3a63976e4b094c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:48 +0000

    Add package scratchplus/1.5.5/scratchplus-1.5.5.tar.gz

---
+++ packages/scratchplus/scratchplus-1.5.5.tar.gz/scratchplus-1.5.5/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+setup(name='scratchplus',
+      version='1.5.5',
+      description='API wrapper for scratch',
+      packages=['scratchplus'],
+      author_email='thefinalspacestudio@gmail.com',
+      author="gagarinten",
+      zip_safe=False,
+      url='https://github.com/noverd/ScratchPlus',
+      install_requires=["requests",
+                        "websocket-client", "pymitter", "bs4"],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      )
