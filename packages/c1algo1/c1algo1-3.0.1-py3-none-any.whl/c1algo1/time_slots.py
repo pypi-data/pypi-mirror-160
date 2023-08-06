22afc64867ea0a9ae85e8bd3173759425f99aec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:49 +0000

    Add package c1algo1/3.0.1/c1algo1-3.0.1-py3-none-any.whl

---
+++ packages/c1algo1/c1algo1-3.0.1-py3-none-any.whl/c1algo1/time_slots.py
@@ -0,0 +1,43 @@
+# Define time slot list indices for start and end indices for each set of days: "TWF", "MTH, "M", "T", "W", "TH", "F"
+LAST_SCHEDULABLE_TWF_SLOT = 25
+LAST_TWF_SLOT = 26
+LAST_SCHEDULABLE_MTH_SLOT = 51
+LAST_MTH_SLOT = 53
+
+
+# List of time slots
+# First X time slots are all TWF (currently the whole list)
+timeslots = [
+    {"start_time": "8:30", "end_time": "8:50"},         # 0
+    {"start_time": "9:00", "end_time": "9:20"},         # 1
+    {"start_time": "9:30", "end_time": "9:50"},         # 2
+    {"start_time": "10:00", "end_time": "10:20"},       # 3
+    {"start_time": "10:30", "end_time": "10:50"},       # 4
+    {"start_time": "11:00", "end_time": "11:20"},       # 5
+    {"start_time": "11:30", "end_time": "11:50"},       # 6
+    {"start_time": "12:00", "end_time": "12:20"},       # 7
+    {"start_time": "12:30", "end_time": "12:50"},       # 8
+    {"start_time": "13:00", "end_time": "13:20"},       # 9
+    {"start_time": "13:30", "end_time": "13:50"},       # 10
+    {"start_time": "14:00", "end_time": "14:20"},       # 11
+    {"start_time": "14:30", "end_time": "14:50"},       # 12
+    {"start_time": "15:00", "end_time": "15:20"},       # 13
+    {"start_time": "15:30", "end_time": "15:50"},       # 14
+    {"start_time": "16:00", "end_time": "16:20"},       # 15
+    {"start_time": "16:30", "end_time": "16:50"},       # 16
+    {"start_time": "17:00", "end_time": "17:20"},       # 17
+    {"start_time": "17:30", "end_time": "17:50"},       # 18
+    {"start_time": "18:00", "end_time": "18:20"},       # 19
+    {"start_time": "18:30", "end_time": "18:50"},       # 20
+    {"start_time": "19:00", "end_time": "19:20"},       # 21
+    {"start_time": "19:30", "end_time": "19:50"},       # 22
+    {"start_time": "20:00", "end_time": "20:20"},       # 23
+    {"start_time": "20:30", "end_time": "20:50"},       # 24
+    {"start_time": "21:00", "end_time": "21:20"},       # 25
+    {"start_time": "21:30", "end_time": "21:50"},       # 26
+    ]
+
+# List of days
+days = ["TWF", "MTH", "M", "T", "W", "TH", "F"]
+
+
