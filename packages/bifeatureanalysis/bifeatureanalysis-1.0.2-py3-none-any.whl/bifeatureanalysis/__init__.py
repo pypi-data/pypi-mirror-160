d7c32d270048eb964ce078142f107e5c01031852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:37 +0000

    Add package bifeatureanalysis/1.0.2/bifeatureanalysis-1.0.2-py3-none-any.whl

---
+++ packages/bifeatureanalysis/bifeatureanalysis-1.0.2-py3-none-any.whl/bifeatureanalysis/__init__.py
@@ -0,0 +1,10 @@
+'''
+Author: BDFD
+Date: 2021-10-27 18:42:50
+LastEditTime: 2022-07-26 11:00:21
+LastEditors: BDFD
+Description: In User Settings Edit
+FilePath: \Section5.3-PyPi_Feature_Engineering\bifeatureanalysis\__init__.py
+'''
+
+from bifeatureanalysis.templateproj import add_one
