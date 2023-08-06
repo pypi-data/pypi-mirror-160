ae09ce07a9ed05ada451ebeb1f1e763b78c036b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:03:40 +0000

    Add package FastaFragmentor/0.0.2/FastaFragmentor-0.0.2.tar.gz

---
+++ packages/FastaFragmentor/FastaFragmentor-0.0.2.tar.gz/FastaFragmentor-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='FastaFragmentor',
+    version='0.0.2',
+    packages=['fasta_digest',
+             ],
+    license='MIT',
+    description="Split fasta sequences into fragments of given length."
+)
