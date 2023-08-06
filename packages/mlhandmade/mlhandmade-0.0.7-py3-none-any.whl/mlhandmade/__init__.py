ae5d14eb2520d4be316504468edaadb6ec125001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:18:21 +0000

    Add package mlhandmade/0.0.7/mlhandmade-0.0.7-py3-none-any.whl

---
+++ packages/mlhandmade/mlhandmade-0.0.7-py3-none-any.whl/mlhandmade/__init__.py
@@ -0,0 +1,21 @@
+from mlhandmade import (
+    linear_models,
+    discriminant_analysis,
+    knn,
+    svm,
+    multiclass,
+    preprocessing,
+    plot,
+    model_selection
+)
+
+__all__ = [
+    "linear_models",
+    "discriminant_analysis",
+    "knn",
+    "svm",
+    "multiclass",
+    "preprocessing",
+    "plot",
+    "model_selection"
+]
