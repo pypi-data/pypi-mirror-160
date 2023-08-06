981b06fda8e330d710115533fb86deb25e1b2eef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:04:20 +0000

    Add package firstbatch-py/0.1.1/firstbatch_py-0.1.1-py3-none-any.whl

---
+++ packages/firstbatch-py/firstbatch_py-0.1.1-py3-none-any.whl/firstbatch/__init__.py
@@ -0,0 +1,12 @@
+from .client import *
+from .request_handler import *
+from.utils import *
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+__all__ = [
+    'FirstBatchClient',
+    'EventTypes',
+    'Gate',
+    'PersonaMap',
+    'PersonhoodMap'
+]
