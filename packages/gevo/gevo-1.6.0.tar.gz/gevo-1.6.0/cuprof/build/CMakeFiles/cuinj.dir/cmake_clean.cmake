7ebd457fcb7dbc575871f90176d19cbfd538d864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:23:27 +0000

    Add package gevo/1.6.0/gevo-1.6.0.tar.gz

---
+++ packages/gevo/gevo-1.6.0.tar.gz/gevo-1.6.0/cuprof/build/CMakeFiles/cuinj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuinj.dir/cuinj.cpp.o"
+  "libcuinj.pdb"
+  "libcuinj.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/cuinj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
