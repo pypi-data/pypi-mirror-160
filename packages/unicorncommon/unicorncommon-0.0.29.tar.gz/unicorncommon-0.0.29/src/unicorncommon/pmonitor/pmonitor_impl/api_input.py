9f10a2dc801ca4f4becee5abb3ef1f32248ab1a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:50 +0000

    Add package unicorncommon/0.0.29/unicorncommon-0.0.29.tar.gz

---
+++ packages/unicorncommon/unicorncommon-0.0.29.tar.gz/unicorncommon-0.0.29/src/unicorncommon/pmonitor/pmonitor_impl/api_input.py
@@ -0,0 +1,106 @@
+from jsonschema import validate, validators, Draft7Validator
+from dateutil.parser import parse
+
+def is_datetime_string(validator, value, instance, schema):
+    try:
+        parse(instance)
+    except (ValueError, TypeError) as e:
+        raise ValidationError(str(e))
+
+types = {
+    "datetime_string": {
+        "type": "string",
+        "is_datetime_string": {
+        }
+    }
+}
+
+models = {
+    "run_application_input": {
+        "type": "object",
+        "properties": {
+            "action": {
+                "type": "string",
+            },
+            "id": {
+                "type": "integer"
+            },
+            "args": {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                }
+            },
+            "input": {
+                "type": ["string", "null"]
+            },
+        },
+        "additionalProperties": False,
+        "required": ["action", "id"]
+    },
+    "run_task_input": {
+        "type": "object",
+        "properties": {
+            "action": {
+                "type": "string",
+            },
+            "id": {
+                "type": "integer"
+            },
+        },
+        "additionalProperties": False,
+        "required": ["action", "id"]
+    },
+    "stop_run_input": {
+        "type": "object",
+        "properties": {
+            "action": {
+                "type": "string",
+            },
+            "id": {
+                "type": "integer"
+            },
+        },
+        "additionalProperties": False,
+        "required": ["action", "id"]
+    },
+    "read_run_artifact_input": {
+        "type": "object",
+        "properties": {
+            "action": {
+                "type": "string",
+            },
+            "id": {
+                "type": "integer"
+            },
+            "artifact_name": {
+                "type": "string"
+            }
+        },
+        "additionalProperties": False,
+        "required": ["action", "id", "artifact_name"]
+    },
+}
+
+types = {
+    "datetime_string": {
+        "type": "string",
+        "is_datetime_string": {
+        }
+    }
+}
+
+def validate_model(model_name, data):
+    MyValidator = validators.extend(
+        Draft7Validator,
+        validators = {
+            'is_datetime_string': is_datetime_string
+        }
+    )
+    schema = {
+        "types": types,
+        "models": models,
+        "$ref": f"#/models/{model_name}"
+    }
+    my_validator = MyValidator(schema=schema)
+    my_validator.validate(data)
