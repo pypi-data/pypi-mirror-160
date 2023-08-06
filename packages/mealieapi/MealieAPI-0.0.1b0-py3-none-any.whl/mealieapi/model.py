b0c88b967b72b65168e421dde19d8a98f2441191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:24 +0000

    Add package mealieapi/0.0.1b0/MealieAPI-0.0.1b0-py3-none-any.whl

---
+++ packages/mealieapi/MealieAPI-0.0.1b0-py3-none-any.whl/mealieapi/model.py
@@ -0,0 +1,23 @@
+import typing as t
+import logging
+from pydantic import BaseModel as BM
+from pydantic.error_wrappers import ValidationError
+
+_LOGGER = logging.getLogger(__name__)
+
+if t.TYPE_CHECKING:
+    from mealieapi.client import MealieClient
+
+
+class BaseModel(BM):
+    pass
+
+
+class InteractiveModel(BaseModel):
+    def __init__(self, *args, _client: "MealieClient", **kwargs):
+        object.__setattr__(self, "_client", _client)
+        try:
+            super().__init__(*args, **kwargs)
+        except ValidationError as err:
+            _LOGGER.debug("%r %r", args, kwargs)
+            raise err
