5947bd77abdef58332a0027ea47f66535e27f02d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:32 +0000

    Add package napari-allencell-annotator/0.0.4/napari-allencell-annotator-0.0.4.tar.gz

---
+++ packages/napari-allencell-annotator/napari-allencell-annotator-0.0.4.tar.gz/napari-allencell-annotator-0.0.4/napari_allencell_annotator/__init__.py
@@ -0,0 +1,8 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+
+# Do not edit this string manually, always use bumpversion
+# Details in CONTRIBUTING.md
+__version__ = "0.0.4"
