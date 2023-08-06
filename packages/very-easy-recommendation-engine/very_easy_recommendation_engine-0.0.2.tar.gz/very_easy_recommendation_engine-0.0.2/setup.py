eb1288c9490acb851563d69f8c724e36ab467fc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:34:05 +0000

    Add package very-easy-recommendation-engine/0.0.2/very_easy_recommendation_engine-0.0.2.tar.gz

---
+++ packages/very-easy-recommendation-engine/very_easy_recommendation_engine-0.0.2.tar.gz/very_easy_recommendation_engine-0.0.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+  "django>=2.2",
+]
+
+VERSION = "0.0.2"
+URL = "https://veryeasyai.com/"
+
+setuptools.setup(
+    name="very_easy_recommendation_engine",
+    version=VERSION,
+    author="veryeasyai",
+    url=URL,
+    description="A Very Easy Recommendation Engine for Django",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    keywords="django, AI, artificial-intelligence, veryeasyai, machine-learning, neural-networks, recommendation-systems",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    py_modules=["very_easy_recommendation_engine"],
+    package_dir={'':'very_easy_recommendation_engine'},
+    install_requires=REQUIRES
+)
