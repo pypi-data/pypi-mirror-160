5d9b280f3610887b555f39bf5d96d1b00ed163fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:09 +0000

    Add package TheMPDBApiSdk/1.2/TheMPDBApiSdk-1.2-py3-none-any.whl

---
+++ packages/TheMPDBApiSdk/TheMPDBApiSdk-1.2-py3-none-any.whl/mpdbapi/__init__.py
@@ -0,0 +1,8 @@
+from mpdbapi.exceptions import ApiLimitReachedError
+from mpdbapi.exceptions import NoApiKeyError
+from mpdbapi.exceptions import UserNotFoundError
+from mpdbapi.exceptions import ModpackNotFoundError
+from mpdbapi.exceptions import UserNotFoundError
+from mpdbapi.exceptions import FileNoFoundError
+
+from mpdbapi.mpdbapi import MpdbApi
