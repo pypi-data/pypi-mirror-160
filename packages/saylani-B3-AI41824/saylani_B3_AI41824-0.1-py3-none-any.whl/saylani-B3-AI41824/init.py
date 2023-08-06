3b14c874e87866be55e38be5ba1bf8631da34331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:07 +0000

    Add package saylani-B3-AI41824/0.1/saylani_B3_AI41824-0.1-py3-none-any.whl

---
+++ packages/saylani-B3-AI41824/saylani_B3_AI41824-0.1-py3-none-any.whl/saylani-B3-AI41824/init.py
@@ -0,0 +1,13 @@
+name = "Ali"
+father_name = "Hamza"
+university = "Bahria University"
+
+message =  """
+PIAIC ISLAMABAD BATCH3
+Name:  {}
+Father: {}
+University: {}
+""".format(name, father_name, university)
+
+
+print(message)
