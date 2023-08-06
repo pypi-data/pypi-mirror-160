24b586299d779dcd39652bca2f84db0d7f8db5eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:40:30 +0000

    Add package capturebaudrate/0.0.2/capturebaudrate-0.0.2.tar.gz

---
+++ packages/capturebaudrate/capturebaudrate-0.0.2.tar.gz/capturebaudrate-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Operating System :: MacOS',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python'
+]
+
+setup(
+    name='capturebaudrate',
+    version='0.0.2',
+    description='A very basic capture',
+    long_description=open('README').read() + '\n\n' + open('CHANGELOG').read(),
+    url='',
+    author='Ao Zhang',
+    author_email='aozhang2022@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='calculator',
+    packages=find_packages(),
+    install_requires=['']
+)
