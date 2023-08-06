6de9509ae700605fd2fb7c611d80dba490a4e101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:05 +0000

    Add package pytreeclass/0.0.6/pytreeclass-0.0.6-py3-none-any.whl

---
+++ packages/pytreeclass/pytreeclass-0.0.6-py3-none-any.whl/pytreeclass/__init__.py
@@ -0,0 +1,7 @@
+from .src import tree_viz
+from .src.decorator import treeclass
+from .src.tree_util import static_field
+
+__all__ = ("tree_viz", "treeclass", "static_field")
+
+__version__ = "0.0.6"
