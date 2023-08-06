74dea1292c53663e4362372f7e6bf745ccb6ca3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:48:39 +0000

    Add package cbl-tools/0.0.6/cbl_tools-0.0.6.tar.gz

---
+++ packages/cbl-tools/cbl_tools-0.0.6.tar.gz/cbl_tools-0.0.6/tests/test_tools.py
@@ -0,0 +1,14 @@
+import unittest
+
+import cbl_tools.tools
+from cbl_tools import tools
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        #self.assertEqual(True, False)  # add assertion here
+        print(tools.version(12))
+
+
+if __name__ == '__main__':
+    unittest.main()
