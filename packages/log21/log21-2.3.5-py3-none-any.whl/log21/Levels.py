9bf084ddcda96f8db992d94f018140caa8fcfcaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package log21/2.3.5/log21-2.3.5-py3-none-any.whl

---
+++ packages/log21/log21-2.3.5-py3-none-any.whl/log21/Levels.py
@@ -0,0 +1,16 @@
+# log21.Levels.py
+# CodeWriter21
+
+import logging as _logging
+
+__all__ = ['CRITICAL', 'FATAL', 'ERROR', 'WARNING', 'WARN', 'INFO', 'DEBUG', 'NOTSET', 'INPUT']
+
+INPUT = 60
+CRITICAL = _logging.CRITICAL
+FATAL = CRITICAL
+ERROR = _logging.ERROR
+WARNING = _logging.WARNING
+WARN = WARNING
+INFO = _logging.INFO
+DEBUG = _logging.DEBUG
+NOTSET = _logging.NOTSET
