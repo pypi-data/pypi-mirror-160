e1a16e4552f98e3e4785afb6cfb9a9cc4225dd12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:02:11 +0000

    Add package clinica/0.7.2/clinica-0.7.2.tar.gz

---
+++ packages/clinica/clinica-0.7.2.tar.gz/clinica-0.7.2/clinica/iotools/converters/nifd_to_bids/nifd_to_bids.py
@@ -0,0 +1,39 @@
+"""Convert the NIFD dataset into BIDS."""
+
+from os import PathLike
+from typing import List
+
+
+def convert_images(
+    path_to_dataset: PathLike,
+    bids_dir: PathLike,
+    path_to_clinical: PathLike,
+) -> List[PathLike]:
+    """Convert the entire dataset in BIDS.
+
+    Scans available files in the path_to_dataset,
+    identifies the patients that have images described by the JSON file,
+    converts the image with the highest quality for each category.
+    """
+    from .nifd_utils import (
+        dataset_to_bids,
+        read_clinical_data,
+        read_imaging_data,
+        write_bids,
+    )
+
+    clinical_data = read_clinical_data(path_to_clinical)
+    imaging_data = read_imaging_data(path_to_dataset)
+
+    participants, sessions, scans = dataset_to_bids(
+        imaging_data=imaging_data, clinical_data=clinical_data
+    )
+
+    written = write_bids(
+        to=bids_dir,
+        participants=participants,
+        sessions=sessions,
+        scans=scans,
+    )
+
+    return written
