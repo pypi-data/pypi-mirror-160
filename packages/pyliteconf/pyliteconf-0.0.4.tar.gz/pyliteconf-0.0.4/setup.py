353f5f6812f9326ec3a2200ceaceb2f376d922ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:38 +0000

    Add package pyliteconf/0.0.4/pyliteconf-0.0.4.tar.gz

---
+++ packages/pyliteconf/pyliteconf-0.0.4.tar.gz/pyliteconf-0.0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name="pyliteconf",
+      version="0.0.4",
+      packages=find_packages(),
+
+      description='Library for creating configs on python',
+
+      author='_co_cy_',
+      author_email="step2038@mail.ru",
+
+      url='https://github.com/co-cy/pyliteconf',
+      zip_safe=False)
