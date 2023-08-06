01ac9d9ed0af56f8437bb8318a2db21a59c5caa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:19 +0000

    Add package gophient/1.0.0/gophient-1.0.0.tar.gz

---
+++ packages/gophient/gophient-1.0.0.tar.gz/gophient-1.0.0/gophient/__init__.py
@@ -0,0 +1,9 @@
+"""gophient - Python library for connection to Gopher servers."""
+from gophient import const, types
+from gophient.types import Gopher
+
+__all__ = (
+    'const',
+    'types',
+    'Gopher',
+)
