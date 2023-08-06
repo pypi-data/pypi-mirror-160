46f76ca5aa04b2cbd21deeac0e1685468f5bb058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:27 +0000

    Add package tf-notify/0.1.0/tf_notify-0.1.0-py3-none-any.whl

---
+++ packages/tf-notify/tf_notify-0.1.0-py3-none-any.whl/tf_notify/__init__.py
@@ -0,0 +1,6 @@
+from .callbacks.slack import SlackCallback
+from .callbacks.telegram import TelegramCallback
+
+__version__ = "0.1.0"
+
+__all__ = ["__version__", "SlackCallback", "TelegramCallback"]
