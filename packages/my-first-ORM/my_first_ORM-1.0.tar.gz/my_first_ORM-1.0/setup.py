85a521588228b16fb75780ee60cbe22632fcc55d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:57 +0000

    Add package my-first-ORM/1.0/my_first_ORM-1.0.tar.gz

---
+++ packages/my-first-ORM/my_first_ORM-1.0.tar.gz/my_first_ORM-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='my_first_ORM',
+version='1.0',
+description='portfolio',
+author='Rodion',
+author_email='drink2getdrunk91@gmail.com',
+url='https://github.com/flood1991/orm_model',
+packages=['../'],
+)
